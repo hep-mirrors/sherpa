261646ffd9f3cb798a79434ac1434c6a2d979a66
Steffen Schumann <steffen.schumann@phys.uni-goettingen.de>
2003-06-11 08:19:02 +0000

    Doxygen comments added

--- AMEGIC++-2.0/Model/Lorentz_Function.C
+++ AMEGIC++-2.0/Model/Lorentz_Function.C
@@ -10,16 +10,16 @@ void Lorentz_Function::AddPermutation(int sign,int a,int b=-1,int c=-1,int d=-1)
   if (NofIndex()>2) newperm[2] = partarg[c];
   if (NofIndex()>3) newperm[3] = partarg[d];
 
-  permlist.push_back(newperm);
-  signlist.push_back(sign);
+  m_permlist.push_back(newperm);
+  m_signlist.push_back(sign);
 }
 
 void Lorentz_Function::InitPermutation()
 {
-  if (!permlist.empty()) {
-    for (short int i=0;i<permlist.size();i++) delete[] permlist[i]; 
-    permlist.clear();
-    signlist.clear();
+  if (!m_permlist.empty()) {
+    for (short int i=0;i<m_permlist.size();i++) delete[] m_permlist[i]; 
+    m_permlist.clear();
+    m_signlist.clear();
   }
 
   switch (type) {
@@ -99,29 +99,29 @@ void Lorentz_Function::InitPermutation()
     AddPermutation( 1,2,0,1,3);  
     break;    
   }
-  permcount = 0;
+  m_permcount = 0;
 }
 
 int Lorentz_Function::ResetPermutation() 
 {
-  permcount=0;
-  for (short int i=0;i<NofIndex();i++) partarg[i]  = permlist[permcount][i];
+  m_permcount=0;
+  for (short int i=0;i<NofIndex();i++) partarg[i]  = m_permlist[m_permcount][i];
 }
 
 int Lorentz_Function::NextPermutation()
 {
   if (NofIndex()<2) return 0;
-  permcount++;
-  if (permcount==permlist.size()) return 0;
+  m_permcount++;
+  if (m_permcount==m_permlist.size()) return 0;
   
-  for (short int i=0;i<NofIndex();i++) partarg[i]  = permlist[permcount][i];
+  for (short int i=0;i<NofIndex();i++) partarg[i]  = m_permlist[m_permcount][i];
 					 
 }
 
 int Lorentz_Function::GetSign() 
 {
-  if (signlist.empty()) return 1;
-  return signlist[permcount];
+  if (m_signlist.empty()) return 1;
+  return m_signlist[m_permcount];
 }
 
 void AMEGIC::Lorentz_Function2MPI(const Lorentz_Function * lf , MPI_Lorentz_Function & mpi_lf) {
