23f77729d0dccf552b969baf399fc6eb076d32bd
Steffen Schumann <steffen.schumann@phys.uni-goettingen.de>
2003-06-25 09:12:20 +0000

    coding conventions

--- ATOOLS-2.0/Org/Run_Parameter.C
+++ ATOOLS-2.0/Org/Run_Parameter.C
@@ -3,12 +3,9 @@
 #include "MathTools.H"
 #include "Message.H"
 
-#include "Running_AlphaS.H"
-
 using namespace AMATOOLS;
 using namespace AORGTOOLS;
 using namespace APHYTOOLS;
-using namespace std;
 
 Run_Parameter AORGTOOLS::rpa;
 
@@ -63,7 +60,6 @@ void Run_Parameter::Init(std::string _path,int argc,char* argv[])
     }
     else ++iarg;
   }
-
 
   consts.aqed_mz         = 1./dr.GetValue<double>("alpha_QED(MZ)");
   consts.sin2_tw         = dr.GetValue<double>("SIN2_TW");
@@ -177,73 +173,5 @@ double Run_Parameter::Consts::RunningMass(Flavour flav,double t)
   //*AS*    if (sqr(flav.Mass()) < 0.3) return 0.;
   if (t<1.) t=1.;
   if ((t<sqr(flav.Mass())) || (!flav.IsQuark())) return flav.Mass(); 
-
-  /*  
-//======================================================================
-//
-//   Mass running:   MSBAR as in Dittmaier
-//
-//                                    alphaS(t)
-// mass_b(t) = mass_b(mass_b) * ( 1 - -------- ( ln (t/mass_b^2) + 4/3))
-//                                      pi
-//
-
-  double mass0   = flav.Mass();
-  double as_t    = mo->Aqcd(t);
-
-  //  for (double s=6. ; s<120; s+=5)
-  //    cout<<" s="<<s <<" as="<<mo->Aqcd(sqr(s))<<endl;
-
-  double mass = mass0 * ( 1. - as_t / M_PI * ( log( t/sqr(mass0)) + 4./3.));
-  cout<<" in Run_Parameter::RunningMass() "<<endl;
-  cout<<" t="<<t<<" mass0= "<<mass0<<" => "<<mass<<endl;
-
-  return mass;
-  */
-
-  //=====================================================================
-  //  if (Run_Mass()==Switch::On) {
-
-    // running for quarks only
-    // alpha_S(M_Z^2)
-    double asMZ=(*as)(sqr(Flavour(kf::Z).PSMass()));
-    //    cout<<" in Run_Parameter::Mass() "<<endl;
-    //    cout<<" alpha_s(Mz^2)="<<asMZ<<endl;
-    // alpha_S(t)
-    double as2=(*as)(t);
-    //    cout<<" alpha_s("<<t<<")="<<as2<<endl;
-
-    double factor = 1.;
-    // expo = 6 * CF/(11 - 2/3 n_f)     /2 !!!!
-    double mas_Z  = Flavour(kf::Z).PSMass();
-    double mas_s2 = Flavour(kf::s).PSMass()*Flavour(kf::s).Mass();
-    double mas_c2 = Flavour(kf::c).PSMass()*Flavour(kf::c).Mass();
-    double mas_b2 = Flavour(kf::b).PSMass()*Flavour(kf::b).Mass();
-    double mas_t2 = Flavour(kf::t).PSMass()*Flavour(kf::t).Mass();
-    
-    double ast = (*as)(mas_t2);
-    double asb = (*as)(mas_b2);
-    double asc = (*as)(mas_c2);
-    double ass = (*as)(mas_s2);
-
-    if (t>mas_t2) {
-      factor*= pow(ast/asb,4./(11.-10./3.));
-      factor*= pow(as2/ast,4./(11.-12./3.));
-    }
-    else if (t>mas_b2) factor*= pow(as2/asb,4./(11.-10./3.));
-    if (((flav==Flavour(kf::c)) || flav==Flavour(kf::c).Bar()) && (t>mas_b2)) 
-      factor*= pow(asb/asc,4./(11.-8./3.));
-    if (((flav==Flavour(kf::c)) || flav==Flavour(kf::c).Bar())&& (t<=mas_b2)) 
-      factor*= pow(as2/asc,4./(11.-8./3.));  
-    // running quark masses below 0.3 GeV are assumed to be massless!!!
-    if (sqr(flav.Mass()*factor) < 0.3) factor = 0.;
-
-    double as_M =(*as)(sqr(flav.Mass()));
-    double factor_b=1.- 4. * as_M / (3. * M_PI);
-
-    factor*=factor_b; //1.- 4. * as2 / (3. * M_PI);
-    return flav.Mass()*factor;
-    // }
-  return flav.Mass();
 }  
 
