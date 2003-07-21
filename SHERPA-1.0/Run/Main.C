454e4d052f4333661c16d7b9453dcd91c71e222c
dreas <sherpa@projects.hepforge.org>
2003-07-21 06:40:13 +0000

    added some output and calling Summarize to create histograms

--- SHERPA-1.0/Run/Main.C
+++ SHERPA-1.0/Run/Main.C
@@ -1,6 +1,7 @@
 #include "Sherpa.H"
 #include "Message.H"
 #include "prof.hh"
+#include "Random.H"
 
 extern "C" {
   void apainit_();
@@ -10,14 +11,21 @@ extern "C" {
 using namespace SHERPA;
 
 int main(int argc,char* argv[]) {  
-  int nevt=10;
 
   Sherpa Generator;
   Generator.InitializeTheRun(std::string("./"));
+  AORGTOOLS::msg.Out()<<"generate "<<AORGTOOLS::rpa.gen.NumberOfEvents()<<" events"<<std::endl;
   Generator.InitializeTheEventHandler();
-  for (int i=0;i<nevt;i++) {
+
+  int nevt=AORGTOOLS::rpa.gen.NumberOfEvents();
+  if (nevt>0) AORGTOOLS::msg.Out()<<"Starting event generation now. "<<std::endl;
+  for (int i=1;i<=nevt;i++) {
+    if (i%500==0) {
+      AORGTOOLS::msg.Out()<<" Event "<<i<<endl;      
+    }
     if (Generator.GenerateOneEvent()) AORGTOOLS::msg.Events()<<"Sherpa : Passed "<<i<<" events."<<std::endl;
   }
+  Generator.SummarizeRun();
   AORGTOOLS::msg.Events()<<"Sherpa did "<<nevt<<" with "<<Generator.NumberOfErrors()<<" errors."<<std::endl;
 }
 
