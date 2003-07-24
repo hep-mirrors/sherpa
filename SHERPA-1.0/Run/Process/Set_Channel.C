d272a5d11f77d8c07c3854633787219ee0a69b02
Frank Krauss <frank.krauss@durham.ac.uk>
2003-07-24 01:57:45 +0000

    Some beauty farming

--- SHERPA-1.0/Run/Process/Set_Channel.C
+++ SHERPA-1.0/Run/Process/Set_Channel.C
@@ -1,6 +1,3 @@
-#include "P2_3/2_3_4_8_56_1_2/P.H"
-#include "P2_3/2_3_4_8_56_1_1/P.H"
-#include "P2_2/2_2_1_3_5_1_1/P.H"
 #include "Phase_Space_Generator.H"
 
 using namespace AMEGIC;
@@ -10,20 +7,8 @@ using namespace std;
 Single_Channel * Phase_Space_Generator::SetChannel(int nin,int nout,APHYTOOLS::Flavour* fl,
 						   int chn,string& pID)
 {
-#ifdef P2_2_1_3_5_1_1_on
-  if (pID==string("P2_2_1_3_5_1_1")) return (new P2_2_1_3_5_1_1(nin,nout,fl,chn));
-#endif
-#ifdef P2_3_4_8_56_1_1_on
-  if (pID==string("P2_3_4_8_56_1_1")) return (new P2_3_4_8_56_1_1(nin,nout,fl,chn));
-#endif
-#ifdef P2_3_4_8_56_1_2_on
-  if (pID==string("P2_3_4_8_56_1_2")) return (new P2_3_4_8_56_1_2(nin,nout,fl,chn));
-#endif
   return 0;
 }
-
-
-
 
 
 
