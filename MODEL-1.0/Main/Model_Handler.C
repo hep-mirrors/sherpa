588affc31d89d66143b1759d0471942e96477946
Steffen Schumann <steffen.schumann@phys.uni-goettingen.de>
2003-07-24 00:56:35 +0000

    bug fix

--- MODEL-1.0/Main/Model_Handler.C
+++ MODEL-1.0/Main/Model_Handler.C
@@ -13,14 +13,14 @@ using namespace APHYTOOLS;
 Model_Base * Model_Handler::GetModel(Data_Read * _dataread,std::string _path,std::string _file) {
   std::string model     = _dataread->GetValue("MODEL",std::string("SM"));
   
-  Model_Base * modelbase;
+  Model_Base * modelbase = 0;
   if (model==std::string("MSSM")) {
     modelbase = new MSSM(_path,_file);
   }
   if (model==std::string("ADD")) {
     modelbase = new ADD(_path,_file);
   }
-  if (model!=std::string("SM")) { 
+  if (model!=std::string("SM") && modelbase==0) { 
     msg.Error()<<"Error in Model_Handler::GetModel :"<<std::endl
 	       <<"   Tried to initialize model : "<<model<<std::endl
 	       <<"   Option not available. Initialize Standard Model instead."<<std::endl;
