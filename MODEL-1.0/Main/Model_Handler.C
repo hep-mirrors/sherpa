b2dedbac9c87c6c64c2a8787b656651ebb9227a9
Steffen Schumann <steffen.schumann@phys.uni-goettingen.de>
2003-08-01 13:27:15 +0000

    *** empty log message ***

--- MODEL-1.0/Main/Model_Handler.C
+++ MODEL-1.0/Main/Model_Handler.C
@@ -15,12 +15,16 @@ Model_Base * Model_Handler::GetModel(Data_Read * _dataread,std::string _path,std
   Model_Base * modelbase = 0;
   if (model==std::string("MSSM")) {
     modelbase = new MSSM(_path,_file);
+    rpa.gen.SetModel(modelbase);
+    return modelbase;
   }
   if (model==std::string("ADD")) {
     modelbase = new ADD(_path,_file);
+    rpa.gen.SetModel(modelbase);
+    return modelbase;
   }
-  if (model!=std::string("SM") && modelbase==0) { 
-    msg.Error()<<"Error in Model_Handler::GetModel :"<<std::endl
+  if (model!=std::string("SM") || modelbase==0) { 
+      msg.Error()<<"Error in Model_Handler::GetModel :"<<std::endl
 	       <<"   Tried to initialize model : "<<model<<std::endl
 	       <<"   Option not available. Initialize Standard Model instead."<<std::endl;
   }
