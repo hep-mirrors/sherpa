d17a56eaa80fbc5f52749b26f3ea9d1c6c06da34
Steffen Schumann <steffen.schumann@phys.uni-goettingen.de>
2003-08-18 13:02:33 +0000

    Parameter files verschoenert

--- SHERPA-1.0/Run/Process/Set_Channel.C
+++ SHERPA-1.0/Run/Process/Set_Channel.C
@@ -1,3 +1,4 @@
+#include "P2_3/2_3_8_4_101_0_1/P.H"
 #include "Phase_Space_Generator.H"
 
 using namespace AMEGIC;
@@ -7,8 +8,12 @@ using namespace std;
 Single_Channel * Phase_Space_Generator::SetChannel(int nin,int nout,ATOOLS::Flavour* fl,
 						   int chn,string& pID)
 {
+#ifdef P2_3_8_4_101_0_1_on
+  if (pID==string("P2_3_8_4_101_0_1")) return (new P2_3_8_4_101_0_1(nin,nout,fl,chn));
+#endif
   return 0;
 }
+
 
 
 
