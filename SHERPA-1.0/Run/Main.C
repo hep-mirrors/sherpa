374cb8643d57965fdef9e0aeb50e2a92069f4bff
dreas <sherpa@projects.hepforge.org>
2003-11-03 14:06:34 +0000

    *** empty log message ***

--- SHERPA-1.0/Run/Main.C
+++ SHERPA-1.0/Run/Main.C
@@ -15,21 +15,25 @@ using namespace SHERPA;
 int main(int argc,char* argv[]) {  
 
   Sherpa Generator;
-  ATOOLS::msg.Out()<<" Process initialization started "<<std::endl;
-  Generator.InitializeTheRun(std::string("./"));
-  ATOOLS::msg.Out()<<"generate "<<ATOOLS::rpa.gen.NumberOfEvents()<<" events"<<std::endl;
-  Generator.InitializeTheEventHandler();
 
-  int nevt=ATOOLS::rpa.gen.NumberOfEvents();
-  if (nevt>0) ATOOLS::msg.Out()<<"Starting event generation now. "<<std::endl;
-  for (int i=1;i<=nevt;i++) {
-    if (i%500==0) {
-      ATOOLS::msg.Out()<<" Event "<<i<<endl;      
+  ATOOLS::msg.Out()<<" Process initialization started "<<std::endl;
+  Generator.InitializeTheRun(argc,argv);
+
+  if (ATOOLS::rpa.gen.NumberOfEvents()>0) {
+    ATOOLS::msg.Out()<<"generate "<<ATOOLS::rpa.gen.NumberOfEvents()<<" events"<<std::endl;
+    Generator.InitializeTheEventHandler();
+
+    int nevt=ATOOLS::rpa.gen.NumberOfEvents();
+    if (nevt>0) ATOOLS::msg.Out()<<"Starting event generation now. "<<std::endl;
+    for (int i=1;i<=nevt;i++) {
+      if (i%500==0) {
+	ATOOLS::msg.Out()<<" Event "<<i<<endl;      
+      }
+      if (Generator.GenerateOneEvent()) ATOOLS::msg.Events()<<"Sherpa : Passed "<<i<<" events."<<std::endl;
     }
-    if (Generator.GenerateOneEvent()) ATOOLS::msg.Events()<<"Sherpa : Passed "<<i<<" events."<<std::endl;
+    Generator.SummarizeRun();
+    ATOOLS::msg.Events()<<"Sherpa did "<<nevt<<" with "<<Generator.NumberOfErrors()<<" errors."<<std::endl;
   }
-  Generator.SummarizeRun();
-  ATOOLS::msg.Events()<<"Sherpa did "<<nevt<<" with "<<Generator.NumberOfErrors()<<" errors."<<std::endl;
   ATOOLS::msg.Out()<<" Simulation finished "<<std::endl;
 }
 
