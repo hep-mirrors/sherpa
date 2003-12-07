6ce8e550491c615f7baae89839f0714f3cd142b8
dreas <sherpa@projects.hepforge.org>
2003-12-07 20:44:00 +0000

    dynamically load process libraries

--- SHERPA-1.0/Run/Process/Set_Channel.C
+++ SHERPA-1.0/Run/Process/Set_Channel.C
@@ -1,14 +1,47 @@
 #include "Phase_Space_Generator.H"
 
+#include <dlfcn.h>
+
 using namespace AMEGIC;
 using namespace PHASIC;
 using namespace std;
 
+typedef Single_Channel * (*Getter_Function)(int nin,int nout,ATOOLS::Flavour* fl,
+						   int chn);
+
 Single_Channel * Phase_Space_Generator::SetChannel(int nin,int nout,ATOOLS::Flavour* fl,
 						   int chn,string& pID)
 {
-  return 0;
+  std::string libname=std::string("libProc_")+pID.substr(1)+std::string(".so");
+  std::string gettername=std::string("Getter_")+pID;
+
+  char * error;
+  void * module;
+  Getter_Function GetterFunction;
+
+  // try loading library 
+  module = dlopen(libname.c_str(),RTLD_LAZY);
+  error  = dlerror();
+  if (module==NULL) {
+    cout<<" Error in loading library "<<libname<<endl;
+    cout<<error<<endl;
+    return 0;
+  }
+
+  GetterFunction = (Getter_Function)dlsym(module,gettername.c_str());
+  error  = dlerror();
+  if (error!=NULL) {
+    cout<<" Error in loading symbol from library "<<endl;
+    cout<<error<<endl;
+    return 0;
+  }
+
+  cout<<" calling Getter for library"<<endl;
+  return GetterFunction(nin,nout,fl,chn);
 }
+
+
+
 
 
 
--- SHERPA-1.0/Run/Process/Set_Values.C
+++ SHERPA-1.0/Run/Process/Set_Values.C
@@ -1,161 +1,38 @@
 #include "String_Handler.H"
 
+#include <dlfcn.h>
+
 using namespace AMEGIC;
 using namespace std;
 
+typedef Values* (*Getter_Function)(Basic_Sfuncs*);
+
 Values* String_Handler::Set_Values(std::string& pID,Basic_Sfuncs* BS)
 {
-  return 0;
+  std::string libname=std::string("libProc_")+pID.substr(1)+std::string(".so");
+  std::string gettername=std::string("Getter_")+pID;
+
+  char * error;
+  void * module;
+  Getter_Function GetterFunction;
+
+  // try loading library 
+  module = dlopen(libname.c_str(),RTLD_LAZY);
+  error  = dlerror();
+  if (module==NULL) {
+    cout<<" Error in loading library "<<libname<<endl;
+    cout<<error<<endl;
+    return 0;
+  }
+
+  GetterFunction = (Getter_Function)dlsym(module,gettername.c_str());
+  error  = dlerror();
+  if (error!=NULL) {
+    cout<<" Error in loading symbol from library "<<endl;
+    cout<<error<<endl;
+    return 0;
+  }
+
+  cout<<" calling Getter for library"<<endl;
+  return GetterFunction(BS);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
