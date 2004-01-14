348bf172209d1df444da9d74768f9d73a7dc42f4
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-01-14 07:06:16 +0000

    *** empty log message ***

--- SHERPA-1.0/PerturbativePhysics/MI_Handler.C
+++ SHERPA-1.0/PerturbativePhysics/MI_Handler.C
@@ -1,8 +1,8 @@
 #include "MI_Handler.H"
-#include "Data_Read.H"
 
-#include "Lund_Wrapper.H"
+#include "Data_Read.H"
 #include "Lund_Interface.H"
+#include "Lund_Wrapper.H"
 
 using namespace SHERPA;
 
@@ -20,14 +20,11 @@ MI_Handler::MI_Handler(std::string path,std::string file,MODEL::Model_Base *mode
   file=read->GetValue<std::string>("INPUT_FILE",file);
   beamfile=read->GetValue<std::string>("BEAM_DATA_FILE","Beam.dat");
   delete read;
-//   Lund_Interface *pyinterface = new Lund_Interface(path,"Lund.dat");
-//   delete pyinterface;
   if (mihandler==std::string("Amisic")) {
     p_amisic = new AMISIC::Amisic(model,beam,isr);
     p_amisic->SetInputPath(path);
     p_amisic->SetOutputPath(path);
     p_amisic->SetInputFile(file);
-//     p_amisic->SetNorm(SigmaND(path,beamfile));
     p_amisic->Initialize();
     m_type=Amisic;
   }
@@ -36,41 +33,6 @@ MI_Handler::MI_Handler(std::string path,std::string file,MODEL::Model_Base *mode
 MI_Handler::~MI_Handler() 
 {
   if (p_amisic!=NULL) delete p_amisic;
-}
-
-double MI_Handler::SigmaND(const std::string path,const std::string file)
-{
-  return pyint7.sigt[5][0][0];
-  /*
-    Fit of total xs according to 
-    A.Donnachie and P.V.Landshoff, Phys.Lett.B 296 (1992) 227
-  */
-  /*
-  double epsilon=0.0808, eta=0.4525;
-  double Xpp=21.70, Ypp=56.08, Yppbar=98.39, Bp=2.3;
-  double g3P=0.318;
-  ATOOLS::Data_Read *read = new ATOOLS::Data_Read(path+file);
-  ATOOLS::Flavour beam[2];
-  for (unsigned int i=0;i<2;++i) {
-    int flav=dataread->GetValue<int>("BEAM_"+i);  
-    beam[i]=ATOOLS::Flavour(kf::code(abs(flav)));
-    if (flav<0) beam[i]=beam[i].Bar();
-  }
-  delete read;
-  double X, Y, B, s=ATOOLS::rpa.gen.Ecms();
-  if ((beam[0].KfCode()==kf::p_plus)&&(beam[1].KfCode()==kf::p_plus)) {
-    X=Xpp; B=4.0*Bp+4.0*pow(s,epsilon)-4.2;
-    if (beam[0].IsAnti()||beam[1].IsAnti()) Y=Ypp;
-    else Y=Yppbar;
-  }
-  double xstot, xsel, xssd1, xssd2, xsdd;
-  xstot=X*pow(s,epsilon)+Y*pow(s,eta);
-  xsel=xstot*xstot/(16.0*M_PI*B);
-  xssd1=;
-  xssd2=;
-  xsdd=;
-  SetNorm(xstot-xsel-xssd1-xssd2-xsdd);
-  */
 }
 
 Matrix_Element_Handler *MI_Handler::HardMEHandler()
