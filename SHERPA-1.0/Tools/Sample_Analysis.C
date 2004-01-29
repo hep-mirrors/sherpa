90d736223daedf74b589d199015d5cd73a864745
dreas <sherpa@projects.hepforge.org>
2004-01-29 16:49:25 +0000

    extended matching analysis

--- SHERPA-1.0/Tools/Sample_Analysis.C
+++ SHERPA-1.0/Tools/Sample_Analysis.C
@@ -10,13 +10,7 @@ namespace SHERPA {
 
   class PHard_Observable : public ATOOLS::Primitive_Observable_Base {  
   public:
-    PHard_Observable(int _type,double _xmin,double _xmax,int _nbins)
-    {
-      type = _type; xmin = _xmin; xmax = _xmax; nbins = _nbins;
-      name  = std::string("pt_hard.dat");
-      histo = new ATOOLS::Histogram(type,xmin,xmax,nbins);
-    };
-
+    PHard_Observable(int _type,double _xmin,double _xmax,int _nbins, std::string _name);
     void Evaluate(double); 
     void Evaluate(int,ATOOLS::Vec4D *,ATOOLS::Flavour *,double w);
     void Evaluate(const ATOOLS::Particle_List &,double w);
@@ -30,10 +24,21 @@ using namespace SHERPA;
 using namespace ATOOLS;
 using namespace std;
 
+int Sample_Analysis::m_njet=2;
+double Sample_Analysis::m_pt_W=0.;
+
+PHard_Observable::PHard_Observable(int _type,double _xmin,double _xmax,int _nbins, std::string _name)
+{
+  type = _type; xmin = _xmin; xmax = _xmax; nbins = _nbins;
+  name  = std::string("pt_hard.dat");
+  name  = _name+std::string(".dat");
+  histo = new ATOOLS::Histogram(type,xmin,xmax,nbins);
+}
 
 void PHard_Observable::Evaluate(double w)
 {
-  histo->Insert(sqrt(amegic_apacic_interface_last_hard_scale),w); 
+  //  histo->Insert(sqrt(amegic_apacic_interface_last_hard_scale),w); 
+  histo->Insert(Sample_Analysis::m_pt_W,w); 
 }
 void PHard_Observable::Evaluate(int,ATOOLS::Vec4D *,ATOOLS::Flavour *,double w)
 {
@@ -62,12 +67,21 @@ Sample_Analysis::Sample_Analysis(std::string _m_path,std::string _m_file):
   if (!(status)) { ana = 0; return; }
   ana        = new Primitive_Analysis();
   ana->AddObservable(new Shower_Observables(11,1.e-6,1.,180,0));
-  ana->AddObservable(new PHard_Observable(00,0.,250.,125));
+  //  ana->AddObservable();
+  //  ana->AddObservable(new PHard_Observable(00,0.,250.,125));
   //  ana->AddObservable(new PT_Distribution(00,0.,250.,125,1,Flavour(kf::photon)));
 //   ana->AddObservable(new PT_Distribution(00,0.,250.,50,6,Flavour(kf::jet)));
 
   obs.push_back(new ME_Rate(00,1.5,7.5,6,"me"));
   obs.push_back(new ME_Rate(00,1.5,7.5,6,"me_nll"));
+  obs.push_back(new PHard_Observable(00,0.,200.,100,"pw_me"));
+  obs.push_back(new PHard_Observable(00,0.,200.,100,"pw_me_2"));
+  obs.push_back(new PHard_Observable(00,0.,200.,100,"pw_me_3"));
+  obs.push_back(new PHard_Observable(00,0.,200.,100,"pw_me_4"));
+  obs.push_back(new PHard_Observable(00,0.,200.,100,"pw_me_pure"));
+  obs.push_back(new PHard_Observable(00,0.,200.,100,"pw_me_pure_2"));
+  obs.push_back(new PHard_Observable(00,0.,200.,100,"pw_me_pure_3"));
+  obs.push_back(new PHard_Observable(00,0.,200.,100,"pw_me_pure_4"));
 }
 
 
@@ -75,6 +89,22 @@ Sample_Analysis::Sample_Analysis(std::string _m_path,std::string _m_file):
 void Sample_Analysis::AfterME(ATOOLS::Blob_List * blobs, double weight) {
   if (!(status)) return;
   obs[0]->Evaluate(*blobs,weight);
+
+  for (Blob_Const_Iterator bit=blobs->begin();bit!=blobs->end();++bit) {
+    if ((*bit)->Type().find("Signal") !=std::string::npos ) {
+      m_njet= (*bit)->NOutP();
+      Vec4D sum;
+      for (int i=0; i<m_njet; ++i) {
+	if ( (*bit)->OutParticle(i)->Flav().IsLepton()) {
+	  sum+=(*bit)->OutParticle(i)->Momentum();
+	}
+	m_pt_W=sqrt(sqr(sum[1])+sqr(sum[2]));
+      }
+    }
+  }
+  obs[6]->Evaluate(*blobs,weight);
+  obs[m_njet+5]->Evaluate(*blobs,weight);
+
 }
 
 void Sample_Analysis::AfterPartonShower(ATOOLS::Blob_List * blobs, double weight) {
@@ -83,6 +113,10 @@ void Sample_Analysis::AfterPartonShower(ATOOLS::Blob_List * blobs, double weight
   // extra statistics
   obs[1]->Evaluate(*blobs,weight);
 
+  obs[2]->Evaluate(*blobs,weight);
+  obs[m_njet+1]->Evaluate(*blobs,weight);
+
+  
   // simple parton level analysis:
   ana->DoAnalysis(*blobs,weight);
 }
@@ -163,8 +197,8 @@ bool  Analysis_Phase::Treat(ATOOLS::Blob_List * bl, double & weight)
   switch (m_mode) {
   case 1:
     // should be if "signal process" has status 1! beware of MI
-
     if (bl->size()==1 && bl->back()->Status()==1) {
+
       p_analysis->AfterME(bl,weight);
       m_status = 1;
     }
