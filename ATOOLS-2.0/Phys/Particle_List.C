10bb72e488168edad5ab1c9d1c5372e79a0f6ccf
dreas <sherpa@projects.hepforge.org>
2004-02-18 13:34:06 +0000

    output operator

--- ATOOLS-2.0/Phys/Particle_List.C
+++ ATOOLS-2.0/Phys/Particle_List.C
@@ -14,13 +14,16 @@ namespace ATOOLS {
 				      std::back_insert_iterator<Particle_List> ,Is_Photon);
   template void copy_if<>( Particle_Iterator, Particle_Iterator , 
 				      std::back_insert_iterator<Particle_List> ,Is_Final_State);
-}
+  template void copy_if<>( Particle_Iterator, Particle_Iterator , 
+				      std::back_insert_iterator<Particle_List> ,Is_Charged);
 
-std::ostream & operator<<(std::ostream & s, Particle_List & pl) {
-  s<<"Particle List with "<<pl.size()<<" elements"<<std::endl;
-  for (Particle_Iterator pit=pl.begin(); pit!=pl.end(); ++pit) {
-    s<<(*pit)<<std::endl; // note still pointer of particle!
+  std::ostream & operator<<(std::ostream & s, Particle_List & pl) {
+    s<<"Particle List with "<<pl.size()<<" elements"<<std::endl;
+    for (Particle_Iterator pit=pl.begin(); pit!=pl.end(); ++pit) {
+      s<<(*pit)<<std::endl; // note still pointer of particle!
+    }
+    return s;
   }
-  return s;
+
 }
  
