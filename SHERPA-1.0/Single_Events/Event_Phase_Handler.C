558763bc1022983c5330301fd99bdeb988b254ab
Frank Krauss <frank.krauss@durham.ac.uk>
2004-02-19 22:51:20 +0000

    *** empty log message ***

--- SHERPA-1.0/Single_Events/Event_Phase_Handler.C
+++ SHERPA-1.0/Single_Events/Event_Phase_Handler.C
@@ -4,11 +4,11 @@
 using namespace SHERPA;
 using namespace ATOOLS;
 
-Event_Phase_Handler::Event_Phase_Handler()
-{
-  m_type = std::string("Unspecified");
-  m_name = std::string("No Name");
-}
+Event_Phase_Handler::Event_Phase_Handler() :
+  m_type(std::string("Unspecified")), m_name(std::string("No Name")) { }
+
+Event_Phase_Handler::Event_Phase_Handler(std::string _name) :
+  m_type(std::string("Unspecified")), m_name(_name) { }
 
 
 Event_Phase_Handler::~Event_Phase_Handler() { }
