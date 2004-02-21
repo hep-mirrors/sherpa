e318fdeb3d9ea5d81d4c36d523b32d71000a1418
Frank Krauss <frank.krauss@durham.ac.uk>
2004-02-21 03:26:11 +0000

    small changes of Hard_Decay_Handler accounted for.

--- SHERPA-1.0/Initialization/Initialization_Handler.C
+++ SHERPA-1.0/Initialization/Initialization_Handler.C
@@ -105,7 +105,7 @@ bool Initialization_Handler::InitializeTheFramework(int nr)
 
   if (!CheckBeamISRConsistency()) return 0.;
   
-  //  okay = okay && InitializeTheHardDecays();
+  okay = okay && InitializeTheHardDecays();
   okay = okay && InitializeTheMatrixElements();
   //  only if events:
   if (rpa.gen.NumberOfEvents()>0) {
@@ -354,12 +354,6 @@ bool Initialization_Handler::CalculateTheHardProcesses()
     ATOOLS::msg.Out()<<endl;
   }
   return ok;
-}
-
-bool Initialization_Handler::InitializeAllHardDecays()
-{
-  return 1;
-  return p_harddecays->InitializeAllHardDecays(m_medat,p_model);
 }
 
 void Initialization_Handler::SetParameter(int nr) {
