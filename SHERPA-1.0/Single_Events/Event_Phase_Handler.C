dccf6a3fdc674d1f543311e87e86111bf00fc167
Frank Krauss <frank.krauss@durham.ac.uk>
2004-03-19 14:13:43 +0000

    Minor changes

--- SHERPA-1.0/Single_Events/Event_Phase_Handler.C
+++ SHERPA-1.0/Single_Events/Event_Phase_Handler.C
@@ -1,8 +1,6 @@
 #include "Event_Phase_Handler.H"
-#include "Message.H"
 
 using namespace SHERPA;
-using namespace ATOOLS;
 
 Event_Phase_Handler::Event_Phase_Handler() :
   m_type(std::string("Unspecified")), m_name(std::string("No Name")) { }
@@ -13,18 +11,3 @@ Event_Phase_Handler::Event_Phase_Handler(std::string _name) :
 
 Event_Phase_Handler::~Event_Phase_Handler() { }
 
-std::string Event_Phase_Handler::Name() {
-  return m_name;
-}
-
-void Event_Phase_Handler::SetName(std::string _name) {
-  m_name = _name;
-}
-
-std::string Event_Phase_Handler::Type() {
-  return m_type;
-}
-
-void Event_Phase_Handler::SetType(std::string _type) {
-  m_type = _type;
-}
