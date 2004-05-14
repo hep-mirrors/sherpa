bae7df06183d3f8cb997b13bf5bb58d41f96c79b
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-05-14 06:35:03 +0000

    exception handling

--- ATOOLS-2.0/Org/Exception.C
+++ ATOOLS-2.0/Org/Exception.C
@@ -1,140 +1,8 @@
 #include "Exception.H"
 
-#include "Run_Parameter.H"
 #include <iostream>
 
 using namespace ATOOLS;
-
-unsigned int Exception::s_exitcode=1;
-std::vector<Exception::Tester> Exception::s_testers=std::vector<Exception::Tester>();
-std::vector<Exception::Terminator> Exception::s_terminators=std::vector<Exception::Terminator>();
-std::vector<Terminator_Base*> Exception::s_objects=std::vector<Terminator_Base*>();
-
-void Terminator_Base::Terminate()
-{
-  msg.Error()<<"Terminator_Base::Terminate(): "
-	     <<"Vitual function called !"<<std::endl;
-}
-
-Exception::Exception(const ex::type type,const std::string info):
-  m_type(type),
-  m_info(info)
-{
-  SetExitCode();
-}
-
-Exception::Exception(const ex::type type,const std::string info,
-		     const std::string cclass,const std::string cmethod):
-  m_type(type),
-  m_info(info),
-  m_class(cclass),
-  m_method(cmethod) 
-{
-  SetExitCode();
-}
-
-void Exception::Terminate() 
-{
-  msg.Error()<<"Exception::Terminate(): Preparing termination ..."<<std::endl;
-  PrepareTerminate();
-  Exit(s_exitcode);
-}
-
-void Exception::Exit(int exitcode)
-{
-  msg.Error()<<om::bold<<"Exception::Exit: "<<om::reset<<om::blue
-	     <<"exiting sherpa with code "<<om::reset<<om::bold<<"("<<om::red<<exitcode
-	     <<om::reset<<om::bold<<")"<<om::reset<<std::endl;
-  exit(exitcode);
-}
-
-void Exception::SignalHandler(int signal) 
-{
-  std::string input="y";
-  static unsigned int bus_signals, segv_signals;
-  msg.Error()<<om::bold<<"Exception::SignalHandler: "<<om::reset<<om::blue
-	     <<"Signal "<<om::reset<<om::bold<<"("<<om::red<<signal
-	     <<om::reset<<om::bold<<")"<<om::reset<<om::blue<<" caught. "<<om::reset<<std::endl;
-  switch (signal) {
-  case SIGSEGV:
-    ++segv_signals;
-    if (segv_signals>3) {
-      msg.Error()<<om::reset<<"   Abort immediately."<<om::reset<<std::endl;
-      abort();
-    }
-  case SIGABRT:
-  case SIGTERM:
-  case SIGXCPU:
-    msg.Error()<<om::reset<<"   Cannot run further."<<om::reset<<std::endl;
-    s_exitcode=2;
-    Terminate();
-    break;
-  case SIGINT:
-    if (!rpa.gen.BatchMode()) {
-      msg.Error()<<"   Do you want to stop the program ? "<<om::reset;
-      std::cin>>input;
-    }
-    if (input!="y" && input!="Y") return;
-    s_exitcode=1;
-    Terminate();
-    break;
-  case SIGBUS:
-    ++bus_signals;
-    if (bus_signals>3) {
-      msg.Error()<<om::reset<<"   Terminating."<<om::reset<<std::endl;
-      s_exitcode=3;
-      Terminate();
-    }
-    msg.Error()<<om::reset<<"   Try to run further."<<om::reset<<std::endl;
-    break;
-  case SIGFPE:
-    msg.Error()<<"   Sherpa does not throw floating point exceptions."<<om::reset<<std::endl;
-    break;
-  default:
-    msg.Error()<<"   Cannot handle signal. Abort."<<om::reset<<std::endl;
-    s_exitcode=1;
-    Terminate();
-  }
-}
-
-bool Exception::ApproveTerminate() const
-{
-  if (s_testers.size()==0) return true;
-  for (size_t i=0;i<s_testers.size();++i) if (s_testers[i](*this)) return true;
-  return false;
-}
-
-void Exception::PrepareTerminate()
-{
-  for (size_t i=0;i<s_objects.size();++i) s_objects[i]->Terminate(); 
-  for (size_t i=0;i<s_terminators.size();++i) s_terminators[i]();
-}
-
-void Exception::UpdateLogFile() const 
-{
-  msg.LogFile()<<"Sherpa";
-  if (m_class.length()>0) {
-    msg.LogFile()<<" : "<<m_class<<"::"<<m_method;
-  }
-  msg.LogFile()<<" throws "<<m_type<<": "<<m_info;
-}
-
-void Exception::RemoveObject(Terminator_Base *const object)
-{
-  for (std::vector<Terminator_Base*>::iterator oit=s_objects.begin();
-       oit!=s_objects.end();) {
-    if (*oit==object) oit=s_objects.erase(oit); 
-    else ++oit;
-  }
-}
-
-void Exception::SetExitCode()
-{
-  s_exitcode=1;
-  if (m_class=="ISR_Handler")            s_exitcode=151;
-  if (m_class=="Matrix_Element_Handler") s_exitcode=201;
-
-}
 
 std::ostream &ATOOLS::operator<<(std::ostream &str,const ex::type &type)
 {
@@ -147,6 +15,44 @@ std::ostream &ATOOLS::operator<<(std::ostream &str,const ex::type &type)
   return str;
 }
 
+bool Tester_Object::ApproveTerminate()
+{
+  msg.Error()<<"Tester_Oject::ApproveTerminate(): "
+	     <<"Vitual function called !"<<std::endl;
+}
+
+void Terminator_Object::PrepareTerminate()
+{
+  msg.Error()<<"Terminator_Object::PrepareTerminate(): "
+	     <<"Vitual function called !"<<std::endl;
+}
+
+Exception::Exception(const ex::type type,const std::string info):
+  m_type(type),
+  m_info(info)
+{
+  Exception_Handler::s_exception=this;
+}
+
+Exception::Exception(const ex::type type,const std::string info,
+		     const std::string cclass,const std::string cmethod):
+  m_type(type),
+  m_info(info),
+  m_class(cclass),
+  m_method(cmethod) 
+{
+  Exception_Handler::s_exception=this;
+}
+
+void Exception::UpdateLogFile() const 
+{
+  msg.LogFile()<<"Sherpa";
+  if (m_class.length()>0) {
+    msg.LogFile()<<" : "<<m_class<<"::"<<m_method;
+  }
+  msg.LogFile()<<" throws "<<m_type<<": "<<m_info;
+}
+
 std::ostream &ATOOLS::operator<<(std::ostream &str,const Exception &exception)
 {
   str<<om::bold<<"Sherpa";
@@ -155,7 +61,7 @@ std::ostream &ATOOLS::operator<<(std::ostream &str,const Exception &exception)
        <<"::"<<exception.m_method;
   }
   return str<<om::reset<<" throws "<<om::bold<<om::red
-	    <<exception.m_type<<om::reset<<om::bold<<": "<<std::endl<<om::reset<<om::red
+	    <<exception.m_type<<om::reset<<om::bold<<": "<<std::endl<<"   "<<om::reset<<om::red
 	    <<exception.m_info<<om::reset;
 }
 
---
+++ ATOOLS-2.0/Org/Exception_Handler.C
@@ -0,0 +1,146 @@
+#include "Exception.H"
+
+#include "Run_Parameter.H"
+
+using namespace ATOOLS;
+
+unsigned int Exception_Handler::s_exitcode=0;
+Exception *Exception_Handler::s_exception=0;
+
+unsigned int Exception_Handler::s_nbus=0;
+unsigned int Exception_Handler::s_nsegv=0;
+
+std::vector<Exception_Handler::Tester_Function> 
+Exception_Handler::s_testerfunctions=std::vector<Exception_Handler::Tester_Function>();
+std::vector<Exception_Handler::Terminator_Function> 
+Exception_Handler::s_terminatorfunctions=std::vector<Exception_Handler::Terminator_Function>();
+std::vector<Tester_Object*> 
+Exception_Handler::s_testerobjects=std::vector<Tester_Object*>();
+std::vector<Terminator_Object*> 
+Exception_Handler::s_terminatorobjects=std::vector<Terminator_Object*>();
+
+bool Exception_Handler::ApproveTerminate()
+{
+  msg.Error()<<"Exception_Handler::ApproveTerminate(): Asking for termination ..."<<std::endl;
+  if (s_testerfunctions.size()==0 && s_testerobjects.size()==0) {
+      msg.Error()<<"... approved."<<std::endl;
+      return true;
+  }
+  if (s_testerfunctions.size()>0) {
+    for (size_t i=0;i<s_testerobjects.size();++i) if (s_testerfunctions[i]()) {
+      msg.Error()<<"... approved."<<std::endl;
+      return true;
+    }
+  }
+  if (s_testerobjects.size()>0) {
+    for (size_t i=0;i<s_testerobjects.size();++i) if (s_testerobjects[i]->ApproveTerminate()) {
+      msg.Error()<<"... approved."<<std::endl;
+      return true;
+    }
+  }
+  msg.Error()<<"... refused."<<std::endl;
+  return false;
+}
+
+void Exception_Handler::PrepareTerminate()
+{
+  msg.Error()<<"Exception_Handler::PrepareTerminate(): Preparing termination ..."<<std::endl;
+  for (size_t i=0;i<s_terminatorobjects.size();++i) s_terminatorobjects[i]->PrepareTerminate(); 
+  for (size_t i=0;i<s_terminatorfunctions.size();++i) s_terminatorfunctions[i](); 
+  msg.Error()<<"... prepared."<<std::endl;
+}
+
+void Exception_Handler::Exit(int exitcode)
+{
+  msg.Error()<<om::bold<<"Exception_Handler::Exit: "<<om::reset<<om::blue
+	     <<"exiting sherpa with code "<<om::reset<<om::bold<<"("<<om::red<<exitcode
+	     <<om::reset<<om::bold<<")"<<om::reset<<std::endl;
+  exit(exitcode);
+}
+
+void Exception_Handler::Terminate() 
+{
+  if (!ApproveTerminate()) {
+    s_exception=NULL;
+    return;
+  }
+  PrepareTerminate();
+  Exit(s_exitcode);
+}
+
+void Exception_Handler::RemoveTesterObject(Tester_Object *const testerobject)
+{
+  for (std::vector<Tester_Object*>::iterator toit=s_testerobjects.begin();
+       toit!=s_testerobjects.end();) {
+    if (*toit==testerobject) toit=s_testerobjects.erase(toit); 
+    else ++toit;
+  }
+}
+
+void Exception_Handler::RemoveTerminatorObject(Terminator_Object *const terminatorobject)
+{
+  for (std::vector<Terminator_Object*>::iterator toit=s_terminatorobjects.begin();
+       toit!=s_terminatorobjects.end();) {
+    if (*toit==terminatorobject) toit=s_terminatorobjects.erase(toit); 
+    else ++toit;
+  }
+}
+
+void Exception_Handler::SetExitCode()
+{
+  s_exitcode=1;
+  if (s_exception==NULL) return;
+  if (s_exception->m_class=="ISR_Handler")            s_exitcode=151;
+  if (s_exception->m_class=="Matrix_Element_Handler") s_exitcode=201;
+
+}
+
+void Exception_Handler::SignalHandler(int signal) 
+{
+  std::string input="y";
+  msg.Error()<<om::bold<<"Exception_Handler::SignalHandler: "<<om::reset<<om::blue
+	     <<"Signal "<<om::reset<<om::bold<<"("<<om::red<<signal
+	     <<om::reset<<om::bold<<")"<<om::reset<<om::blue<<" caught. "<<om::reset<<std::endl;
+  switch (signal) {
+  case SIGSEGV:
+    ++s_nsegv;
+    if (s_nsegv>3) {
+      msg.Error()<<om::reset<<"   Abort immediately."<<om::reset<<std::endl;
+      kill(getpid(),9);
+    }
+  case SIGABRT:
+  case SIGTERM:
+  case SIGXCPU:
+    msg.Error()<<om::reset<<"   Cannot continue."<<om::reset<<std::endl;
+    s_exitcode=2;
+    Terminate();
+    break;
+  case SIGINT:
+    if (!rpa.gen.BatchMode()) {
+      msg.Error()<<"   Do you want to stop the program ? "<<om::reset;
+      std::cin>>input;
+    }
+    if (input!="y" && input!="Y") return;
+    s_exitcode=1;
+    Terminate();
+    break;
+  case SIGBUS:
+    ++s_nbus;
+    if (s_nbus>3) {
+      msg.Error()<<om::reset<<"   Abort immediately."<<om::reset<<std::endl;
+      kill(getpid(),9);
+    }
+    msg.Error()<<om::reset<<"   Cannot continue."<<om::reset<<std::endl;
+    s_exitcode=3;
+    Terminate();
+    break;
+  case SIGFPE:
+    msg.Error()<<"   Sherpa does not throw floating point exceptions."<<om::reset<<std::endl;
+    break;
+  default:
+    msg.Error()<<"   Cannot handle signal."<<om::reset<<std::endl;
+    s_exitcode=1;
+    Terminate();
+  }
+}
+
