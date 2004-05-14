07df094abea05fabb9429f36a0f2a150394a437b
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-05-14 09:01:50 +0000

    bugfix with no effect

--- EXTRA_XS-1.0/Two2Two/Off_Shell_QCD.C
+++ EXTRA_XS-1.0/Two2Two/Off_Shell_QCD.C
@@ -29,11 +29,11 @@ double Off_Shell_gg_qqb::operator()(double s,double t,double u)
   double a3=p[4]*p[2], a4=p[4]*p[3];
   double b3=p[5]*p[2], b4=p[5]*p[3];
   double k12=-p[0].Abs2(), k22=-p[1].Abs2();
-  double delta=2.*S*(2.*(a3*b4-a4*b3)/S-k12*a4/(p[4]*p[1])+k22*b4/(p[5]*p[0])+p[3]*(p[0]-p[1]));
-  double abelian=4.*(S*S/((t-M2)*(u-M2))-1./(k12*k22)*ATOOLS::sqr(S+2.*(a4*b3/(t-M2)+b4*a3/(u-M2))));
+  double delta=2.*S*(2.*(a4*b3-a3*b4)/S-k12*a3/(p[4]*p[1])+k22*b3/(p[5]*p[0])+p[2]*(p[0]-p[1]));
+  double abelian=4.*(S*S/((t-M2)*(u-M2))-1./(k12*k22)*ATOOLS::sqr(S+2.*(a3*b4/(t-M2)+b3*a4/(u-M2))));
   double nonabelian=4.*S*S*(S/(s*(p[4]*p[1])*(p[5]*p[0]))-1./((t-M2)*(u-M2))
-			    -(1/(t-M2)-1/(u-M2))/s*(a3/(p[4]*p[1])-b3/(p[5]*p[0])))
-    +2./(k12*k22)*(S+4.*a4*b3/(t-M2)-delta/s)*(S+4.*a3*b4/(u-M2)+delta/s);
+			    -(1/(t-M2)-1/(u-M2))/s*(a4/(p[4]*p[1])-b4/(p[5]*p[0])))
+    +2./(k12*k22)*(S+4.*a3*b4/(t-M2)-delta/s)*(S+4.*a4*b3/(u-M2)+delta/s);
   return ATOOLS::sqr(4.*M_PI*m_alphas*(p[0]*p[5])*(p[1]*p[4])/(S*S))*
     (abelian/(2.*NC)+nonabelian*NC/(2.*(NC*NC-1.)));
 }
