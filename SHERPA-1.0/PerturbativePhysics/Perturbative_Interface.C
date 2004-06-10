8d7666589ae103ccf609aff4c3c681ed049b82b6
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-06-10 06:41:50 +0000

    added C-file for Perturbative_Interface, typical problem with 'inline virtual'

--- SHERPA-1.0/PerturbativePhysics/Perturbative_Interface.C
+++ SHERPA-1.0/PerturbativePhysics/Perturbative_Interface.C
@@ -13,7 +13,7 @@ Perturbative_Interface::Perturbative_Interface(Matrix_Element_Handler *mehandler
   p_fl(NULL), 
   p_moms(NULL) {}
 
-virtual Perturbative_Interface::~Perturbative_Interface() 
+Perturbative_Interface::~Perturbative_Interface() 
 {
   if (p_fl)   { delete p_fl;   p_fl   = NULL; }
   if (p_moms) { delete p_moms; p_moms = NULL; }
