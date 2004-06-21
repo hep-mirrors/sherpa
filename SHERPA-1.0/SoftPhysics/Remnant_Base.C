ffbdb6173b4679997b2fb2028bb2204ebd06cfb1
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-06-21 12:38:26 +0000

    new momentum shifter treatment

--- SHERPA-1.0/SoftPhysics/Remnant_Base.C
+++ SHERPA-1.0/SoftPhysics/Remnant_Base.C
@@ -1,8 +1,8 @@
 #include "Remnant_Base.H"
 
-#include "MathTools.H"
 #include "Run_Parameter.H"
 #include "Exception.H"
+#include "Momentum_Shifter.H"
 
 using namespace SHERPA;
 
@@ -22,13 +22,8 @@ void Remnant_Base::Clear()
   m_initialized=false;
 }
 
-double Remnant_Base::Lambda2(double sp,double sp1,double sp2) 
-{ 
-  return (sp-sp1-sp2)*(sp-sp1-sp2)-4.0*sp1*sp2;
-}
-
 bool Remnant_Base::AdjustKinematics()
-{
+  {
   if (!m_active) return true;
   if (p_partner==NULL) {
     throw(ATOOLS::Exception(ATOOLS::ex::critical_error,"No partner remnant found.",
@@ -40,53 +35,26 @@ bool Remnant_Base::AdjustKinematics()
 			    "Not enough remnants to ensure four momentum conservation.",
 			    "Remnant_Base","AdjustKinematics"));
   }
-  double Erem=ATOOLS::rpa.gen.Ecms(), pzrem=0.0;
-  for (int i=0;i<p_beamblob->NOutP();++i) {
-    if (p_beamblob->OutParticle(i)!=p_last[0]) {
-      Erem-=p_beamblob->OutParticle(i)->Momentum()[0];
-      pzrem-=p_beamblob->OutParticle(i)->Momentum()[3];
-    }
-  }
-  for (int i=0;i<p_partner->BeamBlob()->NOutP();++i) {
-    if (p_partner->BeamBlob()->OutParticle(i)!=p_last[1]) {
-      Erem-=p_partner->BeamBlob()->OutParticle(i)->Momentum()[0];
-      pzrem-=p_partner->BeamBlob()->OutParticle(i)->Momentum()[3];
+  m_erem=ATOOLS::rpa.gen.Ecms();
+  m_pzrem=0.0;
+  for (size_t i=0;i<2;++i) {
+    ATOOLS::Blob *cur=p_beamblob;
+    if (i==1) cur=p_partner->p_beamblob;
+    for (int j=0;j<cur->NOutP();++j) {
+      if (cur->OutParticle(j)!=p_last[i]) {
+	const ATOOLS::Vec4D &p=cur->OutParticle(j)->Momentum();
+	m_erem-=p[0];
+	m_pzrem-=p[3];
+      }
     }
   }
   ATOOLS::Vec4D pr1=p_last[0]->Momentum(), pr2=p_last[1]->Momentum();
-  double sp, sp1, sp2, E1, E2, pz1, pz2;
-  sp1=ATOOLS::sqr(p_last[0]->Flav().PSMass())+ATOOLS::sqr(pr1[1])+ATOOLS::sqr(pr1[2]);
-  sp2=ATOOLS::sqr(p_last[1]->Flav().PSMass())+ATOOLS::sqr(pr2[1])+ATOOLS::sqr(pr2[2]);
-  sp=Erem*Erem-pzrem*pzrem;
-  double spn, ytn, yto=(Erem+pzrem)/(Erem-pzrem);
-  for (double sign=1.0;sign>=-1.0;sign-=2.0) {
-    E1=0.5/sp*((sp+sp1-sp2)*Erem+sign*sqrt(Lambda2(sp,sp1,sp2))*pzrem);
-    E2=Erem-E1;
-    pz1=ATOOLS::Sign(pr1[3])*sqrt(E1*E1-sp1); pz2=ATOOLS::Sign(pr2[3])*sqrt(E2*E2-sp2);
-    spn=ATOOLS::sqr(Erem)-ATOOLS::sqr(pz1+pz2);
-    if (ATOOLS::dabs((spn-sp)/(spn+sp))>ATOOLS::rpa.gen.Accu()) 
-      { pz1*=-1.0; spn=ATOOLS::sqr(E1+E2)-ATOOLS::sqr(pz1+pz2); }
-    if (ATOOLS::dabs((spn-sp)/(spn+sp))>ATOOLS::rpa.gen.Accu()) continue;
-    ytn=(E1+E2+pz1+pz2)/(E1+E2-pz1-pz2);
-    if (ATOOLS::dabs((ytn-yto)/(ytn+yto))>ATOOLS::rpa.gen.Accu()) 
-      { pz1*=-1.0; pz2*=-1.0; ytn=(E1+E2+pz1+pz2)/(E1+E2-pz1-pz2); }
-    if (ATOOLS::dabs((ytn-yto)/(ytn+yto))>ATOOLS::rpa.gen.Accu()) continue;
-    if (ATOOLS::dabs(pz1)>ATOOLS::dabs(pz2)) { if (ATOOLS::Sign(pz1)==ATOOLS::Sign(pr1[3])) break; }
-    else { if (ATOOLS::Sign(pz2)==ATOOLS::Sign(pr2[3])) break; }
-  }
-  /*
-  if (pz2>0.0&&pz1<0.0) {
-    ATOOLS::msg.Error()<<"Remnant_Base::AdjustKinematics(): "
-		       <<"Warning. Interchanged remnant momenta."<<std::endl
-		       <<"   Former momenta are "<<pr1<<" "<<pr2<<std::endl
-		       <<"   New momenta are    "<<ATOOLS::Vec4D(E1,pr1[1],pr1[2],pz1)
-		       <<" "<<ATOOLS::Vec4D(E2,pr2[1],pr2[2],pz2)<<std::endl;
-  }
-  */
-  pr1=ATOOLS::Vec4D(E1,pr1[1],pr1[2],pz1); p_last[0]->SetMomentum(pr1);
-  pr2=ATOOLS::Vec4D(E2,pr2[1],pr2[2],pz2); p_last[1]->SetMomentum(pr2);
+  ATOOLS::Momentum_Shifter shift(p_last[0],p_last[1]);
+  shift.SetSPerp(ATOOLS::sqr(p_last[0]->Flav().PSMass())+pr1.PPerp2(),1);
+  shift.SetSPerp(ATOOLS::sqr(p_last[1]->Flav().PSMass())+pr2.PPerp2(),2);
+  shift.SetShift(ATOOLS::Vec4D(m_erem-(pr1+pr2)[0],0.,0.,m_pzrem-(pr1+pr2)[3]));
+  if (shift.Scale()!=ATOOLS::ms::no_error) AdjustEnergy();
   for (size_t i=0;i<2;++i) {
-    // the brackets are necessary for 'nan'-values
     if (!(p_last[i]->Momentum()[0]>0.)) {
       ATOOLS::msg.Error()<<"Remnant_Base::AdjustKinematics(): "
 			 <<"Parton ("<<p_last[i]->Number()<<") has non-positive energy "
@@ -117,4 +85,8 @@ void Remnant_Base::UnDo()
 double Remnant_Base::MinimalEnergy(const ATOOLS::Flavour &flavour) 
 {
   return 0.;
+}
+
+void Remnant_Base::AdjustEnergy()
+{
 }
