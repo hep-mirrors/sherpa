b33dc084c6394d00ae1a9ad61b92e98fe063d4f4
Frank Krauss <frank.krauss@durham.ac.uk>
2004-08-03 16:17:07 +0000

    Added MCatNLO

--- SHERPA-1.0/HerwigTools/fiherwig.f
+++ SHERPA-1.0/HerwigTools/fiherwig.f
@@ -1,20 +1,10 @@
 C---HERWIG 6
-      SUBROUTINE HWCINT(CPTMIN,PRNSET) 
-      DOUBLE PRECISION CPTMIN
-      LOGICAL PRNSET
-C---COMMON BLOCKS ARE INCLUDED AS FILE herwig6500.inc
-      INCLUDE 'herwig6500.inc'
+      SUBROUTINE HWCINT() 
+C---COMMON BLOCKS ARE INCLUDED AS FILE HERWIG65.INC
+      INCLUDE 'HERWIG65.INC'
       EXTERNAL HWUDAT
 C---INITIALISE OTHER COMMON BLOCKS
       CALL HWIGIN
-C---USER CAN RESET PARAMETERS AT
-C   THIS POINT, OTHERWISE DEFAULT
-C   VALUES IN HWIGIN WILL BE USED.
-      PTMIN=CPTMIN
-C---DON'T INCLUDE SPIN CORRELATIONS
-      SYSPIN=.FALSE.
-      PRNDEF=.FALSE.
-      IF (PRNSET) PRNDEF=.TRUE.
 C---COMPUTE PARAMETER-DEPENDENT CONSTANTS
       CALL HWUINC
 C---CALL HWUSTA TO MAKE ANY PARTICLE STABLE
@@ -25,6 +15,8 @@ C---INITIALISE ELEMENTARY PROCESS
       CALL HWEINI
       RETURN
       END
+
+
 C---SINGLE EVENT GENERATION
       SUBROUTINE HWCGSE
 C---INITIALISE EVENT
@@ -49,6 +41,9 @@ C---FINISH EVENT
       CALL HWUFNE
       RETURN
       END
+
+
+
 C---TERMINATE ELEMENTARY PROCESS
       SUBROUTINE HWCTRM
       CALL HWEFIN
@@ -56,63 +51,13 @@ C---USER'S TERMINAL CALCULATIONS
       CALL HWAEND
       STOP
       END
+
+
+
 C---DUMMY ROUTINES
-      SUBROUTINE HWAINI
-      RETURN
-      END
       SUBROUTINE HWABEG
       RETURN
       END
       SUBROUTINE HWAEND
       RETURN
       END
-      SUBROUTINE HWHVVJ
-      RETURN
-      END
-C*********************************************************************
- 
-C...UPEVNT
-C...Dummy routine, to be replaced by a user implementing external
-C...processes. Depending on cross section model chosen, it either has
-C...to generate a process of the type IDPRUP requested, or pick a type
-C...itself and generate this event. The event is to be stored in the
-C...HEPEUP commonblock, including (often) an event weight.
- 
-      SUBROUTINE UPEVNT
- 
-      RETURN
-      END
- 
-C*********************************************************************
- 
-C...PDFSET
-C...Dummy routine, to be removed when PDFLIB is to be linked.
- 
-      SUBROUTINE PDFSET(PARM,VALUE)
- 
-      RETURN
-      END
- 
-C*********************************************************************
- 
-C...STRUCTM
-C...Dummy routine, to be removed when PDFLIB is to be linked.
- 
-      SUBROUTINE STRUCTM(XX,QQ,UPV,DNV,USEA,DSEA,STR,CHM,BOT,TOP,GLU)
- 
-      RETURN
-      END
- 
-C*********************************************************************
- 
-C...UPINIT
-C...Dummy routine, to be replaced by a user implementing external
-C...processes. Is supposed to fill the HEPRUP commonblock with info
-C...on incoming beams and allowed processes.
- 
-      SUBROUTINE UPINIT
- 
-      RETURN
-      END
- 
-C*********************************************************************
