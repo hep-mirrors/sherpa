a8e1a42f5fafb154499f4be4376a8cf5fe68251e
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-08-05 07:21:00 +0000

    improvemnents

---
+++ ATOOLS-2.0/Org/Node.C
@@ -0,0 +1,34 @@
+#ifndef Node_C
+#define Node_C
+
+namespace ATOOLS {
+
+  template <class Node_Type>
+  Node<Node_Type>::Node(const Node_Type &node,const bool create): 
+    std::vector<Node_Type>(1,node), 
+    p_next(NULL) 
+  {
+    if (create) p_next = new std::vector<Node<Node_Type>*>();
+  }
+  
+  template <class Node_Type>
+  Node<Node_Type>::~Node() 
+  {
+    if (p_next!=NULL) {
+      for (typename std::vector<Node<Node_Type>*>::iterator nit=p_next->begin();
+	   nit!=p_next->end();++nit) {
+	delete (*nit);
+      }
+      delete p_next;
+    }
+  }
+  
+  template <class Node_Type>
+  std::vector<Node<Node_Type>*> *Node<Node_Type>::operator->() 
+  { 
+    return p_next; 
+  }
+  
+}// end of namespace ATOOLS
+
+#endif
