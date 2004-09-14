0f543665b7d31e3ace82f274645e981b8d9703a2
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-09-14 18:11:31 +0000

    removed couts

--- PDF-1.0/KMR/Doubly_Unintegrated_PDF.C
+++ PDF-1.0/KMR/Doubly_Unintegrated_PDF.C
@@ -150,7 +150,6 @@ double Doubly_Unintegrated_PDF::SmoothIntegrated(ATOOLS::Flavour flavour)
 
 bool Doubly_Unintegrated_PDF::Unintegrate(ATOOLS::Flavour flavour)
 {
-  std::cout<<"new "<<flavour<<" "<<m_kperp2<<std::endl;
   PROFILE_HERE;
   m_unintegrated=m_integrated=0.;
   if (m_kperp2<m_mu02) {
@@ -168,8 +167,6 @@ bool Doubly_Unintegrated_PDF::Unintegrate(ATOOLS::Flavour flavour)
       if (((flavour.IsGluon() && (*sfit)->GetFlA().IsGluon()) ||
 	   (flavour.IsQuark() && (*sfit)->GetFlA().IsQuark()))&& 
 	  m_z*(1.+sqrt(m_kperp2/m_mu2))>1.) continue;
-      std::cout<<" split "<<(*sfit)->GetFlA()<<"->"<<(*sfit)->GetFlB()<<(*sfit)->GetFlC()<<" "
-	       <<(*(*sfit))(m_z)<<" "<<p_pdf->GetXPDF((*sfit)->GetFlA())<<std::endl;
       m_unintegrated+=(*(*sfit))(m_z)*p_pdf->GetXPDF((*sfit)->GetFlA());
       if (flavour.IsGluon() && (*sfit)->GetFlA().IsGluon())
 	m_unintegrated+=(*(*sfit))(m_z)*p_pdf->GetXPDF((*sfit)->GetFlA());
@@ -177,8 +174,6 @@ bool Doubly_Unintegrated_PDF::Unintegrate(ATOOLS::Flavour flavour)
   }
   m_unintegrated*=(*p_alphas)(m_kperp2)/(2.0*M_PI);
   m_unintegrated*=p_sudakov->Delta(flavour)(sqrt(m_mu2),sqrt(m_kperp2));
-  std::cout<<flavour<<" sud / pdf : "<<p_sudakov->Delta(flavour)(sqrt(m_mu2),sqrt(m_kperp2))
-	   <<" / "<<m_unintegrated<<std::endl;
   m_unintegrated/=m_kperp2;
   return true;
 }
