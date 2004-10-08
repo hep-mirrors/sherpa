b18d20136ded213d36039d6c5cfb076bd9d95202
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-10-08 11:16:52 +0000

    added again

---
+++ SHERPA-1.0/SoftPhysics/QCD_Remnant_Base.C
@@ -0,0 +1,143 @@
+#include "QCD_Remnant_Base.H"
+
+#include "Exception.H"
+#include "Random.H"
+#include "Run_Parameter.H"
+#include <iomanip>
+
+#ifdef PROFILE__all
+#define PROFILE__QCD_Remnant_Base
+#endif
+#ifdef PROFILE__QCD_Remnant_Base
+#include "prof.hh" 
+#else
+#define PROFILE_HERE
+#endif
+
+using namespace SHERPA;
+
+QCD_Remnant_Base::QCD_Remnant_Base(PDF::ISR_Handler *isrhandler,
+				   const unsigned int beam,
+				   const double scale,const rtp::code type):
+  Remnant_Base(type,beam), p_start(NULL), m_deltax(0.0125), m_scale(scale),
+  m_ecms(sqrt(isrhandler->Pole())), m_xscheme(1), m_maxtrials(100)
+{
+  if (isrhandler==NULL) {
+    throw(ATOOLS::Exception(ATOOLS::ex::fatal_error,
+			    "QCD remnant needs ISR Handler.",
+			    "QCD_Remnant_Base","QCD_Remnant_Base"));
+  }
+  p_pdfbase=isrhandler->PDF(m_beam)->GetBasicPDF();
+  m_dupdf=isrhandler->KMROn()>0;
+}
+
+void QCD_Remnant_Base::Clear()
+{
+  while (m_connected.size()>0) {
+    delete m_connected.front();
+    m_connected.erase(m_connected.begin());
+  }
+  if (p_start!=NULL) delete p_start;
+  Remnant_Base::Clear();
+}
+
+void QCD_Remnant_Base::AssignRemnants() 
+{
+  PROFILE_HERE;
+  ATOOLS::Particle *startreal=p_start->Begin(qri::real);
+  ATOOLS::Particle *startanti=p_start->Begin(qri::anti);
+  for (ATOOLS::Particle_List::iterator pit=m_extracted.begin();
+       pit!=m_extracted.end();++pit) {
+    if (*pit==startreal || *pit==startanti) continue;
+    Color_Dipole *dipole = new Color_Dipole(*pit,&m_companions);
+    if (p_start->Cat(dipole)) delete dipole;
+    else {
+      Dipole_Vector::iterator dit=m_connected.begin();
+      for (;dit!=m_connected.end();++dit) 
+	if ((*dit)->Cat(dipole)) {
+	  delete dipole;
+	  break;
+	}
+      if (dit==m_connected.end()) m_connected.push_back(dipole);
+    }
+  }
+}
+
+Color_Dipole *QCD_Remnant_Base::FindClosest(const Color_Dipole *dipole,
+					    const qri::type type)
+{
+  Color_Dipole *closest=NULL;
+  const ATOOLS::Vec4D &ref=dipole->End(type)->Momentum();
+  double min=std::numeric_limits<double>::max();
+  for (Dipole_Vector::iterator dit=m_attached.begin();
+       dit!=m_attached.end();++dit) {
+    if (*dit==dipole) continue;
+    double cur=(*dit)->End(ANTI(type))->Momentum().PPerp(ref);
+    if (cur<=min) {
+      min=cur;
+      closest=*dit;
+    }
+  }
+  return closest;
+}
+
+class Compare_PT {
+public:
+  
+  bool operator()(const Color_Dipole *i1,const Color_Dipole *i2) 
+  {
+    double pp21=ATOOLS::Max(i1->End(qri::real)->Momentum().PPerp2(),
+			    i1->End(qri::anti)->Momentum().PPerp2());
+    double pp22=ATOOLS::Max(i2->End(qri::real)->Momentum().PPerp2(),
+			    i2->End(qri::anti)->Momentum().PPerp2());
+    return (pp21<pp22);
+  }
+
+};
+
+bool QCD_Remnant_Base::Connect(const bool sorted) 
+{
+  PROFILE_HERE;
+  m_attached.clear();
+  m_attached.push_back(p_start);
+  std::stable_sort(m_connected.begin(),m_connected.end(),Compare_PT());
+  for (Dipole_Vector::iterator dit=m_connected.begin();
+       dit!=m_connected.end();++dit) {
+    qri::type type=(qri::type)((*dit)->End(qri::real)->Momentum().PPerp2()>=
+			       (*dit)->End(qri::anti)->Momentum().PPerp2());
+    if (!FindClosest(*dit,type)->Insert(*dit,type)) {
+      for (Dipole_Vector::iterator uit=m_connected.begin();
+	   uit!=dit;++uit) (*uit)->UnDo();
+      return false;
+    }
+    m_attached.push_back(*dit);
+  }
+  return true;
+}
+
+bool QCD_Remnant_Base::ConnectRemnants() 
+{
+  if (Connect(true)) return true;
+  ATOOLS::msg.Error()<<"QCD_Remnant_Base::ConnectRemnants(): "
+		     <<"No solution in event ["
+		     <<ATOOLS::rpa.gen.NumberOfDicedEvents()<<"]. Abort."
+		     <<std::endl;
+  return false;
+}
+
+void QCD_Remnant_Base::FillRemnants()
+{
+  for (Dipole_Vector::iterator rit=m_connected.begin();
+       rit!=m_connected.end();++rit) {
+    (*rit)->SetColors();
+  }
+  p_start->SetColors();
+  for (ATOOLS::Particle_List::iterator pit=m_extracted.begin();
+       pit!=m_extracted.end();++pit) {
+    p_beamblob->AddToOutParticles(*pit);
+  }
+  for (ATOOLS::Particle_List::iterator pit=m_companions.begin();
+       pit!=m_companions.end();++pit) {
+    p_beamblob->AddToOutParticles(*pit);
+  }
+}
