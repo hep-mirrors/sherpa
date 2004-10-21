b6e27e0a5a073640cd28be137e233057394cb3db
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-10-21 12:06:57 +0000

    new class: getter functions

--- ANALYSIS-1.0/Observables/Scaled_Observables.C
+++ ANALYSIS-1.0/Observables/Scaled_Observables.C
@@ -1,7 +1,53 @@
 #include "Scaled_Observables.H"
-#include "MyStrStream.H"
 
 using namespace ANALYSIS;
+
+#include "MyStrStream.H"
+
+template <class Class>
+Primitive_Observable_Base *const GetObservable(const String_Matrix &parameters)
+{									
+  if (parameters.size()<1) return NULL;
+  if (parameters.size()==1) {
+    if (parameters[0].size()<4) return NULL;
+    std::string list=parameters[0].size()>5?parameters[0][5]:"Analysed";
+    double ref=parameters.size()>4?ATOOLS::rpa.gen.Ecms():
+      ATOOLS::ToType<double>(parameters[0][4]);
+    return new Class(10*(int)(parameters[0][3]=="Log"),
+		     ATOOLS::ToType<double>(parameters[0][0]),
+		     ATOOLS::ToType<double>(parameters[0][1]),
+		     ATOOLS::ToType<int>(parameters[0][2]),list,ref);
+  }
+  else if (parameters.size()<4) return NULL;
+  double min=0.0, max=1.0;
+  size_t bins=100;
+  std::string list="Analysed", scale="Lin";
+  for (size_t i=0;i<parameters.size();++i) {
+    if (parameters[i].size()<2) continue;
+    if (parameters[i][0]=="MIN") min=ATOOLS::ToType<double>(parameters[i][1]);
+    else if (parameters[i][0]=="MAX") max=ATOOLS::ToType<double>(parameters[i][1]);
+    else if (parameters[i][0]=="REF") ref=ATOOLS::ToType<double>(parameters[i][1]);
+    else if (parameters[i][0]=="BINS") bins=ATOOLS::ToType<int>(parameters[i][1]);
+    else if (parameters[i][0]=="SCALE") scale=parameters[i][1];
+    else if (parameters[i][0]=="LIST") list=parameters[i][1];
+  }
+  return new Class((scale=="Log")*10,min,max,bins,list,ref);
+}									
+
+#define DEFINE_GETTER_METHOD(CLASS,NAME)				\
+  Primitive_Observable_Base *const					\
+  NAME::operator()(const String_Matrix &parameters) const		\
+  { return GetObservable<CLASS>(parameters); }
+
+#define DEFINE_PRINT_METHOD(NAME)					\
+  void NAME::PrintInfo(std::ostream &str,const size_t width) const	\
+  { str<<"min max bins Lin|Log ref [list]"; }
+
+#define DEFINE_OBSERVABLE_GETTER(CLASS,NAME,TAG)			\
+  DECLARE_GETTER(NAME,TAG,Primitive_Observable_Base,String_Matrix);	\
+  DEFINE_GETTER_METHOD(CLASS,NAME);					\
+  DEFINE_PRINT_METHOD(NAME)
+
 using namespace ATOOLS;
 using namespace std;
 
