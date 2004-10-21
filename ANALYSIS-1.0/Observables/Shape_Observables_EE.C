d3b2d1f2758ec72d32dfae496bd011c1e8eff272
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-10-21 09:56:47 +0000

    new class: getter classes

--- ANALYSIS-1.0/Observables/Shape_Observables_EE.C
+++ ANALYSIS-1.0/Observables/Shape_Observables_EE.C
@@ -1,7 +1,52 @@
 #include "Shape_Observables_EE.H"
-#include "Primitive_Analysis.H"
 
 using namespace ANALYSIS;
+
+#include "MyStrStream.H"
+
+template <class Class>
+Primitive_Observable_Base *const GetObservable(const String_Matrix &parameters)
+{									
+  if (parameters.size()<1) return NULL;
+  if (parameters.size()==1) {
+    if (parameters[0].size()<4) return NULL;
+    std::string list=parameters[0].size()>4?parameters[0][4]:"Analysed";
+    return new Class(10*(int)(parameters[0][3]=="Log"),
+		     ATOOLS::ToType<double>(parameters[0][0]),
+		     ATOOLS::ToType<double>(parameters[0][1]),
+		     ATOOLS::ToType<int>(parameters[0][2]),list);
+  }
+  else if (parameters.size()<4) return NULL;
+  double min=0.0, max=1.0;
+  size_t bins=100, scale=0;
+  std::string list="Analysed";
+  for (size_t i=0;i<parameters.size();++i) {
+    if (parameters[i].size()<2) continue;
+    if (parameters[i][0]=="MIN") min=ATOOLS::ToType<double>(parameters[i][1]);
+    else if (parameters[i][0]=="MAX") max=ATOOLS::ToType<double>(parameters[i][1]);
+    else if (parameters[i][0]=="BINS") bins=ATOOLS::ToType<int>(parameters[i][1]);
+    else if (parameters[i][0]=="SCALE") scale=ATOOLS::ToType<int>(parameters[i][1]);
+    else if (parameters[i][0]=="LIST") list=parameters[i][1];
+  }
+  return new Class(scale,min,max,bins,list);
+}									
+
+#define DEFINE_GETTER_METHOD(CLASS,NAME)				\
+  Primitive_Observable_Base *const					\
+  NAME::operator()(const String_Matrix &parameters) const		\
+  { return GetObservable<CLASS>(parameters); }
+
+#define DEFINE_PRINT_METHOD(NAME)					\
+  void NAME::PrintInfo(std::ostream &str,const size_t width) const	\
+  { str<<"min max bins Lin|Log [list]"; }
+
+#define DEFINE_OBSERVABLE_GETTER(CLASS,NAME,TAG)			\
+  DECLARE_GETTER(NAME,TAG,Primitive_Observable_Base,String_Matrix);	\
+  DEFINE_GETTER_METHOD(CLASS,NAME);					\
+  DEFINE_PRINT_METHOD(NAME)
+
+#include "Primitive_Analysis.H"
+
 using namespace ATOOLS;
 using namespace std;
 
@@ -19,6 +64,8 @@ Event_Shapes_Observable_Base::Event_Shapes_Observable_Base(int _type,double _min
 //================================================================================
 //================================================================================
 //================================================================================
+
+DEFINE_OBSERVABLE_GETTER(Thrust,Thrust_Getter,"Thrust");
 
 Thrust::Thrust(int _type,double _min,double _max,int _nbins,const string & _name) :
   Event_Shapes_Observable_Base(_type,_min,_max,_nbins,_name) {}
@@ -41,6 +88,8 @@ Primitive_Observable_Base * Thrust::Copy() const
 //================================================================================
 //================================================================================
 
+DEFINE_OBSERVABLE_GETTER(Major,Major_Getter,"Major");
+
 Major::Major(int _type,double _min,double _max,int _nbins,const string & _name) :
   Event_Shapes_Observable_Base(_type,_min,_max,_nbins,_name) {}
 
@@ -61,6 +110,8 @@ Primitive_Observable_Base * Major::Copy() const
 //================================================================================
 //================================================================================
 //================================================================================
+
+DEFINE_OBSERVABLE_GETTER(Minor,Minor_Getter,"Minor");
 
 Minor::Minor(int _type,double _min,double _max,int _nbins,const string & _name) :
   Event_Shapes_Observable_Base(_type,_min,_max,_nbins,_name) {}
@@ -83,6 +134,8 @@ Primitive_Observable_Base * Minor::Copy() const
 //================================================================================
 //================================================================================
 
+DEFINE_OBSERVABLE_GETTER(Oblateness,Oblateness_Getter,"Oblat");
+
 Oblateness::Oblateness(int _type,double _min,double _max,int _nbins,const string & _name) :
   Event_Shapes_Observable_Base(_type,_min,_max,_nbins,_name) {}
 
@@ -103,6 +156,8 @@ Primitive_Observable_Base * Oblateness::Copy() const
 //================================================================================
 //================================================================================
 //================================================================================
+
+DEFINE_OBSERVABLE_GETTER(PT_In_Thrust,PT_In_Thrust_Getter,"PTIn");
 
 PT_In_Thrust::PT_In_Thrust(int _type,double _min,double _max,int _nbins,
 			   const std::string & _inlistname,const string & _name) :
@@ -130,6 +185,8 @@ Primitive_Observable_Base * PT_In_Thrust::Copy() const
 //================================================================================
 //================================================================================
 //================================================================================
+
+DEFINE_OBSERVABLE_GETTER(PT_Out_Thrust,PT_Out_Thrust_Getter,"PTOut");
 
 PT_Out_Thrust::PT_Out_Thrust(int _type,double _min,double _max,int _nbins,
 			     const std::string & _inlistname,const string & _name) :
