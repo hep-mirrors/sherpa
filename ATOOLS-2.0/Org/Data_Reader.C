29ee66754195f2db05ed2dda7e500ce8591c949d
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-10-25 08:34:35 +0000

    bugfix: removed changes

--- ATOOLS-2.0/Org/Data_Reader.C
+++ ATOOLS-2.0/Org/Data_Reader.C
@@ -229,13 +229,8 @@ Data_Reader::M_VectorFromString(std::string parameter, std::string inputstring,V
     std::cout<<std::endl;
 #endif
     bool stop=false;
-    for (unsigned int j=0;j<Separator().size();++j) {
-      size_t pos=value.find(Separator()[j]);
-      if (pos!=std::string::npos) {
-	value=value.substr(0,pos);
-	stop=true;
-      }
-    }
+    for (unsigned int j=0;j<Separator().size();++j) if (value==Separator()[j]) stop=true;
+    if (stop) break;
     values.push_back(M_ReadFromString<Read_Type>(nullstring,value));
     if (tempvtype==VVertical) break;
     inputstring=inputstring.substr(inputstring.find(value)+value.length());
@@ -248,7 +243,6 @@ Data_Reader::M_VectorFromString(std::string parameter, std::string inputstring,V
       }
     }
     value=M_ReadFromString<std::string>(nullstring,inputstring);
-    if (stop) break;
   }
 #ifdef DEBUG__Data_Reader
   std::cout<<"   returned values are [ ";
