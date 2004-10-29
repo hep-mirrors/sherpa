a478839593b0f65c4157953375172915f2ccd310
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-10-29 10:06:14 +0000

    cha+#nges:

--- SHERPA-1.0/Single_Events/Multiple_Interactions.C
+++ SHERPA-1.0/Single_Events/Multiple_Interactions.C
@@ -37,15 +37,15 @@ Multiple_Interactions::~Multiple_Interactions()
 bool Multiple_Interactions::CheckBlobList(ATOOLS::Blob_List *const bloblist) 
 {
   p_bloblist=bloblist;
-  bool diced=false;
-  double pperpmax=m_pperpmax=std::numeric_limits<double>::max();
+  m_diced=false;
+  double ptmax=m_ptmax=std::numeric_limits<double>::max();
   for (size_t i=0;i<2;++i) {
     m_xmax[i]=1.0;
     p_remnants[i]->QuickClear();
   }
   for (ATOOLS::Blob_List::const_iterator bit=bloblist->begin();
        bit!=bloblist->end();++bit) {
-    if ((*bit)->Type()==ATOOLS::btp::Hard_Collision) diced=true;
+    if ((*bit)->Type()==ATOOLS::btp::Hard_Collision) m_diced=true;
     if ((*bit)->Type()==ATOOLS::btp::Hard_Collision ||
 	(*bit)->Type()==ATOOLS::btp::Signal_Process) 
       if ((*bit)->Status()!=0) return false;
@@ -54,9 +54,9 @@ bool Multiple_Interactions::CheckBlobList(ATOOLS::Blob_List *const bloblist)
        bit!=bloblist->end();++bit) {
     if ((*bit)->Type()==ATOOLS::btp::ME_PS_Interface_FS) {
       for (int i=0;i<(*bit)->NInP();++i) 
-	pperpmax=ATOOLS::Min(pperpmax,(*bit)->InParticle(i)->Momentum().PPerp());
-      if (!diced && VetoHardProcess(*bit)) {
-	m_pperpmax=0.0;
+	ptmax=ATOOLS::Min(ptmax,(*bit)->InParticle(i)->Momentum().PPerp());
+      if (!m_diced && VetoHardProcess(*bit)) {
+	m_ptmax=0.0;
 	break;
       }
     }
@@ -68,13 +68,13 @@ bool Multiple_Interactions::CheckBlobList(ATOOLS::Blob_List *const bloblist)
       p_remnants[(*bit)->Beam()]->Extract((*bit)->InParticle(0)); 
     }
   }
-  if (pperpmax>=m_pperpmax) {
+  if (ptmax>=m_ptmax) {
     for (ATOOLS::Blob_List::const_iterator bit=bloblist->begin();
 	 bit!=bloblist->end();++bit) {
       if ((*bit)->Type()==ATOOLS::btp::Hard_Collision || 
 	  (*bit)->Type()==ATOOLS::btp::Signal_Process) {
 	for (int i=0;i<(*bit)->NOutP();++i) 
-	  pperpmax=ATOOLS::Min(pperpmax,(*bit)->OutParticle(i)->Momentum().PPerp());
+	  ptmax=ATOOLS::Min(ptmax,(*bit)->OutParticle(i)->Momentum().PPerp());
 	for (int i=0;i<(*bit)->NInP();++i) {
 	  p_mihandler->ISRHandler()->
 	    Extract((*bit)->InParticle(i)->Flav(),
@@ -84,7 +84,7 @@ bool Multiple_Interactions::CheckBlobList(ATOOLS::Blob_List *const bloblist)
       }
     }
   }
-  return (m_pperpmax=pperpmax)!=std::numeric_limits<double>::max();
+  return (m_ptmax=ptmax)!=std::numeric_limits<double>::max();
 }
 
 bool Multiple_Interactions::Treat(ATOOLS::Blob_List *bloblist,double &weight)
@@ -98,14 +98,14 @@ bool Multiple_Interactions::Treat(ATOOLS::Blob_List *bloblist,double &weight)
     return false;
   }
   if (!CheckBlobList(bloblist)) return false;
-  p_mihandler->SetScaleMax(m_pperpmax,0);
+  p_mihandler->SetScaleMax(ATOOLS::Min(m_ptmax,m_lastptmax),0);
   p_mihandler->SetScaleMin(p_mihandler->ScaleMin(0),0);
   p_mihandler->SetScaleMax(m_xmax[0],2);
   p_mihandler->SetScaleMax(m_xmax[1],3);
   ATOOLS::Blob *blob = new ATOOLS::Blob();
   p_mihandler->Reset();
   bool success=false;
-  if (!m_vetoed && m_pperpmax>p_mihandler->ScaleMin(0)) {
+  if (!m_vetoed && m_ptmax>p_mihandler->ScaleMin(0)) {
     success=p_mihandler->GenerateHardProcess(blob);
   }
   else if (m_vetoed) {
@@ -117,14 +117,15 @@ bool Multiple_Interactions::Treat(ATOOLS::Blob_List *bloblist,double &weight)
     blob->AddData("ME_Weight",new ATOOLS::Blob_Data<double>(1.0));
     blob->AddData("ME_NumberOfTrials",new ATOOLS::Blob_Data<int>(1));
   }
-//   std::cout<<"result "<<m_vetoed<<" "<<success<<" "<<m_pperpmax<<"\n";
   if (success) {
     blob->SetId(bloblist->size());
     for (size_t i=0;i<(size_t)blob->NInP();++i) {
       p_mihandler->ISRHandler()->Reset(i);
       if (!p_remnants[i]->Extract(blob->InParticle(i))) {
+	m_lastptmax=blob->OutParticle(0)->Momentum().PPerp();
+	std::cout<<"MI: vetoed "<<m_diced<<" "<<m_lastptmax<<"\n";
 	delete blob;
-	return false;
+	return true;
       }
     }
     bloblist->push_back(blob);
@@ -162,7 +163,6 @@ bool Multiple_Interactions::VetoHardProcess(ATOOLS::Blob *const blob)
   if (p_mihandler->VetoHardProcess(blob)) {
     m_deleted.clear();
     DeleteConnected(blob);
-//     std::cout<<"veto "<<*p_bloblist<<"\n";
     return m_vetoed=true;
   }
   return false;
