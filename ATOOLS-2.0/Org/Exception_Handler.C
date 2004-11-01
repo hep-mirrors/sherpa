87d614e83c66066b41f707539d980922f7ac993a
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-11-01 11:34:03 +0000

    bugfix:

--- ATOOLS-2.0/Org/Exception_Handler.C
+++ ATOOLS-2.0/Org/Exception_Handler.C
@@ -48,33 +48,30 @@ Exception_Handler::s_terminatorobjects=std::vector<Terminator_Object*>();
 
 bool Exception_Handler::ApproveTerminate()
 {
-  if (s_print) msg_Tracking()<<"Exception_Handler::ApproveTerminate(): Asking for termination ..."<<std::endl;
+  if (s_print) msg_Tracking()<<"Exception_Handler::ApproveTerminate(): "
+			     <<"Asking for termination ..."<<std::endl;
   if (s_testerfunctions.size()==0 && s_testerobjects.size()==0) {
     if (s_print) msg_Tracking()<<"... approved."<<std::endl;
     return true;
   }
-  if (s_testerfunctions.size()>0) {
-    for (size_t i=0;i<s_testerobjects.size();++i) if (s_testerfunctions[i]()) {
-      if (s_print) msg_Tracking()<<"... approved."<<std::endl;
-      return true;
-    }
-  }
-  if (s_testerobjects.size()>0) {
-    for (size_t i=0;i<s_testerobjects.size();++i) if (s_testerobjects[i]->ApproveTerminate()) {
-      if (s_print) msg_Tracking()<<"... approved."<<std::endl;
-      return true;
-    }
-  }
-  if (s_print) msg_Tracking()<<"... refused."<<std::endl;
-  return false;
+  bool approved=true;
+  for (size_t i=0;i<s_testerfunctions.size();++i) 
+    if (!s_testerfunctions[i]()) approved=false;
+  for (size_t i=0;i<s_testerobjects.size();++i) 
+    if (!s_testerobjects[i]->ApproveTerminate()) approved=false;
+  if (approved && s_print) msg_Tracking()<<"... approved."<<std::endl;
+  else if (s_print) msg_Tracking()<<"... refused."<<std::endl;
+  return approved;
 }
 
 void Exception_Handler::PrepareTerminate()
 {
   static size_t trials=0;
   if (++trials>3) kill(getpid(),9);
-  if (s_print) msg_Tracking()<<"Exception_Handler::PrepareTerminate(): Preparing termination ..."<<std::endl;
-  for (size_t i=0;i<s_terminatorobjects.size();++i) s_terminatorobjects[i]->PrepareTerminate(); 
+  if (s_print) msg_Tracking()<<"Exception_Handler::PrepareTerminate(): "
+			     <<"Preparing termination ..."<<std::endl;
+  for (size_t i=0;i<s_terminatorobjects.size();++i) 
+    s_terminatorobjects[i]->PrepareTerminate(); 
   for (size_t i=0;i<s_terminatorfunctions.size();++i) s_terminatorfunctions[i](); 
   if (s_print) msg_Tracking()<<"... prepared."<<std::endl;
 }
@@ -82,10 +79,11 @@ void Exception_Handler::PrepareTerminate()
 void Exception_Handler::Exit(int exitcode)
 {
   if (s_print) msg.Error()<<om::bold<<"Exception_Handler::Exit: "<<om::reset<<om::blue
-			  <<"Exiting Sherpa with code "<<om::reset<<om::bold<<"("<<om::red<<exitcode
-			  <<om::reset<<om::bold<<")"<<om::reset<<std::endl;
-  msg.LogFile()<<"Exception_Handler::Exit: Exiting Sherpa with code ("
-			    <<exitcode<<")"<<std::endl;
+			  <<"Exiting Sherpa with code "<<om::reset<<om::bold<<"("
+			  <<om::red<<exitcode<<om::reset<<om::bold<<")"
+			  <<om::reset<<std::endl;
+  msg.LogFile()<<"Exception_Handler::Exit: "
+	       <<"Exiting Sherpa with code ("<<exitcode<<")"<<std::endl;
   exit(exitcode);
 }
 
@@ -107,18 +105,22 @@ void Exception_Handler::Terminate()
 void Exception_Handler::RemoveTesterObject(Tester_Object *const testerobject)
 {
   for (std::vector<Tester_Object*>::iterator toit=s_testerobjects.begin();
-       toit!=s_testerobjects.end();) {
-    if (*toit==testerobject) toit=s_testerobjects.erase(toit); 
-    else ++toit;
+       toit!=s_testerobjects.end();++toit) {
+    if (*toit==testerobject) {
+      toit=s_testerobjects.erase(toit); 
+      break;
+    }
   }
 }
 
 void Exception_Handler::RemoveTerminatorObject(Terminator_Object *const terminatorobject)
 {
   for (std::vector<Terminator_Object*>::iterator toit=s_terminatorobjects.begin();
-       toit!=s_terminatorobjects.end();) {
-    if (*toit==terminatorobject) toit=s_terminatorobjects.erase(toit); 
-    else ++toit;
+       toit!=s_terminatorobjects.end();++toit) {
+    if (*toit==terminatorobject) {
+      toit=s_terminatorobjects.erase(toit); 
+      break;
+    }
   }
 }
 
@@ -139,9 +141,10 @@ void Exception_Handler::SignalHandler(int signal)
   s_signal=signal;
   s_print=true;
   std::string input="y";
-  msg.Error()<<std::endl<<om::bold<<"Exception_Handler::SignalHandler: "<<om::reset<<om::blue
-	     <<"Signal "<<om::reset<<om::bold<<"("<<om::red<<signal
-	     <<om::reset<<om::bold<<")"<<om::reset<<om::blue<<" caught. "<<om::reset<<std::endl;
+  msg.Error()<<std::endl<<om::bold<<"Exception_Handler::SignalHandler: "
+	     <<om::reset<<om::blue<<"Signal "<<om::reset<<om::bold
+	     <<"("<<om::red<<signal<<om::reset<<om::bold<<")"
+	     <<om::reset<<om::blue<<" caught. "<<om::reset<<std::endl;
   switch (signal) {
   case SIGSEGV:
     ++s_nsegv;
@@ -204,7 +207,8 @@ void Exception_Handler::SignalHandler(int signal)
     Terminate();
     break;
   case SIGFPE:
-    msg.Error()<<"   Sherpa does not throw floating point exceptions."<<om::reset<<std::endl;
+    msg.Error()<<"   Sherpa does not throw floating point exceptions."
+	       <<om::reset<<std::endl;
     break;
   default:
     msg.Error()<<"   Cannot handle signal."<<om::reset<<std::endl;
