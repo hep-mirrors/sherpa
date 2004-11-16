03f0cf9aa6e6d77b24f449d2cfda66d9bef5f9bd
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-11-16 17:03:00 +0000

    changes: output for bad events

--- SHERPA-1.0/SoftPhysics/Beam_Remnant_Handler.C
+++ SHERPA-1.0/SoftPhysics/Beam_Remnant_Handler.C
@@ -218,6 +218,7 @@ FillBeamBlobs(ATOOLS::Blob_List *const bloblist,
   }
   for (short unsigned int i=0;i<2;++i) 
     if (p_beamblob[i]) if (!p_beampart[i]->FillBlob(p_beamblob[i],particlelist)) {
+      ATOOLS::msg.Error()<<*bloblist<<std::endl;
       if (i==0) p_beampart[1]->FillBlob(p_beamblob[i],particlelist);
       while (bloblist->size()>0) {
 	delete *bloblist->begin();
