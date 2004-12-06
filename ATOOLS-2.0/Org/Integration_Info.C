1360f77a117b01333235494b94818f094d85cb8c
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-12-06 07:39:44 +0000

    changes: security against static key release

--- ATOOLS-2.0/Org/Integration_Info.C
+++ ATOOLS-2.0/Org/Integration_Info.C
@@ -8,6 +8,21 @@ Integration_Info::Integration_Info()
 {
 }
 
+Integration_Info::~Integration_Info() 
+{
+  for (String_MapPair_Map::const_iterator kmit=m_keymap.begin();
+       kmit!=m_keymap.end();++kmit) {
+    const String_KeyPair_Map &keymap=kmit->second.second;
+    for (String_KeyPair_Map::const_iterator kit=keymap.begin();
+	 kit!=keymap.end();++kit) {
+      const std::vector<Info_Key*> keys=kit->second.second;
+      for (std::vector<Info_Key*>::const_iterator it=keys.begin();
+	   it!=keys.end();++it) 
+	(*it)->p_info=NULL;
+    }
+  }
+}
+
 void Integration_Info::ResetAll()
 {
   for (size_t i=0;i<m_doubles.size();++i) {
@@ -20,23 +35,23 @@ void Integration_Info::ResetAll()
   }
 }
 
-void Integration_Info::AssignKey(Info_Key &key,const size_t doubles,const size_t vectors)
+void Integration_Info::AssignKey(Info_Key &key,const size_t doubles,
+				 const size_t vectors)
 {
   if (m_keymap.find(key.m_name)==m_keymap.end()) {
-    m_keymap[key.m_name]=std::pair<size_t,std::map<std::string,
-      std::pair<size_t,std::vector<Info_Key*> > > >
-      (m_doubles.size(),std::map<std::string,std::pair<size_t,std::vector<Info_Key*> > >());
+    m_keymap[key.m_name]=SizeT_KeyMap_Pair(m_doubles.size(),
+					   String_KeyPair_Map());
     m_doubles.push_back(Double_Container(doubles));
     m_vectors.push_back(Vector_Container(vectors));
     m_weights.push_back(Double_Container());
     m_status.push_back(si::idle);
   }
   key.m_valuekey=m_keymap[key.m_name].first;
-  std::map<std::string,std::pair<size_t,std::vector<Info_Key*> > > &keys=m_keymap[key.m_name].second;
-  std::map<std::string,std::pair<size_t,std::vector<Info_Key*> > >::iterator kit=keys.find(key.m_info);
+  String_KeyPair_Map &keys=m_keymap[key.m_name].second;
+  String_KeyPair_Map::iterator kit=keys.find(key.m_info);
   if (kit==keys.end()) {
-    keys[key.m_info]=std::pair<size_t,std::vector<Info_Key*> >(m_weights[key.m_valuekey].size(),
-							       std::vector<Info_Key*>());
+    keys[key.m_info]=SizeT_KeyVector_Pair(m_weights[key.m_valuekey].size(),
+					  Key_Vector());
     m_weights[key.m_valuekey].push_back(0.);
     kit=keys.find(key.m_info);
   }
@@ -46,24 +61,24 @@ void Integration_Info::AssignKey(Info_Key &key,const size_t doubles,const size_t
 
 void Integration_Info::ReleaseKey(Info_Key &key)
 {
-  std::map<std::string,std::pair<size_t,
-    std::map<std::string,std::pair<size_t,std::vector<Info_Key*> > > > >::iterator 
-    vit=m_keymap.find(key.m_name);
+  String_MapPair_Map::iterator vit=m_keymap.find(key.m_name);
   if (vit==m_keymap.end()) return;
-  std::map<std::string,std::pair<size_t,std::vector<Info_Key*> > > &keys=vit->second.second;
-  std::map<std::string,std::pair<size_t,std::vector<Info_Key*> > >::iterator 
+  String_KeyPair_Map &keys=vit->second.second;
+  String_KeyPair_Map::iterator 
     wit=keys.find(key.m_info);
   if (wit==keys.end()) return;
-  for (std::vector<Info_Key*>::iterator kit=wit->second.second.begin();
+  for (Key_Vector::iterator kit=wit->second.second.begin();
        kit!=wit->second.second.end();++kit) {
     if (*kit==&key) { 
       wit->second.second.erase(kit);
+      key.p_info=NULL;
       return;
     }
   }
 }
 
-std::ostream &ATOOLS::operator<<(std::ostream &str,const Double_Container &doubles)
+std::ostream &ATOOLS::operator<<(std::ostream &str,
+				 const Double_Container &doubles)
 {
   if (doubles.size()==0) return str<<"{<no entries>}";
 #ifdef __GNUC__
@@ -86,7 +101,8 @@ std::ostream &ATOOLS::operator<<(std::ostream &str,const Double_Container &doubl
   return str;
 }
  
-std::ostream &ATOOLS::operator<<(std::ostream &str,const Vector_Container &vectors)
+std::ostream &ATOOLS::operator<<(std::ostream &str,
+				 const Vector_Container &vectors)
 {
   if (vectors.size()==0) return str<<"{<no entries>}";
 #ifdef __GNUC__
@@ -108,12 +124,14 @@ std::ostream &ATOOLS::operator<<(std::ostream &str,const Vector_Container &vecto
   return str;
 }
 
-std::ostream &ATOOLS::operator<<(std::ostream &str,const Integration_Info &info)
+std::ostream &ATOOLS::operator<<(std::ostream &str,
+				 const Integration_Info &info)
 {
   str<<"Integration_Info("<<&info<<") {\n";
   for (size_t i=0;i<info.m_doubles.size();++i) {
-    str<<"  (*this)["<<i<<"] = "<<info.m_doubles[i]<<" "<<info.m_vectors[i]<<" => "
-       <<info.m_weights[i]<<" => ("<<info.m_status[i]<<")\n";
+    str<<"  (*this)["<<i<<"] = "<<info.m_doubles[i]<<" "
+       <<info.m_vectors[i]<<" => "<<info.m_weights[i]
+       <<" => ("<<info.m_status[i]<<")\n";
   }
   str<<"}";
   return str;
