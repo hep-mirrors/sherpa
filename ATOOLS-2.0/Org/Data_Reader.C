f37790568ce90db4ac9c59a6ac9db731bcfcfa4b
Stefan Hoeche <shoeche@slac.stanford.edu>
2004-12-31 11:08:49 +0000

    changes: output handling

--- ATOOLS-2.0/Org/Data_Reader.C
+++ ATOOLS-2.0/Org/Data_Reader.C
@@ -208,13 +208,14 @@ Read_Type Data_Reader::M_ReadFromFile(std::string parameter,std::string filename
       filename=InputFile();
     }
     else {
-      msg_Tracking()<<"Data_Reader: No input file specified ! No default available !"<<std::endl
-			    <<"   Abort reading."<<std::endl;
+      msg_Tracking()<<"Data_Reader: No input file specified. "
+		    <<"No default available."<<std::endl
+		    <<"   Abort reading."<<std::endl;
       return value;
     }
   }
   if(!OpenInFile()) {
-    msg.Out()<<"Data_Reader: Error opening "<<filename<<" !"<<std::endl;
+    msg.Tracking()<<"Data_Reader: File '"<<filename<<"' not found."<<std::endl;
     return value;
   }
   for (unsigned int i=0;i<FileContent().size();++i) {
@@ -223,7 +224,8 @@ Read_Type Data_Reader::M_ReadFromFile(std::string parameter,std::string filename
   }
   CloseInFile();
   if (value==Default<Read_Type>()) {
-    msg_Tracking()<<"Data_Reader: Parameter "<<parameter<<" not specified in "<<filename<<" !"<<std::endl;
+    msg_Debugging()<<"Data_Reader: Parameter '"<<parameter
+		   <<"' not specified in '"<<filename<<"' !"<<std::endl;
   }
   return value;
 }
@@ -249,8 +251,9 @@ Data_Reader::M_VectorFromString(std::string parameter, std::string inputstring,V
       inputstring=String();
     }
     else {
-      msg_Tracking()<<"Data_Reader: No input string specified ! No default available !"<<std::endl
-			    <<"   Abort reading."<<std::endl;
+      msg_Tracking()<<"Data_Reader: No input string specified. "
+		    <<"No default available !"<<std::endl
+		    <<"   Abort reading."<<std::endl;
       return values;
     }
   }
