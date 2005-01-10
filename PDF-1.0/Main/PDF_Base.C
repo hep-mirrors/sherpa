0758d93d3774742815aed7276b88b526dc4c2aab
dreas <sherpa@projects.hepforge.org>
2005-01-10 17:48:35 +0000

    enable new scale manipulation used in apacic

--- PDF-1.0/Main/PDF_Base.C
+++ PDF-1.0/Main/PDF_Base.C
@@ -1,13 +1,19 @@
 #include "PDF_Base.H"
 
 #include "Info_Key.H"
+#include "Run_Parameter.H"
 
 using namespace PDF;
 
 PDF_Base::PDF_Base():
   m_type("none"),
   m_exponent(1.),
-  m_rescale(1.) {}
+  m_rescale(1.),
+  m_renormalization_scale_factor(1.)
+{
+  m_renormalization_scale_factor = ATOOLS::rpa.gen.RenormalizationScaleFactor();
+  //  std::cout<<" mu_R-fac in PDF:"<<m_renormalization_scale_factor;
+}
 
 PDF_Base::~PDF_Base() {}
 
@@ -24,6 +30,12 @@ PDF_Base *PDF_Base::GetBasicPDF()
 double PDF_Base::Cut(const std::string &type)
 {
   return ATOOLS::UNDEFINED_LOWER;
+}
+
+void PDF_Base::Calculate(double x,double Q2)
+{
+  //  std::cout<<"PDF_Base::Calculate("<<Q2<<") Called"<<std::endl;
+  Calculate(x,0.,0.,Q2*m_renormalization_scale_factor);
 }
 
 void PDF_Base::Extract(const ATOOLS::Flavour flavour,const double x) 
