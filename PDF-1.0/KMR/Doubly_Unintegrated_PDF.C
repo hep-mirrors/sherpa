94914e87a9af27840f41b4963f923944eabac9ab
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-01-18 11:44:39 +0000

    new option: original kmr prescription

--- PDF-1.0/KMR/Doubly_Unintegrated_PDF.C
+++ PDF-1.0/KMR/Doubly_Unintegrated_PDF.C
@@ -1,7 +1,11 @@
 #include "Doubly_Unintegrated_PDF.H"
 
+#include "Primitive_Integrator.H"
 #include "NLL_Sudakov_Base.H"
 #include "MyStrStream.H"
+#include "MathTools.H"
+#include "Random.H"
+#include "Exception.H"
 
 #ifdef PROFILE__All
 #include "prof.hh"
@@ -13,20 +17,99 @@
 #endif
 #endif
 
-#include "Exception.H"
-#ifdef TEST__Sudakov
-#ifdef ROOT_SUPPORT
-#include "Scaling.H"
+#if defined TEST__Sudakov || defined TEST__Weights || defined TEST__DPDF || defined TEST__IDPDF
+#ifndef ROOT_SUPPORT 
+#error ROOT is disabled
+#endif
 #include "My_Root.H"
 #include "TH1D.h"
 #include "TH2D.h"
-static bool initialized=false;
-#else
-#error ROOT is disabled
-#endif
+#include "TH3D.h"
 #endif
 
 using namespace PDF;
+
+static size_t m_gridpoints=25;
+
+class DGLAP_PDF: public ATOOLS::Primitive_Integrand {
+private:
+
+  PDF_Base *p_pdf;
+  double    m_q0, m_x, m_z, m_kp2, m_kp;
+
+  ATOOLS::Flavour m_fl;
+
+public:
+
+  DGLAP_PDF(PDF_Base *const pdf,const double &mu02); 
+
+  double Integrate(const double &x,const double &z,const double &kp2);
+
+  double operator()(const std::vector<double> &point) const;
+
+};// end of class DGLAP_PDF
+
+DGLAP_PDF::DGLAP_PDF(PDF_Base *const pdf,const double &mu02): 
+  p_pdf(pdf), m_q0(sqrt(mu02)), m_fl(ATOOLS::kf::gluon) {}
+
+double DGLAP_PDF::operator()(const std::vector<double> &point) const
+{
+  double q=pow(10.0,point[0]), phi=point[1];
+  double kpp2=ATOOLS::sqr((1.0-m_z)*q)+m_kp2+2.0*m_kp*(1.0-m_z)*q*cos(phi);
+  if (kpp2<p_pdf->Q2Min()) return 0.0;
+  // d x*pdf(x,q^2) / d log(q^2)
+  p_pdf->Calculate(m_x/m_z,0.0,0.0,kpp2*(1.0+1.0e-9));
+  double cur=p_pdf->GetXPDF(m_fl);
+  p_pdf->Calculate(m_x/m_z,0.0,0.0,kpp2);
+  cur=(cur-p_pdf->GetXPDF(m_fl))/1.0e-9;
+  // integrand
+  cur*=m_kp2/kpp2;
+  msg_Debugging()<<"DGLAP_PDF::Value(): kp = "<<m_kp<<" GeV, q = "<<q
+		 <<", phi = "<<(phi/M_PI*180.0)<<" ° -> "
+		 <<cur<<"     "<<ATOOLS::bm::cr<<std::flush;
+#ifdef TEST__IDPDF
+  static TH2D *pint, *wint;
+  static bool initi=false;
+  if (!initi) {
+    initi=true;
+    pint = new TH2D("pint","pint",50,0.0,2.0*M_PI,100,-1.0,4.0);
+    wint = new TH2D("wint","wint",50,0.0,2.0*M_PI,100,-1.0,4.0);
+    MYROOT::myroot->AddObject(pint,"wint");
+    MYROOT::myroot->AddObject(wint,"rint");
+  }
+  pint->Fill(phi,log10(q),1.0);
+  wint->Fill(phi,log10(q),cur);
+#endif
+  return cur;
+}
+
+double DGLAP_PDF::Integrate(const double &x,const double &z,const double &kp2)
+{
+  msg_Tracking()<<ATOOLS::bm::cr<<"DGLAP_PDF::Integrate("
+		<<x<<","<<z<<","<<kp2<<"):";
+  m_x=x;
+  m_z=z;
+  m_kp=sqrt(m_kp2=kp2);
+  ATOOLS::Primitive_Integrator integrator;
+  std::vector<double> bound(2);
+  bound[0]=log10(m_q0);
+  bound[1]=0.0;
+  integrator.SetMin(bound);
+  bound[0]=log10(ATOOLS::rpa.gen.Ecms()/2.0);
+  bound[1]=2.0*M_PI;
+  integrator.SetMax(bound);
+  integrator.SetMode(0);
+  integrator.SetNOpt(1000);
+  integrator.SetNCells(1000);
+  integrator.SetNMax(100000);
+  integrator.SetError(0.01);
+  int old=ATOOLS::msg.Level();
+  ATOOLS::msg.SetLevel(0);
+  double cur=integrator.Integrate(this);
+  ATOOLS::msg.SetLevel(old);
+  msg_Tracking()<<" Result = "<<cur<<"      "<<ATOOLS::bm::cr<<std::flush;
+  return cur;
+}
 
 Doubly_Unintegrated_PDF::
 Doubly_Unintegrated_PDF(PDF_Base *_p_pdf,MODEL::Running_AlphaS *_p_alphas,
@@ -36,7 +119,9 @@ Doubly_Unintegrated_PDF(PDF_Base *_p_pdf,MODEL::Running_AlphaS *_p_alphas,
   m_mu02(mu02),
   m_epsilon(1.e-2),
   m_kperpscheme(kps::function),
-  m_fixedktexponent(0.125)
+  m_fixedktexponent(0.125),
+  m_mode(0),
+  p_integral(new ATOOLS::Grid(3,m_gridpoints))
 {
   m_type=std::string("DUPDF(")+p_pdf->Type()+std::string(")");
   m_xmin=m_xmax=0.0;
@@ -45,32 +130,32 @@ Doubly_Unintegrated_PDF(PDF_Base *_p_pdf,MODEL::Running_AlphaS *_p_alphas,
   m_q2max=ATOOLS::sqr(p_pdf->Q2Max());
   m_bunch=p_pdf->Bunch();
   m_partons=p_pdf->Partons();
-  for (size_t i=0;i<m_partons.size();++i) {
-    if (m_partons[i].Size()>1) {
-      for (int j=0;j<m_partons[i].Size();++j) {
-	m_branching[m_partons[i][j]] = 
-	  new LL_Branching(m_partons[i][j],p_alphas);
-      }
-    }
-    else {
-      m_branching[m_partons[i]] = new LL_Branching(m_partons[i],p_alphas);
-    }
-  }
-  p_sudakov = new LL_Sudakov(p_alphas);
-  std::string ecms;
-  MyStrStream sstr;
-  sstr<<ATOOLS::rpa.gen.Ecms();
-  sstr>>ecms;
-  p_sudakov->SetOutPath(std::string("DUPDF_")+ecms);
-  p_sudakov->Initialize();
 }
 
 Doubly_Unintegrated_PDF::~Doubly_Unintegrated_PDF()
 {
+#ifdef TEST__Weights
+  static TH2D *p_weights;
+  static bool initw=false;
+  if (!initw) {
+    initw=true;
+    p_weights = new TH2D("DUPDF_Weights","DUPDF_Weights",
+			 100,-2.0,3.0,100,-2.0,3.0);
+    MYROOT::myroot->AddObject(p_weights,"DUPDF_Weights");
+  }
+  for (double i=-2.0;i<3.0;i+=0.05) {
+    for (double j=-2.0;j<3.0;j+=0.05) {
+      Calculate(0.01,0.1,exp(i),exp(j));
+      std::cout<<i<<" "<<j<<" "<<exp(i)<<" "<<exp(j)<<std::endl;
+      p_weights->Fill(i,j,GetXPDF(ATOOLS::kf::gluon));
+    }
+  }
+  THROW(normal_exit,"finished histogram");
+#endif
 #ifdef TEST__Sudakov
-#ifdef ROOT_SUPPORT
-  if (!initialized) {
-    initialized=true;
+  static bool inits=false;
+  if (!inits) {
+    inits=true;
     double min=0.082, max=91.0, step=0.1;
     TH1D *sudu = new TH1D(ATOOLS::ToString((long int)this).c_str(),
 			  "Quark_Sudakov",(int)((log(max)-log(min))/step),
@@ -127,15 +212,80 @@ Doubly_Unintegrated_PDF::~Doubly_Unintegrated_PDF()
     }
     THROW(normal_exit,"Finished Sudakov histograms");
   }
-#else
-#error ROOT is disabled
-#endif
 #endif
   while (m_branching.size()>0) {
     delete (*m_branching.begin()).second;
     m_branching.erase(m_branching.begin());
   }
   delete p_sudakov;
+  delete p_integral;
+}
+
+bool Doubly_Unintegrated_PDF::Initialize()
+{
+  for (size_t i=0;i<m_partons.size();++i) {
+    if (m_partons[i].Size()>1) {
+      for (int j=0;j<m_partons[i].Size();++j) {
+	m_branching[m_partons[i][j]] = 
+	  new LL_Branching(m_partons[i][j],p_alphas);
+      }
+    }
+    else {
+      m_branching[m_partons[i]] = new LL_Branching(m_partons[i],p_alphas);
+    }
+  }
+  p_sudakov = new LL_Sudakov(p_alphas);
+  p_sudakov->SetOutPath(std::string("DUPDF_")+
+			ATOOLS::ToString(ATOOLS::rpa.gen.Ecms()));
+  p_sudakov->Initialize();
+  if (m_mode>0) return InitializeGluonPDF();
+  return true;
+}
+
+bool Doubly_Unintegrated_PDF::InitializeGluonPDF()
+{
+  const std::string gridname="GDUPDF_"+p_pdf->Type()+"_"+
+    ATOOLS::ToString(m_mu02)+"_"+ATOOLS::ToString(ATOOLS::rpa.gen.Ecms());
+  if (!p_integral->ReadIn(gridname)) {
+    msg_Info()<<"Doubly_Unintegrated_PDF::Doubly_Unintegrated_PDF(..):"
+	      <<"Initializing DUPDF grid ... "<<std::flush;
+    DGLAP_PDF pdf(p_pdf,m_mu02);
+    std::vector<size_t> i(3);
+    std::vector<double> xz(m_gridpoints), q2(m_gridpoints);
+    double dxz=6.0/(m_gridpoints+1);
+    double dq=4.0/m_gridpoints;
+    for (i[0]=0;i[0]<m_gridpoints;++i[0]) {
+      xz[i[0]]=-6.0+i[0]*dxz;
+      for (i[1]=i[0]+1;i[1]<m_gridpoints;++i[1]) {
+	double z=-6.0+i[1]*dxz;
+	for (i[2]=0;i[2]<m_gridpoints;++i[2]) {
+	  q2[i[2]]=0.1+i[2]*dq;
+	  double cur=pdf.Integrate(pow(10.0,xz[i[0]]),pow(10.0,z),
+				   pow(10.0,2.0*q2[i[2]]));
+	  p_integral->SetY(i,cur);
+#ifdef TEST__DPDF
+	  static TH3D *dpdfh;
+	  static bool ini=false;
+	  if (!ini) {
+	    dpdfh = new TH3D("GUPDF","GUPDF",
+			     m_gridpoints,-6.0,-6.0+dxz*(m_gridpoints-1),
+			     m_gridpoints,-6.0,-6.0+dxz*(m_gridpoints-1),
+			     m_gridpoints,0.1,0.1+dq*m_gridpoints);
+	    MYROOT::myroot->AddObject(dpdfh,"GUPDF");
+	    ini=true;
+	  }
+	  dpdfh->Fill(xz[i[0]],z,q2[i[2]],cur);
+#endif
+	}
+      }
+    }
+    p_integral->SetX(0,xz);
+    p_integral->SetX(1,xz);
+    p_integral->SetX(2,q2);
+    p_integral->WriteOut(gridname);
+    msg_Info()<<"done."<<std::endl;
+  }
+  return true;
 }
 
 void Doubly_Unintegrated_PDF::AssignKeys(ATOOLS::Integration_Info *const info)
@@ -253,16 +403,53 @@ bool Doubly_Unintegrated_PDF::Unintegrate(ATOOLS::Flavour flavour)
     }
     return false;     
   }
-  LL_Branching::SF_Set::iterator sfit=LL_Branching::AllSplittings().begin();
-  for (;sfit!=LL_Branching::AllSplittings().end();++sfit) {
-    if ((*sfit)->GetFlB()==flavour) {
-      if (((flavour.IsGluon() && (*sfit)->GetFlA().IsGluon()) ||
-	   (flavour.IsQuark() && (*sfit)->GetFlA().IsQuark())) && 
-	  m_z*(1.+sqrt(m_kperp2/m_mu2))>1.) continue;
-      m_unintegrated+=(*(*sfit))(m_z)*p_pdf->GetXPDF((*sfit)->GetFlA());
-      if (flavour.IsGluon() && (*sfit)->GetFlA().IsGluon())
-	m_unintegrated+=(*(*sfit))(m_z)*p_pdf->GetXPDF((*sfit)->GetFlA());
+  switch (m_mode) {
+  case 1: {
+#ifdef TEST__Order_All
+    if (m_z*(1.+sqrt(m_kperp2/m_mu2))>1.) return true;
+#endif
+    LL_Branching::SF_Set::iterator sfit=
+      LL_Branching::AllSplittings().begin();
+    for (;sfit!=LL_Branching::AllSplittings().end();++sfit) {
+      if ((*sfit)->GetFlB()==flavour) {
+	if ((*sfit)->GetFlC().IsGluon() && 
+	    m_z*(1.+sqrt(m_kperp2/m_mu2))>1.) continue;
+	double cur=(*(*sfit))(m_z);
+	if (flavour.IsGluon() && (*sfit)->GetFlA().IsGluon()) {
+	  cur-=6.0/m_z;
+	  cur*=2.0;
+	}
+	m_unintegrated+=cur*p_pdf->GetXPDF((*sfit)->GetFlA());
+      }
     }
+    if (flavour==ATOOLS::kf::gluon && 
+	m_z*(1.+sqrt(m_kperp2/m_mu2))<=1.) {
+      std::vector<double> x(3,log10(m_kperp2));
+      x[0]=log10(m_x);
+      x[1]=log10(m_z);
+      p_pdf->Calculate(m_x/m_z,0.0,0.0,m_kperp2*(1.0+1.0e-9));
+      double cur=p_pdf->GetXPDF(flavour);
+      p_pdf->Calculate(m_x/m_z,0.0,0.0,m_kperp2);
+      cur=(cur-p_pdf->GetXPDF(flavour))/1.0e-9;
+      m_unintegrated+=2.0*6.0/m_z*((*p_integral)(x)/M_PI-
+				   cur/2.0*(log(m_kperp2)-log(m_mu02)));
+    }
+    break;
+  }
+  default: {
+    LL_Branching::SF_Set::iterator sfit=
+      LL_Branching::AllSplittings().begin();
+    for (;sfit!=LL_Branching::AllSplittings().end();++sfit) {
+      if ((*sfit)->GetFlB()==flavour) {
+	if ((*sfit)->GetFlC().IsQuark() && 
+	    m_z*(1.+sqrt(m_kperp2/m_mu2))>1.) continue;
+	m_unintegrated+=(*(*sfit))(m_z)*p_pdf->GetXPDF((*sfit)->GetFlA());
+	if (flavour.IsGluon() && (*sfit)->GetFlA().IsGluon())
+	  m_unintegrated+=(*(*sfit))(m_z)*p_pdf->GetXPDF((*sfit)->GetFlA());
+      }
+    }
+    break;
+  }
   }
   m_unintegrated*=(*p_alphas)(m_kperp2)/(2.0*M_PI);
   m_unintegrated*=p_sudakov->Delta(flavour)(sqrt(m_mu2),sqrt(m_kperp2));
@@ -284,8 +471,7 @@ void Doubly_Unintegrated_PDF::Calculate(double x,double z,
   m_kperp2=kperp2;
   m_mu2=mu2;
   m_calculate=true;
-  if (m_z<m_x || m_kperp2>p_pdf->Q2Max() ||
-      (m_mu2<p_pdf->Q2Min() && m_mu02<m_kperp2)) {
+  if (m_z<m_x || m_kperp2>p_pdf->Q2Max()) {
     m_calculate=false;
     return; 
   }
