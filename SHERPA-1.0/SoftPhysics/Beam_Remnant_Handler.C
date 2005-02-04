0085145a470efcbd8831e85d9be43895502ec6e0
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-02-04 16:49:19 +0000

    changes: adjust remnants failure treatment changed

--- SHERPA-1.0/SoftPhysics/Beam_Remnant_Handler.C
+++ SHERPA-1.0/SoftPhysics/Beam_Remnant_Handler.C
@@ -18,51 +18,6 @@
 
 using namespace SHERPA;
 
-#define TEST__Beam_Remnant_Handler
-
-#ifndef TEST__Beam_Remnant_Handler
-inline bool SumMomenta(ATOOLS::Blob *bl) 
-{
-  return true;
-}
-#else
-std::set<ATOOLS::Blob*> checked_blobs;
-
-void SumMomenta(ATOOLS::Blob *bl,ATOOLS::Vec4D &inisum,
-		ATOOLS::Vec4D & finsum,bool iterate=true) 
-{
-  // check if caught in loop
-  std::set<ATOOLS::Blob*>::const_iterator bit=checked_blobs.find(bl);
-  if (bit!=checked_blobs.end()) return;
-  checked_blobs.insert(bl);
-  for (int i=0;i<bl->NInP();++i) {
-    ATOOLS::Particle * p =bl->InParticle(i);
-    if (p->ProductionBlob()==NULL) inisum+=p->Momentum(); 
-    else if (iterate) SumMomenta(p->ProductionBlob(),inisum,finsum);
-    else inisum+=p->Momentum();
-  }
-  for (int i=0;i<bl->NOutP();++i) {
-    ATOOLS::Particle * p =bl->OutParticle(i);
-    if (p->DecayBlob()==NULL) finsum+=p->Momentum(); 
-    else if (iterate) SumMomenta(p->DecayBlob(),inisum,finsum);
-    else finsum+=p->Momentum();
-  }
-}
-
-bool SumMomenta(ATOOLS::Blob *bl)
-{
-  ATOOLS::Vec4D inisum,finsum;
-  checked_blobs.clear();
-  SumMomenta(bl,inisum,finsum);
-  bool test=inisum==finsum;
-  if (!test) {
-    ATOOLS::msg.Error()<<"SumMomenta(..): Summation does not agree."<<std::endl
-		       <<"initial = "<<inisum<<" vs. final = "<<finsum<<std::endl;
-  }
-  return test;
-}
-#endif
-
 Beam_Remnant_Handler::
 Beam_Remnant_Handler(const std::string path,const std::string file,
 		     PDF::ISR_Handler *const isr,
@@ -161,84 +116,96 @@ FillBeamBlobs(ATOOLS::Blob_List *const bloblist,
   p_bloblist=bloblist;
   p_particlelist=particlelist;
   if (!m_fill) return false;
-  ATOOLS::Blob_Iterator endblob = bloblist->end(); 
-  ATOOLS::Blob * blob;
-  bool okay=false, treat[2];
-  for (short unsigned int i=0;i<2;++i) {
-    p_beampart[i]->Clear();
-    p_beampart[i]->ClearErrors();
-    okay=false;
-    treat[i]=false;
-    p_beamblob[i]=NULL;
-    for (ATOOLS::Blob_List::iterator biter=bloblist->begin();
-	 biter!=endblob;++biter) {
-      if ((*biter)->Beam()==i && (*biter)->Type()==ATOOLS::btp::IS_Shower) { 
-	if (p_beampart[i]->Type()&rtp::qcd_remnant) {
-	  if (!okay) {
+  bool adjusted=false;
+  bool okay=false;
+  while (!adjusted) {
+    ATOOLS::Blob_List::iterator endblob = bloblist->end(); 
+    ATOOLS::Blob * blob;
+    bool treat[2];
+    for (short unsigned int i=0;i<2;++i) {
+      p_beampart[i]->Clear();
+      p_beampart[i]->ClearErrors();
+      okay=false;
+      treat[i]=false;
+      p_beamblob[i]=NULL;
+      for (ATOOLS::Blob_List::iterator biter=bloblist->begin();
+	   biter!=endblob;++biter) {
+	if ((*biter)->Beam()==i && (*biter)->Type()==ATOOLS::btp::IS_Shower) { 
+	  if (p_beampart[i]->Type()&rtp::qcd_remnant) {
+	    if (!okay) {
+	      blob = new ATOOLS::Blob();
+	      bloblist->insert(bloblist->begin(),blob);
+	      blob->SetId();
+	      blob->SetType(ATOOLS::btp::Beam);
+	      blob->SetBeam(i);
+	      blob->SetStatus(1);
+	      ATOOLS::Particle *p = 
+		new ATOOLS::Particle(-1,p_isr->Flav(i),
+				     p_beam->GetBeam(i)->OutMomentum());
+	      p->SetStatus(2);
+	      blob->AddToInParticles(p);
+	      p_beamblob[i]=blob;
+	      okay=true;
+	    }
+	    p_beampart[i]->Extract((*biter)->InParticle(0));
+	    (*biter)->SetStatus(2);
+	    treat[i]=true;
+	  }
+	  else if (!ATOOLS::IsEqual((*biter)->InParticle(0)->E(),
+				    p_beam->GetBeam(i)->OutMomentum()[0])) {
+	    (*biter)->SetStatus(2);
 	    blob = new ATOOLS::Blob();
-	    bloblist->insert(bloblist->begin(),blob);
-	    blob->SetId();
 	    blob->SetType(ATOOLS::btp::Beam);
 	    blob->SetBeam(i);
 	    blob->SetStatus(1);
-	    ATOOLS::Particle *p = 
-	      new ATOOLS::Particle(-1,p_isr->Flav(i),
-				   p_beam->GetBeam(i)->OutMomentum());
+	    p_beampart[i]->Extract((*biter)->InParticle(0));
+	    ATOOLS::Particle *p = new 
+	      ATOOLS::Particle(-1,p_isr->Flav(i),
+			       p_beam->GetBeam(i)->OutMomentum());
+	    if (particlelist!=NULL) p->SetNumber(-particlelist->size());
+	    else p->SetNumber(0);
 	    p->SetStatus(2);
 	    blob->AddToInParticles(p);
+	    bloblist->insert(bloblist->begin(),blob);
+	    blob->SetId();
 	    p_beamblob[i]=blob;
 	    okay=true;
+	    treat[i]=true;
 	  }
-	  p_beampart[i]->Extract((*biter)->InParticle(0));
-	  (*biter)->SetStatus(2);
-	  treat[i]=true;
-	}
-	else if (!ATOOLS::IsEqual((*biter)->InParticle(0)->E(),
-				  p_beam->GetBeam(i)->OutMomentum()[0])) {
-	  (*biter)->SetStatus(2);
-	  blob = new ATOOLS::Blob();
-	  blob->SetType(ATOOLS::btp::Beam);
-	  blob->SetBeam(i);
-	  blob->SetStatus(1);
-	  p_beampart[i]->Extract((*biter)->InParticle(0));
-	  ATOOLS::Particle *p = new 
-	    ATOOLS::Particle(-1,p_isr->Flav(i),
-			     p_beam->GetBeam(i)->OutMomentum());
-	  if (particlelist!=NULL) p->SetNumber(-particlelist->size());
-	  else p->SetNumber(0);
-	  p->SetStatus(2);
-	  blob->AddToInParticles(p);
-	  bloblist->insert(bloblist->begin(),blob);
-	  blob->SetId();
-	  p_beamblob[i]=blob;
-	  okay=true;
-	  treat[i]=true;
 	}
       }
     }
-  }
-  for (short unsigned int i=0;i<2;++i) 
-    if (p_beamblob[i]) if (!p_beampart[i]->FillBlob(p_beamblob[i],particlelist)) {
-      ATOOLS::msg.Error()<<*bloblist<<std::endl;
-      if (i==0) p_beampart[1]->FillBlob(p_beamblob[i],particlelist);
-      while (bloblist->size()>0) {
-	delete *bloblist->begin();
-	bloblist->erase(bloblist->begin());
+    for (short unsigned int i=0;i<2;++i) 
+      if (p_beamblob[i]) if (!p_beampart[i]->FillBlob(p_beamblob[i],particlelist)) {
+	ATOOLS::msg.Error()<<*bloblist<<std::endl;
+	if (i==0) p_beampart[1]->FillBlob(p_beamblob[i],particlelist);
+	while (bloblist->size()>0) {
+	  delete *bloblist->begin();
+	  bloblist->erase(bloblist->begin());
+	}
+	return false;
       }
-      return false;
+    if (p_beampart[0]->Type()==rtp::hadron || 
+	p_beampart[1]->Type()==rtp::hadron) {
+      p_kperp->CreateKPerp(p_beamblob[0],p_beamblob[1]);
+      for (short unsigned int i=0;i<2;++i) p_kperp->FillKPerp(p_beamblob[i]);
     }
-  if (p_beampart[0]->Type()==rtp::hadron || 
-      p_beampart[1]->Type()==rtp::hadron) {
-    p_kperp->CreateKPerp(p_beamblob[0],p_beamblob[1]);
-    for (short unsigned int i=0;i<2;++i) p_kperp->FillKPerp(p_beamblob[i]);
+    adjusted=true;
+    for (short unsigned int i=0;i<2;++i) 
+      if (!p_beampart[i]->AdjustKinematics()) adjusted=false;
+    if (!adjusted) {
+      ATOOLS::Blob *lastmi(bloblist->FindLast(ATOOLS::btp::Hard_Collision));
+      if (lastmi!=NULL) {
+	for (short unsigned int i=0;i<2;++i) bloblist->Delete(p_beamblob[i]);
+	bloblist->DeleteConnected(lastmi);
+      }
+      else break;
+    }
   }
-  for (short unsigned int i=0;i<2;++i) p_beampart[i]->AdjustKinematics();
-  if (!SumMomenta(bloblist->front())) {
-    // msg_Info()<<ATOOLS::rpa.gen.NumberOfDicedEvents()<<" "<<*bloblist<<std::endl;
-    while (bloblist->size()>0) {
-      delete bloblist->back();
-      bloblist->pop_back();
-    }
+  if (!bloblist->FourMomentumConservation()) {
+    msg_Info()<<"Beam_Remnant_Handler::FillBeamBlobs(..): Retry event "
+	      <<ATOOLS::rpa.gen.NumberOfDicedEvents()<<"."<<std::endl;
+    bloblist->Clear();
   }
   return okay;
 }
