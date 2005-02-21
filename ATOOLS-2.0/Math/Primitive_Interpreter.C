a8c6bc1deaecc32a3b5ba4db39923d5cd7e558e8
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-02-21 13:49:16 +0000

    algebra tool

---
+++ ATOOLS-2.0/Math/Primitive_Interpreter.C
@@ -0,0 +1,356 @@
+#include "Primitive_Interpreter.H"
+
+#include "MyStrStream.H"
+#include "MathTools.H"
+#include "Exception.H"
+#include "Message.H"
+
+using namespace ATOOLS;
+
+Function::~Function() {}
+
+std::string Function::Evaluate(const std::vector<std::string> &args) const
+{
+  THROW(fatal_error,"No evaluation rule.");
+}
+
+Operator::~Operator() {}
+
+DEFINE_BINARY_OPERATOR(Binary_Plus,"+",1)
+{
+  double arg0=ToType<double>(args[0]);
+  double arg1=ToType<double>(args[1]);
+  return ToString(arg0+arg1);
+}
+
+DEFINE_BINARY_OPERATOR(Binary_Minus,"-",1)
+{
+  double arg0=ToType<double>(args[0]);
+  double arg1=ToType<double>(args[1]);
+  return ToString(arg0-arg1);
+}
+
+DEFINE_BINARY_OPERATOR(Binary_Times,"*",2)
+{
+  double arg0=ToType<double>(args[0]);
+  double arg1=ToType<double>(args[1]);
+  return ToString(arg0*arg1);
+}
+
+DEFINE_BINARY_OPERATOR(Binary_Divide,"/",2)
+{
+  double arg0=ToType<double>(args[0]);
+  double arg1=ToType<double>(args[1]);
+  return ToString(arg0/arg1);
+}
+
+DEFINE_UNARY_OPERATOR(Unary_Not,"!",3)
+{
+  double arg0=ToType<int>(args[0]);
+  return ToString(!arg0);
+}
+
+DEFINE_FUNCTION(Power,"pow")
+{
+  if (args.size()!=2) THROW(fatal_error,"Pow requires 2 arguments.")
+  double arg0=ToType<double>(args[0]);
+  double arg1=ToType<double>(args[1]);
+  return ToString(pow(arg0,arg1));
+}
+
+DEFINE_FUNCTION(Logarithm,"log")
+{
+  if (args.size()!=1) THROW(fatal_error,"Log requires 1 argument.")
+  double arg0=ToType<double>(args[0]);
+  return ToString(log(arg0));
+}
+
+DEFINE_FUNCTION(Logarithm10,"log10")
+{
+  if (args.size()!=1) THROW(fatal_error,"Log10 requires 1 argument.")
+  double arg0=ToType<double>(args[0]);
+  return ToString(log10(arg0));
+}
+
+DEFINE_FUNCTION(Exponential,"exp")
+{
+  if (args.size()!=1) THROW(fatal_error,"Exp requires 1 argument.")
+  double arg0=ToType<double>(args[0]);
+  return ToString(exp(arg0));
+}
+
+DEFINE_FUNCTION(Absolute_Value,"abs")
+{
+  if (args.size()!=1) THROW(fatal_error,"Abs requires 1 argument.")
+  double arg0=ToType<double>(args[0]);
+  return ToString(dabs(arg0));
+}
+
+DEFINE_FUNCTION(Square,"sqr")
+{
+  if (args.size()!=1) THROW(fatal_error,"Sqr requires 1 argument.")
+  double arg0=ToType<double>(args[0]);
+  return ToString(sqr(arg0));
+}
+
+DEFINE_FUNCTION(Square_Root,"sqrt")
+{
+  if (args.size()!=1) THROW(fatal_error,"Sqrt requires 1 argument.")
+  double arg0=ToType<double>(args[0]);
+  return ToString(sqrt(arg0));
+}
+
+DEFINE_INTERPRETER_FUNCTION(Resolve_Bracket)
+{
+  if (expr.find("(")==std::string::npos ||
+      expr.find(")")==std::string::npos) return expr;
+  static int cnt=0;
+  if (++cnt==100) exit(0);
+  int open=0, take=1;
+  size_t l=std::string::npos, r=std::string::npos;
+  for (size_t i=0;i<expr.length();++i) {
+    if (expr[i]=='(') {
+      ++open;
+      if (l==std::string::npos) {
+	Primitive_Interpreter::Function_Set::const_iterator fit=
+	  p_interpreter->Functions().begin();
+	for (;fit!=p_interpreter->Functions().end();++fit) {
+	  size_t pos=expr.rfind((*fit)->Tag(),i);
+	  if (pos!=std::string::npos &&
+	      pos+(*fit)->Tag().length()==i) break;
+	}
+	if (fit==p_interpreter->Functions().end()) {
+	  l=i;
+	  take=open;
+	}
+      }    
+    }  
+    else if (expr[i]==')') {
+      if (open==take && l!=std::string::npos && 
+	  r==std::string::npos) r=i;
+      --open;
+    }
+  }
+  if (open!=0) THROW(fatal_error,"Ambiguous bracket structure .");
+  if (l==std::string::npos) return expr;
+  std::string left=expr.substr(0,l);
+  std::string right=expr.substr(r+1);
+  msg_Tracking()<<"Resolve_Bracket -> '"
+		<<left<<"' '"<<expr.substr(l+1,r-l-1)<<"' '"<<right<<"'\n";
+  return p_interpreter->
+    Iterate(left+p_interpreter->
+	    Iterate(expr.substr(l+1,r-l-1))+right);
+}
+
+DEFINE_INTERPRETER_FUNCTION(Interprete_Function)
+{
+  if (expr.find("(")==std::string::npos ||
+      expr.find(")")==std::string::npos) return expr;
+  Function *func=NULL;
+  size_t pos=std::string::npos;
+  for (Primitive_Interpreter::Function_Set::const_reverse_iterator 
+	 fit=p_interpreter->Functions().rbegin();
+       fit!=p_interpreter->Functions().rend();++fit) 
+    if ((pos=expr.rfind((*fit)->Tag()))!=std::string::npos) {
+      func=*fit;
+      break;
+    }
+  if (func==NULL) return expr;
+  size_t last=pos+func->Tag().length()+1, open=0;
+  std::vector<std::string> args;
+  size_t i=last-1;
+  for (;i<expr.length();++i) {
+    if (expr[i]=='(') ++open;
+    if (expr[i]==')') --open;
+    if (open==0 || expr[i]==',') {
+      args.push_back(expr.substr(last,i-last));
+      last=i+1;
+      if (open==0) break;
+    }
+  }
+  std::string left=expr.substr(0,pos);
+  std::string right=expr.substr(i+1);
+  if (msg.LevelIsTracking()) {
+    std::string out=args[0];
+    for (size_t j=1;j<args.size();++j) out+=","+args[j];
+    msg_Tracking()<<"Interprete_Function -> '"<<left
+		  <<"' '"<<func->Tag()<<"("<<out
+		  <<")' '"<<right<<"'\n";
+  }
+  for (size_t j=0;j<args.size();++j) 
+    args[j]=p_interpreter->Iterate(args[j]);
+  return p_interpreter->Iterate(left+func->Evaluate(args)+right);
+}
+
+DEFINE_INTERPRETER_FUNCTION(Interprete_Binary)
+{
+  if (expr.find("(")!=std::string::npos ||
+      expr.find(")")!=std::string::npos) return expr;
+  Operator *op=NULL;
+  size_t pos=std::string::npos;
+  for (Primitive_Interpreter::Operator_Map::const_reverse_iterator 
+	 oit=p_interpreter->Operators().rbegin();
+       oit!=p_interpreter->Operators().rend();++oit) 
+    if ((pos=expr.rfind(oit->second->Tag()))!=std::string::npos &&
+	expr[pos-1]!='e' && expr[pos-1]!='E') 
+      if (oit->second->Binary()) {
+	op=oit->second;
+	break;
+      }
+      else {
+	return expr;
+      }
+  if (op==NULL) return expr;
+  std::string lrstr, lstr=expr.substr(0,pos);
+  size_t lfpos=0;
+  for (Primitive_Interpreter::Operator_Map::const_reverse_iterator 
+	 oit=p_interpreter->Operators().rbegin();
+       oit!=p_interpreter->Operators().rend();++oit) {
+    size_t tlfpos=lstr.rfind(oit->second->Tag());
+    if (tlfpos!=std::string::npos) {
+      lfpos=Max(lfpos,tlfpos+oit->second->Tag().length());
+    }
+  }
+  lrstr=lstr.substr(0,lfpos);
+  lstr=lstr.substr(lfpos);
+  std::string rrstr, rstr=expr.substr(pos+op->Tag().length());
+  size_t rfpos=rstr.length();
+  for (Primitive_Interpreter::Operator_Map::const_reverse_iterator 
+	 oit=p_interpreter->Operators().rbegin();
+       oit!=p_interpreter->Operators().rend();++oit) {
+    size_t trfpos=rstr.find(oit->second->Tag());
+    if (trfpos!=std::string::npos) {
+      rfpos=Min(rfpos,trfpos);
+    }
+  }
+  rrstr=rstr.substr(rfpos);
+  rstr=rstr.substr(0,rfpos);
+  std::vector<std::string> args(2);
+  args[0]=p_interpreter->Iterate(lstr);
+  args[1]=p_interpreter->Iterate(rstr);
+  msg_Tracking()<<"Interprete_Binary -> '"
+		<<lrstr<<"' '"<<lstr<<op->Tag()
+		<<rstr<<"' '"<<rrstr<<"'\n";
+  return p_interpreter->
+    Iterate(lrstr+op->Evaluate(args)+rrstr);
+}
+
+DEFINE_INTERPRETER_FUNCTION(Interprete_Unary)
+{
+  if (expr.find("(")!=std::string::npos ||
+      expr.find(")")!=std::string::npos) return expr;
+  Operator *op=NULL;
+  size_t pos=std::string::npos;
+  for (Primitive_Interpreter::Operator_Map::const_reverse_iterator 
+	 oit=p_interpreter->Operators().rbegin();
+       oit!=p_interpreter->Operators().rend();++oit) 
+    if ((pos=expr.rfind(oit->second->Tag()))!=std::string::npos &&
+	expr[pos-1]!='e' && expr[pos-1]!='E') 
+      if (!oit->second->Binary()) {
+	op=oit->second;
+	break;
+      }
+      else {
+	return expr;
+      }
+  if (op==NULL) return expr;
+  std::string lrstr=expr.substr(0,pos);
+  std::string rrstr, rstr=expr.substr(pos+op->Tag().length());
+  size_t rfpos=rstr.length();
+  for (Primitive_Interpreter::Operator_Map::const_reverse_iterator 
+	 oit=p_interpreter->Operators().rbegin();
+       oit!=p_interpreter->Operators().rend();++oit) {
+    size_t trfpos=rstr.find(oit->second->Tag());
+    if (trfpos!=std::string::npos) {
+      rfpos=Min(rfpos,trfpos);
+    }
+  }
+  rrstr=rstr.substr(rfpos);
+  rstr=rstr.substr(0,rfpos);
+  std::vector<std::string> args(1);
+  args[0]=p_interpreter->Iterate(rstr);
+  msg_Tracking()<<"Interprete_Unary -> '"
+		<<lrstr<<"' '"<<op->Tag()<<rstr<<"' '"<<rrstr<<"'\n";
+  return p_interpreter->
+    Iterate(lrstr+op->Evaluate(args)+rrstr);
+}
+
+Primitive_Interpreter::Primitive_Interpreter(const bool standard) 
+{
+  m_interpreters.insert(new Resolve_Bracket(this));
+  m_interpreters.insert(new Interprete_Binary(this));
+  m_interpreters.insert(new Interprete_Unary(this));
+  m_interpreters.insert(new Interprete_Function(this));
+  if (!standard) return;
+  AddOperator(new Binary_Plus());
+  AddOperator(new Binary_Minus());
+  AddOperator(new Binary_Times());
+  AddOperator(new Binary_Divide());
+  AddOperator(new Unary_Not());
+  AddFunction(new Power());
+  AddFunction(new Logarithm());
+  AddFunction(new Logarithm10());
+  AddFunction(new Exponential());
+  AddFunction(new Absolute_Value());
+  AddFunction(new Square());
+  AddFunction(new Square_Root());
+}
+
+Primitive_Interpreter::~Primitive_Interpreter()
+{
+  while (!m_operators.empty()) {
+    delete m_operators.begin()->second;
+    m_operators.erase(m_operators.begin());
+  }
+  while (!m_functions.empty()) {
+    delete *m_functions.begin();
+    m_functions.erase(m_functions.begin());
+  }
+}
+
+std::string &Primitive_Interpreter::ReplaceTags(std::string &expr) const
+{
+  size_t pos=std::string::npos;
+  for (String_Map::const_iterator sit=m_tags.begin();
+       sit!=m_tags.end();++sit) 
+    if ((pos=expr.find(sit->first))!=std::string::npos) 
+      return ReplaceTags(expr.replace(pos,sit->first.length(),sit->second));
+  return expr;
+}
+
+std::string Primitive_Interpreter::Interprete(const std::string &expr)
+{
+  return Iterate(expr);
+}
+
+void Primitive_Interpreter::AddFunction(Function *const f)
+{
+  m_functions.insert(f); 
+}
+
+void Primitive_Interpreter::AddOperator(Operator *const b)
+{ 
+  m_operators.insert(Operator_Pair(b->Priority(),b)); 
+}
+
+std::string Primitive_Interpreter::Iterate(const std::string &expr)
+{
+  msg_Indent();
+  std::string res=expr;
+  ReplaceTags(res);
+  Interpreter_Set::const_iterator iit=m_interpreters.begin();
+  for (;iit!=m_interpreters.end();++iit) 
+    res=(*iit)->Interprete(res);
+  return res;
+}
+
+void Primitive_Interpreter::AddTag(const std::string &tag,
+				   const std::string &value)
+{
+  m_tags[tag]=value;
+}
+
+void Primitive_Interpreter::SetTags(const String_Map &tags)
+{
+  m_tags=tags;
+}
