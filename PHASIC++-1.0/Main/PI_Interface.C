768ba842a9af09ffc4911df308e841db647b9f8c
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-02-23 11:31:22 +0000

    bugfix for psi

--- PHASIC++-1.0/Main/PI_Interface.C
+++ PHASIC++-1.0/Main/PI_Interface.C
@@ -18,8 +18,8 @@ using namespace PHASIC;
 PI_Interface::PI_Interface(Phase_Space_Handler *const pshandler,
 			   const std::string &key,const size_t dim):
   p_integrator(new ATOOLS::Primitive_Integrator()), p_pshandler(pshandler), 
-  m_key(key), m_initialize(false), m_point(dim), m_integral(0.0),
-  m_nopt(2000), m_nmax(1000000), m_ncells(dim*100)
+  m_key(key), m_initialize(false), m_stop(false), m_point(dim), 
+  m_integral(0.0), m_nopt(2000), m_nmax(1000000), m_ncells(dim*100)
 {
   p_integrator->SetDimension(dim);
   p_integrator->SetFunction(this);
@@ -36,7 +36,7 @@ bool PI_Interface::Initialize()
 {
   PROFILE_HERE;
   p_integrator->SetError(p_pshandler->Error());
-  p_integrator->SetMode(0);
+  p_integrator->SetMode(1);
   p_integrator->SetShuffleMode(1);
   p_integrator->SetNOpt(m_nopt);
   p_integrator->SetNMax(m_nmax);
@@ -72,7 +72,8 @@ double PI_Interface::operator()(const std::vector<double> &x)
 
 void PI_Interface::AddPoint(const double weight)
 {
-  p_pshandler->Integrator()->AddPoint(m_value*weight);
+  if (!m_initialize) return;
+  m_stop=m_stop|p_pshandler->Integrator()->AddPoint(m_value*weight);
 }
 
 void PI_Interface::FinishConstruction(const double apweight)
