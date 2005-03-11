8ef580410d9b335a4d7501a2f4e8c06729114f03
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-03-11 08:54:00 +0000

    bugfix for beam radiation

--- PDF-1.0/Main/ISR_Handler.C
+++ PDF-1.0/Main/ISR_Handler.C
@@ -272,16 +272,8 @@ bool ISR_Handler::MakeISR(Vec4D *const p,const size_t n,
   m_cmsboost=Poincare(Vec4D(cosh(m_ykey[2]),0.,0.,sinh(m_ykey[2])));
   m_cmsboost.BoostBack(p_cms[0]);
   m_cmsboost.BoostBack(p_cms[1]);
-  double xb1=sqrt(m_beamspkey[3]/m_beamspkey[2]);
-  double xb2=xb1*exp(-m_beamykey[2]);
-  xb1*=exp(m_beamykey[2]);
-  Vec4D lab1=p_cms[0], lab2=p_cms[1];
-  ATOOLS::Poincare cmsboost=
-    Poincare(Vec4D(cosh(m_beamykey[2]),0.,0.,sinh(m_beamykey[2])));
-  cmsboost.BoostBack(lab1);
-  cmsboost.BoostBack(lab2);
-  m_x[0]=lab1.PPlus()/(2.0*xb1*m_ebeam[0]);
-  m_x[1]=lab2.PMinus()/(2.0*xb2*m_ebeam[1]);
+  m_x[0]=p_cms[0].PPlus()/Q;
+  m_x[1]=p_cms[1].PMinus()/Q;
   m_flux=.25;
   m_flux/=sqrt(sqr(p[0]*p[1])-p[0].Abs2()*p[1].Abs2());
   if (!m_kmrmode) {
