982c5ef9c2dff770e1a9c568bb66ede9310a12f4
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-05-11 10:47:37 +0000

    cosmetics and new istream operator

--- ATOOLS-2.0/Math/Vector.C
+++ ATOOLS-2.0/Math/Vector.C
@@ -7,8 +7,8 @@
 
 #include "Vector.H"
 #include "Run_Parameter.H"
-
-
+#include "Exception.H"
+#include "MyStrStream.H"
 
 using namespace ATOOLS;
 
@@ -43,6 +43,42 @@ std::ostream& ATOOLS::operator<< (std::ostream& s, const Vec3D& vec)
   return s<<'('<<vec[1]<<','<<vec[2]<<','<<vec[3]<<')';
 }
 
+std::istream& ATOOLS::operator>>(std::istream& s,Vec4D& vec)
+{
+  std::string out;
+  s>>out;
+  if (out.length()==0 || out[0]!='(' || out[out.length()-1]!=')')
+    THROW(critical_error,"String to vector translation failed.");
+  out=out.substr(0,out.length()-1).substr(1);
+  for (short unsigned int i=0;i<4;++i) {
+    size_t pos=out.find(",");
+    vec[i]=ToType<double>(out.substr(0,pos));
+    if (pos!=std::string::npos) out=out.substr(pos+1);
+    else out="";
+  }
+  if (out.length()>0)
+    THROW(critical_error,"Vector is not a four vector.");
+  return s;
+}
+
+std::istream& ATOOLS::operator>>(std::istream& s,Vec3D& vec)
+{
+  std::string out;
+  s>>out;
+  if (out.length()==0 || out[0]!='(' || out[out.length()-1]!=')')
+    THROW(critical_error,"String to vector translation failed.");
+  out=out.substr(0,out.length()-1).substr(1);
+  for (short unsigned int i=0;i<3;++i) {
+    size_t pos=out.find(",");
+    vec[i]=ToType<double>(out.substr(0,pos));
+    if (pos!=std::string::npos) out=out.substr(pos+1);
+    else out="";
+  }
+  if (out.length()>0)
+    THROW(critical_error,"Vector is not a three vector.");
+  return s;
+}
+
 bool ATOOLS::operator==(const Vec4D& v1, const Vec4D& v2) 
 {
   double maxp=Max(v1[0],Max(v1[1],Max(v1[2],v1[3]))); 
@@ -65,4 +101,35 @@ const double Vec4D::PPerp2(const Vec4D &ref) const
 { 
   return sqr(PPerp(ref)); 
 }
+  
+const double Vec4D::Theta(const Vec4D &ref) const 
+{ 
+  return acos(CosTheta(ref));
+}
+  
+const double Vec4D::Theta() const 
+{
+  return acos(CosTheta());
+}
+
+const double Vec4D::CosDPhi(const Vec4D &ref) const 
+{ 
+  Vec3D pref=Vec3D(ref[1],ref[2],0.0), p=Vec3D(m_x[1],m_x[2],0.0);
+  return Max(Min(pref*p/(pref.Abs()*p.Abs()),1.0),-1.0);
+}
+
+const double Vec4D::CosTheta(const Vec4D &ref) const 
+{ 
+  Vec3D pref=Vec3D(ref), p=Vec3D(*this);
+  return Max(Min(pref*p/(pref.Abs()*p.Abs()),1.0),-1.0);
+}
+
+const double Vec4D::Eta(const Vec4D &ref) const 
+{ 
+  double cos=CosTheta(ref);
+  return (2.0*cos>M_PI?-0.5:0.5)*log(sqr(1.0+cos)/(1.0-cos*cos));
+}
+  
+  
+  
   
