f950ab175b4c476f92b27b249c7301e7cb581884
timo <sherpa@projects.hepforge.org>
2005-05-30 13:27:30 +0000

    New Random Generator implemented

--- ATOOLS-2.0/Math/Random.C
+++ ATOOLS-2.0/Math/Random.C
@@ -4,6 +4,12 @@
 #include "MyStrStream.H"
 #include <iostream>
 
+#ifdef PROFILE__all
+#include "prof.hh"
+#else
+#define PROFILE_HERE
+#endif
+
 using namespace ATOOLS;
 using namespace std;
 
@@ -24,7 +30,8 @@ using namespace std;
 #define EPS 1.2e-7
 #define RNMX (1.0-EPS)
 
-ATOOLS::Random ATOOLS::ran(1234);
+ATOOLS::Random ATOOLS::ran(1234, 4321);
+
 
 Random::Random(long nid): 
   p_outstream(NULL) 
@@ -33,6 +40,7 @@ Random::Random(long nid):
   Exception_Handler::AddTerminatorObject(this);
   SaveStatus();
 }
+
 
 Random::~Random() 
 { 
@@ -51,6 +59,7 @@ static long siv[NTAB];
 
 double Random::Ran2(long *idum)
 {
+  PROFILE_HERE;
   int   j;
   long  k;
   double temp;
@@ -104,6 +113,7 @@ double Random::Ran2(long *idum)
 #define EPS 1.2e-7
 #define RNMX (1.0-EPS)
 
+
 double Random::Ran1(long *idum)
 {
   int j;
@@ -145,6 +155,7 @@ double Random::Ran1(long *idum)
 #define MZ 0
 #define FAC (1.0/MBIG)
 
+
 void Random::InitRan3(long *idum)
 {
   long mj,mk;
@@ -170,6 +181,7 @@ void Random::InitRan3(long *idum)
   m_inextp=31;
 }
 
+
 double Random::Ran3()
 {
   if (++m_inext == 56) m_inext=1;
@@ -187,7 +199,10 @@ double Random::Ran3()
 
 
 int Random::WriteOutStatus(const char * filename){
+  // if Ran4 is the active Random Generator, then use its method
+  if (activeGenerator==4) {return WriteOutStatus4(filename);}
   // write out every Statusregister of Random Number generator
+
 
   //  sprintf(m_outname,"%s%i.dat",filename,m_written); 
   if ((p_outstream!=0) && (std::strcmp(filename,m_outname)!=0)) {
@@ -224,7 +239,20 @@ int Random::WriteOutStatus(const char * filename){
   return m_written++;
 }
 
+
 void Random::ReadInStatus(const char * filename, long int index){
+  // check what type of data is in target file
+  ifstream file(filename);
+      // Check if the first 20 bytes in file are a known identifier and
+      // set the activeGenerator variable accordingly
+      file.read((char*) &status.idTag, sizeof(status.idTag));
+      if (strcmp(status.idTag,  "Rnd4_G_Marsaglia"))
+      { activeGenerator = 2; } else { activeGenerator = 4; }
+  file.close();
+
+  // use readin method for the Generator identified Generator
+  if (activeGenerator==4) { ReadInStatus4(filename, index);} else {
+
   // read in every Statusregister of Random Number generator
   msg_Info()<<"Random::ReadInStatus from "<<filename<<" index "<<index<<endl;
   std::ifstream myinstream(filename);
@@ -246,7 +274,9 @@ void Random::ReadInStatus(const char * filename, long int index){
     myinstream.close();
   } 
   else msg.Error()<<"ERROR in Random::ReadInStatus : "<<filename<<" not found!!"<<endl;
+  }
 }
+
 
 double Random::GetNZ() 
 {
@@ -255,6 +285,7 @@ double Random::GetNZ()
   return ran1;
 }
 
+
 void Random::SetSeed(long int nid) 
 {
   m_id = nid<0 ? nid : -nid;
@@ -262,10 +293,13 @@ void Random::SetSeed(long int nid)
   m_written=0;    
   p_outstream=0;
   std::strcpy(m_outname,"");
+  activeGenerator = 2;
 }
+
 
 void Random::SaveStatus()
 {
+  if (activeGenerator==4) { return SaveStatus4(); };
   m_sid=m_id; 
   m_sinext=m_inext; 
   m_sinextp=m_inextp;
@@ -275,8 +309,10 @@ void Random::SaveStatus()
   for (int i=0;i<NTAB;++i) siv[i]=iv[i];
 }
 
+
 void Random::RestoreStatus()
 {
+  if (activeGenerator==4) { return RestoreStatus4(); };
   m_id=m_sid; 
   m_inext=m_sinext; 
   m_inextp=m_sinextp;
@@ -285,6 +321,7 @@ void Random::RestoreStatus()
   idum2=sidum2;
   for (int i=0;i<NTAB;++i) iv[i]=siv[i];
 }
+
 
 void Random::PrepareTerminate()
 {
@@ -305,3 +342,159 @@ void Random::PrepareTerminate()
   WriteOutStatus((name+ATOOLS::ToString(i)+
 		  std::string(".random")).c_str());
 }
+
+// ----------------- Methods for new Random Number Generator -------------
+
+/*
+   This is the random number generator proposed by George Marsaglia in
+   Florida State University Report: FSU-SCRI-87-50
+*/
+/*
+   This is the initialization routine for the random number generator.
+   NOTE: The seed variables can have values between:    0 <= IJ <= 31328
+                                                        0 <= KL <= 30081
+   The random number sequences created by these two seeds are of sufficient
+   length to complete an entire calculation with. For example, if sveral
+   different groups are working on different parts of the same calculation,
+   each group could be assigned its own IJ seed. This would leave each group
+   with 30000 choices for the second seed. That is to say, this random
+   number generator can create 900 million different subsequences -- with
+   each subsequence having a length of approximately 10^30.
+*/
+Random::Random(int ij,int kl) : p_outstream(NULL)
+{  
+   SetSeed(ij, kl); 
+   Exception_Handler::AddTerminatorObject(this);
+   SaveStatus(); 
+};
+
+
+void Random::SetSeed(int ij, int kl)
+{
+   // mark Generator 4 as used one and set idTag for file output
+   activeGenerator = 4;
+   strcpy(status.idTag, "Rnd4_G_Marsaglia");
+
+   m_written=0;    
+   p_outstream=0;
+   std::strcpy(m_outname,"");
+
+   // Init routine of the Random Generator Rnd4
+   double s,t;
+   int ii,i,j,k,l,jj,m;
+
+   /*  Handle the seed range errors
+         First random number seed must be between 0 and 31328
+         Second seed must have a value between 0 and 30081    */
+   if (ij < 0 || ij > 31328 || kl < 0 || kl > 30081) {
+		ij = 1802;
+		kl = 9373;
+   }
+
+   i = (ij / 177) % 177 + 2;
+   j = (ij % 177)       + 2;
+   k = (kl / 169) % 178 + 1;
+   l = (kl % 169);
+
+   for (ii=0; ii<97; ii++) {
+      s = 0.0;
+      t = 0.5;
+      for (jj=0; jj<24; jj++) {
+         m = (((i * j) % 179) * k) % 179;
+         i = j;
+         j = k;
+         k = m;
+         l = (53 * l + 1) % 169;
+         if (((l * m % 64)) >= 32)
+            s += t;
+         t *= 0.5;
+      }
+      status.u[ii] = s;
+   }
+
+   status.c    = 362436.0 / 16777216.0;
+   status.cd   = 7654321.0 / 16777216.0;
+   status.cm   = 16777213.0 / 16777216.0;
+   status.i97  = 97;
+   status.j97  = 33;
+}
+
+
+double Random::Ran4()
+{
+  PROFILE_HERE;
+   double uni;
+
+   uni = status.u[status.i97-1] - status.u[status.j97-1];
+   if (uni <= 0.0)
+      ++uni;
+   status.u[status.i97-1] = uni;
+   //   --status.i97;
+   if (--status.i97 == 0)
+      status.i97 = 97;
+   // --status.j97;
+   if (--status.j97 == 0)
+      status.j97 = 97;
+   status.c -= status.cd;
+   if (status.c < 0.0)
+      status.c += status.cm;
+   uni -= status.c;
+   if (uni < 0.0)
+      ++uni;
+
+   // ** the following check is for debug purposes only
+   if ((uni<0.)||(uni>1.0))
+       { PRINT_INFO("ERROR in Random Generator; number created is"); cout << uni << endl;} 
+   return(uni);
+}
+
+
+int Random::WriteOutStatus4(const char * filename)
+{
+  // dunno what this is good for - kept from old routine
+  if ((p_outstream!=0) && (std::strcmp(filename,m_outname)!=0)) {
+    p_outstream->close();
+    p_outstream = 0;
+  }
+  if (p_outstream == 0){
+    msg_Tracking()<<"Random::WriteOutStatus4 : Saving Random Number Generator Status to "<<filename<<endl;
+
+    // open file and append status of Random Generator at the end if possible
+    std::ofstream file(filename, ios::binary | ios::app);
+    // ** possibly check if file is really a RandGen Writeout-file
+
+    // if file is ok, append data at EoF and return
+    if (file.good()) { file.write((char*) (&status), sizeof(Ran4Status));
+                       return file.tellp()/sizeof(Ran4Status); }    
+    else {
+    // file not ok for some reason: Warn user and return 0 
+       msg_Tracking()<< "Random::WriteOutStatus4 : WARNING: Output file was not OK";
+       return 0; }   
+  }
+  return 0;
+}
+
+
+void Random::ReadInStatus4(const char * filename, long int index)
+{
+  msg_Info()<<"Random::ReadInStatus from "<<filename<<" index "<<index<<endl;
+
+  std::ifstream file(filename, ios::binary);
+  if (file.good()) {
+    // if the infile is ok, try to read in Status
+    file.seekg(index*sizeof(Ran4Status));
+    file.read((char*) (&status), sizeof(Ran4Status));
+
+    if (strcmp(status.idTag, "Rnd4_G_Marsaglia")) {
+      // Data read in was not from a RndGen of the same type
+      msg.Error()<<"WARNING in Random::ReadInStatus4: Data read from "<<filename;
+      msg.Error()<<" at Position "<<index<< " is not of the expected type."<<endl;
+    }
+  }  
+  else 
+    msg.Error()<<"ERROR in Random::ReadInStatus4 : "<<filename<<" not found!!"<<endl;
+}
+
+
+void Random::SaveStatus4() { backupStat = status; };
+void Random::RestoreStatus4() { status = backupStat; };
