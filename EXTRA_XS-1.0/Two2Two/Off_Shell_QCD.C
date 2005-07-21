4a72eabe64c27bb7077d1a4bd1bb02001fc79a65
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-07-21 14:57:04 +0000

    new form of gg->ng bfkl me

--- EXTRA_XS-1.0/Two2Two/Off_Shell_QCD.C
+++ EXTRA_XS-1.0/Two2Two/Off_Shell_QCD.C
@@ -6,11 +6,17 @@
 #include "ISR_Handler.H"
 #include "Data_Reader.H"
 #include "Random.H"
+#include "Running_AlphaS.H"
 #include "Flow.H"
+#include "MathTools.H"
+#include "Combined_Selector.H"
+#include "Standard_Selector.H"
 
 #define NC 3
 
 using namespace EXTRAXS;
+using namespace ATOOLS;
+using namespace MODEL;
 
 template <> 
 Single_XS *Single_XS::GetProcess<Off_Shell_gg_qqb>(const size_t nin,const size_t nout,
@@ -44,16 +50,23 @@ Off_Shell_gg_qqb::Off_Shell_gg_qqb(const size_t nin,const size_t nout,
 double Off_Shell_gg_qqb::operator()(double s,double t,double u) 
 {
   ATOOLS::Vec4D *p=p_momenta;
-  double S=p[4]*p[5], M2=p[2].Abs2();
-  double a3=p[4]*p[2], a4=p[4]*p[3];
-  double b3=p[5]*p[2], b4=p[5]*p[3];
+  double S2=p[4]*p[5], M2=p[2].Abs2();
+  double a3=p[5]*p[2], a4=p[5]*p[3];
+  double b3=p[4]*p[2], b4=p[4]*p[3];
   double k12=-p[0].Abs2(), k22=-p[1].Abs2();
-  double delta=2.*S*(2.*(a4*b3-a3*b4)/S-k12*a3/(p[4]*p[1])+k22*b3/(p[5]*p[0])+p[2]*(p[0]-p[1]));
-  double abelian=4.*(S*S/((t-M2)*(u-M2))-1./(k12*k22)*ATOOLS::sqr(S+2.*(a3*b4/(t-M2)+b3*a4/(u-M2))));
-  double nonabelian=4.*S*S*(S/(s*(p[4]*p[1])*(p[5]*p[0]))-1./((t-M2)*(u-M2))
-			    -(1/(t-M2)-1/(u-M2))/s*(a4/(p[4]*p[1])-b4/(p[5]*p[0])))
-    +2./(k12*k22)*(S+4.*a3*b4/(t-M2)-delta/s)*(S+4.*a4*b3/(u-M2)+delta/s);
-  return ATOOLS::sqr(4.*M_PI*m_alphas*(p[0]*p[5])*(p[1]*p[4])/(S*S))*
+  std::swap<double>(t,u);
+  double delta=
+    2.*S2*(2.*(a4*b3-a3*b4)/S2
+	  -k12*b4/(p[4]*p[1])+k22*a4/(p[5]*p[0])+p[3]*(p[0]-p[1]));
+  double abelian=
+    4.*(S2*S2/((t-M2)*(u-M2))
+	-1./(k12*k22)*ATOOLS::sqr(S2+2.*(a3*b4/(t-M2)+b3*a4/(u-M2))));
+  double nonabelian=
+    4.*S2*S2*(S2/(s*(p[4]*p[1])*(p[5]*p[0]))-1./((t-M2)*(u-M2))
+	    -(1/(t-M2)-1/(u-M2))/s*(b3/(p[4]*p[1])-a3/(p[5]*p[0])))
+    +2./(k12*k22)*(S2+4.*a3*b4/(t-M2)-delta/s)*(S2+4.*a4*b3/(u-M2)+delta/s);
+  std::swap<double>(t,u);
+  return ATOOLS::sqr(4.*M_PI*m_alphas*(p[0]*p[5])*(p[1]*p[4])/(S2*S2))*
     (abelian/(2.*NC)+nonabelian*NC/(2.*(NC*NC-1.)));
 }
 
@@ -84,12 +97,9 @@ Single_XS *Single_XS::GetProcess<Off_Shell_gg_gg>(const size_t nin,const size_t 
 						  const ATOOLS::Flavour *flavours,
 					      const size_t nqed, const size_t nqcd)
 {
-  if (flavours[2].IsGluon() && flavours[3].IsGluon() && 
-      flavours[0].IsGluon() && flavours[1].IsGluon()){ 
-    if (nqcd==2 && nqed==0) {
-      return new Off_Shell_gg_gg(nin,nout,flavours); 
-    }
-  }
+  for (size_t i=0;i<nin+nout;++i) if (!flavours[i].IsGluon()) return NULL;
+  if (nqcd==nin+nout-2 && nqed==0) 
+    return new Off_Shell_gg_gg(nin,nout,flavours); 
   return NULL;
 }
 
@@ -110,94 +120,46 @@ Off_Shell_gg_gg::Off_Shell_gg_gg(const size_t nin,const size_t nout,
   ATOOLS::Data_Reader *reader = new ATOOLS::Data_Reader();
   if (!reader->ReadFromFile(m_jets,"LIPATOV_JETS","")) m_jets=2;
   delete reader;
-  m_nstrong=4;
+  m_nstrong=m_nin+m_nout;
 }
-
-#include "MyComplex.H"
-
-Complex App(const ATOOLS::Vec4D *p)
-{
-  double kp[4];
-  Complex kt[4], qt[3];
-  for (short unsigned int i=2;i<4;++i) {
-    kp[i]=p[i].PPlus();
-    double spt=p[i].PPerp();
-    kt[i]=Complex(spt*p[i].CosPhi(),spt*p[i].SinPhi());
-    ATOOLS::Vec4D pi=p[i-2];
-    if (i==3) pi=-1.0*pi;
-    spt=pi.PPerp();
-    qt[i-1]=Complex(spt*pi.CosPhi(),spt*pi.SinPhi());
-  }
-  Complex ab23=sqrt(kp[3]/kp[2])*kt[2]-sqrt(kp[2]/kp[3])*kt[3];
-  return 2.0*std::conj(qt[1])*qt[2]/kt[2]*sqrt(kp[2]/kp[3])/ab23;
-}
-
-Complex Apm(const ATOOLS::Vec4D *p)
-{
-  double kp[4], km[4];
-  Complex kt[4], qt[3];
-  for (short unsigned int i=2;i<4;++i) {
-    kp[i]=p[i].PPlus();
-    km[i]=p[i].PMinus();
-    double spt=p[i].PPerp();
-    kt[i]=Complex(spt*p[i].CosPhi(),spt*p[i].SinPhi());
-    ATOOLS::Vec4D pi=p[i-2];
-    if (i==3) pi=-1.0*pi;
-    spt=pi.PPerp();
-    qt[i-1]=Complex(spt*pi.CosPhi(),spt*pi.SinPhi());
-  }
-  double qt12=p[0].PPerp2(), qt22=p[1].PPerp2();
-  double s23=(p[2]+p[3]).Abs2();
-  double s3bb=(p[3]-p[1]).Abs2();
-  return -2.0*std::conj(kt[2])/kt[2]*
-    (-1.0/s23*(kt[3]*kt[3]*qt12/((km[2]+km[3])*kp[3])+
-	       kt[2]*kt[2]*qt22/((kp[2]+kp[3])*km[2])+
-	       s3bb*kt[2]*kt[3]/(km[2]*kp[3]))
-     +(qt[2]+kt[3])*(qt[2]+kt[3])/s3bb
-     -(qt[2]+kt[3])/s23*((km[2]+km[3])/km[2]*kt[2]-
-			 (kp[2]+kp[3])/kp[3]*kt[3]));
-}
-
-Complex Lpp(const ATOOLS::Vec4D *p)
-{
-  double kp[4], km[4];
-  Complex kt[4], qt[3];
-  for (short unsigned int i=2;i<4;++i) {
-    kp[i]=p[i].PPlus();
-    km[i]=p[i].PMinus();
-    double spt=p[i].PPerp();
-    kt[i]=Complex(spt*p[i].CosPhi(),spt*p[i].SinPhi());
-    ATOOLS::Vec4D pi=p[i-2];
-    if (i==3) pi=-1.0*pi;
-    spt=pi.PPerp();
-    qt[i-1]=Complex(spt*pi.CosPhi(),spt*pi.SinPhi());
-  }
-  return 2.0*std::conj(qt[1])*(p[0]-p[2]).PPerp2()/(p[0]-p[2]).Abs2()*qt[2]
-    /kt[2]/kt[3];
-}
-
-// #define USING__NLO_LEV
 
 double Off_Shell_gg_gg::operator()(double s,double t,double u) 
 {
   ATOOLS::Vec4D *const p=p_momenta;
-#ifdef USING__NLO_LEV
-  // nlo lipatov vertex
-  Complex app34=App(p);
-  Complex apm34=Apm(p);
-  std::swap<ATOOLS::Vec4D>(p[2],p[3]);
-  Complex app43=App(p);
-  Complex amp43=Apm(p);
-  std::swap<ATOOLS::Vec4D>(p[2],p[3]);
-  double Mpp=std::abs(app34*std::conj(app34)+app43*std::conj(app43)
-		      +0.5*(app34*std::conj(app43)+std::conj(app34)*app43));
-  double Mpm=std::abs(apm34*std::conj(apm34)+amp43*std::conj(amp43));
-  double M=2.0*(Mpp+Mpm);
-#else
-  Complex lpp34=Lpp(p);
-  double M=2.0*(2.0*std::abs(lpp34*std::conj(lpp34)));
-#endif
-  return ATOOLS::sqr(4.0*M_PI*m_alphas)*(NC*NC)/(NC*NC-1)/(2.0*4.0)*M;
+  double colfac=(NC*NC)/(NC*NC-1)/(2.0*4.0);
+  for (size_t i=0;i<m_nout-2;++i) colfac*=NC/4.0;
+  double scale=sqrt((p[m_nin+m_nout]-p[0]).PPerp2()*
+		    (p[m_nin+m_nout+1]-p[1]).PPerp2());
+  double asmean((*MODEL::as)(scale));
+  if (p[0].PPerp()<m_qtcut || p[1].PPerp()<m_qtcut) return 0.0;
+  double lasty((p[m_nin+m_nout]-p[0]).Y());
+  Vec4D prop(p[0]);
+  if (p_selector->Name()=="Combined_Selector") {
+    Selector_Base *ipt = 
+      ((Combined_Selector*)p_selector)->GetSelector("IPT_Selector");
+    if (ipt==NULL) THROW(fatal_error,"BFKL ME needs internal pt selector");
+    if (!ipt->GetValue("qcut",m_qtcut)) 
+      THROW(fatal_error,"Selector corrupted");
+  }
+  double M=4.0*sqr((p[0]+p[1]).Abs2());
+  for (size_t i=0;i<m_nout;++i) {
+    double y(p[m_nin+i].Y());
+    if (y>=lasty) return 0.0;
+    M*=2/p[m_nin+i].PPerp2();
+    if (i>0) {
+      double sud(exp(-asmean*(lasty-y)*log(prop.PPerp2()/m_qtcut)));
+      if (sud>1.0) 
+	msg.Error()<<"Off_Shell_gg_gg::operator()(..):"
+		   <<"Sudakov "<<i<<" exceeds unity, \\Delta = "
+		   <<sud<<std::endl;
+      M*=sud;
+    }
+    prop-=p[m_nin+i];
+    lasty=y;
+  }
+  double y((p[m_nin+m_nout+1]-p[1]).Y());
+  if (y>=lasty) return 0.0;
+  return pow(4.0*M_PI*m_alphas,m_nout)*colfac*M;
 }
 
 bool Off_Shell_gg_gg::SetColours(double s,double t,double u) 
