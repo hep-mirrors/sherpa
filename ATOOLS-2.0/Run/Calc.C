7f68d9f204df02b9eafa36a51acb26c6304b8509
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-08-15 15:27:59 +0000

    changed output levels

--- ATOOLS-2.0/Run/Calc.C
+++ ATOOLS-2.0/Run/Calc.C
@@ -37,7 +37,7 @@ int main(int argc,char **argv)
     }
     std::cout<<"Calc: interpreting formula -> "<<expr<<" = "
 	     <<interpreter.Interprete(expr)<<std::endl;
-    if (msg.LevelIsDebugging()) interpreter.PrintEquation();
+    if (msg.LevelIsTracking()) interpreter.PrintEquation();
     std::cout.precision(12);
     std::cout<<"Calc: recalculating tree   -> "<<expr<<" = "
 	     <<((TDouble*)interpreter.Calculate())->m_value<<std::endl;
