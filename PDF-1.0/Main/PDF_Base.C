1d6cbb2fd3ac4511d0ab1746136524340052e37b
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-09-14 11:35:23 +0000

    moved extract and reset to new recursive methods

--- PDF-1.0/Main/PDF_Base.C
+++ PDF-1.0/Main/PDF_Base.C
@@ -7,12 +7,10 @@ using namespace PDF;
 
 PDF_Base::PDF_Base():
   m_type("none"),
-  m_exponent(1.),
-  m_rescale(1.),
-  m_renormalization_scale_factor(1.)
+  m_exponent(1.), m_rescale(1.),
+  m_ren_scale_factor(1.)
 {
-  m_renormalization_scale_factor = ATOOLS::rpa.gen.RenormalizationScaleFactor();
-  //  std::cout<<" mu_R-fac in PDF:"<<m_renormalization_scale_factor;
+  m_ren_scale_factor = ATOOLS::rpa.gen.RenormalizationScaleFactor();
 }
 
 PDF_Base::~PDF_Base() {}
@@ -34,28 +32,25 @@ double PDF_Base::Cut(const std::string &type)
 
 void PDF_Base::Calculate(double x,double Q2)
 {
-  //  std::cout<<"PDF_Base::Calculate("<<Q2<<") Called"<<std::endl;
-  Calculate(x,0.,0.,Q2*m_renormalization_scale_factor);
+  Calculate(x,0.,0.,Q2*m_ren_scale_factor);
 }
 
-void PDF_Base::Extract(const ATOOLS::Flavour flavour,const double x) 
-{ 
-  PDF_Base *pdf=GetBasicPDF();
-  pdf->m_rescale-=x;
-  pdf->m_extracted.push_back(std::pair<ATOOLS::Flavour,double>(flavour,x));
-  for (std::vector<PDF_Base*>::iterator cit=pdf->m_copies.begin();
-       cit!=pdf->m_copies.end();++cit) {
-    (*cit)->Extract(flavour,x);
+void PDF_Base::SingleExtract(const ATOOLS::Flavour flavour,const double x) 
+{
+  m_rescale-=x;
+  m_extracted.push_back(std::pair<ATOOLS::Flavour,double>(flavour,x));
+  for (std::vector<PDF_Base*>::iterator cit(m_copies.begin());
+       cit!=m_copies.end();++cit) {
+    (*cit)->SingleExtract(flavour,x);
   }
 }
 
-void PDF_Base::Reset()
+void PDF_Base::SingleReset()
 { 
-  PDF_Base *pdf=GetBasicPDF();
-  pdf->m_rescale=1.;
-  pdf->m_extracted.clear();
-  for (std::vector<PDF_Base*>::iterator cit=pdf->m_copies.begin();
-       cit!=pdf->m_copies.end();++cit) {
-    (*cit)->Reset();
+  m_rescale=1.;
+  m_extracted.clear();
+  for (std::vector<PDF_Base*>::iterator cit(m_copies.begin());
+       cit!=m_copies.end();++cit) {
+    (*cit)->SingleReset();
   }
 }
