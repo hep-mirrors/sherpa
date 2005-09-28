73a88ea17dfe888ee7fdb4dac817f0a0e33518a8
Thomas Laubrich <sherpa@projects.hepforge.org>
2005-09-28 12:05:11 +0000

    cosmetics in notation of private attributes

--- HADRONS++-0.0/PS_Library/ResonanceFlavour.C
+++ HADRONS++-0.0/PS_Library/ResonanceFlavour.C
@@ -7,20 +7,20 @@ using namespace std;
 
 ResonanceFlavour::ResonanceFlavour( string _name, double _mass, double _width )
 {
-  name = _name;
-  mass = _mass;
-  width = _width;
+  m_name = _name;
+  m_mass = _mass;
+  m_width = _width;
 }
 ResonanceFlavour::ResonanceFlavour( kf::code _kfc, double _mass, double _width )
 {
-  name = Flavour(_kfc).Name();
-  mass = _mass;
-  width = _width;
+  m_name = Flavour(_kfc).Name();
+  m_mass = _mass;
+  m_width = _width;
 }
  
 void ResonanceFlavour::Set( kf::code _kfc, double _mass, double _width )
 {
-  name = Flavour(_kfc).Name();
-  mass = _mass;
-  width = _width;
+  m_name = Flavour(_kfc).Name();
+  m_mass = _mass;
+  m_width = _width;
 }
