a6279efb0c00a73dbc98cc0b81720fee8b398e15
Frank Krauss <frank.krauss@durham.ac.uk>
2005-11-23 12:45:12 +0000

    First b decays

---
+++ HADRONS++-0.0/ME_Library/B_Meson_Decay_MEs.C
@@ -0,0 +1,86 @@
+#include "B_Meson_Decay_MEs.H"
+#include "Message.H"
+#include "XYZFuncs.H"
+#include "Histogram.H"
+#include "Traces.H"
+
+using namespace HADRONS;
+using namespace ATOOLS;
+using namespace std;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//  leptonic decay  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Semileptonic_B_Meson::Semileptonic_B_Meson( int _nout, Flavour *_fl ) :
+  HD_ME_Base(_nout,_fl),
+  m_had(-1),m_nu(-1),m_lep(-1)
+{
+  m_metype = string("Semileptonic_B_Meson");
+  for( int i=1; i<4; i++ ) {
+    if( p_flavs[i].Kfcode() == kf::e ||
+	p_flavs[i].Kfcode() == kf::mu ||
+	p_flavs[i].Kfcode() == kf::tau ) m_lep = i; // that's the lepton
+    else if( p_flavs[i].Kfcode() == kf::nue ||
+	     p_flavs[i].Kfcode() == kf::numu ||
+	     p_flavs[i].Kfcode() == kf::nutau ) m_nu = i; // that's the lepton
+    else m_had = i;
+  }
+  m_Bmass   = p_flavs[0].Mass();
+  m_hadmass = p_flavs[m_had].Mass();
+}
+ 
+void Semileptonic_B_Meson::SetModelParameters( GeneralModel _md ) 
+{ 
+}
+ 
+double Semileptonic_B_Meson::operator()( const Vec4D *_p )
+{
+  double  T  = Using_Hels(_p);
+  return T;
+}
+
+double Semileptonic_B_Meson::Using_Hels( const Vec4D * _p )
+{
+  Complex FFplus  = Formfactor_plus(_p);
+  Complex FFminus = Formfactor_minus(_p);
+  XYZFunc F(m_nout,_p,p_flavs);
+  double ret = 0.;
+  for( int h=0; h<4; h++ ) {				// only nulep and lep have a helicity !
+    ret += norm(( F.X(m_lep,0,m_nu,h,0.,1.)/m_Bmass +
+		  F.X(m_lep,m_had,m_nu,h,0.,1.)/m_hadmass)*FFplus +
+		( F.X(m_lep,0,m_nu,h,0.,1.)/m_Bmass - 
+		  F.X(m_lep,m_had,m_nu,h,0.,1.)/m_hadmass)*FFminus);
+  }
+  F.Delete();
+  return ret;
+} 
+ 
+Complex Semileptonic_B_Meson::Formfactor_plus( const Vec4D *_p )
+{
+  return Complex(0.,0.);
+}
+
+Complex Semileptonic_B_Meson::Formfactor_minus( const Vec4D *_p )
+{
+  double vvprime = _p[0]*_p[m_had]/(m_Bmass*m_hadmass);
+  double xi      = 1.-0.7*(vvprime-1.);
+  return Complex(xi,0.);
+}
+
+double Semileptonic_B_Meson::Using_Traces_Simple( const Vec4D *_p )
+{
+  return 0.;
+}
+
+double Semileptonic_B_Meson::Using_Traces( const Vec4D *_p )
+{
+  return 0.;
+}
+
+double Semileptonic_B_Meson::Using_Traces_2( const Vec4D *_p )
+{
+  return 0.;
+}
+
+
--- HADRONS++-0.0/ME_Library/HD_ME_Selector.C
+++ HADRONS++-0.0/ME_Library/HD_ME_Selector.C
@@ -2,6 +2,7 @@
 #include "HD_ME_Selector.H"
 #include "Message.H"
 
+#include "B_Meson_Decay_MEs.H"
 #include "Tau_Decay_MEs.H"
 #include "Two_Body_MEs.H"
 #include "Three_Body_MEs.H"
@@ -32,24 +33,29 @@ HD_ME_Base * HD_ME_Selector::GetME(int nin,int nout,Flavour * flavs,
 
   // Select ME depending on decaying particle
   switch (flavs[0]) {
-	case (kf::mu):
- 	case (kf::tau):
-	  SelectTauDecay(nout,flavs,hdme);
- 	  break;
-	case (kf::pi):
-	case (kf::K_plus):
-	case (kf::eta):
-	case (kf::eta_prime_958):
-	  SelectLightPseudoScalarDecay(nout,flavs,hdme);
-	  break;
-	case (kf::rho_770):
-	case (kf::rho_770_plus):
-	case (kf::omega_782):
-	  //  case (kf::Kstar_892):
-	  //  case (kf::Kstar_892_plus):
-	case (kf::phi_1020):
-	  //SelectLightVectorDecay(nout,flavs,hdme);
-	  break;
+  case (kf::mu):
+  case (kf::tau):
+    SelectTauDecay(nout,flavs,hdme);
+    break;
+  case (kf::pi):
+  case (kf::K_plus):
+  case (kf::eta):
+  case (kf::eta_prime_958):
+    SelectLightPseudoScalarDecay(nout,flavs,hdme);
+    break;
+  case (kf::rho_770):
+  case (kf::rho_770_plus):
+  case (kf::omega_782):
+    //  case (kf::Kstar_892):
+    //  case (kf::Kstar_892_plus):
+  case (kf::phi_1020):
+    //SelectLightVectorDecay(nout,flavs,hdme);
+    break;
+  case (kf::B):
+  case (kf::B_plus):
+  case (kf::B_s):
+    SelectBMesonDecay(nout,flavs,hdme);
+    break;
   }
 
   if (hdme==NULL) hdme = new Isotropic(nout,flavs);
@@ -57,120 +63,140 @@ HD_ME_Base * HD_ME_Selector::GetME(int nin,int nout,Flavour * flavs,
 }
 
 
+// Select the corresponding B meson decay ME
+void HD_ME_Selector::SelectBMesonDecay(int nout,Flavour * flavs,
+				       HD_ME_Base *& hdme )
+{
+  std::cout<<"HD_ME_Selector::SelectBMesonDecay 1->"<<nout<<endl;
+  switch( nout ) {
+  case 3: {
+    int nLep(0), nHeavy(0), nLight(0);
+    for( int i=1; i<4; i++ ) {
+      if( flavs[i].IsLepton() ) nLep++;
+    }
+    std::cout<<"Semileptonic"<<std::endl;
+    if (nLep==2) hdme = new Semileptonic_B_Meson( nout, flavs ); 
+    break;
+  }
+  default: 
+    msg.Error()<<nout<<"-body decays of B's do not have any ME yet."<<std::endl;
+    abort();
+  }
+}
+
 // Select the corresponding tau decay ME
-void HD_ME_Selector::SelectTauDecay(
-	int nout,Flavour * flavs,
-	HD_ME_Base *& hdme )
+void HD_ME_Selector::SelectTauDecay(int nout,Flavour * flavs,
+				    HD_ME_Base *& hdme )
 {
   msg_Tracking()<<"HD_ME_Selector::SelectTauDecay 1->"<<nout<<endl;
   switch( nout ) {
-	case 2:
-	  {
-		int nPion(0), nKaon(0);
-		for( int i=1; i<3; i++ ) {
-		  if( flavs[i].Kfcode() == kf::pi_plus ) nPion++;
-		  if( flavs[i].Kfcode() == kf::K_plus ) nKaon++;
-		}
-		if( nPion == 1 ) {
-		  hdme = new Tau_Pseudo( nout, flavs );
-		}
-		if( nKaon == 1 ) {
-		  hdme = new Tau_Pseudo( nout, flavs );
-		}
-		break;
-	  }
-	case 3:
-	  {
-		int nLep(0), nPion(0), nKaon(0);
-		for( int i=1; i<4; i++ ) {
-		  if( flavs[i].IsLepton() ) nLep++;
-		  if( flavs[i].Kfcode() == kf::pi_plus ||
-			  flavs[i].Kfcode() == kf::pi ) nPion++;
-		  if( flavs[i].Kfcode() == kf::K_plus ||
-			  flavs[i].Kfcode() == kf::K ) nKaon++;
-		}
-		if( nLep == 3 ) {
-		  hdme = new Tau_Lepton( nout, flavs ); 
-		}
-		if( nPion == 2 ) {
-		  hdme = new Tau_Two_Pion( nout, flavs );
-		}
-		if( nKaon == 2 ) {
-		  hdme = new Tau_Two_Pion( nout, flavs );
-		}
-		break;
-	  }
-	case 4:
-	  {
-		int nPseudo(0), nEta(0);
-		// count number of pseudoscalars
-		for( int i=1; i<5; i++ ) {
-		  if( flavs[i].Kfcode() == kf::pi_plus ||
-			  flavs[i].Kfcode() == kf::pi ||
-			  flavs[i].Kfcode() == kf::K_plus ||
-			  flavs[i].Kfcode() == kf::K ) nPseudo++;
-		  if( flavs[i].Kfcode() == kf::eta ) nEta++;  
-		}
-		if( nPseudo==3 ) 
-		  hdme = new Tau_Three_Pseudo( nout, flavs );
-		if( nEta==1 && nPseudo==2 ) 
-		  hdme = new Tau_Eta_Two_Pion( nout, flavs );
+  case 2:
+    {
+      int nPion(0), nKaon(0);
+      for( int i=1; i<3; i++ ) {
+	if( flavs[i].Kfcode() == kf::pi_plus ) nPion++;
+	if( flavs[i].Kfcode() == kf::K_plus ) nKaon++;
+      }
+      if( nPion == 1 ) {
+	hdme = new Tau_Pseudo( nout, flavs );
+      }
+      if( nKaon == 1 ) {
+	hdme = new Tau_Pseudo( nout, flavs );
+      }
+      break;
+    }
+  case 3:
+    {
+      int nLep(0), nPion(0), nKaon(0);
+      for( int i=1; i<4; i++ ) {
+	if( flavs[i].IsLepton() ) nLep++;
+	if( flavs[i].Kfcode() == kf::pi_plus ||
+	    flavs[i].Kfcode() == kf::pi ) nPion++;
+	if( flavs[i].Kfcode() == kf::K_plus ||
+	    flavs[i].Kfcode() == kf::K ) nKaon++;
+      }
+      if( nLep == 3 ) {
+	hdme = new Tau_Lepton( nout, flavs ); 
+      }
+      if( nPion == 2 ) {
+	hdme = new Tau_Two_Pion( nout, flavs );
+      }
+      if( nKaon == 2 ) {
+	hdme = new Tau_Two_Pion( nout, flavs );
+      }
+      break;
+    }
+  case 4:
+    {
+      int nPseudo(0), nEta(0);
+      // count number of pseudoscalars
+      for( int i=1; i<5; i++ ) {
+	if( flavs[i].Kfcode() == kf::pi_plus ||
+	    flavs[i].Kfcode() == kf::pi ||
+	    flavs[i].Kfcode() == kf::K_plus ||
+	    flavs[i].Kfcode() == kf::K ) nPseudo++;
+	if( flavs[i].Kfcode() == kf::eta ) nEta++;  
+      }
+      if( nPseudo==3 ) 
+	hdme = new Tau_Three_Pseudo( nout, flavs );
+      if( nEta==1 && nPseudo==2 ) 
+	hdme = new Tau_Eta_Two_Pion( nout, flavs );
 
-	  }
-	case 5:
-	  {
-		int nPion_ch (0), nPion_0 (0);
-		// count number of pions
-		for( int i=1; i<6; i++ ) {
-		  if( flavs[i].Kfcode() == kf::pi_plus ) nPion_ch++;
-		  if( flavs[i].Kfcode() == kf::pi )      nPion_0++;
-		}
-		if( nPion_ch==3 && nPion_0==1 ) {
-		  hdme = new Tau_Four_Pion_3( nout, flavs );
-		}
-		if( nPion_ch==1 && nPion_0==3 ) {
-		  hdme = new Tau_Four_Pion_1( nout, flavs );
-		}
-	  }
+    }
+  case 5:
+    {
+      int nPion_ch (0), nPion_0 (0);
+      // count number of pions
+      for( int i=1; i<6; i++ ) {
+	if( flavs[i].Kfcode() == kf::pi_plus ) nPion_ch++;
+	if( flavs[i].Kfcode() == kf::pi )      nPion_0++;
+      }
+      if( nPion_ch==3 && nPion_0==1 ) {
+	hdme = new Tau_Four_Pion_3( nout, flavs );
+      }
+      if( nPion_ch==1 && nPion_0==3 ) {
+	hdme = new Tau_Four_Pion_1( nout, flavs );
+      }
+    }
   }
 }
 
 // Select the corresponding ME of light pseudoscalar decay
 void HD_ME_Selector::SelectLightPseudoScalarDecay(
-	int nout,Flavour * flavs,
-	HD_ME_Base *& hdme)
+						  int nout,Flavour * flavs,
+						  HD_ME_Base *& hdme)
 {
   msg_Tracking()<<"HD_ME_Selector::SelectLightPseudoScalarDecay 1->"<<nout<<endl;
   switch (nout) {
-	case 2:
-	  if (flavs[1]==Flavour(kf::photon) &&
-		  flavs[2]==Flavour(kf::photon)) 
-		hdme = new P_2Gamma(nout,flavs);
-	  break;
-	case 3: 
-	  if (flavs[1].IsLepton() &&
-		  flavs[2].IsLepton() && flavs[2].IsAnti() &&
-		  flavs[3]==Flavour(kf::photon)) {
-		hdme = new P_GammaFF(nout,flavs);
-	  }
-	  if (IsPseudoScalar(flavs[1]) &&
-		  flavs[2]==Flavour(kf::photon) &&
-		  flavs[3]==Flavour(kf::photon)) { 
-		hdme = new P_P2Gamma(nout,flavs);
-	  }
-	  if (flavs[1]==Flavour(kf::pi_plus) &&
-		  flavs[2]==Flavour(kf::pi_plus).Bar() &&
-		  flavs[3]==Flavour(kf::photon)) {
-		hdme = new P_2PGamma(nout,flavs);
-		SetVector_For_2PS(nout,1,2,flavs,hdme);
-	  }
-	  if (IsPseudoScalar(flavs[1]) &&
-		  IsPseudoScalar(flavs[2]) &&
-		  IsPseudoScalar(flavs[3])) {
-		hdme = new P_3P_Dalitz(nout,flavs);
-		SetDalitz_For_3P(nout,flavs,hdme);
-	  }
-	  break;
+  case 2:
+    if (flavs[1]==Flavour(kf::photon) &&
+	flavs[2]==Flavour(kf::photon)) 
+      hdme = new P_2Gamma(nout,flavs);
+    break;
+  case 3: 
+    if (flavs[1].IsLepton() &&
+	flavs[2].IsLepton() && flavs[2].IsAnti() &&
+	flavs[3]==Flavour(kf::photon)) {
+      hdme = new P_GammaFF(nout,flavs);
+    }
+    if (IsPseudoScalar(flavs[1]) &&
+	flavs[2]==Flavour(kf::photon) &&
+	flavs[3]==Flavour(kf::photon)) { 
+      hdme = new P_P2Gamma(nout,flavs);
+    }
+    if (flavs[1]==Flavour(kf::pi_plus) &&
+	flavs[2]==Flavour(kf::pi_plus).Bar() &&
+	flavs[3]==Flavour(kf::photon)) {
+      hdme = new P_2PGamma(nout,flavs);
+      SetVector_For_2PS(nout,1,2,flavs,hdme);
+    }
+    if (IsPseudoScalar(flavs[1]) &&
+	IsPseudoScalar(flavs[2]) &&
+	IsPseudoScalar(flavs[3])) {
+      hdme = new P_3P_Dalitz(nout,flavs);
+      SetDalitz_For_3P(nout,flavs,hdme);
+    }
+    break;
   }
 }
 
