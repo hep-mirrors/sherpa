34d2c39617142093f020bf37a54485797018fe3a
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-12-06 11:04:06 +0000

    added jv / ljv switch, bugfix in single fs veto, new cluster algorithm

--- APACIC++-2.0/Showers/Jet_Veto.C
+++ APACIC++-2.0/Showers/Jet_Veto.C
@@ -10,10 +10,34 @@
 using namespace APACIC;
 using namespace ATOOLS;
 
+double PT_Measure::operator()(const ATOOLS::Vec4D &p1) 
+{ 
+  return p_jf->MTij2(Vec4D(1.,0.,0.,1.),p1); 
+}
+
+double PT_Measure::operator()
+  (const ATOOLS::Vec4D &p1,const ATOOLS::Vec4D &p2) 
+{ 
+  return p_jf->MTij2(p1,p2); 
+}
+
+Vec4D E_Scheme::operator()(const ATOOLS::Vec4D &p1,const ATOOLS::Vec4D &p2) 
+{ 
+  return p1+p2; 
+}
+
+#include "Cluster_Algorithm.C"
+
+template class Cluster_Algorithm<Vec4D,PT_Measure,E_Scheme>;
+
 Jet_Veto::Jet_Veto(ATOOLS::Jet_Finder *const jf,
 		   Timelike_Kinematics *const kin):
-  p_jf(jf), p_kin(kin), p_istrees(NULL), p_fstree(NULL), 
-  m_mode(jv::none), m_maxjets(2), m_cmode(0) {}
+  p_jf(jf), p_cluster(new Cluster_Type()), 
+  p_kin(kin), p_istrees(NULL), p_fstree(NULL), 
+  m_mode(jv::none), m_maxjets(2), m_cmode(0), m_jmode(1), m_ljmode(1) 
+{
+  p_cluster->Measure().SetJetFinder(p_jf);
+}
 
 int Jet_Veto::TestKinematics(const int mode,Knot *const mo)
 {
@@ -24,7 +48,8 @@ int Jet_Veto::TestKinematics(const int mode,Knot *const mo)
   }
   if (mode==0 && !(m_mode&jv::final) && !(m_mode&jv::initial)) return 1;
   msg_Debugging()<<METHOD<<"("<<mode<<"): p_{t jet} = "
-		 <<sqrt(p_jf->ShowerPt2())<<" {"<<std::endl;
+		 <<sqrt(p_jf->ShowerPt2())<<" / "
+		 <<sqrt(m_ycut)*rpa.gen.Ecms()<<" {"<<std::endl;
   msg_Indent();
   size_t hard(0);
   std::vector<Vec4D> jets;
@@ -44,48 +69,23 @@ int Jet_Veto::TestKinematics(const int mode,Knot *const mo)
   std::vector<ATOOLS::Vec4D> savejets(jets);
   m_rates.resize(jets.size());
   size_t nmin(p_jf->Type()==1?1:0);
-  while (jets.size()>nmin) {
-    double pt2min(std::numeric_limits<double>::max());
-    std::vector<Vec4D>::iterator jit(jets.begin());
-    std::vector<Vec4D>::iterator jwin(jets.end()), kwin(jets.end());
-    for (size_t j(0);j<jets.size();++j) {
-      if (p_jf->Type()!=1) {
- 	double pt2j(p_jf->MTij2(Vec4D(1.,0.,0.,1.),*jit));
- 	if (pt2j<pt2min) {
- 	  pt2min=pt2j;
- 	  jwin=jit;
- 	  kwin=jets.end();
- 	}
-      }
-      std::vector<Vec4D>::iterator kit(jit);
-      for (size_t k(j+1);k<jets.size();++k) {
-	++kit;
-	double pt2jk(p_jf->MTij2(*jit,*kit));
-	if (pt2jk<pt2min) {
-	  pt2min=pt2jk;
-	  jwin=jit;
-	  kwin=kit;
-	}
-      }
-      ++jit;
-    }
-    if (jwin!=jets.end()) {
-      msg_Debugging()<<"jetrate Q_{"<<jets.size()<<"->"
-		     <<jets.size()-1<<"} = "<<sqrt(pt2min)<<"\n";
-      m_rates[jets.size()-1]=pt2min;
-      if (kwin!=jets.end()) *kwin+=*jwin;
-      jets.erase(jwin);
-    }
-    else {
-      msg.Error()<<METHOD<<"("<<mode<<"): No min p_T. Abort."<<std::endl;
-      return 0;
-    }
+  p_cluster->SetPoints(jets);
+  p_cluster->Cluster(nmin,cs::num);
+  for (size_t i(0);i<jets.size();++i) {
+    m_rates[jets.size()-i-1]=p_cluster->DMins()[i];
+    msg_Debugging()<<"jetrate Q_{"<<(jets.size()-i)<<"->"
+		   <<(jets.size()-i-1)<<"} = "
+		   <<sqrt(p_cluster->DMins()[i])<<"\n";
   }
   size_t njets(nmin), nljets(nmin);
   double jcrit(p_jf->ShowerPt2()), ljcrit(m_ycut*sqr(rpa.gen.Ecms()));
+  if (m_jmode==0) jcrit=std::numeric_limits<double>::max();
+  if (m_ljmode==0) ljcrit=0.0;
+  msg_Debugging()<<"jet veto ("<<m_jmode<<") "<<jcrit
+		 <<", lose jet veto ("<<m_ljmode<<") "<<ljcrit<<"\n";
   for (;njets<m_rates.size();++njets) if (m_rates[njets]<jcrit) break;
   for (;nljets<m_rates.size();++nljets) if (m_rates[nljets]<ljcrit) break;
-  msg_Debugging()<<"produced "<<njets
+  msg_Debugging()<<"produced "<<njets<<" / "<<nljets
 		 <<" jets out of "<<hard<<", nmax = "<<m_maxjets<<"\n";
   if (njets>hard) {
     msg_Debugging()<<"produced "<<(njets-hard)
@@ -157,10 +157,11 @@ int Jet_Veto::TestISKinematics(Knot *const knot)
   double z(p_kin->LightConeZ(knot->right->z,E2,knot->tout,
 			       knot->right->t,knot->left->t));
   double pt2(z*(1.0-z)*knot->tout-(1.0-z)*knot->right->t-z*knot->left->t);
-  msg_Debugging()<<" pt = "<<sqrt(pt2)<<"\n";
+  msg_Debugging()<<"pt_old = "<<sqrt(knot->right->pt2lcm)
+		 <<" pt = "<<sqrt(pt2)<<"\n";
   if (knot->part->Info()!='H') {
-    knot->pt2lcm=pt2;
     if (pt2<0.0 || pt2>p_jf->ShowerPt2()) return 0;
+    knot->left->pt2lcm=knot->pt2lcm=pt2;
   }
   return 1;
 }
@@ -182,30 +183,9 @@ int Jet_Veto::TestFSKinematics(Knot *const knot)
 		   <<sqrt(knot->pt2lcm)<<" <- "
 		   <<knot->left->part->Momentum()<<" "
 		   <<knot->right->part->Momentum()<<"\n";
+    if (pt2>p_jf->ShowerPt2()) return 0;
     knot->left->pt2lcm=knot->right->pt2lcm=pt2;
-    if (pt2>knot->pt2lcm || pt2>p_jf->ShowerPt2()) return 0;
   }
-  /*
-  if (knot->part->Info()=='H' && knot->prev!=NULL) {
-    size_t hard(0);
-    std::vector<Vec4D> jets;
-    p_cur=knot;
-    m_cmode=1;
-    if (m_mode&jv::initial && p_istrees!=NULL) {
-      if (!CollectISMomenta(p_istrees[0]->GetRoot(),jets,hard)) return 0;
-      if (!CollectISMomenta(p_istrees[1]->GetRoot(),jets,hard)) return 0;
-    }
-    if (m_mode&jv::final) {
-      if (!CollectFSMomenta(p_fstree->GetRoot(),jets,hard)) return 0;
-    }
-    m_cmode=0;
-    for (size_t i(0);i<jets.size();++i) {
-      double pt2(p_jf->MTij2(knot->part->Momentum(),jets[i]));
-      msg_Debugging()<<" ljv pt = "<<sqrt(pt2)<<"\n";
-      if (pt2<=p_jf->ShowerPt2()) return 0;
-    }
-  }
-  */
   return 1;
 }
 
