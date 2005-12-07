759a835c399967ad2111dd9e795f250e42af6657
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-12-07 14:51:28 +0000

    implementation of new factorisation formula

--- EXTRA_XS-1.0/BFKL/Ladder.C
+++ EXTRA_XS-1.0/BFKL/Ladder.C
@@ -9,6 +9,9 @@
 
 #define NC 3.0
 
+// #define USING__Collinear_Factorization
+#define USING__Two_To_Zero_ME
+
 using namespace EXTRAXS;
 using namespace PHASIC;
 using namespace PDF;
@@ -43,7 +46,7 @@ bool Ladder::Initialize()
     if (p_pdfs[i]==NULL || 
 	p_pdfs[i]->Type().find("DUPDF")==std::string::npos)
       THROW(fatal_error,"BFKL ME needs UPDF.");
-    m_kt2min=ATOOLS::Max(m_kt2min,p_pdfs[i]->Cut("kp"));// is kt2 in fact :(
+    m_kt2min=ATOOLS::Max(m_kt2min,p_pdfs[i]->Cut("kp"));
   }
   p_sudakov->SetKT2Min(m_kt2min);
   delete [] p_momenta;
@@ -72,57 +75,76 @@ void Ladder::CreateISRChannels()
 
 double Ladder::Jacobian() const
 {
-  double jacobian(m_a1);
-  jacobian*=(m_a2*m_z2*(1.0-m_z2))/(m_k2[0]*m_k2[3])/(2.0*M_PI);
-  return jacobian;
+#ifdef USING__Collinear_Factorization
+  // jacobian for ds' dct -> dkt2 dz 
+  return 2.0/(m_z1*(1.0-m_z1));
+#else
+  // jacobian for ds' -> dz 
+  return 2.0/(m_z1*(1.0-m_z1));
+#endif
 }
 
 double Ladder::Flux() const
 {
-  double flux(2.0*m_q2);
-  double b1(m_kt21/(m_a1*m_q2)), b2(m_kt22/(m_a2*m_q2));
-  flux*=m_a1*m_a2-b1*b2;
-  return flux;
+#ifdef USING__Collinear_Factorization
+  return 2.0*m_q2*m_a1*m_a2/(m_z1*m_z2);
+#else
+  // // naive flux is zero for 2->0, since a1=b2 and a2=b1
+  // double b1(m_moms[0].PMinus()/m_pb.PMinus());
+  // double b2(m_moms[1].PPlus()/m_pa.PPlus());
+  // return 2.0*m_q2*sqr(m_q2)*(m_a1*m_a2-b1*b2);
+  return 2.0*m_q2*m_a1*m_a2/(m_z1*m_z2);
+#endif
+}
+
+bool Ladder::CheckEnergy() const
+{
+  return -m_q.PMinus()/(1.0+m_q.PPerp2()/m_q.Abs2())<m_pb.PMinus();
 }
 
 bool Ladder::GeneratePDFJet()
 {
-  double rn[4], Q(sqrt(m_q2)), mu(sqrt(m_kt2min));
-  while (true) {
-    for (short unsigned int i(0);i<4;++i) rn[i]=ran.Get();
-    double ymin(m_yb), ymax(m_ya);
-    if (ymin>=ymax) THROW(fatal_error,"No allowed y range.");
-    // dice y
-    m_y1=ymin+rn[0]*(ymax-ymin);
-    m_weight=ymax-ymin;
-    double kt2max(ATOOLS::Min(m_kt2min*exp(ymax-ymin),m_q2/4.0));
-    if (m_kt2min>=kt2max) continue;
-    // dice kt2
-    m_kt21=m_kt2min*pow(kt2max/m_kt2min,rn[1]);
-    m_weight*=log(kt2max/m_kt2min)*m_kt21;
-    // dice phi
-    m_phi1=2.0*M_PI*rn[2];
-    // no weight, averaging
-    double kt1(sqrt(m_kt21)), a(kt1/Q*exp(m_y1));
-    if (a>1.0) continue;
-    // construct emission
-    double pp(a*m_pa.PPlus()), pm(m_kt21/pp+m_pa.PMinus());
-    m_k1=Vec4D(0.5*(pp+pm),kt1*cos(m_phi1),kt1*sin(m_phi1),0.5*(pp-pm));
-    double zmin(mu/(mu+kt1)), zmax(Min(kt1/(mu+kt1),1.0-a));
-    // dice z
-    m_z1=zmin+rn[3]*(zmax-zmin);
-    m_weight*=zmax-zmin;
-    m_a1=m_z1/(1.0-m_z1)*a;
-    pp=m_a1*m_pa.PPlus();
-    pm=m_pa.PMinus()-pm;
-    m_moms[0]=m_q=Vec4D(0.5*(pp+pm),-m_k1[1],-m_k1[2],0.5*(pp-pm));
-    if (a>1.0 || m_a1>1.0 || m_z1>1.0) {
-      msg.Error()<<METHOD<<"(): LCM out of range."<<std::endl;
-      return false;
-    }
-    break;
+  m_weight=1.0;
+  double rn[4], Q(sqrt(m_q2)), mu(sqrt(m_kt2min)), kt2max(0.25*m_q2);
+  double kt2min(0.25*m_q2*p_pdfs[0]->XMin()*p_pdfs[1]->XMin());
+  kt2min=ATOOLS::Max(m_kt2min,kt2min);
+  for (short unsigned int i(0);i<4;++i) rn[i]=ran.Get();
+  // dice kt2
+  m_kt21=kt2min*pow(kt2max/kt2min,rn[0]);
+  m_weight*=log(kt2max/kt2min)*m_kt21/m_q2;
+  // dice z
+  double zmax(sqrt(0.25-m_kt21/m_q2)), zmin(0.5-zmax);
+  zmax+=0.5;
+  m_z1=zmin+rn[1]*(zmax-zmin);
+  m_weight*=zmax-zmin;
+  // dice phi
+  m_phi1=2.0*M_PI*rn[2];
+  m_weight*=2.0*M_PI;
+  // dice y
+  double shat(m_kt21/(m_z1*(1.0-m_z1))), Qhat(sqrt(shat));
+  double ymin(log(Qhat/Q)), ymax(-ymin);
+  if (ymin>=ymax) THROW(fatal_error,"No allowed y range.");
+  double y(ymin+rn[3]*(ymax-ymin));
+  m_weight*=ymax-ymin;
+  // construct emission
+  double kt1(sqrt(m_kt21)), E1(Qhat/2.0);
+  m_k1=Vec4D(E1,kt1*cos(m_phi1),kt1*sin(m_phi1),sqrt(E1*E1-m_kt21));
+  Poincare cms(Vec4D(cosh(y),0.0,0.0,sinh(y)));
+  cms.BoostBack(m_k1);
+  m_y1=m_k1.Y();
+  // construct propagator
+  Vec4D pi1(Qhat/2.,0.,0.,Qhat/2.);
+  cms.BoostBack(pi1);
+  m_moms[0]=m_q=pi1-m_k1;
+  m_a1=m_q.PPlus()/m_pa.PPlus();
+  m_z1=m_q.PPlus()/pi1.PPlus();
+  // phase space weight
+  m_weight/=32.0*sqr(M_PI);
+  if (m_a1>1.0 || m_z1>1.0 || m_a1/m_z1>1.0) {
+    msg.Error()<<METHOD<<"(): LCM out of range."<<std::endl;
+    return false;
   }
-  if (m_k1.Nan() || m_q.Nan()) return false;
+  if (!CheckEnergy() || m_k1.Nan() || m_q.Nan()) return false;
   return true;
 }
 
@@ -140,44 +162,35 @@ bool Ladder::ConstructRung()
 
 bool Ladder::GenerateLadder()
 {
-  while (true) {
-    m_moms.resize(m_nin);
-    m_flavs.resize(m_nin);
-    p_sudakov->Initialize();
-    if (!GeneratePDFJet()) return false;
-    p_sudakov->SetKT2Max(m_q2/4.0);
-    p_sudakov->SetYMax(m_y1);
-    while (false) {
-//     while (p_sudakov->Dice()) {
-      if (ConstructRung()) {
-	if (p_sudakov->CalculateWeight(m_moms,m_q)) {
-	  m_weight*=p_sudakov->Weight();
-	}
-	else {
-	  m_moms.pop_back();
-	  m_flavs.pop_back();
-	  m_q=m_oldq;
-	}
+  m_moms.resize(m_nin);
+  m_flavs.resize(m_nin);
+  p_sudakov->Initialize();
+  if (!GeneratePDFJet()) return false;
+#ifndef USING__Two_To_Zero_ME
+  p_sudakov->SetKT2Max(m_kt21);
+  p_sudakov->SetYMax(m_y1);
+  while (p_sudakov->Dice()) {
+    if (ConstructRung()) {
+      if (CheckEnergy() && p_sudakov->CalculateWeight(m_moms,m_q)) {
+	m_weight*=p_sudakov->Weight();
+      }
+      else {
+	m_moms.pop_back();
+	m_flavs.pop_back();
+	m_q=m_oldq;
       }
     }
-    double pp(m_q.PPlus()+m_pb.PPlus()), pm(m_q.PPerp2()/pp);
-    m_k2=Vec4D(0.5*(pp+pm),m_q[1],m_q[2],0.5*(pp-pm));
-    m_a2=-m_q.PMinus()/m_pb.PMinus();
-    m_kt22=m_k2.PPerp2();
-    m_moms[1]=-1.0*m_q;
-    m_q=m_q-m_k2;
-    m_z2=-m_moms[1].PMinus()/m_q.PMinus();
-    double mu(sqrt(m_kt2min)), kt2(sqrt(m_kt22));
-    if (m_z2<mu/(mu+kt2) || m_z2>kt2/(mu+kt2)) continue;
-    if (m_moms.size()>m_nin) {
-      if (m_moms.back().Y()<=m_k2.Y()) continue;
-    }
-    else {
-      if (m_y1<=m_k2.Y()) continue;
-    }
-    if (!m_q.Nan() && m_q[0]<0.0 && m_q[0]>-m_pb[0]) break; 
   }
-  m_weight*=Jacobian();
+#endif
+  double pp(m_q.PPlus()), pm(m_q.PPerp2()/pp);
+  m_k2=Vec4D(0.5*(pp+pm),m_q[1],m_q[2],0.5*(pp-pm));
+  m_a2=-m_q.PMinus()/m_pb.PMinus();
+  m_kt22=m_k2.PPerp2();
+  m_moms[1]=-1.0*m_q;
+  m_q=m_q-m_k2;
+  m_z2=-m_moms[1].PMinus()/m_q.PMinus();
+  if (m_moms.size()>m_nin && m_moms.back().Y()<=m_k2.Y()) return false;
+  if (m_q.Nan() || m_q[0]>=0.0 || m_q[0]<=-m_pb[0]) return false; 
   return true;
 }
 
@@ -206,19 +219,50 @@ double Ladder::Differential(const ATOOLS::Vec4D *momenta)
   p_sudakov->SetYMax(m_ya=m_pa.Y());
   p_sudakov->SetYMin(m_yb=m_pb.Y());
   if (!GenerateLadder()) {
-    msg.Error()<<METHOD<<"(..): No ME generated. Set weight 0."<<std::endl;
+    msg_Debugging()<<METHOD<<"(..): No ME generated. Set weight 0.\n";
     return 0.0;
   }
   if (!SetScales()) {
     msg.Error()<<METHOD<<"(..): Invalid scales. Set weight 0."<<std::endl;
     return 0.0;
   }
+#ifdef USING__Collinear_Factorization
+  /*
+    double s((m_k1+m_k2).Abs2()), t(m_moms[0].Abs2());
+    double u((m_moms[0]+m_k1-m_k2).Abs2()), Q2(2.0*s*t*u/(s*s+u*u+t*t));
+    p_pdfs[0]->GetBasicPDF()->Calculate(m_a1/m_z1,0.0,0.0,Q2);
+    p_pdfs[1]->GetBasicPDF()->Calculate(m_a2/m_z1,0.0,0.0,Q2);
+    m_weight*=p_pdfs[0]->GetBasicPDF()->GetXPDF(m_flavs[0])/m_a1*m_z1;
+    m_weight*=p_pdfs[1]->GetBasicPDF()->GetXPDF(m_flavs[1])/m_a2*m_z2;
+    double Ms(1.0-t*u/(s*s)), Mt(1.0-s*u/(t*t)), Mu(1.0-s*t/(u*u));
+    m_weight*=sqr(4.0*M_PI*(*MODEL::as)(Q2))*9.0/4.0*(Mt+Mu+Ms);
+  */
+  // ll approximation of gg->gg me
+  /*
+    m_weight*=4.0*sqr(M_PI)*sqr(M_PI)*
+      sqr((*MODEL::as)(Q2)/(2.0*M_PI*m_kt21)*2.0*3.0/(m_z1*(1.0-m_z1)))*
+        4.0*sqr((m_moms[0]+m_moms[1]+m_k1+m_k2).Abs2())/(NC*NC-1.0)*
+        sqr(m_z1*(1.0-m_z1));
+  */
+  // ll approximation of gg->gg me
+  p_pdfs[0]->GetBasicPDF()->Calculate(m_a1/m_z1,0.0,0.0,m_kt21);
+  p_pdfs[1]->GetBasicPDF()->Calculate(m_a2/m_z1,0.0,0.0,m_kt22);
+  m_weight*=p_pdfs[0]->GetBasicPDF()->GetXPDF(m_flavs[0])/m_a1*m_z1;
+  m_weight*=p_pdfs[1]->GetBasicPDF()->GetXPDF(m_flavs[1])/m_a2*m_z2;
+  m_weight*=4.0*pow(M_PI,4.0)*sqr(m_kt21)*
+    sqr((*MODEL::as)(m_kt21)/(2.0*M_PI*m_kt21)*
+	2.0*3.0*sqr(1.0-m_z1*(1.0-m_z1))/(m_z1*(1.0-m_z1)));
+#else
+  p_pdfs[0]->SetSudakovMode(1);
   p_pdfs[1]->SetSudakovMode(0);
   p_pdfs[0]->Calculate(m_a1,m_z1,m_kt21,m_mu21);
   p_pdfs[1]->Calculate(m_a2,m_z2,m_kt22,m_mu22);
-  m_weight*=p_pdfs[0]->GetXPDF(m_flavs[0])/m_a1;
-  m_weight*=p_pdfs[1]->GetXPDF(m_flavs[1])/m_a2;
-  m_weight*=4.0*sqr((m_moms[0]+m_moms[1]+m_k1+m_k2).Abs2())/(NC*NC-1.0);
+  m_weight*=p_pdfs[0]->GetXPDF(m_flavs[0])/m_a1*m_z1;
+  m_weight*=p_pdfs[1]->GetXPDF(m_flavs[1])/m_a2*m_z2;
+  // ll approximation of gg->gg me
+  m_weight*=4.0*pow(M_PI,4.0)*sqr(m_kt21);
+#endif  
+  m_weight*=Jacobian();
   m_weight/=Flux();
   if (m_ncols<m_moms.size()) {
     for (size_t i(0);i<m_ncols;++i) delete [] p_colours[i]; 
