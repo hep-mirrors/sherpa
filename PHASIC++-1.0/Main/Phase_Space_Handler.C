b344ef0a3fcd5caa5514a474c55f4eacc5c475de
Stefan Hoeche <shoeche@slac.stanford.edu>
2005-12-22 11:00:48 +0000

    set no proc weight for standard weighted

--- PHASIC++-1.0/Main/Phase_Space_Handler.C
+++ PHASIC++-1.0/Main/Phase_Space_Handler.C
@@ -705,8 +705,7 @@ ATOOLS::Blob_Data_Base *Phase_Space_Handler::WeightedEvent(int mode)
       m_trials=i;
       return new Blob_Data<Weight_Info>
 	(Weight_Info(m_weight,p_process->GMin()>=0.0?
-		     selected->ProcWeight()/p_process->Parent()->ProcWeight():
-		     p_process->EnhanceFactor(),
+		     selected->ProcWeight()/p_process->Parent()->ProcWeight():1.0,
 		     m_weight,m_trials,m_trials));
     }
     // call from amisic
