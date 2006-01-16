a5ae5d1af0e3ce8c67fcc2f3f7defc9b0ec45a08
Tanju Gleisberg <sherpa@projects.hepforge.org>
2006-01-16 13:54:34 +0000

    JetVeto scale setting for pure jets fixed

--- SHERPA-1.0/PerturbativePhysics/Cluster_Partons_Base.C
+++ SHERPA-1.0/PerturbativePhysics/Cluster_Partons_Base.C
@@ -549,7 +549,7 @@ void   Cluster_Partons_Base::JetvetoPt2(double & q2i, double & q2f)
   double qmin2(Max(m_qmin_i,m_qmin_f));
   qmin2*=qmin2;
   if ((m_njet==m_maxjetnumber && m_njet>2) ||
-      (m_njet==2 && p_ct->OrderStrong()>0)) FixJetvetoPt2(qmin2);
+      (m_maxjetnumber==2 && p_ct->OrderStrong()>0)) FixJetvetoPt2(qmin2);
   q2i = Max(qmin2,sqr(m_qmin_i));
   q2f = Max(qmin2,sqr(m_qmin_f)); 
   p_jf->SetShowerPt2(qmin2);
