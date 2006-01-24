f2aa8fa95dc53896d63e57f53b8600c67d99b58c
Jan Winter <jwinter@pa.msu.edu>
2006-01-24 21:04:46 +0000

    II dipoles added

--- ADICIC++-0.0/Input/Evolution_Strategy.hpp
+++ ADICIC++-0.0/Input/Evolution_Strategy.hpp
@@ -1,13 +1,16 @@
 //bof
-//Version: 2 ADICIC++-0.0/2004/08/09
+//Version: 3 ADICIC++-0.0/2005/07/22
 
 //Possibility of having information at compile time.
-//Globally defined parameters influencing Chain_Handler.H.
+//Globally defined parameters influencing the evolution of chains and cascades.
 
 
 
 #ifndef _Evolution_Strategy_hpp_
 #define _Evolution_Strategy_hpp_ _Evolution_Strategy_hpp_
+
+
+#include <cstdlib>
 
 
 
@@ -19,40 +22,37 @@ namespace ADICIC {
 
   namespace Chain_Evolution_Strategy {
 
-
-
-    //Set the corresponding value:
-    static const int Label=1;
-
-
-
-    struct Unknown {};       //All other Labels.
-    struct Production {};    //Label==1.
-    struct Emission {};      //Label==2.
-    struct Mass {};          //Label==3.
-
-
-
-    template<int label> struct Map {
-      typedef Unknown Ret;
-    };
-    template<> struct Map<1> {
-      typedef Production Ret;
-    };
-    template<> struct Map<2> {
-      typedef Emission Ret;
-    };
-    template<> struct Map<3> {
-      typedef Mass Ret;
+    enum Type {
+      Unknown    = 0,
+      Production = 1,
+      Emission   = 11,
+      Mass       = 21,
+      stop       = -77
     };
 
+    static const size_t NumberOfTypes = 5;
 
-
-    //typedef Map<Label>::Ret Ret;
-
-
+    static const Type List[NumberOfTypes]={
+      Unknown,
+      Production,
+      Emission,
+      Mass,
+      stop    //Keep this at the bottom.
+    };
 
   }    //eo namespace Chain_Evolution_Strategy
+
+
+
+
+
+  //Enhance readability -> chain evolution labels. Do not change numbers.
+  namespace cel {
+    enum code {
+      def  = 0,
+      stop = 1
+    };
+  }
 
 
 
--- ADICIC++-0.0/Input/Recoil_Strategy.hpp
+++ ADICIC++-0.0/Input/Recoil_Strategy.hpp
@@ -1,13 +1,19 @@
 //bof
-//Version: 2 ADICIC++-0.0/2004/08/27
+//Version: 3 ADICIC++-0.0/2005/09/21
 
 //Possibility of having information at compile time.
-//Globally defined parameter sets influencing Dipole_Handler.H.
+//Globally defined parameter sets influencing the calculation of recoils.
 
 
 
 #ifndef _Recoil_Strategy_hpp_
 #define _Recoil_Strategy_hpp_ _Recoil_Strategy_hpp_
+
+
+#include <vector>
+#include <enumextra>
+#include "Vector.H"
+#include "Poincare.H"
 
 
 
@@ -19,73 +25,143 @@ namespace ADICIC {
 
   namespace Recoil_Strategy {
 
+    //The settings here must agree with the ones in ADICIC::MakeRecos(..),
+    //see Recoil_Calculator.H
 
-
-    //Set the corresponding values:
-
-    static const int Label_qgqbar = 2;
-    static const int Label_qgg    = 3;
-    static const int Label_ggqbar = 1;
-    static const int Label_ggg    = 4;
-
-    static const int Label_qqbarq    = 1;//4;3;
-    static const int Label_qbarqqbar = 3;//4;1;
-    static const int Label_gqbarq    = 1;//1;1;
-    static const int Label_qbarqg    = 3;//3;3;
-
-
-
-    struct Unknown {};       //All other Labels.
-    struct FixDir1 {};       //Label==1.
-    struct Kleiss {};        //Label==2.
-    struct FixDir3 {};       //Label==3.
-    struct MinimizePt {};    //Label==4.
-    struct Lonnblad {};      //Label==5.
-    struct OldAdicic {};     //Label==6.
-    struct Test {};          //Label==7.
-
-
-
-    template<int label> struct Map {
-      typedef Unknown Ret;
-    };
-    template<> struct Map<1> {
-      typedef FixDir1 Ret;
-    };
-    template<> struct Map<2> {
-      typedef Kleiss Ret;
-    };
-    template<> struct Map<3> {
-      typedef FixDir3 Ret;
-    };
-    template<> struct Map<4> {
-      typedef MinimizePt Ret;
-    };
-    template<> struct Map<5> {
-      typedef Lonnblad Ret;
-    };
-    template<> struct Map<6> {
-      typedef OldAdicic Ret;
-    };
-    template<> struct Map<7> {
-      typedef Test Ret;
+    enum Type {
+      Unknown    = 0,
+      Kleiss     = 2,
+      FixDir1    = 1,
+      FixDir3    = 3,
+      MinimizePt = 4,
+      Lonnblad   = 5,
+      OldAdicic  = 6,
+      Test       = 7,
+      Ktii       = 8,
+      stop       = -99
     };
 
+    static const size_t NumberOfTypes = 10;
 
-
-    typedef Map<Label_qgqbar>::Ret Ret_qgqbar;
-    typedef Map<Label_qgg>::Ret    Ret_qgg;
-    typedef Map<Label_ggqbar>::Ret Ret_ggqbar;
-    typedef Map<Label_ggg>::Ret    Ret_ggg;
-
-    typedef Map<Label_qqbarq>::Ret    Ret_qqbarq;
-    typedef Map<Label_qbarqqbar>::Ret Ret_qbarqqbar;
-    typedef Map<Label_gqbarq>::Ret    Ret_gqbarq;
-    typedef Map<Label_qbarqg>::Ret    Ret_qbarqg;
-
-
+    static const Type List[NumberOfTypes]={
+      Unknown,
+      Kleiss,
+      FixDir1,
+      FixDir3,
+      MinimizePt,
+      Lonnblad,
+      OldAdicic,
+      Test,
+      Ktii,
+      stop    //Keep this at the bottom.
+    };
 
   }    //eo namespace Recoil_Strategy
+
+
+
+
+
+  //Collection of fourvectors.
+  typedef std::vector<ATOOLS::Vec4D> Multi4vec;
+
+
+
+
+
+  //Enhance readability! Do not change numbers.
+  //Dipole shower mode dsm (j i.e. just).
+  namespace dsm {
+    enum code {
+      off  = 0,
+      jff  = 1,
+      jfi  = 2,
+      jif  = 4,
+      jii  = 8,
+      iiff = 9,
+      all  = 15
+    };
+  }
+  //Recoil labels rl (qbar i.e. a).
+  namespace rl {
+    enum code {
+      qag   = 0,
+      qgg   = 1,
+      gag   = 2,
+      ggg   = 3,
+      iiaqg = 4,
+      iiagg = 5,
+      iigqg = 6,
+      iiggg = 7,
+      //-----
+      qga   = 8,
+      gaq   = 9,
+      gga   = 10,
+      ggq   = 11,
+      iiaqa = 12,
+      iiaqq = 13,
+      iiagq = 14,
+      iigqa = 15,
+      //-----
+      stop  = 16
+    };
+  }
+  //Recoil results rr.
+  namespace rr {
+    enum code {
+      p1   = 0,    //TopBranch.
+      //----
+      p2   = 1,
+      axis = 1,
+      //----
+      p3   = 2,    //BotBranch.
+      stop = 3
+    };
+  }
+  //Recoil distribution type rdt.
+  namespace rdt {
+    enum code {
+      local    = 0,
+      iirecoil = 1,
+      stop     = 2
+    };
+  }
+
+
+
+
+
+  //Interfacing the results of the Recoil calculation.
+  struct Recoil_Result {
+    xbool     Poc;    //Partner of compensation.
+    Multi4vec Vec;
+    //------------------
+    Recoil_Result();
+    ~Recoil_Result() {}
+    //------------------
+    inline void Reset();
+    void Print() const;
+  };
+
+
+
+
+
+  //Interfacing the results of the Recoil distribution.
+  class Recoil_Tool {
+  private:
+    rdt::code         m_mode;
+    ATOOLS::Poincare* p_fly;
+    ATOOLS::Poincare* p_flyprime;
+  public:
+    Recoil_Tool(rdt::code c=rdt::local);
+    ~Recoil_Tool();
+    inline const rdt::code Mode() const;
+    inline bool KeepThisBoost(ATOOLS::Poincare&);
+    inline bool KeepThisBackBoost(ATOOLS::Poincare&);
+    inline ATOOLS::Poincare& GetBoost() const;
+    inline ATOOLS::Poincare& GetBackBoost() const;
+  };
 
 
 
@@ -101,6 +177,9 @@ namespace ADICIC {
 
 
 
+
+
+#include "Recoil_Strategy.inl.hh"
 
 
 #endif    //eo _Recoil_Strategy_hpp_
