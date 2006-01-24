ada3004619fc4fb88f7d173a1be5d328a8ff0877
Jan Winter <jwinter@pa.msu.edu>
2006-01-24 21:17:27 +0000

    II dipoles added

--- ADICIC++-0.0/Main/Adicic.C
+++ ADICIC++-0.0/Main/Adicic.C
@@ -1,5 +1,5 @@
 //bof
-//Version: 2 ADICIC++-0.0/2004/09/10
+//Version: 3 ADICIC++-0.0/2005/08/05
 
 //Implementation of Adicic.H.
 
@@ -26,31 +26,17 @@ using namespace ADICIC;
 
 
 
-Adicic::Adicic() : m_total(0), m_fail(0), p_handler(NULL), m_cascade() {
-  cout<<om::green;/////////////////////////////////////////////////////////////
-  cout<<"Dipole Flavour Initialization ......."<<endl;/////////////////////////
-  cout<<"  "<<Dipole_Flavour_Init::Status()<<endl;/////////////////////////////
-  cout<<"  "<<Dipole_Flavour_Init::DoIt()<<endl;
-  cout<<"  "<<Dipole_Flavour_Init::DoIt()<<endl;///////////////////////////////
-  cout<<"  "<<Dipole_Flavour_Init::Status()<<endl;/////////////////////////////
-  cout<<"Dipole Parameter Initialization .......\n\n";/////////////////////////
-  cout<<"  Dip Param Init?: "<<Dipole_Parameter_Init::Status()<<endl;//////////
-  cout<<"\n  Do Init: "<<Dipole_Parameter_Init::DoIt()<<"\n";
-  cout<<"  Do Init: "<<Dipole_Parameter_Init::DoIt()<<"\n";////////////////////
-  cout<<"  Dip Param Init?: "<<Dipole_Parameter_Init::Status()<<"\n\n";////////
-  cout<<"Sudakov Calculator Initialization ......."<<endl;/////////////////////
-  cout<<"  Running?="<<Sudakov_Calculator::IsAlphaSRunning()<<endl;////////////
-  cout<<"  MinScale="<<Sudakov_Calculator::MinOfK2t()<<endl;///////////////////
-  cout<<"  MaxScale="<<Sudakov_Calculator::MaxOfK2t()<<endl;///////////////////
-  cout<<"  ASFix="<<Sudakov_Calculator::AlphaSFix()<<endl;/////////////////////
-  cout<<"  ASApp="<<Sudakov_Calculator::AlphaSApprox()<<endl;//////////////////
-  cout<<"  ASCor="<<Sudakov_Calculator::AlphaSCorr(700.0)<<endl;///////////////
-  cout<<"  ASCor="<<Sudakov_Calculator::AlphaSCorr(8100.0)<<endl;//////////////
+Adicic::Adicic()
+  : m_parcountcorr(2),
+    m_total(0), m_fail(0), m_noem(0), p_handler(NULL), m_cascade(),
+    p_blist(NULL), l_paic() {
   cout<<om::brown;/////////////////////////////////////////////////////////////
-  cout<<"  SudakovInit(NULL)="<<Sudakov_Calculator::Init(NULL)<<endl;
-  cout<<om::green;/////////////////////////////////////////////////////////////
+  cout<<"Dipole Flavour Initialization .......\n  ";///////////////////////////
+  Dipole_Flavour_Init dfi;
+  cout<<"Dipole Parameter Initialization .......\n  ";/////////////////////////
+  Dipole_Parameter_Init dpi;
+  cout<<"Sudakov Calculator Initialization happened with NULL ......."<<endl;//
   cout<<"  Running?="<<Sudakov_Calculator::IsAlphaSRunning()<<endl;////////////
-  cout<<"  ASFix="<<Sudakov_Calculator::AlphaSFix()<<endl;/////////////////////
   cout<<"  ASApp="<<Sudakov_Calculator::AlphaSApprox()<<endl;//////////////////
   cout<<"  ASCor="<<Sudakov_Calculator::AlphaSCorr(700.0)<<endl;///////////////
   cout<<"  ASCor="<<Sudakov_Calculator::AlphaSCorr(8100.0)<<endl;//////////////
@@ -61,42 +47,33 @@ Adicic::Adicic() : m_total(0), m_fail(0), p_handler(NULL), m_cascade() {
 
   p_handler=new Cascade_Handler();
   assert(p_handler);
-
 }
 
 
 
 
 
-Adicic::Adicic(MODEL::Model_Base* pmod)
-  : m_total(0), m_fail(0), p_handler(NULL), m_cascade() {
+Adicic::Adicic(const string& path, MODEL::Model_Base* pmod)
+  : m_parcountcorr(2),
+    m_total(0), m_fail(0), m_noem(0), p_handler(NULL), m_cascade(),
+    p_blist(NULL), l_paic() {
   cout<<om::green;/////////////////////////////////////////////////////////////
-  cout<<"Dipole Flavour Initialization ......."<<endl;/////////////////////////
-  cout<<"  "<<Dipole_Flavour_Init::Status()<<endl;/////////////////////////////
-  cout<<"  "<<Dipole_Flavour_Init::DoIt()<<endl;
-  cout<<"  "<<Dipole_Flavour_Init::DoIt()<<endl;///////////////////////////////
-  cout<<"  "<<Dipole_Flavour_Init::Status()<<endl;/////////////////////////////
-  cout<<"Dipole Parameter Initialization .......\n\n";/////////////////////////
-  cout<<"  Dip Param Init?: "<<Dipole_Parameter_Init::Status()<<endl;//////////
-  cout<<"\n  Do Init: "<<Dipole_Parameter_Init::DoIt()<<"\n";
-  cout<<"  Do Init: "<<Dipole_Parameter_Init::DoIt()<<"\n";////////////////////
-  cout<<"  Dip Param Init?: "<<Dipole_Parameter_Init::Status()<<"\n\n";////////
-  cout<<"Sudakov Calculator Initialization ......."<<endl;/////////////////////
+  cout<<"Dipole Flavour Initialization .......\n  ";///////////////////////////
+  Dipole_Flavour_Init dfi;
+  cout<<"Dipole Parameter Initialization .......\n  ";/////////////////////////
+  Dipole_Parameter_Init dpi;
+  cout<<"Sudakov Calculator Initialization so far ......."<<endl;//////////////
   cout<<"  Running?="<<Sudakov_Calculator::IsAlphaSRunning()<<endl;////////////
-  cout<<"  MinScale="<<Sudakov_Calculator::MinOfK2t()<<endl;///////////////////
-  cout<<"  MaxScale="<<Sudakov_Calculator::MaxOfK2t()<<endl;///////////////////
-  cout<<"  ASFix="<<Sudakov_Calculator::AlphaSFix()<<endl;/////////////////////
   cout<<"  ASApp="<<Sudakov_Calculator::AlphaSApprox()<<endl;//////////////////
   cout<<"  ASCor="<<Sudakov_Calculator::AlphaSCorr(700.0)<<endl;///////////////
   cout<<"  ASCor="<<Sudakov_Calculator::AlphaSCorr(8100.0)<<endl;//////////////
   cout<<om::red;///////////////////////////////////////////////////////////////
-  //cout<<"SudakovInit="<<Sudakov_Calculator::Init(NULL)<<endl;
-  //cout<<"SudakovInit="<<Sudakov_Calculator::Init(pmod)<<endl;
-  //cout<<"SudakovInit="<<Sudakov_Calculator::Init(NULL)<<endl;
-  cout<<"  SudakovInit(MODEL)="<<Sudakov_Calculator::Init(pmod)<<endl;
-  cout<<om::green;/////////////////////////////////////////////////////////////
+  //cout<<"SudakovInit="<<Sudakov_Calculator::AdjustEnvironment(pmod)<<endl;
+  //cout<<"SudakovInit="<<Sudakov_Calculator::AdjustEnvironment(NULL)<<endl;
+  cout<<"  SudakovInit(MODEL)="
+      <<Sudakov_Calculator::AdjustEnvironment(path,pmod);
+  cout<<endl<<om::green;///////////////////////////////////////////////////////
   cout<<"  Running?="<<Sudakov_Calculator::IsAlphaSRunning()<<endl;////////////
-  cout<<"  ASFix="<<Sudakov_Calculator::AlphaSFix()<<endl;/////////////////////
   cout<<"  ASApp="<<Sudakov_Calculator::AlphaSApprox()<<endl;//////////////////
   cout<<"  ASCor="<<Sudakov_Calculator::AlphaSCorr(700.0)<<endl;///////////////
   cout<<"  ASCor="<<Sudakov_Calculator::AlphaSCorr(8100.0)<<endl;//////////////
@@ -107,7 +84,6 @@ Adicic::Adicic(MODEL::Model_Base* pmod)
 
   p_handler=new Cascade_Handler();
   assert(p_handler);
-
 }
 
 
@@ -117,8 +93,11 @@ Adicic::Adicic(MODEL::Model_Base* pmod)
 Adicic::~Adicic() {
   m_cascade|0;
   p_handler->PrintCounter();
-  cout<<"Total number = "<<m_total<<"\tTotal number of failures = "<<m_fail;
-  cout<<"   ("<<1.0*m_fail/m_total<<")."<<endl;
+  cout<<"Total number="<<m_total
+      <<".   Total number of no emissions="<<m_noem
+      <<" ("<<1.0*m_noem/m_total<<")."
+      <<"   Total number of failures="<<m_fail
+      <<" ("<<1.0*m_fail/m_total<<")."<<endl;
   if(p_handler) delete p_handler;
 }
 
@@ -130,10 +109,12 @@ Adicic::~Adicic() {
 
 int Adicic::PerformShowers() {
   ++m_total;
+  //if(m_cascade.INumber()) m_cascade.Print();/////////////////////////////////
   assert(m_cascade|(*p_handler));
-  if(p_handler->EvolveCascade());
-  else ++m_fail;
+  if(p_handler->EvolveCascade()); else ++m_fail;
   m_cascade|0;
+  if(m_cascade.DipoleNumber()==1) ++m_noem;
+  //if(m_cascade.INumber()) m_cascade.Print();/////////////////////////////////
   return 1;
 }
 
@@ -141,14 +122,20 @@ int Adicic::PerformShowers() {
 
 
 
-bool Adicic::ExtractPartons(ATOOLS::Blob_List* blobs, ATOOLS::Particle_List*) {
+bool Adicic::ExtractPartons(ATOOLS::Blob_List* blobs) {
+
+  assert(blobs);
+  p_blist=blobs;
 
   if(m_total==1 || m_total%1000==0)
     cout<<"                "<<om::red<<om::blackbg<<m_total<<om::reset<<"\n";
 
-  int num;
-  Blob* blob;
-  list<Particle_List> lists;
+  if(!m_cascade.ExtractPartons(l_paic)) {
+    msg.Error()<<__PRETTY_FUNCTION__<<": Error: "
+	       <<"Cannot extract partons from cascade!\n"<<endl;
+    return false;
+  }
+  assert(l_paic.size()==1);////////////////////////////////////////////////////
 
 #ifdef ADICIC_OUTPUT
   cout<<"Blob list......: "<<blobs->size()<<endl;//////////////////////////////
@@ -156,49 +143,160 @@ bool Adicic::ExtractPartons(ATOOLS::Blob_List* blobs, ATOOLS::Particle_List*) {
   cout<<"Current  number: "<<ATOOLS::Particle::CurrentNumber()<<"\n"<<endl;////
 #endif
 
-  for(Blob_List::iterator blit=blobs->begin(); blit!=blobs->end(); ++blit) {
-
-    if((*blit)->Type()==btp::FS_Shower) {
-      if(!m_cascade.ExtractPartons(lists)) {
-	msg.Error()<<__PRETTY_FUNCTION__<<endl;
-	return false;
-      }
-      assert(lists.size()==1);/////////////////////////////////////////////////
-      Particle_List& plist=lists.front();
-      blob=*blit;
-#ifdef ADICIC_OUTPUT
-      cout<<om::greenbg<<"Initial 2nd blob:"<<om::reset<<" \n"<<*blob<<endl;///
-#endif
-      for(int i=0; i<blob->NInP(); ++i) blob->InParticle(i)->SetStatus(2);
-      num=Max(blob->InParticle(0)->Number(),blob->InParticle(1)->Number());
-      num=num+1-plist.front()->Number();
-      for(Particle_List::iterator piter=plist.begin(); piter!=plist.end(); ++piter) {
-	blob->AddToOutParticles(*piter);
-	(*piter)->SetInfo('F');
-	(*piter)->SetNumber(-((*piter)->Number()+num/*-2*/));
-      }
-#ifdef ADICIC_OUTPUT
-      cout<<om::greenbg<<"Final 2nd blob:"<<om::reset<<" \n"<<*blob<<endl;/////
-      cout<<"Particle number: "<<ATOOLS::Particle::Counter()<<endl;////////////
-      cout<<"Current  number: "<<ATOOLS::Particle::CurrentNumber()<<"\n"<<endl;
-#endif
-      return true;
+  Blob* blob=NULL, * pis=NULL, * pisme=NULL, * pfs=NULL;
+  for(Blob_List::iterator blit=p_blist->begin(); blit!=p_blist->end();
+      ++blit) {
+    switch((*blit)->Type()) {
+    case btp::Signal_Process    : blob=*blit; break;
+    case btp::IS_Shower         : pis=*blit; break;
+    case btp::ME_PS_Interface_IS: pisme=*blit; break;
+    case btp::FS_Shower         : pfs=*blit; break;
+    default: assert(0);
     }
+  }
+  assert(blob);
+  assert(pfs);
 
 #ifdef ADICIC_OUTPUT
-    cout<<om::brownbg<<"Initial 1st blob:"<<om::reset<<" \n"<<**blit<<endl;////
+  cout<<om::brownbg<<"Initial 1st blob:"<<om::reset<<" \n"<<*blob<<endl;///////
 #endif
-    blob=*blit;
-    for(int i=0; i<blob->NInP(); ++i)
-      blob->InParticle(i)->SetNumber(-(1+i));
-    for(int i=0; i<blob->NOutP(); ++i)
-      blob->OutParticle(i)->SetNumber(-(blob->NInP()+1+i));
+  for(int i=0; i<blob->NInP(); ++i)
+    blob->InParticle(i)->SetNumber(-(1+i));
+  for(int i=0; i<blob->NOutP(); ++i)
+    blob->OutParticle(i)->SetNumber(-(blob->NInP()+1+i));
 #ifdef ADICIC_OUTPUT
-    cout<<om::brownbg<<"Final 1st blob:"<<om::reset<<" \n"<<**blit<<endl;//////
+  cout<<om::brownbg<<"Final 1st blob:"<<om::reset<<" \n"<<*blob<<endl;/////////
 #endif
+
+  bool result=false;
+  if(m_cascade.INumber()==0) {
+    assert(p_blist->size()==2);
+    result=FExtract(pfs);
+  }
+  else if(m_cascade.INumber()==2) {
+    assert(p_blist->size()==4);
+    assert(pis);
+    assert(pisme);
+    result=IExtract(pis,pisme,pfs);
+  }
+  else {
+    msg.Error()<<__PRETTY_FUNCTION__<<": Error: "
+	       <<"Cannot handle this cascade!\n"<<endl;
+    return false;
   }
 
-  return false;
+#ifdef ADICIC_OUTPUT
+  cout<<"Particle number: "<<ATOOLS::Particle::Counter()<<endl;////////////////
+  cout<<"Current  number: "<<ATOOLS::Particle::CurrentNumber()<<"\n"<<endl;////
+#endif
+
+  return result;
+
+}
+
+
+
+//=============================================================================
+
+
+
+bool Adicic::FExtract(Blob* pfs) {
+
+  Particle_List& plist=l_paic.front();
+#ifdef ADICIC_OUTPUT
+  cout<<om::greenbg<<"Initial 2nd blob:"<<om::reset<<" \n"<<*pfs<<endl;////////
+#endif
+  for(int i=0; i<pfs->NInP(); ++i) pfs->InParticle(i)->SetStatus(2);
+  int num=Max(pfs->InParticle(0)->Number(),pfs->InParticle(1)->Number());
+  num=num+1-plist.front()->Number();
+  for(Particle_List::iterator piter=plist.begin(); piter!=plist.end();
+      ++piter) {
+    pfs->AddToOutParticles(*piter);
+    (*piter)->SetInfo('F');
+    (*piter)->SetNumber(-((*piter)->Number()+num-m_parcountcorr));
+  }
+#ifdef ADICIC_OUTPUT
+  cout<<om::greenbg<<"Final 2nd blob:"<<om::reset<<" \n"<<*pfs<<endl;//////////
+#endif
+  return true;
+
+}
+
+
+
+
+
+bool Adicic::IExtract(Blob* pis, Blob* pisme, Blob* pfs) {
+
+  Particle_List& plist=l_paic.front();
+
+#ifdef ADICIC_OUTPUT
+  cout<<om::bluebg<<"Initial IS blob:"<<om::reset<<" \n"<<*pis<<endl;//////////
+  cout<<om::redbg<<"Initial ISME blob:"<<om::reset<<" \n"<<*pisme<<endl;///////
+  cout<<om::greenbg<<"Initial FS blob:"<<om::reset<<" \n"<<*pfs<<endl;/////////
+#endif
+
+  for(int i=0; i<pfs->NInP(); ++i) pfs->InParticle(i)->SetStatus(2);
+  int num=Max(pfs->InParticle(0)->Number(),pfs->InParticle(1)->Number());
+  num=num+1-plist.front()->Number();
+  for(Particle_List::iterator piter=plist.begin(); piter!=plist.end();
+      ++piter) {
+    if((*piter)->Info()=='i') {
+      pis->AddToInParticles(*piter);
+      (*piter)->SetInfo('I');
+      (*piter)->SetNumber(-((*piter)->Number()+num-2*m_parcountcorr));
+    } else {
+      pis->AddToOutParticles(*piter);
+      (*piter)->SetInfo('F');
+      (*piter)->SetNumber(-((*piter)->Number()+num-m_parcountcorr));
+    }
+  }
+  if(pis->InParticle(0)->Momentum()[3]<pis->InParticle(1)->Momentum()[3])
+    pis->SwapInParticles(0,1);
+
+  Particle* par=new Particle(*pisme->OutParticle(0));
+  Particle* per=new Particle(*pisme->OutParticle(1));
+  pis->AddToOutParticles(par);
+  pis->AddToOutParticles(per);
+  pisme->AddToInParticles(par);
+  pisme->AddToInParticles(per);
+
+  Particle* pic=new Particle(*pfs->InParticle(0));
+  Particle* poc=new Particle(*pfs->InParticle(1));
+  pfs->AddToOutParticles(pic);
+  pfs->AddToOutParticles(poc);
+  pic->SetStatus(1);
+  poc->SetStatus(1);
+
+  const Vec4D PZ=par->Momentum()+per->Momentum();
+  assert(PZ==pic->Momentum()+poc->Momentum());
+  const Vec4D& PZprime=-1*m_cascade.Momentum();
+  assert(dabs(PZ.Abs2()-PZprime.Abs2())<1.0e-7);
+  Poincare fly(PZ);
+  Poincare flyprime(PZprime);
+  Vec4D p[4]={
+    par->Momentum(), per->Momentum(), pic->Momentum(), poc->Momentum()};
+  for(size_t i=0; i<4; ++i) {
+    fly.Boost(p[i]); flyprime.BoostBack(p[i]);
+  }
+  par->SetMomentum(p[0]);
+  per->SetMomentum(p[1]);
+  pic->SetMomentum(p[2]);
+  poc->SetMomentum(p[3]);
+
+  pis->SetBeam(0);
+  pisme->SetStatus(0);
+
+#ifdef ADICIC_OUTPUT
+  p[1]=pis->CheckMomentumConservation();
+  p[2]=pisme->CheckMomentumConservation();
+  p[3]=pfs->CheckMomentumConservation();
+  cout<<om::bluebg<<"Final IS blob:"<<om::reset<<" \n"<<*pis<<p[1]<<endl;//////
+  cout<<om::redbg<<"Final ISME blob:"<<om::reset<<" \n"<<*pisme<<p[2]<<endl;///
+  cout<<om::greenbg<<"Final FS blob:"<<om::reset<<" \n"<<*pfs<<p[3]<<endl;/////
+#endif
+
+  return true;
 
 }
 
--- ADICIC++-0.0/Main/Paraminit.C
+++ ADICIC++-0.0/Main/Paraminit.C
@@ -1,5 +1,5 @@
 //bof
-//Version: 2 ADICIC++-0.0/2004/08/11
+//Version: 3 ADICIC++-0.0/2005/09/13
 
 //Implementation of Paraminit.H.
 
@@ -9,7 +9,6 @@
 #include "Dipole_Parameter.H"
 #include "Sudakov_Calculator.H"
 #include "Dipole_Handler.H"
-#include "Chain_Handler.H"
 #include "Paraminit.H"
 
 
@@ -28,42 +27,64 @@ using namespace ADICIC;
 
 
 
-unsigned Dipole_Parameter_Init::s_status=0;
-
-
-
-//=============================================================================
-
-
-
-const bool Dipole_Parameter_Init::DoIt() {    //Static.
-
-  if(s_status) return false;
-
-  ++s_status;
+const bool Dipole_Parameter_Init::Do() {    //Static.
 
 #ifdef PARAMINIT_OUTPUT
+  cout<<"{ Executing ... "<<__PRETTY_FUNCTION__<<" ...\n";
   Dipole_Parameter::Show();
 #endif
 
   //###########################################################################
   //AlphaS treatment flag
-  Dipole_Parameter::s_isalphasrun=true;//false;//true;
-  //coupling
-  Dipole_Parameter::s_alphasfix=0.15;//0.12;
+  Dipole_Parameter::Sud::s_runalphas=true;//false;//true;
+  //Coupling
+  Dipole_Parameter::Sud::s_alphasfix=0.15;//0.12;
+  //Number of quark flavours
+  Dipole_Parameter::Sud::s_nffix=2;
+  //Radiation type to build up the Sudakov groups
+  Dipole_Parameter::Sud::s_radiatype=Radiation::gduscb;
   //GeV^2
-  Dipole_Parameter::s_k2tmin=1.0;
-  Dipole_Parameter::s_k2tmax=8100.0;
-  //recoil strategies (compare with Recoil_Strategy.hpp)
-  Dipole_Parameter::s_restratqqbar=2;    //7;//2;
-  Dipole_Parameter::s_restratqg   =3;    //6;//3;
-  Dipole_Parameter::s_restratgqbar=1;    //2;//1;
-  Dipole_Parameter::s_restratgg   =4;//7;//5;//4;
-  //chain evolution strategy (compare with Evolution_Strategy.hpp)
-  Dipole_Parameter::s_chevolstrat =1;//2;//1;
+  Dipole_Parameter::Sud::s_k2tmin=1.0;
+  Dipole_Parameter::Sud::s_k2tmax=8100.0;
+  //GeV^2
+  Dipole_Parameter::Sud::s_k2tiimin=1.0;//0.25;
+  Dipole_Parameter::Sud::s_k2tiifac=12.005;//4.0;
+  Dipole_Parameter::Sud::s_k2tiifixscale=8100.0;
+  //II dipole efficiency factor
+  Dipole_Parameter::Sud::s_iieffexp=1.0;
+  //Dipole shower mode
+  Dipole_Parameter::Kin::s_dsmode = dsm::iiff;
+  //Recoil strategies (compare with Recoil_Strategy.hpp)
+  //    qqbar, qg, gqbar, gg radiating g
+  Dipole_Parameter::Kin::v_recostrat[rl::qag] = Recoil_Strategy::Kleiss;
+  Dipole_Parameter::Kin::v_recostrat[rl::qgg] = Recoil_Strategy::FixDir3;
+  Dipole_Parameter::Kin::v_recostrat[rl::gag] = Recoil_Strategy::FixDir1;
+  Dipole_Parameter::Kin::v_recostrat[rl::ggg] = Recoil_Strategy::MinimizePt;
+  //    iiqbarq, iiqbarg, iigq, iigg radiating g
+  Dipole_Parameter::Kin::v_recostrat[rl::iiaqg] = Recoil_Strategy::Ktii;
+  Dipole_Parameter::Kin::v_recostrat[rl::iiagg] = Recoil_Strategy::Ktii;
+  Dipole_Parameter::Kin::v_recostrat[rl::iigqg] = Recoil_Strategy::Ktii;
+  Dipole_Parameter::Kin::v_recostrat[rl::iiggg] = Recoil_Strategy::Ktii;
+  //    qg radiating qbarbot, gqbar radiating qtop, gg radiating qbarbot, qtop
+  Dipole_Parameter::Kin::v_recostrat[rl::qga] = Recoil_Strategy::FixDir1;
+  Dipole_Parameter::Kin::v_recostrat[rl::gaq] = Recoil_Strategy::FixDir3;
+  Dipole_Parameter::Kin::v_recostrat[rl::gga] = Recoil_Strategy::FixDir1;
+  Dipole_Parameter::Kin::v_recostrat[rl::ggq] = Recoil_Strategy::FixDir3;
+  //    iiqbarq radiating qbarend, qfront,
+  //    iiqbarg radiating qfront, iigq radiating qbarend
+  Dipole_Parameter::Kin::v_recostrat[rl::iiaqa] = Recoil_Strategy::Ktii;//stop;
+  Dipole_Parameter::Kin::v_recostrat[rl::iiaqq] = Recoil_Strategy::Ktii;//stop;
+  Dipole_Parameter::Kin::v_recostrat[rl::iiagq] = Recoil_Strategy::Ktii;//stop;
+  Dipole_Parameter::Kin::v_recostrat[rl::iigqa] = Recoil_Strategy::Ktii;//Unknown;
+  //Chain evolution strategy (compare with Evolution_Strategy.hpp)
+  Dipole_Parameter::Evo::v_chevostrat[cel::def] =
+    Chain_Evolution_Strategy::Production;
   //###########################################################################
 
-  //Dipole_Parameter::Reset();
+  //Re-calculate the maximum k2t for II dipoles.
+  dpv.sud.SetMaxIIScale(Dipole_Parameter::Sud::s_k2tiivarscale);
+
+  //Dipole_Parameter::SetWithStatics();    //Test.
 
 #ifdef PARAMINIT_OUTPUT
   Dipole_Parameter::Show();
@@ -72,21 +93,29 @@ const bool Dipole_Parameter_Init::DoIt() {    //Static.
   extern Run_Parameter ATOOLS::rpa;
   Dipole_Parameter::Check(rpa.gen.Ecms());
 #ifdef PARAMINIT_OUTPUT
-  cout<<"\n  Ecms="<<rpa.gen.Ecms()<<"\n";
+  cout<<"\n    Ecms="<<rpa.gen.Ecms()<<"\n\n";
 #endif
 
-  Sudakov_Calculator::AdjustParameters();
+  Sudakov_Calculator::AdjustEnvironment();
   Dipole_Handler::AdjustCalcBox();
-  Chain_Handler::AdjustParameters();
 
 #ifdef PARAMINIT_OUTPUT
-  Sudakov_Calculator::ShowParameters();
+  Sudakov_Calculator::ShowEnvironment();
   Dipole_Handler::ShowCalcBox();
-  Chain_Handler::ShowParameters();
+  cout<<"}\n"<<endl;
 #endif
 
   return true;
 
+}
+
+
+
+
+
+Dipole_Parameter_Init::Dipole_Parameter_Init() {
+  static bool init=Do();
+  assert(init);
 }
 
 
