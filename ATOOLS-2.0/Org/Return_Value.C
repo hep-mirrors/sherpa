767d0d551705dc35193b6044b82f5617f2974fe1
Frank Krauss <frank.krauss@durham.ac.uk>
2006-08-01 16:25:17 +0000

    Return value class added.

---
+++ ATOOLS-2.0/Org/Return_Value.C
@@ -0,0 +1,53 @@
+#include "Return_Value.H"
+#include "Message.H"
+
+using namespace ATOOLS;
+using namespace std;
+
+
+std::map<std::string,unsigned long int> Return_Value::s_warning_counter;
+std::map<std::string,unsigned long int> Return_Value::s_error_counter;
+std::map<std::string,unsigned long int> Return_Value::s_retry_method_counter;
+std::map<std::string,unsigned long int> Return_Value::s_retry_phase_counter;
+unsigned long int                       Return_Value::s_retry_event_counter;
+Return_Value rvalue;
+
+void Return_Value::Statistics()
+{
+  msg.Out()<<METHOD<<": Return value statistics: "<<endl<<endl
+	   <<"   Retry events: "<<s_retry_event_counter<<endl;
+  msg.Out()<<endl<<"   Retry phases: "<<endl;
+  for (map<string,unsigned long int>::iterator it=s_retry_phase_counter.begin();
+       it!=s_retry_phase_counter.end();it++)
+    msg.Out()<<"   "<<it->first<<" : "<<it->second<<endl;
+  msg.Out()<<endl<<"   Retry methods: "<<endl;
+  for (map<string,unsigned long int>::iterator it=s_retry_method_counter.begin();
+       it!=s_retry_method_counter.end();it++)
+    msg.Out()<<"   "<<it->first<<" : "<<it->second<<endl;
+}
+
+void Return_Value::IncWarnings(std::string name) {
+  if (s_warning_counter.find(name)!=s_warning_counter.end())
+    s_warning_counter.find(name)->second++;
+  else s_warning_counter[name] = 1;
+}
+
+void Return_Value::IncError(std::string name) {
+  if (s_error_counter.find(name)!=s_error_counter.end())
+    s_error_counter.find(name)->second++;
+  else s_error_counter[name] = 1;
+}
+
+void Return_Value::IncRetryMethod(std::string name){
+  if (s_retry_method_counter.find(name)!=s_retry_method_counter.end())
+    s_retry_method_counter.find(name)->second++;
+  else s_retry_method_counter[name] = 1;
+}
+
+void Return_Value::IncRetryPhase(std::string name) {
+  if (s_retry_phase_counter.find(name)!=s_retry_phase_counter.end())
+    s_retry_phase_counter.find(name)->second++;
+  else s_retry_phase_counter[name] = 1;
+}
+
+void Return_Value::IncRetryEvent(std::string name){ s_retry_event_counter++; }
