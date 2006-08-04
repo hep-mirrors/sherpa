2f86fbfe648ef91d423563142fd015556d89fecf
Frank Siegert <frank.siegert@cern.ch>
2006-08-04 11:40:18 +0000

    Bugfix for r6358 commit (includes r6361 of rel-1-0-9), Return_Value changes should compile now.

--- ATOOLS-2.0/Org/Return_Value.C
+++ ATOOLS-2.0/Org/Return_Value.C
@@ -1,10 +1,8 @@
-<<<<<<< .working
 #include "Return_Value.H"
 #include "Message.H"
 
 using namespace ATOOLS;
 using namespace std;
-
 
 std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_warning_counter;
 std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_error_counter;
@@ -13,61 +11,7 @@ std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_retry_phase_coun
 std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_retry_event_counter;
 ATOOLS::Return_Value ATOOLS::rvalue;
 
-void Return_Value::Statistics()
-{
-  msg.Out()<<METHOD<<": Return value statistics: "<<endl<<endl
-	   <<"   Retry events: "<<s_retry_event_counter<<endl;
-  msg.Out()<<endl<<"   Retry phases: "<<endl;
-  for (map<string,unsigned long int>::iterator it=s_retry_phase_counter.begin();
-       it!=s_retry_phase_counter.end();it++)
-    msg.Out()<<"   "<<it->first<<" : "<<it->second<<endl;
-  msg.Out()<<endl<<"   Retry methods: "<<endl;
-  for (map<string,unsigned long int>::iterator it=s_retry_method_counter.begin();
-       it!=s_retry_method_counter.end();it++)
-    msg.Out()<<"   "<<it->first<<" : "<<it->second<<endl;
-}
-
-void Return_Value::IncWarning(std::string name) {
-  if (s_warning_counter.find(name)!=s_warning_counter.end())
-    s_warning_counter.find(name)->second++;
-  else s_warning_counter[name] = 1;
-}
-
-void Return_Value::IncError(std::string name) {
-  if (s_error_counter.find(name)!=s_error_counter.end())
-    s_error_counter.find(name)->second++;
-  else s_error_counter[name] = 1;
-}
-
-void Return_Value::IncRetryMethod(std::string name){
-  if (s_retry_method_counter.find(name)!=s_retry_method_counter.end())
-    s_retry_method_counter.find(name)->second++;
-  else s_retry_method_counter[name] = 1;
-}
-
-void Return_Value::IncRetryPhase(std::string name) {
-  if (s_retry_phase_counter.find(name)!=s_retry_phase_counter.end())
-    s_retry_phase_counter.find(name)->second++;
-  else s_retry_phase_counter[name] = 1;
-}
-
-void Return_Value::IncRetryEvent(std::string name){ s_retry_event_counter++; }
-=======
-#include "Return_Value.H"
-#include "Message.H"
-
-using namespace ATOOLS;
-using namespace std;
-
-
-std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_warning_counter;
-std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_error_counter;
-std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_retry_method_counter;
-std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_retry_phase_counter;
-std::map<std::string,unsigned long int> ATOOLS::Return_Value::s_retry_event_counter;
-ATOOLS::Return_Value ATOOLS::rvalue;
-
-void Return_Value::Statistics()
+void ATOOLS::Return_Value::Statistics()
 {
   msg.Out()<<METHOD<<": Return value statistics: "<<endl<<endl
 	   <<"   Retry events: "<<endl;
@@ -84,34 +28,32 @@ void Return_Value::Statistics()
     msg.Out()<<"   "<<it->first<<" : "<<it->second<<endl;
 }
 
-void Return_Value::IncWarning(std::string name) {
+void ATOOLS::Return_Value::IncWarning(std::string name) {
   if (s_warning_counter.find(name)!=s_warning_counter.end())
     s_warning_counter.find(name)->second++;
   else s_warning_counter[name] = 1;
 }
 
-void Return_Value::IncError(std::string name) {
+void ATOOLS::Return_Value::IncError(std::string name) {
   if (s_error_counter.find(name)!=s_error_counter.end())
     s_error_counter.find(name)->second++;
   else s_error_counter[name] = 1;
 }
 
-void Return_Value::IncRetryMethod(std::string name){
+void ATOOLS::Return_Value::IncRetryMethod(std::string name){
   if (s_retry_method_counter.find(name)!=s_retry_method_counter.end())
     s_retry_method_counter.find(name)->second++;
   else s_retry_method_counter[name] = 1;
 }
 
-void Return_Value::IncRetryPhase(std::string name) {
+void ATOOLS::Return_Value::IncRetryPhase(std::string name) {
   if (s_retry_phase_counter.find(name)!=s_retry_phase_counter.end())
     s_retry_phase_counter.find(name)->second++;
   else s_retry_phase_counter[name] = 1;
 }
 
-void Return_Value::IncRetryEvent(std::string name) {
+void ATOOLS::Return_Value::IncRetryEvent(std::string name) {
   if (s_retry_event_counter.find(name)!=s_retry_event_counter.end())
     s_retry_event_counter.find(name)->second++;
   else s_retry_event_counter[name] = 1;
 }
-
->>>>>>> .merge-right.r6357
