b94634414bf93b87de40cc19390d8038879ebf2d
Frank Krauss <frank.krauss@durham.ac.uk>
2006-08-25 12:23:10 +0000

    Shifted the pair popper

---
+++ AHADIC++-1.0/Tools/Pair_Popper.C
@@ -0,0 +1,70 @@
+#include "Pair_Popper.H"
+#include "Hadronisation_Parameters.H"
+#include "Random.H"
+
+using namespace AHADIC;
+using namespace ATOOLS;
+using namespace std;
+
+
+Pair_Popper::Pair_Popper() :
+  p_constituents(hadpars.GetConstituents()), 
+  m_tension(hadpars.Get(string("Tension"))),
+  m_totweight(0)
+{
+  double  wt(1.),m_maxmass(0.), m_minmass(100.), mass;
+  Flavour flav;
+  for (FlavCCMap_Iterator iter=p_constituents->CCMap.begin();
+       iter!=p_constituents->CCMap.end();iter++) { 
+    flav = iter->first;
+    mass = p_constituents->Mass(flav);
+    wt   = iter->second->TotWeight() * exp(-M_PI*sqr(mass)/m_tension);
+    if (mass>m_maxmass) m_maxmass = mass;
+    if (mass<m_minmass) m_minmass = mass;
+    m_totweight += wt;
+  }
+}
+
+
+Flavour Pair_Popper::SelectFlavour(const double upper)
+{
+  Flavour flav = Flavour(kf::none);
+  if (m_minmass>upper) return flav;
+  double mass, totweight(m_totweight);
+  if (upper>m_maxmass) {
+    for (FlavCCMap_Iterator iter=p_constituents->CCMap.begin();
+	 iter!=p_constituents->CCMap.end();iter++) { 
+      flav = iter->first;
+      mass = p_constituents->Mass(flav);
+      if (2.*mass>upper) {
+	totweight -= iter->second->TotWeight() * exp(-M_PI*sqr(mass)/m_tension);
+      }
+    }
+  }
+  double wt(1.);
+  while (wt>0.) {
+    wt = totweight*ran.Get();
+    for (FlavCCMap_Iterator iter=p_constituents->CCMap.begin();
+	 iter!=p_constituents->CCMap.end();iter++) { 
+      flav = iter->first;
+      mass = p_constituents->Mass(flav);
+      if (2.*mass<upper) {
+	wt -= iter->second->TotWeight()*exp(-M_PI*sqr(mass)/m_tension);
+      }
+      if (wt<=1.e-12) { wt=-1.; break; }
+    }
+  }
+  if (flav.IsDiQuark()) flav = flav.Bar();
+  return flav;
+}
+
+double Pair_Popper::SelectPT(const double upper)
+{
+  double pt(1.1*upper);
+  while (pt>upper) pt = sqrt(-m_tension/M_PI*log(ran.Get()));
+  return pt;
+}
+
+double Pair_Popper::PopWeight(Flavour & flav) {
+  return exp(-M_PI*sqr(p_constituents->Mass(flav))/m_tension);
+}
