6c4043f34816f150c638e405144b7b8c72bcaf7e
Stefan Hoeche <shoeche@slac.stanford.edu>
2006-11-14 16:32:32 +0000

    removed warnings in compile, cosmetics in output

--- APACIC++-2.0/Main/Knot.C
+++ APACIC++-2.0/Main/Knot.C
@@ -32,8 +32,8 @@ std::ostream &APACIC::operator<<(std::ostream& s,const Knot &k)
      <<k.part->Momentum()<<","<<(k.part->Momentum()).Abs2()
      <<": ("<<k.part->GetFlow(1)<<", "<<k.part->GetFlow(2)<<") {"
      <<(k.part->ProductionBlob()?k.part->ProductionBlob()->Id():0)<<","
-     <<(k.part->DecayBlob()?k.part->DecayBlob()->Id():0)<<"} "<<k.didkin<<"\n";
-    s<<" phi="<<k.phi<<" pol="<<k.polinfo<<", dir="<<k.dir<<"\n";
+     <<(k.part->DecayBlob()?k.part->DecayBlob()->Id():0)<<"} didkin="
+     <<k.didkin<<" dir="<<k.dir<<"\n phi="<<k.phi<<" pol="<<k.polinfo<<"\n";
   } 
   else { 
     s<<"***empty knot***\n"; 
@@ -44,22 +44,22 @@ std::ostream &APACIC::operator<<(std::ostream& s,const Knot &k)
 Knot::Knot():
   prev(NULL), left(NULL), right(NULL), decay(NULL),
   part(NULL), stat(0), kn_no(-1),
-  shower(1), maxjets(1000), didkin(false), 
+  shower(1), maxjets(1000), dir(0), didkin(false), 
   t(0.0), tout(0.0), tmax(0.0), z(0.0), zs(0.0),
   E2(0.0), costh(0.0), phi(0.0), thcrit(M_PI),
   maxpt2(1.0e10), x(0.), pt2lcm(0.0), smaxpt2(1.0e10), sthcrit(M_PI),
-  minpt2(0.0), qjv(1.0e10), qljv(0.0), tmo(0.0), lz(0.0), lE2(0.0), dir(0) {}
+  minpt2(0.0), qjv(1.0e10), qljv(0.0), tmo(0.0), lz(0.0), lE2(0.0) {}
 
 Knot::Knot(Knot * k):
   prev(k->prev), left(k->left), right(k->right), decay(k->decay), 
   part(new ATOOLS::Particle(*k->part)), lp(k->lp), 
   stat(k->stat), kn_no(k->kn_no),
-  shower(k->shower), maxjets(k->maxjets), didkin(k->didkin), 
+  shower(k->shower), maxjets(k->maxjets), dir(k->dir), didkin(k->didkin), 
   t(k->t), tout(k->tout), tmax(k->tmax), z(k->z), zs(0.0),
   E2(k->E2), costh(k->costh), phi(k->phi), thcrit(k->thcrit), 
   maxpt2(k->maxpt2), x(k->x), pt2lcm(0.0), smaxpt2(k->smaxpt2), 
   sthcrit(k->sthcrit), minpt2(k->minpt2), qjv(k->qjv), qljv(k->qljv), 
-  tmo(k->tmo), lz(k->lz), lE2(k->lE2), polinfo(k->polinfo), dir(k->dir) 
+  tmo(k->tmo), lz(k->lz), lE2(k->lE2), polinfo(k->polinfo)
 {
   part->SetProductionBlob(k->part->ProductionBlob());
   part->SetDecayBlob(k->part->DecayBlob());
