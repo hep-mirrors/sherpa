9140a2981de16ee15021476812f0e25b80c6cacd
Stefan Hoeche <shoeche@slac.stanford.edu>
2007-01-11 16:06:17 +0000

    bugfix for ordering observables

--- ANALYSIS-1.0/Observables/Particle_Ordering.C
+++ ANALYSIS-1.0/Observables/Particle_Ordering.C
@@ -24,7 +24,7 @@ Primitive_Observable_Base *const GetOrdering(const Argument_Matrix &parameters)
 }									
 
 #define DEFINE_GETTER_METHOD(CLASS,NAME)				\
-  Primitive_Observable_Base *					\
+  Primitive_Observable_Base *						\
   NAME::operator()(const Argument_Matrix &parameters) const		\
   { return GetOrdering<CLASS>(parameters); }
 
@@ -32,117 +32,63 @@ Primitive_Observable_Base *const GetOrdering(const Argument_Matrix &parameters)
   void NAME::PrintInfo(std::ostream &str,const size_t width) const	\
   { str<<"inlist outlist"; }
 
-#define DEFINE_ORDERING_GETTER(CLASS,NAME,TAG)			\
+#define DEFINE_ORDERING_GETTER(CLASS,NAME,TAG)				\
   DECLARE_GETTER(NAME,TAG,Primitive_Observable_Base,Argument_Matrix);	\
   DEFINE_GETTER_METHOD(CLASS,NAME)					\
   DEFINE_PRINT_METHOD(NAME)
 
 using namespace ATOOLS;
 
+#define DEFINE_PARTICLE_ORDERING(NAME,GNAME,CNAME,VAR,TAG)		\
+  class CNAME {								\
+  public:								\
+    bool operator()(const Particle *a,const Particle *b)		\
+    {									\
+      return a->Momentum().VAR()>b->Momentum().VAR();			\
+    }									\
+  };									\
+  class NAME: public Primitive_Observable_Base {			\
+  private:								\
+    std::string m_outlist;						\
+  public:								\
+    NAME(const std::string &inlist,const std::string &outlist);		\
+    void Evaluate(const ATOOLS::Particle_List &particlelist,		\
+		  double weight,int ncount);				\
+    Primitive_Observable_Base *Copy() const;				\
+    void EndEvaluation(double scale);					\
+  };									\
+  DEFINE_ORDERING_GETTER(NAME,GNAME,TAG)				\
+  NAME::NAME(const std::string &inlist,const std::string &outlist):	\
+    m_outlist(outlist)							\
+  {									\
+    m_splitt_flag = false;						\
+    m_listname=inlist;							\
+  }									\
+  void NAME::Evaluate(const ATOOLS::Particle_List &particlelist,	\
+		      double weight,int ncount)				\
+  {									\
+    Particle_List *outlist(new Particle_List());			\
+    outlist->resize(particlelist.size());				\
+    for (size_t i(0);i<particlelist.size();++i)				\
+      (*outlist)[i] = new ATOOLS::Particle(*particlelist[i]);		\
+    std::sort(outlist->begin(),outlist->end(),CNAME());			\
+    p_ana->AddParticleList(m_outlist,outlist);				\
+  }									\
+  Primitive_Observable_Base *NAME::Copy() const				\
+  {									\
+    return new NAME(m_listname,m_outlist);				\
+  }									\
+  void NAME::EndEvaluation(double scale)				\
+  {									\
+  }									
+  
+
 namespace ANALYSIS {
 
-  class Order_PT {
-  public:
-    bool operator()(const Particle *a,const Particle *b)
-    {
-      return a->Momentum().PPerp2()>b->Momentum().PPerp2();
-    }
-  };
-
-  class PT_Ordering: public Primitive_Observable_Base {
-  private:
-
-    std::string m_outlist;
-
-  public:
-
-    PT_Ordering(const std::string &inlist,const std::string &outlist);
-    void Evaluate(const ATOOLS::Particle_List &particlelist,
-		  double weight,int ncount);
-    Primitive_Observable_Base *Copy() const;
-    void EndEvaluation(double scale);
-
-  };
-
-  DEFINE_ORDERING_GETTER(PT_Ordering,PT_Ordering_Getter,"PTOrder")
-    
-  PT_Ordering::PT_Ordering(const std::string &inlist,
-			   const std::string &outlist):
-    m_outlist(outlist)
-  {
-    m_splitt_flag = false;
-    m_listname=inlist;
-  }
-
-  void PT_Ordering::Evaluate(const ATOOLS::Particle_List &particlelist,
-			     double weight,int ncount)
-  {
-    Particle_List *outlist(new Particle_List(particlelist.size()));
-    p_ana->AddParticleList(m_outlist,outlist);
-    for (size_t i(0);i<particlelist.size();++i) 
-      (*outlist)[i] = new ATOOLS::Particle(*particlelist[i]);
-    std::sort(outlist->begin(),outlist->end(),Order_PT());
-  }
-
-  Primitive_Observable_Base *PT_Ordering::Copy() const
-  {
-    return new PT_Ordering(m_listname,m_outlist);
-  }
-
-  void PT_Ordering::EndEvaluation(double scale)
-  {
-  }
-  
-  class Order_ET {
-  public:
-    bool operator()(const Particle *a,const Particle *b)
-    {
-      return a->Momentum().EPerp()>b->Momentum().EPerp();
-    }
-  };
-
-  class ET_Ordering: public Primitive_Observable_Base {
-  private:
-
-    std::string m_outlist;
-
-  public:
-
-    ET_Ordering(const std::string &inlist,const std::string &outlist);
-    void Evaluate(const ATOOLS::Particle_List &particlelist,
-		  double weight,int ncount);
-    Primitive_Observable_Base *Copy() const;
-    void EndEvaluation(double scale);
-
-  };
-
-  DEFINE_ORDERING_GETTER(ET_Ordering,ET_Ordering_Getter,"ETOrder")
-    
-  ET_Ordering::ET_Ordering(const std::string &inlist,
-			   const std::string &outlist):
-    m_outlist(outlist)
-  {
-    m_splitt_flag = false;
-    m_listname=inlist;
-  }
-
-  void ET_Ordering::Evaluate(const ATOOLS::Particle_List &particlelist,
-			     double weight,int ncount)
-  {
-    Particle_List *outlist(new Particle_List());
-    p_ana->AddParticleList(m_outlist,outlist);
-    for (size_t i(0);i<particlelist.size();++i) 
-      outlist->push_back(new ATOOLS::Particle(*particlelist[i]));
-    std::sort(outlist->begin(),outlist->end(),Order_ET());
-  }
-
-  Primitive_Observable_Base *ET_Ordering::Copy() const
-  {
-    return new ET_Ordering(m_listname,m_outlist);
-  }
-
-  void ET_Ordering::EndEvaluation(double scale)
-  {
-  }
+  DEFINE_PARTICLE_ORDERING(Order_PT,Order_PT_Getter,Sort_PT,PPerp2,"PTOrder")
+  DEFINE_PARTICLE_ORDERING(Order_ET,Order_ET_Getter,Sort_ET,EPerp,"ETOrder")
+  DEFINE_PARTICLE_ORDERING(Order_Y,Order_Y_Getter,Sort_Y,Y,"YOrder")
+  DEFINE_PARTICLE_ORDERING(Order_Eta,Order_Eta_Getter,Sort_Eta,Eta,"EtaOrder")
+  DEFINE_PARTICLE_ORDERING(Order_Phi,Order_Phi_Getter,Sort_Phi,Phi,"PhiOrder")
   
 }
