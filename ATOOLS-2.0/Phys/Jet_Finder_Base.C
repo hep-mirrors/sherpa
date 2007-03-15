ea80401b8142440a268abd9ffdcbed55c20e3bd3
Stefan Hoeche <shoeche@slac.stanford.edu>
2007-03-15 15:16:40 +0000

    added new base class for jet finders

---
+++ ATOOLS-2.0/Phys/Jet_Finder_Base.C
@@ -0,0 +1,57 @@
+#include "Jet_Finder_Base.H"
+
+#include "Message.H"
+
+namespace ATOOLS {
+
+  std::vector<int> ID(size_t id)
+  {
+    std::vector<int> ids;
+    for (size_t n(0);id>0;++n) {
+      if (id&(1<<n)) {
+	ids.push_back(n);
+	id-=1<<n;
+      }
+    }
+    return ids;
+  }
+
+  template <typename Type>
+  size_t ID(const std::vector<Type> &ids)
+  {
+    size_t id(0);
+    for (size_t i(0);i<ids.size();++i) 
+      if (ids[i]>(Type)0) id+=1<<i;
+    return id;
+  }
+
+  template size_t ID(const std::vector<int> &ids);
+  template size_t ID(const std::vector<double> &ids);
+
+
+}
+
+using namespace ATOOLS;
+
+Jet_Finder_Base::Jet_Finder_Base():
+  m_ycut(-1.0), m_gycut(-1.0), m_cycut(-1.0), m_gcycut(-1.0), m_delta_r(1.0)
+{
+}
+
+Jet_Finder_Base::~Jet_Finder_Base() 
+{
+}
+
+void Jet_Finder_Base::SetDeltaR(double dr) 
+{ 
+  if (dr<=1.e-6) {
+    msg.Error()<<METHOD<<"(): \\delta_R to small, ignore and set to "
+	       <<m_delta_r<<"."<<std::endl;
+    return;
+  }
+  m_delta_r=dr; 
+}
+
+void Jet_Finder_Base::FillCombinations()
+{
+}
