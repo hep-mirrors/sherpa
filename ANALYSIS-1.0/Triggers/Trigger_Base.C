0313f74aa2c12fb80743751cf983bfd36528b3c7
Stefan Hoeche <shoeche@slac.stanford.edu>
2007-04-06 13:52:31 +0000

    added n list trigger base

--- ANALYSIS-1.0/Triggers/Trigger_Base.C
+++ ANALYSIS-1.0/Triggers/Trigger_Base.C
@@ -49,4 +49,25 @@ void Two_List_Trigger_Base::Evaluate(const ATOOLS::Blob_List &bl,
   p_ana->AddParticleList(m_outlist,outlist);
 }
 
+N_List_Trigger_Base::N_List_Trigger_Base
+(const std::vector<std::string> &inlists,const std::string &outlist):
+  m_inlists(inlists), m_outlist(outlist) {}
+
+void N_List_Trigger_Base::Evaluate(const ATOOLS::Blob_List &bl, 
+				   double weight, int ncount)
+{
+  std::vector<const Particle_List*> inlists(m_inlists.size());
+  for (size_t i(0);i<m_inlists.size();++i) {
+    inlists[i]=p_ana->GetParticleList(m_inlists[i]);
+    if (inlists[i]==NULL) {
+      msg.Error()<<METHOD<<"(): List "<<i<<" '"<<m_inlists[i]
+		 <<"' not found."<<std::endl;
+      return;
+    }
+  }
+  Particle_List *outlist(new Particle_List());
+  Evaluate(inlists,*outlist,weight,ncount);
+  p_ana->AddParticleList(m_outlist,outlist);
+}
+
 
