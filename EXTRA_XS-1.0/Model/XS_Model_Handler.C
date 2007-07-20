2a911500678634d8e0649cfa6921f1ca95c31296
Frank Krauss <frank.krauss@durham.ac.uk>
2007-07-20 16:28:12 +0000

    bugfix

--- EXTRA_XS-1.0/Model/XS_Model_Handler.C
+++ EXTRA_XS-1.0/Model/XS_Model_Handler.C
@@ -1,11 +1,13 @@
 #include "XS_Model_Handler.H"
 
 #include "XS_Model_SM.H"
+#include "XS_Model_Phantom_U1.H"
 
 using namespace EXTRAXS;
 
 XS_Model_Base *XS_Model_Handler::GetModel(const std::string &name)
 {
   if (name=="SM") return new XS_Model_SM();
+  if (name=="SM+Phantom_U1") return new XS_Model_Phantom_U1();
   return NULL;
 }
