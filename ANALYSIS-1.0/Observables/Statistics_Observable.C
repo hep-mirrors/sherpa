670fc4b09866b845ab29b13b7c259fb2db600271
Stefan Hoeche <shoeche@slac.stanford.edu>
2007-10-15 16:29:53 +0000

    corrected statistics_observable, checked for standard weighted and unweighted events

--- ANALYSIS-1.0/Observables/Statistics_Observable.C
+++ ANALYSIS-1.0/Observables/Statistics_Observable.C
@@ -1,4 +1,56 @@
-#include "Statistics_Observable.H"
+#ifndef Statistics_Observable_H
+#define Statistics_Observable_H
+
+#include "Primitive_Observable_Base.H"
+#include <iostream>
+#include <iomanip>
+
+namespace ANALYSIS {
+
+  struct Statistics_Data {
+
+    long unsigned int m_xsn, m_cutxsn, m_n, m_cutn;
+    double m_wsum, m_cutwsum, m_xssum, m_cutxssum;
+    double m_sudsum, m_cutsudsum;
+    double m_sum, m_cutsum, m_sum2, m_cutsum2;
+
+    Statistics_Data():
+      m_xsn(0), m_cutxsn(0), m_n(0), m_cutn(0), m_wsum(0.0), m_cutwsum(0.0),
+      m_xssum(0.0), m_cutxssum(0.0), m_sudsum(0.0), m_cutsudsum(0.0),
+      m_sum(0.0), m_cutsum(0.0), m_sum2(0.0), m_cutsum2(0.0) {} 
+
+    void EndEvaluation(const double scale=1.0);
+    void Output(const std::string &pname);
+
+  };// end of struct Statistics_Data
+
+  typedef std::map<std::string,Statistics_Data> Statistics_Map;
+
+  class Statistics_Observable: public Primitive_Observable_Base {  
+  protected:
+
+    unsigned long int m_n;
+    Statistics_Map m_stats;
+
+  public:
+
+    Statistics_Observable(const std::string &inlistname,int mode=0);
+
+    void Evaluate(const ATOOLS::Blob_List &blobs,
+		  double weight=1.0,int ncount=1);
+
+    void EndEvaluation(double scale=1.0);
+    void Output(const std::string & pname);
+
+    Primitive_Observable_Base * Copy() const;
+
+  };// end of class Statistics_Observable
+
+  std::ostream & operator<<(std::ostream & str,const Statistics_Data &sd);
+
+}// end of namespace ANALYSIS
+
+#endif
 
 using namespace ANALYSIS;
 
@@ -6,15 +58,17 @@ DECLARE_GETTER(Statistics_Observable_Getter,"Statistics",
 	       Primitive_Observable_Base,Argument_Matrix);
 
 Primitive_Observable_Base * 
-Statistics_Observable_Getter::operator()(const Argument_Matrix &parameters) const
+Statistics_Observable_Getter::operator()
+  (const Argument_Matrix &parameters) const
 {
-  std::string listname="Analysed";
-  if (parameters.size()>0 && parameters[0].size()>0) listname=parameters[0][0];
+  std::string listname="FinalState";
+  if (parameters.size()>0 && parameters[0].size()>0) 
+    listname=parameters[0][0];
   return new Statistics_Observable(listname);
 }
 
-void Statistics_Observable_Getter::PrintInfo(std::ostream &str,
-					     const size_t width) const
+void Statistics_Observable_Getter::
+PrintInfo(std::ostream &str,const size_t width) const
 { 
   str<<"[list]"; 
 }
@@ -27,69 +81,95 @@ void Statistics_Observable_Getter::PrintInfo(std::ostream &str,
 
 using namespace ATOOLS;
 
-Statistics_Observable::Statistics_Observable(const std::string & listname, int mode)
+Statistics_Observable::Statistics_Observable
+(const std::string & listname, int mode)
 {
-  m_name  = "Statistics_Observable";
-  m_type  = mode;
-  m_listname    = listname;
-  m_splitt_flag = false;
+  m_listname=listname;
+  m_name="Statistics_Observable_"+m_listname;
+  m_type=mode;
+  m_splitt_flag=false;
 }
 
-void Statistics_Observable::Evaluate(const Blob_List &  blobs,double value, int ncount)
+void Statistics_Observable::Evaluate
+(const Blob_List &  blobs,double value, int ncount)
 {
-  Particle_List * pl     = p_ana->GetParticleList("FinalState");
-  Particle_List * pl_cut = p_ana->GetParticleList(m_listname);
+  Particle_List *pl_cut(p_ana->GetParticleList(m_listname));
 
-  std::string key="unknown";
-  unsigned long blcount=0;
-  for (Blob_List::const_iterator bit=blobs.begin();bit!=blobs.end();++bit) {
-    ++blcount;
-    if ((*bit)->Type()==btp::Signal_Process) key  = (*bit)->TypeSpec();
-  }
+  Blob *sp(blobs.FindFirst(btp::Signal_Process));
+  std::string key(sp!=NULL?sp->TypeSpec():"<unknown>");
 
-  Statistics_Map::iterator cit=m_signal_process_statistics.find(key);
-  if (cit==m_signal_process_statistics.end()) {
-    m_signal_process_statistics[key]=Statistics_Data();
-    cit=m_signal_process_statistics.find(key);
+  Statistics_Map::iterator cit(m_stats.find(key));
+  if (cit==m_stats.end()) {
+    m_stats[key]=Statistics_Data();
+    cit=m_stats.find(key);
   }
 
   Blob_Data_Base *data((*p_ana)["XS_Weight"]);
-  double xsecweight(data?data->Get<double>():1.0);
+  double xsweight(data?data->Get<double>():1.0);
   data=(*p_ana)["Sud_Weight"];
   double sudweight(data?data->Get<double>():1.0);
-  m_nevt+=ncount;
-  cit->second.nevt+=ncount;
-  cit->second.xsnevt+=1;
-  cit->second.nblobssum+=blcount;
-  cit->second.nplsum+=pl->size();
-  cit->second.weightsum+=value;
-  cit->second.xssum+=xsecweight;
-  cit->second.sudsum+=sudweight;
+  data=(*p_ana)["Process_Weight"];
+  double procweight(data?data->Get<double>():1.0);
+  xsweight/=procweight;
+  sudweight*=xsweight;
+  m_n+=ncount;
+  cit->second.m_n+=ncount;
+  cit->second.m_xsn+=ncount;
+  cit->second.m_wsum+=value;
+  cit->second.m_xssum+=xsweight;
+  cit->second.m_sum+=sudweight;
+  cit->second.m_sum2+=sqr(sudweight);
 
-  if (pl_cut) {
-    if (pl_cut->size()>0) {
-      cit->second.nevt_cut+=ncount;
-      cit->second.nblobssum_cut+=blcount;
-      cit->second.nplsum_cut+=pl_cut->size();
-      cit->second.weightsum_cut+=value;
-    }
+  if (pl_cut && pl_cut->size()>0) {
+    cit->second.m_cutn+=ncount;
+    cit->second.m_cutxsn+=ncount;
+    cit->second.m_cutwsum+=value;
+    cit->second.m_cutxssum+=xsweight;
+    cit->second.m_cutsum+=sudweight;
+    cit->second.m_cutsum2+=sqr(sudweight);
   }
 }
 
-void Statistics_Observable::EndEvaluation(double scale) {
+void Statistics_Observable::EndEvaluation(double scale) 
+{
 }
 
-void Statistics_Observable::Output(const std::string & pname) {
-  int  mode_dir = 448;
-  ATOOLS::MakeDir((pname).c_str(),mode_dir); 
-  std::ofstream file((pname+std::string("/")+m_name).c_str());
-
-  double  sum(0.0);
-  for (Statistics_Map::const_iterator cit=m_signal_process_statistics.begin();cit!=m_signal_process_statistics.end();++cit) {
-    file<<cit->first<<" : "<<cit->second<<std::endl;
-    sum+=cit->second.xssum*cit->second.sudsum/sqr(double(cit->second.xsnevt));
+void Statistics_Observable::Output(const std::string & pname) 
+{
+  ATOOLS::MakeDir((pname).c_str(),448); 
+  std::ofstream file((pname+"/"+m_name).c_str());
+  file<<std::setw(30)<<std::left<<"# Process Name"<<std::right
+      <<" "<<std::setw(9)<<"# evts"
+      <<" "<<std::setw(15)<<"<w>"
+      <<" "<<std::setw(15)<<"<sigma>"
+      <<" "<<std::setw(15)<<"<Delta>"
+      <<"   "<<std::setw(9)<<"# evts cut"
+      <<" "<<std::setw(15)<<"<w> cut"
+      <<" "<<std::setw(15)<<"<sigma> cut"
+      <<" "<<std::setw(15)<<"<Delta> cut\n";
+  double sig(0.0), dsig(0.0);
+  double csig(0.0), cdsig(0.0);
+  for (Statistics_Map::const_iterator cit(m_stats.begin());
+       cit!=m_stats.end();++cit) {
+    file<<std::setw(30)<<std::left<<cit->first<<std::right
+	<<" "<<cit->second<<std::endl;
+    double wm(cit->second.m_sum/cit->second.m_xsn);
+    double w2m(cit->second.m_sum2/cit->second.m_xsn);
+    sig+=wm;
+    if (cit->second.m_xsn>1) 
+      dsig+=(w2m-sqr(wm))/(cit->second.m_xsn-1.0);
+    double cwm(cit->second.m_cutsum/cit->second.m_cutxsn);
+    double cw2m(cit->second.m_cutsum2/cit->second.m_cutxsn);
+    csig+=cwm;
+    if (cit->second.m_cutxsn>1)
+      cdsig+=(cw2m-sqr(cwm))/(cit->second.m_cutxsn-1.0);
   }
-  file<<"total xs: "<<sum<<"\n";
+//   dsig=sqrt(dsig);
+//   cdsig=sqrt(cdsig);
+  file<<"\n# Total XS      : "<<sig<<" +- ( "
+      <<dsig<<" = "<<int(dsig*100/sig)/100.0<<" % )\n";
+  file<<"# Total XS (cut): "<<csig<<" +- ( "
+      <<cdsig<<" = "<<int(cdsig*100/csig)/100.0<<" % )\n\n";
   file.close();
 }
 
@@ -98,13 +178,16 @@ Primitive_Observable_Base * Statistics_Observable::Copy() const
   return new Statistics_Observable(m_listname,m_type);
 }
 
-std::ostream & ANALYSIS::operator<<(std::ostream & str, const Statistics_Data & sd) {
-  str<<sd.nevt<<","<<double(sd.nblobssum)/double(sd.nevt)<<","
-     <<double(sd.nplsum)/double(sd.nevt)<<","
-     <<sd.weightsum/double(sd.nevt)<<", "<<sd.xssum/double(sd.xsnevt)
-     <<","<<sd.sudsum/double(sd.xsnevt)<<",    ";
-  str<<sd.nevt_cut<<","<<double(sd.nblobssum_cut)/double(sd.nevt_cut)<<","
-     <<double(sd.nplsum_cut)/double(sd.nevt_cut)<<","
-     <<sd.weightsum/double(sd.nevt_cut);
+std::ostream & ANALYSIS::operator<<
+  (std::ostream & str,const Statistics_Data &sd) 
+{
+  str<<std::setw(9)<<sd.m_n
+     <<" "<<std::setw(15)<<sd.m_wsum/double(sd.m_xsn)
+     <<" "<<std::setw(15)<<sd.m_xssum/double(sd.m_xsn)
+     <<" "<<std::setw(15)<<sd.m_sum/sd.m_xssum
+     <<"   "<<std::setw(9)<<sd.m_cutn
+     <<" "<<std::setw(15)<<sd.m_cutwsum/double(sd.m_cutxsn)
+     <<" "<<std::setw(15)<<sd.m_cutxssum/double(sd.m_cutxsn)
+     <<" "<<std::setw(15)<<sd.m_cutsum/sd.m_cutxssum;
   return str;
 }
