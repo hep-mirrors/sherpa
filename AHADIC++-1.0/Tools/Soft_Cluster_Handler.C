9a912ab5ed032bbdea6c0173991c4e7c606454dc
Jan Winter <jwinter@pa.msu.edu>
2008-04-09 07:03:34 +0000

    bugfix for double hadronization

--- AHADIC++-1.0/Tools/Soft_Cluster_Handler.C
+++ AHADIC++-1.0/Tools/Soft_Cluster_Handler.C
@@ -31,7 +31,6 @@ bool Soft_Cluster_Handler::TreatClusterList(Cluster_List * clin, Blob * blob)
   }
   if (size==0) return true;
 
-
   if (clin->size()==1) {
     cluster = (*clin->begin());
     switch (cluster->size()) {
@@ -42,6 +41,8 @@ bool Soft_Cluster_Handler::TreatClusterList(Cluster_List * clin, Blob * blob)
       return false;
     case 2:
       FixHHDecay(cluster,blob);
+      cluster->SetActive(false);
+      clin->clear();
     case 0:
     default:
       return true;
