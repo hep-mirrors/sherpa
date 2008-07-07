d03a7279e9112566e536aded252ba43377d508b9
Frank Siegert <frank.siegert@cern.ch>
2008-07-07 15:38:35 +0000

    Improve HepMC GenEvent output (write start and end tag in each file).

--- SHERPA/Tools/Output_HepMC2_Genevent.C
+++ SHERPA/Tools/Output_HepMC2_Genevent.C
@@ -10,13 +10,37 @@ using namespace SHERPA;
 using namespace ATOOLS;
 using namespace std;
 
-void Output_HepMC2_Genevent::Output(Blob_List* blobs, const double weight) 
+Output_HepMC2_Genevent::Output_HepMC2_Genevent(std::string basename,
+                                               std::string ext,int precision) :
+  Output_Base(basename, ext, precision)
 {
-  m_hepmc2.Sherpa2HepMC(blobs);
 #ifdef USING__HEPMC2__IOGENEVENT
-  HepMC::IO_GenEvent io(m_outstream);
-  io.write_event(m_hepmc2.GenEvent());
+  p_iogenevent = new HepMC::IO_GenEvent(m_outstream);
 #else
   THROW(fatal_error,"HepMC::IO_GenEvent asked for, but HepMC version too old.");
+#endif
+}
+
+Output_HepMC2_Genevent::~Output_HepMC2_Genevent()
+{
+#ifdef USING__HEPMC2__IOGENEVENT
+  delete p_iogenevent;
+#endif
+}
+
+void Output_HepMC2_Genevent::Output(Blob_List* blobs, const double weight) 
+{
+#ifdef USING__HEPMC2__IOGENEVENT
+  m_hepmc2.Sherpa2HepMC(blobs);
+  p_iogenevent->write_event(m_hepmc2.GenEvent());
+#endif
+}
+
+void Output_HepMC2_Genevent::ChangeFile(string number)
+{
+#ifdef USING__HEPMC2__IOGENEVENT
+  delete p_iogenevent;
+  Output_Base::ChangeFile(number);
+  p_iogenevent = new HepMC::IO_GenEvent(m_outstream);
 #endif
 }
