7398f4a600252a6da0b0b1ef8d15e916040ec164
Stefan Hoeche <shoeche@slac.stanford.edu>
2009-07-04 15:54:48 +0000

    added d0 run ii jet analysis

--- Examples/Tevatron_QCD/compare.conf/1960.C
+++ Examples/Tevatron_QCD/compare.conf/1960.C
@@ -1,4 +1,5 @@
 (initial){
+  PIECE_SETUP arXiv_08022400.C (initial){ }(initial);
   PIECE_SETUP hep-ex_0701051.C (initial){ }(initial);
   PIECE_SETUP hep-ex_0409040.C (initial){ }(initial);
 }(initial);
