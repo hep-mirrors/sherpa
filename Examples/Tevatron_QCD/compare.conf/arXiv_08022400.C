b68a8f0bcf65194177676c857b88c2d3bfeb66c8
Stefan Hoeche <shoeche@slac.stanford.edu>
2009-07-04 21:33:30 +0000

    added d0 run ii jet analysis plot sup

---
+++ Examples/Tevatron_QCD/compare.conf/arXiv_08022400.C
@@ -0,0 +1,357 @@
+(initial){
+  PIECE_SETUP arXiv_08022400.C (ptall){ }(ptall);
+  PIECE_SETUP arXiv_08022400.C (dptall){ }(dptall);
+  PIECE_SETUP arXiv_08022400.C (logptall){ }(logptall);
+  PIECE_SETUP arXiv_08022400.C (dlogptall){ }(dlogptall);
+  TOP_AXIS YES; RIGHT_AXIS YES;
+  DEFINE_COLOURS VIOLET1 195 0 185,;
+  DEFINE_COLOURS BLUE1 25 25 205,;
+  DEFINE_COLOURS GREEN1 30 145 35,;
+  DEFINE_COLOURS YELLOW1 240 215 10,;
+  DEFINE_COLOURS RED1 195 15 15,;
+  DEFINE_COLOURS BLUE2 15 20 180,;
+  DEFINE_COLOURS GREEN2 15 125 25,;
+  DEFINE_COLOURS RED2 175 5 5,;
+}(initial);
+
+// standard plots
+
+(ptall){
+  PIECE_SETUP arXiv_08022400.C (yranges){ }(yranges);
+  DRAW YES;
+  X_TITLE_OFFSET 1.1; BOTTOM_MARGIN 0.11;
+  Y_TITLE_OFFSET 1.6; LEFT_MARGIN 0.14;
+  RIGHT_MARGIN 0.035; TOP_MARGIN 0.025;
+  X_MIN 0.001; X_MAX 799.999;
+  X_AXIS_TITLE k_{#perp }^{jet} #left[ GeV #right]
+  Y_MIN 1.001e-10; Y_MAX 1.999e10; Y_SCALING Log_B_10;
+  @@ YSFAC 1;
+  Y_AXIS_TITLE d#sigma / dk_{#perp }^{jet}dy^{jet} #left[ pb/GeV #right]
+  HISTOGRAM_NAME Jet_PT;
+  FIGURE_CAPTION Jet $P_T$; WEBPAGE_CAPTION P<sub>T,jet</sub>;
+  LEG_LEFT 0.65; LEG_RIGHT 0.85; LEG_TOP 0.925; LEG_TEXT_SIZE 0.03;
+  WATERMARK Cone\, D = 0.7 | COLOUR 1 SIZE 0.03 LEFT 0.25 TOP 0.9 ALIGN 13\;;
+  WATERMARK \\\|y^{jet }\\\|<0.4 (x10^{5}) | COLOUR 1 SIZE 0.025 LEFT 0.825 TOP 0.475 ALIGN 22\;;
+  WATERMARK 0.4<\\\|y^{jet }\\\|<0.8 (x10^{3}) | COLOUR 1 SIZE 0.025 LEFT 0.7875 TOP 0.375 ALIGN 22\;;
+  WATERMARK 0.8<\\\|y^{jet }\\\|<1.2 (x10^{1}) | COLOUR 1 SIZE 0.025 LEFT 0.675 TOP 0.3 ALIGN 22\;;
+  WATERMARK 1.2<\\\|y^{jet }\\\|<1.6 (x10^{-1}) | COLOUR 1 SIZE 0.025 LEFT 0.525 TOP 0.225 ALIGN 22\;;
+  WATERMARK 1.6<\\\|y^{jet }\\\|<2.0 (x10^{-3}) | COLOUR 1 SIZE 0.025 LEFT 0.475 TOP 0.1375 ALIGN 22\;;
+  WATERMARK 2.0<\\\|y^{jet }\\\|<2.4 (x10^{-5}) | COLOUR 1 SIZE 0.025 LEFT 0.425 TOP 0.05 ALIGN 22\;;
+  WATERMARK parton level | SIZE .035 COLOUR RED1 LEFT .925 TOP .1 ALIGN 32 PRIORITY 10\;;
+  WATERMARK SHERPA | SIZE .04 COLOUR 19 LEFT .95 TOP .05 ALIGN 32 PRIORITY -10\;;
+}(ptall);
+
+(logptall){
+  PIECE_SETUP arXiv_08022400.C (yranges){ }(yranges);
+  DRAW YES;
+  X_TITLE_OFFSET 1.1; BOTTOM_MARGIN 0.11;
+  Y_TITLE_OFFSET 1.6; LEFT_MARGIN 0.14;
+  RIGHT_MARGIN 0.035; TOP_MARGIN 0.025;
+  X_MIN 45; X_MAX 1500; X_SCALING Log_B_10;
+  X_AXIS_TITLE k_{#perp }^{jet} #left[ GeV #right]
+  Y_MIN 1.001e-10; Y_MAX 1.999e10; Y_SCALING Log_B_10;
+  @@ YSFAC 1;
+  Y_AXIS_TITLE d#sigma / dk_{#perp }^{jet}dy^{jet} #left[ pb/GeV #right]
+  HISTOGRAM_NAME Jet_PT;
+  FIGURE_CAPTION Jet $P_T$; WEBPAGE_CAPTION log(P<sub>T,jet</sub>);
+  LEG_LEFT 0.65; LEG_RIGHT 0.85; LEG_TOP 0.95; LEG_TEXT_SIZE 0.03;
+  WATERMARK Cone\, D = 0.7 | COLOUR 1 SIZE 0.03 LEFT 0.35 TOP 0.925 ALIGN 13\;;
+  WATERMARK \\\|y^{jet }\\\|<0.4 (x10^{5 }) | COLOUR 1 SIZE 0.025 LEFT 0.85 TOP 0.475 ALIGN 22\;;
+  WATERMARK 0.4<\\\|y^{jet }\\\|<0.8 (x10^{3 }) | COLOUR 1 SIZE 0.025 LEFT 0.8375 TOP 0.4 ALIGN 22\;;
+  WATERMARK 0.8<\\\|y^{jet }\\\|<1.2 (x10^{1 }) | COLOUR 1 SIZE 0.025 LEFT 0.825 TOP 0.325 ALIGN 22\;;
+  WATERMARK 1.2<\\\|y^{jet }\\\|<1.6 (x10^{-1 }) | COLOUR 1 SIZE 0.025 LEFT 0.76 TOP 0.25 ALIGN 22\;;
+  WATERMARK 1.6<\\\|y^{jet }\\\|<2.0 (x10^{-3 }) | COLOUR 1 SIZE 0.025 LEFT 0.675 TOP 0.15 ALIGN 22\;;
+  WATERMARK 2.0<\\\|y^{jet }\\\|<2.4 (x10^{-5 }) | COLOUR 1 SIZE 0.025 LEFT 0.6 TOP 0.05 ALIGN 22\;;
+  WATERMARK parton level | SIZE .035 COLOUR RED1 LEFT .075 TOP .1 ALIGN 12 PRIORITY 10\;;
+  WATERMARK SHERPA | SIZE .04 COLOUR 19 LEFT .05 TOP .05 ALIGN 12 PRIORITY -10\;;
+}(logptall);
+
+(yranges){ 
+  PIECE_SETUP arXiv_08022400.C (0.0-0.1){ }(0.0-0.1);
+  PIECE_SETUP arXiv_08022400.C (0.1-0.7){ }(0.1-0.7);
+  PIECE_SETUP arXiv_08022400.C (0.7-1.1){ }(0.7-1.1);
+  PIECE_SETUP arXiv_08022400.C (1.1-1.6){ }(1.1-1.6);
+  PIECE_SETUP arXiv_08022400.C (1.6-2.1){ }(1.6-2.1);
+  PIECE_SETUP arXiv_08022400.C (2.1-2.4){ }(2.1-2.4);
+}(yranges);
+
+(0.0-0.1){ 
+  PIECE_SETUP arXiv_08022400.C (samples){ }(samples);
+  FILE_PIECE ConeJets_0.0-0.4_jet_1_1_pt_0.dat;
+  @@ DATSFAC 1e5; @@ YRANGE (0.4-0.0)
+  LEGEND_ENABLED YES;
+}(0.0-0.1);
+(0.1-0.7){ 
+  PIECE_SETUP arXiv_08022400.C (samples){ }(samples);
+  FILE_PIECE ConeJets_0.4-0.8_jet_1_1_pt_0.dat;
+  @@ DATSFAC 1e3; @@ YRANGE (0.8-0.4)
+}(0.1-0.7);
+(0.7-1.1){ 
+  PIECE_SETUP arXiv_08022400.C (samples){ }(samples);
+  FILE_PIECE ConeJets_0.8-1.2_jet_1_1_pt_0.dat;
+  @@ DATSFAC 1e1; @@ YRANGE (1.2-0.8)
+}(0.7-1.1);
+(1.1-1.6){ 
+  PIECE_SETUP arXiv_08022400.C (samples){ }(samples);
+  FILE_PIECE ConeJets_1.2-1.6_jet_1_1_pt_0.dat;
+  @@ DATSFAC 1e-1; @@ YRANGE (1.6-1.2)
+}(1.1-1.6);
+(1.6-2.1){ 
+  PIECE_SETUP arXiv_08022400.C (samples){ }(samples);
+  FILE_PIECE ConeJets_1.6-2.0_jet_1_1_pt_0.dat;
+  @@ DATSFAC 1e-3; @@ YRANGE (2.0-1.6)
+}(1.6-2.1);
+(2.1-2.4){ 
+  PIECE_SETUP arXiv_08022400.C (samples){ }(samples);
+  FILE_PIECE ConeJets_2.0-2.4_jet_1_1_pt_0.dat;
+  @@ DATSFAC 1e-5; @@ YRANGE (2.4-2.0)
+}(2.1-2.4);
+
+(samples){
+  PIECE_SETUP arXiv_08022400.C (data){ }(data);
+  PIECE_SETUP arXiv_08022400.C (bfkl){ }(bfkl);
+  LEGEND_ENABLED NO;
+}(samples);
+
+(data){ 
+  DATA_TYPE ASCII;
+  PATH_PIECE data/arXiv_08022400/;
+  Y_FUNCTION y*DATSFAC;
+  DRAW_OPTION P; DRAW_PRIORITY 20;
+  X_VALUE 1; Y_VALUE 2;
+  X_ERROR_MINUS 3; X_ERROR_PLUS 4;
+  Y_ERROR_MINUS 5; Y_ERROR_PLUS 6;
+  LEGEND_TITLE DO Data;
+}(data);
+(bfkl){
+  PIECE_SETUP arXiv_08022400.C (paths){ }(paths);
+  READER_PARAMETERS ADOPT_BINS ./data/arXiv_08022400/CURRENT_FILE_PIECE;
+}(bfkl);
+
+(paths){
+  PIECE_SETUP arXiv_08022400.C (path1){ }(path1);
+  PIECE_SETUP arXiv_08022400.C (path2){ }(path2);
+}(paths);
+
+(path1){
+  PIECE_SETUP arXiv_08022400.C (jets){ }(jets);
+  PATH_PIECE BPATH1/arXiv_08022400/; LINE_STYLE 1; @@ SUBJ 1;
+  @@ KFAC BKF1;
+  if (BKF1!=1) @@ LTITLE BTITLE1 (x KFAC);
+  else @@ LTITLE BTITLE1;
+}(path1);
+(path2){
+  PIECE_SETUP arXiv_08022400.C (jets){ }(jets);
+  PATH_PIECE BPATH2/arXiv_08022400/; LINE_STYLE 3; @@ SUBJ 0;
+  @@ KFAC BKF2;
+  if (BKF2!=1) @@ LTITLE BTITLE2 (x KFAC);
+  else @@ LTITLE BTITLE2;
+}(path2);
+
+(jets){ 
+  if (SUBJ) PIECE_SETUP arXiv_08022400.C (j[2[+1]5]){ }(j[2[+1]5]);
+  PIECE_SETUP arXiv_08022400.C (sum){ }(sum);
+  Y_FUNCTION y*DATSFAC/YSFAC/YRANGE*KFAC;
+  DRAW_OPTION L;
+}(jets);
+(sum){ 
+  LINE_COLOUR 1; LINE_WIDTH 2;
+  DRAW_PRIORITY 10;
+  LEGEND_TITLE LTITLE;
+}(sum);
+(j2){ 
+  PATH_PIECE j2/;
+  DATA_TYPE ATOOLS;
+  LINE_COLOUR RED1;
+  LINE_STYLE 2;
+  LEGEND_TITLE 2-jet;
+}(j2);
+(j3){ 
+  PATH_PIECE j3/;
+  DATA_TYPE ATOOLS;
+  LINE_COLOUR GREEN1;
+  LINE_STYLE 2;
+  LEGEND_TITLE 3-jet;
+}(j3);
+(j4){ 
+  PATH_PIECE j4/;
+  DATA_TYPE ATOOLS;
+  LINE_COLOUR BLUE1;
+  LINE_STYLE 2;
+  LEGEND_TITLE 4-jet;
+}(j4);
+(j5){ 
+  PATH_PIECE j5/;
+  DATA_TYPE ATOOLS;
+  LINE_COLOUR YELLOW1;
+  LINE_STYLE 2;
+  LEGEND_TITLE 5-jet;
+}(j5);
+
+// diff plots
+
+(dptall){
+  PIECE_SETUP arXiv_08022400.C (dyranges){ }(dyranges);
+  X_MIN 0.001; X_MAX 799.999;
+  X_AXIS_TITLE k_{#perp }^{jet} #left[ GeV #right]
+  Y_MIN -0.999; Y_MAX 0.999; Y_SCALING Id;
+  @@ YSFAC 1;
+  Y_AXIS_TITLE d#sigma_{th} / d#sigma_{exp} - 1;
+  HISTOGRAM_NAME Jet_PT_D;
+  FIGURE_CAPTION Jet $P_T$; WEBPAGE_CAPTION P<sub>T,jet </sub> (diff);
+  LEG_LEFT 0.65; LEG_RIGHT 0.85; LEG_TOP 0.95; LEG_TEXT_SIZE 0.03;
+  Y_AXIS_NDIVISIONS 305; Y_AXIS_LABEL_SIZE 0.04;
+  X_TITLE_OFFSET 1.1; Y_TITLE_OFFSET 1.6; 
+  @@ GBM 0.11; @@ GTM 0.025;
+  @@ JMAX 6; @@ SDIV (1-GBM-GTM)/JMAX;
+  LEFT_MARGIN 0.14; RIGHT_MARGIN 0.035;
+}(dptall);
+
+(dlogptall){
+  PIECE_SETUP arXiv_08022400.C (dyranges){ }(dyranges);
+  X_MIN 45; X_MAX 1500; X_SCALING Log_B_10;
+  X_AXIS_TITLE k_{#perp }^{jet} #left[ GeV #right]
+  Y_MIN -0.999; Y_MAX 0.999; Y_SCALING Id;
+  @@ YSFAC 1;
+  Y_AXIS_TITLE d#sigma_{th} / d#sigma_{exp} - 1;
+  HISTOGRAM_NAME Jet_PT_D;
+  FIGURE_CAPTION Jet $P_T$; WEBPAGE_CAPTION log(P<sub>T,jet</sub>) (diff);
+  LEG_LEFT 0.65; LEG_RIGHT 0.85; LEG_TOP 0.95; LEG_TEXT_SIZE 0.03;
+  Y_AXIS_NDIVISIONS 305; Y_AXIS_LABEL_SIZE 0.04;
+  X_TITLE_OFFSET 1.1; Y_TITLE_OFFSET 1.6; 
+  @@ GBM 0.11; @@ GTM 0.025;
+  @@ JMAX 6; @@ SDIV (1-GBM-GTM)/JMAX;
+  LEFT_MARGIN 0.14; RIGHT_MARGIN 0.035;
+}(dlogptall);
+
+(dyranges){
+  PIECE_SETUP arXiv_08022400.C (d0.0-0.1){ }(d0.0-0.1);
+  PIECE_SETUP arXiv_08022400.C (d0.1-0.7){ }(d0.1-0.7);
+  PIECE_SETUP arXiv_08022400.C (d0.7-1.1){ }(d0.7-1.1);
+  PIECE_SETUP arXiv_08022400.C (d1.1-1.6){ }(d1.1-1.6);
+  PIECE_SETUP arXiv_08022400.C (d1.6-2.1){ }(d1.6-2.1);
+  PIECE_SETUP arXiv_08022400.C (d2.1-2.4){ }(d2.1-2.4);
+}(dyranges);
+
+(d0.0-0.1){ 
+  PIECE_SETUP arXiv_08022400.C (dsamples){ }(dsamples);
+  FILE_PIECE ConeJets_0.0-0.4_jet_1_1_pt_0.dat;
+  @@ DATSFAC 32; @@ YRANGE (0.4-0.0)
+  DRAW YES; TOP_MARGIN GTM;
+  BOTTOM_MARGIN GBM+(JMAX-1)*SDIV;
+  Y_TITLE_SIZE 0.04; X_TITLE_SIZE 0; X_AXIS_LABEL_SIZE 0;
+  WATERMARK \\\|y^{jet }\\\|<0.4 | COLOUR 1 SIZE 0.025 LEFT 0.85 TOP 0.65 ALIGN 22 PRIORITY 20\;;
+}(d0.0-0.1);
+(d0.1-0.7){ 
+  PIECE_SETUP arXiv_08022400.C (dsamples){ }(dsamples);
+  FILE_PIECE ConeJets_0.4-0.8_jet_1_1_pt_0.dat;
+  @@ DATSFAC 16; @@ YRANGE (0.8-0.4)
+  DRAW YES; DIFF_PLOT YES; TOP_MARGIN GTM+SDIV;
+  BOTTOM_MARGIN GBM+(JMAX-2)*SDIV;
+  Y_TITLE_SIZE 0; X_TITLE_SIZE 0; X_AXIS_LABEL_SIZE 0;
+  WATERMARK 0.4<\\\|y^{jet }\\\|<0.8 | COLOUR 1 SIZE 0.025 LEFT 0.85 TOP 0.65 ALIGN 22 PRIORITY 20\;;
+}(d0.1-0.7);
+(d0.7-1.1){ 
+  PIECE_SETUP arXiv_08022400.C (dsamples){ }(dsamples);
+  FILE_PIECE ConeJets_0.8-1.2_jet_1_1_pt_0.dat;
+  @@ DATSFAC 8; @@ YRANGE (1.2-0.8)
+  DRAW YES; DIFF_PLOT YES; TOP_MARGIN GTM+2*SDIV;
+  BOTTOM_MARGIN GBM+(JMAX-3)*SDIV;
+  Y_TITLE_SIZE 0; X_TITLE_SIZE 0; X_AXIS_LABEL_SIZE 0;
+  WATERMARK 0.8<\\\|y^{jet }\\\|<1.2 | COLOUR 1 SIZE 0.025 LEFT 0.85 TOP 0.65 ALIGN 22 PRIORITY 20\;;
+}(d0.7-1.1);
+(d1.1-1.6){ 
+  PIECE_SETUP arXiv_08022400.C (dsamples){ }(dsamples);
+  FILE_PIECE ConeJets_1.2-1.6_jet_1_1_pt_0.dat;
+  @@ DATSFAC 4; @@ YRANGE (1.6-1.2)
+  DRAW YES; DIFF_PLOT YES;
+  TOP_MARGIN GTM+3*SDIV;
+  BOTTOM_MARGIN GBM+(JMAX-4)*SDIV;
+  Y_TITLE_SIZE 0; X_TITLE_SIZE 0; X_AXIS_LABEL_SIZE 0;
+  WATERMARK 1.2<\\\|y^{jet }\\\|<1.6 | COLOUR 1 SIZE 0.025 LEFT 0.85 TOP 0.65 ALIGN 22 PRIORITY 20\;;
+}(d1.1-1.6);
+(d1.6-2.1){ 
+  PIECE_SETUP arXiv_08022400.C (dsamples){ }(dsamples);
+  FILE_PIECE ConeJets_1.6-2.0_jet_1_1_pt_0.dat;
+  @@ DATSFAC 2; @@ YRANGE (2.0-1.6)
+  DRAW YES; DIFF_PLOT YES;
+  TOP_MARGIN GTM+4*SDIV;
+  BOTTOM_MARGIN GBM+(JMAX-5)*SDIV;
+  Y_TITLE_SIZE 0; X_TITLE_SIZE 0; X_AXIS_LABEL_SIZE 0;
+  WATERMARK 1.6<\\\|y^{jet }\\\|<2.0 | COLOUR 1 SIZE 0.025 LEFT 0.85 TOP 0.65 ALIGN 22 PRIORITY 20\;;
+}(d1.6-2.1);
+(d2.1-2.4){ 
+  PIECE_SETUP arXiv_08022400.C (dsamples){ }(dsamples);
+  FILE_PIECE ConeJets_2.0-2.4_jet_1_1_pt_0.dat;
+  @@ DATSFAC 1; @@ YRANGE (2.4-2.0)
+  DRAW YES; DIFF_PLOT YES;
+  TOP_MARGIN GTM+5*SDIV;
+  BOTTOM_MARGIN GBM+(JMAX-6)*SDIV;
+  Y_TITLE_SIZE 0; X_TITLE_SIZE 0.04; X_AXIS_LABEL_SIZE 0.04;
+  WATERMARK 2.0<\\\|y^{jet }\\\|<2.4 | COLOUR 1 SIZE 0.025 LEFT 0.85 TOP 0.65 ALIGN 22 PRIORITY 20\;;
+  WATERMARK parton level | SIZE .035 COLOUR RED1 LEFT .975 TOP .25 ALIGN 32 PRIORITY 10\;;
+  WATERMARK SHERPA | SIZE .04 COLOUR 19 LEFT .05 TOP .25 ALIGN 12 PRIORITY -20\;;
+}(d2.1-2.4);
+
+
+(dsamples){
+  PIECE_SETUP arXiv_08022400.C (ddata){ }(ddata);
+  PIECE_SETUP arXiv_08022400.C (ddatap){ }(ddatap);
+  PIECE_SETUP arXiv_08022400.C (dbfkl){ }(dbfkl);
+  LEGEND_ENABLED NO;
+}(dsamples);
+
+(ddata){
+  PATH_PIECE data/arXiv_08022400/;
+  Y_FUNCTION 0|dy/y;
+  X_VALUE 1; Y_VALUE 2;
+  X_ERROR_MINUS 3; X_ERROR_PLUS 4;
+  Y_ERROR_MINUS 5; Y_ERROR_PLUS 6;
+  DRAW_OPTION P2;
+  FILL_COLOUR 5;
+  MARKER_COLOUR 5;
+  DRAW_PRIORITY -5;
+  DRAW_LINES H 0 | STYLE 1 COLOUR 1\;;
+}(ddata);
+(ddatap){
+  PATH_PIECE data/arXiv_08022400/;
+  Y_FUNCTION 0|dy/y;
+  X_VALUE 1; Y_VALUE 2;
+  X_ERROR_MINUS 3; X_ERROR_PLUS 4;
+  Y_ERROR_MINUS 5; Y_ERROR_PLUS 6;
+  DRAW_OPTION P;
+  MARKER_COLOUR 12;
+  LINE_COLOUR 12;
+  DRAW_PRIORITY 5;
+  DRAW_LINES H 0 | STYLE 1 COLOUR 1\;;
+}(ddatap);
+(dbfkl){
+  PIECE_SETUP arXiv_08022400.C (dpaths){ }(dpaths);
+  READER_PARAMETERS ADOPT_BINS ./data/arXiv_08022400/CURRENT_FILE_PIECE;
+  DATA_TYPE ALGEBRA(y[1]/y[0]-1);
+  DRAW_PRIORITY 10;
+}(dbfkl);
+
+(dpaths){
+  PIECE_SETUP arXiv_08022400.C (dpath1){ }(dpath1);
+  PIECE_SETUP arXiv_08022400.C (dpath2){ }(dpath2);
+}(dpaths);
+
+(dpath1){
+  PIECE_SETUP arXiv_08022400.C (djets){ }(djets);
+  @@ BPPIECE BPATH1/arXiv_08022400/; LINE_STYLE 1;
+}(dpath1);
+(dpath2){
+  PIECE_SETUP arXiv_08022400.C (djets){ }(djets);
+  @@ BPPIECE BPATH2/arXiv_08022400/; LINE_STYLE 3;
+}(dpath2);
+
+(djets){ 
+  PIECE_SETUP arXiv_08022400.C (dsum){ }(dsum);
+}(djets);
+(dsum){ 
+  PATH_PIECE data/arXiv_08022400/ BPPIECE;
+  LINE_COLOUR 1; LINE_WIDTH 2;
+  DRAW_PRIORITY 10;
+  LEGEND_TITLE LTITLE;
+}(dsum);
