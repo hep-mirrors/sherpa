de05e8b975f8c4bcedb17127e2ab8c087c81a243
Frank Siegert <frank.siegert@cern.ch>
2009-07-09 22:27:39 +0000

    Clean up Tevatron_QCD setup. All data and compare.conf files for internal analysis can be found in benchmark.

--- Examples/Tevatron_QCD/compare.conf/1960.C
+++
@@ -1,5 +0,0 @@
-(initial){
-  PIECE_SETUP arXiv_08022400.C (initial){ }(initial);
-  PIECE_SETUP hep-ex_0701051.C (initial){ }(initial);
-  PIECE_SETUP hep-ex_0409040.C (initial){ }(initial);
-}(initial);
