1b1d8c833e4da527f6c464b39497e07f6dfa718f
Stefan Hoeche <shoeche@slac.stanford.edu>
2010-01-28 13:36:34 +0000

    updated fkl analysis for lh09 comparison

--- AddOns/Analysis/Observables/BFKL_Analysis.C
+++ AddOns/Analysis/Observables/BFKL_Analysis.C
@@ -5,17 +5,17 @@ namespace ANALYSIS {
   class BFKL_Analysis: public Primitive_Observable_Base {  
   private:
 
-    std::string m_reflist;
+    std::string m_reflist, m_jetslist;
 
     std::vector<Normalized_Observable*> m_dists;
     std::vector<ATOOLS::Histogram*>     m_histos;
 
-    double m_dy, m_ptmin;
-
   public:
 
-    BFKL_Analysis(const std::string & listname,
-		  const double &dy,const double &ptmin);
+    BFKL_Analysis(const std::string &listname,
+		  const std::string &jetsname,
+		  const std::string &refname);
+
     ~BFKL_Analysis();
     
     Analysis_Object &operator+=(const Analysis_Object &obj);
@@ -38,35 +38,40 @@ namespace ANALYSIS {
 using namespace ANALYSIS;
 using namespace ATOOLS;
 
-BFKL_Analysis::BFKL_Analysis
-(const std::string & listname,
- const double &dy,const double &ptmin) :
-  Primitive_Observable_Base(1,0.,1.,100),
-  m_dy(dy), m_ptmin(ptmin)
+BFKL_Analysis::BFKL_Analysis(const std::string &listname,
+			     const std::string &jetsname,
+			     const std::string &refname):
+  Primitive_Observable_Base(1,0.,1.,100)
 {
   m_listname=listname;
-  m_name=m_listname+"_JS_Dy"+ToString(m_dy)
-    +"_pT"+ToString(m_ptmin);
-  m_dists.resize(9,NULL);
+  m_jetslist=jetsname;
+  m_reflist=refname;
+  m_name=m_listname+"_JS";
+  m_dists.resize(2,NULL);
   // N_{jet}(\Delta y_{1n})
-  m_dists[0] = new Normalized_Observable(1,0.0,10.0,100);
-  // \Delta\phi_{1n}(\Delta y_{1n})
-  m_dists[1] = new Normalized_Observable(1,0.0,10.0,100);
-  // 2-jets(\Delta y_{1n})
-  m_dists[2] = new Normalized_Observable(1,0.0,10.0,100);
-  // 3-jets(\Delta y_{1n})
-  m_dists[3] = new Normalized_Observable(1,0.0,10.0,100);
-  // 4-jets(\Delta y_{1n})
-  m_dists[4] = new Normalized_Observable(1,0.0,10.0,100);
-  // 5-jets(\Delta y_{1n})
-  m_dists[5] = new Normalized_Observable(1,0.0,10.0,100);
-  // p_{T,mid lead}(\Delta y_{1n})
-  m_dists[6] = new Normalized_Observable(1,0.0,10.0,100);
-  // H_T(\Delta y_{1n})
-  m_dists[7] = new Normalized_Observable(1,0.0,10.0,100);
-  // H_T/N_{jet}(\Delta y_{1n})
-  m_dists[8] = new Normalized_Observable(1,0.0,10.0,100);
+  m_dists[0] = new Normalized_Observable(1,0.0,10.0,40);
+  // \cos(\pi-\Delta\phi_{1n})(\Delta y_{1n})
+  m_dists[1] = new Normalized_Observable(1,0.0,10.0,40);
+  m_histos.resize(6,NULL);
+  // d\sigma/d\Delta y_{1n}
+  m_histos[0] = new Histogram(1,0.0,10.0,40);
+  // d\sigma/d\Delta y_{12}
+  m_histos[1] = new Histogram(1,0.0,10.0,40);
+  // d\sigma/dp_{T,H}
+  m_histos[2] = new Histogram(1,0.0,500.0,50);
+  // d\sigma/dy_c
+  m_histos[3] = new Histogram(1,0.0,4.0,40);
+  // d\sigma/dy_c 30
+  m_histos[4] = new Histogram(1,0.0,4.0,40);
+  // d\sigma/dy_c 20
+  m_histos[5] = new Histogram(1,0.0,4.0,40);
 }
+
+class JS_Order_PT {
+public:
+  bool operator()(const Particle *a,const Particle *b)
+  { return a->Momentum().PPerp2()>b->Momentum().PPerp2(); }
+};// end of class JS_Order_PT
 
 class JS_Order_Y {
 public:
@@ -87,41 +92,55 @@ void BFKL_Analysis::Evaluate(const ATOOLS::Particle_List& pl,
 			     double weight, double ncount)
 {
   Particle_List jets(*p_ana->GetParticleList(m_listname));
-  for (Particle_List::iterator 
-	 pit(jets.begin());pit!=jets.end();) {
-    // jets only
-    if ((*pit)->Flav().Kfcode()!=kf_jet) pit=jets.erase(pit);
-    // y cut
-    else if (dabs((*pit)->Momentum().Y())>m_dy) pit=jets.erase(pit);
-    // pt cut
-    else if ((*pit)->Momentum().PPerp()<m_ptmin) pit=jets.erase(pit);
-    else ++pit;
-  }
+  Particle_List alljets(*p_ana->GetParticleList(m_jetslist));
   if (jets.size()<2) AddZero(weight,ncount);
-  // sort
+  // sort pt
+  std::sort(jets.begin(),jets.end(),JS_Order_PT());
+  m_histos[1]->Insert(jets[0]->Momentum().Y()-
+		      jets[1]->Momentum().Y(),weight,ncount);
+  // sort y
   std::sort(jets.begin(),jets.end(),JS_Order_Y());
+  m_histos[0]->Insert(jets[0]->Momentum().Y()-
+		      jets[1]->Momentum().Y(),weight,ncount);
   double dy(jets.front()->Momentum().Y()-
 	    jets.back()->Momentum().Y());
   double cdphi(cos(M_PI-jets.front()->Momentum().DPhi
 		   (jets.back()->Momentum())));
   m_dists[0]->Fill(dy,jets.size(),weight,ncount);
   m_dists[1]->Fill(dy,cdphi,weight,ncount);
-  for (size_t i(2);i<6;++i) {
-    if (i==jets.size()) m_dists[i]->Fill(dy,1.0,weight,ncount);
-    else m_dists[i]->Fill(dy,0.0,weight,ncount);
+  if (dy>4.0) {
+    Particle_List ref(*p_ana->GetParticleList(m_reflist));
+    if (ref.size()==1) {
+      m_histos[2]->Insert
+	(ref.front()->Momentum().PPerp(),weight,ncount);
+    }
+    else {
+      m_histos[2]->Insert(0.0,0.0,ncount);
+    }
+    double ymean=0.5*(jets.front()->Momentum().Y()+
+		      jets.back()->Momentum().Y());
+    double yc[3]={100.0,100.0,100.0};
+    for (size_t i(0);i<alljets.size();++i) {
+      if (alljets[i]->Momentum()==jets.front()->Momentum() ||
+          alljets[i]->Momentum()==jets.back()->Momentum()) continue;
+      double pt(alljets[i]->Momentum().PPerp());
+      double dy(alljets[i]->Momentum().Y()-ymean);
+      if (pt>40.0) yc[0]=Min(yc[0],dabs(dy));
+      if (pt>30.0) yc[1]=Min(yc[1],dabs(dy));
+      if (pt>20.0) yc[2]=Min(yc[2],dabs(dy));
+    }
+    for (int j(0);j<3;++j) {
+      for (double y(0.05);y<=yc[j];y+=0.1)
+	m_histos[3+j]->Insert(y,weight,0);
+      m_histos[3+j]->Insert(0.0,0.0,ncount);
+    }
   }
-  double ptml(0.0), ht(0.0);
-  for (size_t i(1);i<jets.size()-1;++i) {
-    double pt(jets[i]->Momentum().PPerp());
-    if (pt>ptml) ptml=pt;
-    ht+=pt;
+  else {
+    m_histos[2]->Insert(0.0,0.0,ncount);
+    m_histos[3]->Insert(0.0,0.0,ncount);
+    m_histos[4]->Insert(0.0,0.0,ncount);
+    m_histos[5]->Insert(0.0,0.0,ncount);
   }
-  ht+=jets.front()->Momentum().PPerp();
-  ht+=jets.back()->Momentum().PPerp();
-  if (ptml>0.0) m_dists[6]->Fill(dy,ptml,weight,ncount);
-  else m_dists[6]->Fill(dy,0.0,0.0,ncount);
-  m_dists[7]->Fill(dy,ht,weight,ncount);
-  m_dists[8]->Fill(dy,ht/jets.size(),weight,ncount);
 }
 
 Analysis_Object &BFKL_Analysis::operator+=
@@ -183,7 +202,7 @@ BFKL_Analysis::~BFKL_Analysis()
 
 Primitive_Observable_Base *BFKL_Analysis::Copy() const 
 {
-  return new BFKL_Analysis(m_listname,m_dy,m_ptmin);
+  return new BFKL_Analysis(m_listname,m_jetslist,m_reflist);
 }
 
 DECLARE_ND_GETTER(JS_Getter,"BFKL",
@@ -193,13 +212,12 @@ Primitive_Observable_Base *
 JS_Getter::operator()(const Argument_Matrix &parameters) const
 {
   if (parameters.size()==0 || parameters[0].size()<3) return NULL;
-  return new BFKL_Analysis(parameters[0][2],
-			   ToType<double>(parameters[0][0]),
-			   ToType<double>(parameters[0][1]));
+  return new BFKL_Analysis
+    (parameters[0][0],parameters[0][1],parameters[0][2]);
 }
 
 void JS_Getter::PrintInfo(std::ostream &str,const size_t width) const
 { 
-  str<<"deltay ptmin list"; 
+  str<<"list jetlist reflist"; 
 }
 
