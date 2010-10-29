0adadf58fa5afbdff2f559e2ff51a4e22ca194ef
Stefan Hoeche <shoeche@slac.stanford.edu>
2010-10-29 04:53:18 +0000

    added siscone plugin to fastjet finder

--- AddOns/Analysis/Triggers/Fastjet_Interface.C
+++ AddOns/Analysis/Triggers/Fastjet_Interface.C
@@ -8,6 +8,7 @@
 #include "ATOOLS/Org/Exception.H"
 #include "fastjet/PseudoJet.hh"
 #include "fastjet/ClusterSequence.hh"
+#include "fastjet/SISConePlugin.hh"
 
 using namespace ANALYSIS;
 using namespace ATOOLS;
@@ -16,6 +17,8 @@ class Fastjet_Interface: public Trigger_Base {
 private:
 
   fastjet::JetDefinition m_jdef;
+
+  fastjet::JetDefinition::Plugin *p_plug;
 
   size_t m_njets;
   double m_ptmin;
@@ -26,15 +29,21 @@ public:
   Fastjet_Interface(const std::string &inlist,
 		    const std::string &outlist,
 		    const fastjet::JetDefinition &jdef,
+ 		    fastjet::JetDefinition::Plugin *const plug,
 		    const size_t &njets,const double &ptmin):
-    Trigger_Base(inlist,outlist), m_jdef(jdef),
+    Trigger_Base(inlist,outlist), m_jdef(jdef), p_plug(plug),
     m_njets(njets), m_ptmin(ptmin) {}
+
+  ~Fastjet_Interface()
+  {
+    if (p_plug) delete p_plug;
+  }
 
   // member functions
   Analysis_Object *GetCopy() const 
   {
     return new Fastjet_Interface
-      (m_inlist,m_outlist,m_jdef,m_njets,m_ptmin);
+      (m_inlist,m_outlist,m_jdef,NULL,m_njets,m_ptmin);
   }
 
   void Evaluate(const ATOOLS::Particle_List &plist,
@@ -74,8 +83,8 @@ operator()(const Argument_Matrix &parameters) const
   fastjet::JetAlgorithm algo(fastjet::kt_algorithm);
   fastjet::RecombinationScheme recom(fastjet::E_scheme);
   fastjet::Strategy strategy(fastjet::Best);
-  double R=0.4, p=1.0, ptmin=0.0;
-  size_t njets=0;
+  double R=0.4, f=0.75, p=1.0, ptmin=0.0;
+  size_t njets=0, siscone=0;
   std::string inlist="FinalState", outlist="FastJets";
   for (size_t i=0;i<parameters.size();++i) {
     if (parameters[i].size()<2) continue;
@@ -85,6 +94,7 @@ operator()(const Argument_Matrix &parameters) const
       if (parameters[i][1]=="kt") algo=fastjet::kt_algorithm;
       if (parameters[i][1]=="cambridge") algo=fastjet::cambridge_algorithm;
       if (parameters[i][1]=="antikt") algo=fastjet::antikt_algorithm;
+      if (parameters[i][1]=="siscone") siscone=1;
     }
     else if (parameters[i][0]=="Scheme") {
       if (parameters[i][1]=="E") recom=fastjet::E_scheme;
@@ -96,6 +106,7 @@ operator()(const Argument_Matrix &parameters) const
       if (parameters[i][1]=="BIpt2") recom=fastjet::BIpt2_scheme;
     }
     else if (parameters[i][0]=="R") R=ATOOLS::ToType<double>(parameters[i][1]);
+    else if (parameters[i][0]=="f") f=ATOOLS::ToType<double>(parameters[i][1]);
     else if (parameters[i][0]=="p") p=ATOOLS::ToType<double>(parameters[i][1]);
     else if (parameters[i][0]=="Strategy") {
       if (parameters[i][1]=="N2Plain") strategy=fastjet::N2Plain;
@@ -108,8 +119,13 @@ operator()(const Argument_Matrix &parameters) const
     else if (parameters[i][0]=="NJets") njets=ATOOLS::ToType<int>(parameters[i][1]);
     else if (parameters[i][0]=="PTMin") ptmin=ATOOLS::ToType<int>(parameters[i][1]);
   }
+  if (siscone) {
+    fastjet::JetDefinition::Plugin *plug(new fastjet::SISConePlugin(R,f));
+    fastjet::JetDefinition jdef(plug);
+    return new Fastjet_Interface(inlist,outlist,jdef,plug,njets,ptmin);
+  }
   fastjet::JetDefinition jdef(algo,R,recom,strategy);
-  return new Fastjet_Interface(inlist,outlist,jdef,njets,ptmin);
+  return new Fastjet_Interface(inlist,outlist,jdef,NULL,njets,ptmin);
 }									
 
 void FastJet_Getter::PrintInfo(std::ostream &str,const size_t width) const
