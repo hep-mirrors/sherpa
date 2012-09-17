544126ee5d4e3ff0d48c87acaa8deeb4b26eb50d
Stefan Hoeche <shoeche@slac.stanford.edu>
2012-09-17 07:59:07 +0000

    svn merge -c20154 branches/rel-1-4-1

--- MCATNLO/Main/CS_Gamma.C
+++ MCATNLO/Main/CS_Gamma.C
@@ -54,9 +54,7 @@ Weight_Map CS_Gamma::CalculateWeight
   std::string rname(Process_Base::GenerateName(rampl));
   p_rproc=(*(*procs)[nlo_type::lo])[rname]->Get<Single_Process>();
   if (p_rproc==NULL) {
-    msg_Debugging()<<"invalid real process '"<<rname<<"'\n";
-    rampl->Delete();
-    return Weight_Map();
+    THROW(fatal_error,"Process '"+rname+"' not found");
   }
   Weight_Map ws;
   int stat(CalculateWeights(rampl,idmap,ws,mode));
