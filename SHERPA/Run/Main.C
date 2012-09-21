616a66abc94854935315b0020f4dea7bcd7e5a01
Frank Siegert <frank.siegert@cern.ch>
2012-09-21 12:26:19 +0000

    svn merge -c20023 svn+ssh://svn.hepforge.org/hepforge/svn/sherpa/branches/nlomerging . (Improve normal event generation output: Instead of displaying every 100evts, progressively display every 1, 10, 100, 1k, 10k, 100k evts depending on event number.)

--- SHERPA/Run/Main.C
+++ SHERPA/Run/Main.C
@@ -35,29 +35,29 @@ int main(int argc,char* argv[])
       msg->SetLevel(read.GetValue<int>("EVT_OUTPUT",msg->Level()));
       double starttime=rpa->gen.Timer().RealTime();
       for (size_t i=1;i<=rpa->gen.NumberOfEvents();) {
-	if (i%100==0 && i<rpa->gen.NumberOfEvents()) {
-	  double diff=rpa->gen.Timer().RealTime()-starttime;
-	  msg_Info()<<"  Event "<<i<<" ( "
-		    <<FormatTime(size_t(diff))<<" elapsed / "
-		    <<FormatTime(size_t((nevt-i)/(double)i*diff))
-		    <<" left ) -> ETA: "<<rpa->gen.Timer().
-	    StrFTime("%a %b %d %H:%M",time_t((nevt-i)/(double)i*diff))<<"  ";
-          if (i%1000==0) {
+        if (Generator->GenerateOneEvent()) {
+          msg_Events()<<"Sherpa : Passed "<<i<<" events."<<std::endl;
+          int exp;
+          for (exp=5; i/int(pow(10,exp))==0; --exp) {}
+          if (i%int(pow(10,exp))==0 && i<rpa->gen.NumberOfEvents()) {
+            double diff=rpa->gen.Timer().RealTime()-starttime;
+            msg_Info()<<"  Event "<<i<<" ( "
+                      <<FormatTime(size_t(diff))<<" elapsed / "
+                      <<FormatTime(size_t((nevt-i)/(double)i*diff))
+                      <<" left ) -> ETA: "<<rpa->gen.Timer().
+              StrFTime("%a %b %d %H:%M",time_t((nevt-i)/(double)i*diff))<<"  ";
             double xs(Generator->GetEventHandler()->TotalXS()),
-                   err(Generator->GetEventHandler()->TotalErr());
-	    if (!(rpa->gen.BatchMode()&2)) msg_Info()<<"\n  ";
+              err(Generator->GetEventHandler()->TotalErr());
+            if (!(rpa->gen.BatchMode()&2)) msg_Info()<<"\n  ";
             msg_Info()<<"XS = "<<xs<<" pb +- ( "<<err<<" pb = "
                       <<((int(err/xs*10000))/100.0)<<" % )  ";
-	    if (!(rpa->gen.BatchMode()&2))
-	      msg_Info()<<mm(1,mm::up);
+            if (!(rpa->gen.BatchMode()&2))
+              msg_Info()<<mm(1,mm::up);
+            if (rpa->gen.BatchMode()&2) { msg_Info()<<std::endl; }
+            else { msg_Info()<<bm::cr<<std::flush; }
           }
-	  if (rpa->gen.BatchMode()&2) { msg_Info()<<std::endl; }
-	  else { msg_Info()<<bm::cr<<std::flush; }
-	}
-	if (Generator->GenerateOneEvent()) {
-	  msg_Events()<<"Sherpa : Passed "<<i<<" events."<<std::endl;
-	  ++i;
-	}
+          ++i;
+        }
       }
       msg_Info()<<"  Event "<<rpa->gen.NumberOfEvents()<<" ( "
 		<<size_t(rpa->gen.Timer().RealTime()-starttime)
