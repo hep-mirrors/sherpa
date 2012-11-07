3916a5aafc7d979fa0decf9720262b3b349e7a4c
Stefan Hoeche <shoeche@slac.stanford.edu>
2012-11-07 20:55:13 +0000

    menlops for strong core procs

--- PHASIC++/Process/MCatNLO_Process.C
+++ PHASIC++/Process/MCatNLO_Process.C
@@ -15,6 +15,7 @@
 #include "PDF/Main/Jet_Criterion.H"
 #include "PDF/Main/Cluster_Definitions_Base.H"
 #include "PDF/Main/ISR_Handler.H"
+#include "MODEL/Main/Running_AlphaS.H"
 #include "ATOOLS/Math/MathTools.H"
 #include "ATOOLS/Math/Random.H"
 #include "ATOOLS/Org/Run_Parameter.H"
@@ -226,6 +227,40 @@ double MCatNLO_Process::LocalKFactor(const Cluster_Amplitude &ampl)
   size_t mcmode(bviproc->SetMCMode(3));
   double bvi(bviproc->Differential(ampl,4|rm));
   bviproc->SetMCMode(mcmode);
+  double asr(1.0);
+  if (bproc->OrderQCD()) {
+    msg_Debugging()<<"\\alpha_s renormalization {\n";
+    size_t oqc(0);
+    double mur2(ampl.MuR2()), asc(1.0);
+    const Cluster_Amplitude *campl(&ampl);
+    for (;campl->Next();campl=campl->Next()) {
+      double cas(MODEL::as->BoundedAlphaS(campl->KT2()));
+      double coqcd(campl->OrderQCD()-campl->Next()->OrderQCD());
+      asc*=pow(cas,coqcd);
+      oqc+=coqcd;
+      msg_Debugging()<<"  \\mu = "<<sqrt(campl->KT2())
+		     <<", O(QCD) = "<<coqcd<<"\n";
+    }
+    if (campl->OrderQCD()) {
+      double cas(MODEL::as->BoundedAlphaS(campl->KT2()));
+      double coqcd(campl->OrderQCD());
+      asc*=pow(cas,coqcd);
+      oqc+=coqcd;
+      msg_Debugging()<<"  \\mu = "<<sqrt(campl->KT2())
+		     <<", O(QCD) = "<<coqcd<<"\n";
+    }
+    if (oqc!=bproc->OrderQCD()) THROW(fatal_error,"Invalid amplitude");
+    asc=pow(asc,1.0/oqc);
+    double muc2(MODEL::as->WDBSolve
+		(asc,MODEL::as->CutQ2(),1.01*rpa->gen.CplScale()));
+    if (!IsEqual((*MODEL::as)(muc2),asc))
+      msg_Error()<<METHOD<<"(): Failed to determine \\mu."<<std::endl; 
+    msg_Debugging()<<"  \\mu_r = "<<sqrt(mur2)
+		   <<", \\mu_c = "<<sqrt(muc2)<<"\n";
+    asr-=bproc->OrderQCD()*(*MODEL::as)(mur2)*
+      (11.0-2.0/3.0*MODEL::as->Nf(mur2))/(4.0*M_PI)*log(muc2/mur2);
+    msg_Debugging()<<"} -> "<<asr<<"\n";
+  }
   double k(bvi/b*(1.0-rs/r)+rs/r);
   msg_Debugging()<<"BVI = "<<bvi<<", B = "<<b
 		 <<" -> K = "<<k<<"\n";
