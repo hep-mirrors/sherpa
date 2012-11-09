961d39bab8805d2f56e74dc827caca8bb814d739
Frank Siegert <frank.siegert@cern.ch>
2012-11-09 20:09:02 +0000

    simple top spin correlation setup.

--- Examples/Xamples_DecayTreatment/SpinCorrelations/testsc.sh
+++ Examples/Xamples_DecayTreatment/SpinCorrelations/testsc.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-if test -z $1; then
-  SHERPAPATH=$PWD/../../bin
+if test -z $2; then
+  SHERPAPATH=$PWD/../../../bin
 else
-  SHERPAPATH=$1
+  SHERPAPATH=$2
 fi
+
+if test -z $1; then
+  PROCESSES="z_tautau w_taunu wp_taunu h_tautau hm_tautau z_toptop"
+else
+  PROCESSES=$1
+fi
+
 EVENTS=100000
 
 rivetplotstr=""
-for process in z_tautau w_taunu wp_taunu h_tautau hm_tautau; do
+for process in $PROCESSES; do
     echo "Testing $process";
-    cd $process;
-    for gen in Amegic Comix; do
+    cd $process && for gen in Amegic Comix; do
         echo "  with production in $gen and tau decays in HADRONS";
         for sc in 1 0; do
             echo "    for SPINCORRTAG:=$sc";
@@ -22,14 +28,12 @@ for process in z_tautau w_taunu wp_taunu h_tautau hm_tautau; do
                     ${SHERPAPATH}/Sherpa EVENTS=${EVENTS} GENTAG:=$gen SPINCORRTAG:=$sc LOG_FILE=out.$gen.$sc.log >> errors 2>&1 || exit 1;
                 fi
             fi
-            ../../../AddOns/Rivet/sherpa2aida -a MC_$process Analysis${gen}SC${sc} > /dev/null || exit 1
+            ../../../../AddOns/Rivet/sherpa2aida -a MC_$process Analysis${gen}SC${sc} > /dev/null || exit 1
             rivetplotstr="$rivetplotstr ${process}/Analysis${gen}SC${sc}.aida:Title=${gen}SSC${sc}:LineWidth=0.1pt"
         done
-    done
-    cd -;
+    done && cd -;
 
-    cd ${process}_hard;
-    for gen in Amegic Comix; do
+    cd ${process}_hard && for gen in Amegic Comix; do
         echo "  with production in $gen and hard tau decays";
         for sc in 1 0; do
             echo "    for SPINCORRTAG:=$sc";
@@ -40,11 +44,10 @@ for process in z_tautau w_taunu wp_taunu h_tautau hm_tautau; do
                     ${SHERPAPATH}/Sherpa EVENTS=${EVENTS} GENTAG:=$gen SPINCORRTAG:=$sc LOG_FILE=out.$gen.$sc.log >> errors 2>&1 || exit 1;
                 fi
             fi
-            ../../../AddOns/Rivet/sherpa2aida -a MC_$process Analysis${gen}SC${sc} > /dev/null || exit 1
+            ../../../../AddOns/Rivet/sherpa2aida -a MC_$process Analysis${gen}SC${sc} > /dev/null || exit 1
             rivetplotstr="$rivetplotstr ${process}_hard/Analysis${gen}SC${sc}.aida:Title=${gen}HSC${sc}:LineWidth=0.1pt"
         done
-    done
-    cd -;
+    done && cd -;
 
     cd ${process}_ME;
     echo "  with production and decay in ME";
@@ -52,7 +55,7 @@ for process in z_tautau w_taunu wp_taunu h_tautau hm_tautau; do
         rm -rf Process/ Results/Comix Results/Amegic;
         ${SHERPAPATH}/Sherpa EVENTS=${EVENTS} LOG_FILE=out.log >> errors 2>&1;
     fi;
-    ../../../AddOns/Rivet/sherpa2aida -a MC_$process Analysis > /dev/null || exit 1
+    ../../../../AddOns/Rivet/sherpa2aida -a MC_$process Analysis > /dev/null || exit 1
     rivetplotstr="$rivetplotstr ${process}_ME/Analysis.aida:Title=ComixME:LineWidth=0.1pt"
     cd -;
 done
