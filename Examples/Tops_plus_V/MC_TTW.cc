182dc50b8f58121ac6b7994f4e02dc7426bd0631
Jennifer Thompson <thompson@thphys.uni-heidelberg.de>
2012-11-13 14:09:30 +0000

    MC_TTW analysis

--- Examples/Tops_plus_V/MC_TTW.cc
+++ Examples/Tops_plus_V/MC_TTW.cc
@@ -15,13 +15,14 @@
 #include "Rivet/RivetAIDA.hh"
 
 namespace Rivet {
-
-
+  
+  
   class MC_TTW : public Analysis {
   private:
     double _leta, _mupt, _ept, _lpt, _missET;
     double _isoE_lj, _isoR_lj, _jeta, _jR, _jpt; 
-
+    double _mW, _mt, _mZ;
+    
     AIDA::IHistogram1D *_h_njets; //HT?
     AIDA::IHistogram1D *_h_mass_ll_ss,*_h_mass_ll_os,*_h_mass_lll;
     AIDA::IHistogram1D *_h_pT_l1,*_h_pT_l2,*_h_pT_l3,*_h_missET;
@@ -29,9 +30,9 @@ namespace Rivet {
     AIDA::IHistogram1D *_h_pT_t1,*_h_pT_t2,*_h_pT_W,*_h_pT_tt,*_h_pT_ttW;
     AIDA::IHistogram1D *_h_mass_tt,*_h_mass_ttW;
     AIDA::IHistogram1D *_h_rap_t1,*_h_rap_t2,*_h_rap_W,*_h_rap_tt,*_h_rap_ttW;
-    AIDA::IHistogram1D *_h_pT_b1,*_h_pT_b2,*_h_pT_lj;
-    AIDA::IHistogram1D *_h_eta_b1,*_h_eta_b2,*_h_eta_lj; 
-
+    AIDA::IHistogram1D *_h_pT_b1,*_h_pT_b2; //,*_h_pT_lj;
+    //   AIDA::IHistogram1D *_h_eta_b1,*_h_eta_b2,*_h_eta_lj; 
+    
     void inithistos() {
       _h_njets      = bookHistogram1D("jet_mult", 11, -0.5, 10.5);
       _h_mass_ll_ss = bookHistogram1D("mass_ll_ss", 50, 0.0, 150.0);
@@ -58,17 +59,18 @@ namespace Rivet {
       _h_rap_ttW    = bookHistogram1D("rap_ttW", 20, -5., 5.);
       _h_pT_b1      = bookHistogram1D("pT_b1", logspace(10.0, 500.0, 50));
       _h_pT_b2      = bookHistogram1D("pT_b2", logspace(10.0, 500.0, 50));
-      _h_pT_lj      = bookHistogram1D("pT_b2", logspace(10.0, 500.0, 50));
-      _h_eta_b1     = bookHistogram1D("eta_b1", 20, -_jeta, _jeta);
-      _h_eta_b2     = bookHistogram1D("eta_b2", 20, -_jeta, _jeta); 
-      _h_eta_lj     = bookHistogram1D("eta_b2", 20, -_jeta, _jeta); 
+      //_h_pT_lj      = bookHistogram1D("pT_b2", logspace(10.0, 500.0, 50));
+      //_h_eta_b1     = bookHistogram1D("eta_b1", 20, -_jeta, _jeta);
+      //_h_eta_b2     = bookHistogram1D("eta_b2", 20, -_jeta, _jeta); 
+      //_h_eta_lj     = bookHistogram1D("eta_b2", 20, -_jeta, _jeta); 
     }
   public:
     MC_TTW() : 
       Analysis("MC_TTW"),
       _leta(2.5), _mupt(10.), _ept(10.), _lpt(min(_ept,_mupt)),
       _missET(50.), _isoE_lj(0.05), _isoR_lj(0.2), 
-      _jeta(5.), _jR(0.4), _jpt(20.) 
+      _jeta(5.), _jR(0.4), _jpt(20.), _mW(80.419), _mt(172.5), 
+      _mZ(90.188)
     {}
     
     void init() {
@@ -101,7 +103,7 @@ namespace Rivet {
 	applyProjection<MissingMomentum>(event, "MissingET");
       const Jets alljets = 
 	applyProjection<FastJets>(event, "Jets").jetsByPt();
-
+      
       double misset = met.vectorEt().mod();
       if (leptons.size()<3 || neutrinos.size()<3 ||
 	  misset < _missET*GeV) {
@@ -120,28 +122,138 @@ namespace Rivet {
 	  isolatedLeptons.push_back(lepton);
       }
       if (isolatedLeptons.size()!=3) vetoEvent;
-
+      
       Jets bjets, ljets;
       foreach (const Jet& jet, alljets) {
         if (jet.containsBottom()) bjets.push_back(jet);
         else ljets.push_back(jet);
       }
       if (bjets.size()<2) vetoEvent;
-
-      // reconstruct 3 W's.
       FourMomentum W1, W2, W3;
-      FourMomentum lepmoms[3], neumoms[3];
+      FourMomentum t1, t2;
+      FourMomentum W1_test, W2_test, W3_test;
+      FourMomentum t1_test, t2_test;
+      FourMomentum lepmoms[3], neumoms[3], bmoms[2];
+      FourMomentum lepton_ss1, lepton_ss2, lepton_os;
       for (size_t i=0;i<3;i++) {
-	lepmoms[i] = leptons[i].momentum();
-	neumoms[i] = neutrinos[i].momentum();
+       	lepmoms[i] = leptons[i].momentum();
+       	neumoms[i] = neutrinos[i].momentum();
       }
-      std::set<int> lepIDs, neuIDs;
-      lepIDs.insert(0);lepIDs.insert(1);lepIDs.insert(2);
-      neuIDs.insert(0);neuIDs.insert(1);neuIDs.insert(2);
+      for (size_t i=0;i<2;i++) {
+       	bmoms[i] = bjets[i].momentum();
+      }
+      float chi_t1;
+      float chi_t2;
+      float chi_W1;
+      float chi_W2;
+      float chi_W3;
+      
+      float chisq=10000000000.;
+      float chisq_test;
+       for (size_t ltag1=0;ltag1<3;ltag1++){
+      	for (size_t nutag1=0;nutag1<3;nutag1++){
+      	  for (size_t btag=0;btag<2;btag++){
+      	    W1_test=lepmoms[ltag1]+neumoms[nutag1];
+      	    t1_test=lepmoms[ltag1]+neumoms[nutag1]+bmoms[btag];
+      	    for (size_t ltag2=0;ltag2<2;ltag2++){
+      	      if (ltag1==ltag2)
+		continue;
+	      if (PID::charge(leptons[ltag1].pdgId()==PID::charge(leptons[ltag2].pdgId()))){
+		lepton_ss1=lepmoms[ltag1];
+		lepton_ss2=lepmoms[ltag2];
+		lepton_os=lepmoms[3-ltag1-ltag2];
+		continue;
+	      }
+	      
+      	      else{
+      		for (size_t nutag2=0;nutag2<3;nutag2++){
+      		  if (nutag1==nutag2) continue;
+      		  else{
+      		    W2_test=lepmoms[ltag2]+neumoms[nutag2];
+      		    t2_test=lepmoms[ltag2]+neumoms[nutag2]+bmoms[1-btag];
+      		    W3_test=lepmoms[3-ltag1-ltag2]+neumoms[3-nutag1-nutag2];
+      		    chi_t1= pow((sqrt(fabs(t1*t1))-_mt),2)/(17.*17.);
+      		    chi_t2= pow((sqrt(fabs(t2*t2))-_mt),2)/(17.*17.);
+      		    chi_W1= pow((sqrt(fabs(W1*W1))-_mW),2)/(10.*10.);
+      		    chi_W2= pow((sqrt(fabs(W2*W2))-_mW),2)/(10.*10.);
+      		    chi_W3= pow((sqrt(fabs(W3*W3))-_mW),2)/(10.*10.);
+      		    chisq_test=chi_t1+chi_t2+chi_W1+chi_W2+chi_W3;
+      		    if (chisq_test<chisq){
+      		      chisq=chisq_test;
+      		      W1=W1_test;
+      		      W2=W2_test;
+      		      W3=W3_test;
+      		      t1=t1_test;
+      		      t2=t2_test;
+      		    }
+      		  }
+      		}
+      	      }
+      	    }
+      	  }
+      	}
+      }
+      
+      // std::cout << "W1  "<<W1<<std::endl;
+      //  std::cout << "Done W/t" << std::endl;
+      _h_njets->fill(alljets.size(),weight);
+      _h_mass_ll_ss->fill(lepton_ss1.mass()/GeV+lepton_ss2.mass()/GeV,weight);
+      float Zmass2(pow(_mZ,2));
+      float test;
+      test=fabs((lepton_ss1+lepton_os)*(lepton_ss1+lepton_os))-Zmass2;
+      if (fabs((lepton_ss2+lepton_os)*(lepton_ss2+lepton_os))-Zmass2<test)
+	_h_mass_ll_os->fill((lepton_ss2+lepton_os).mass(),weight); 
+      else
+	_h_mass_ll_os->fill((lepton_ss1+lepton_os).mass(),weight); 
+      _h_mass_lll->fill((lepmoms[0]+lepmoms[1]+lepmoms[2]).mass()/GeV,weight);
+      _h_pT_l1->fill(lepmoms[0].pT()/GeV,weight);
+      _h_pT_l2->fill(lepmoms[1].pT()/GeV,weight);
+      _h_pT_l3->fill(lepmoms[2].pT()/GeV,weight);
+      _h_eta_l1->fill(lepmoms[0].pseudorapidity(),weight);
+      _h_eta_l2->fill(lepmoms[1].pseudorapidity(),weight);
+      _h_eta_l3->fill(lepmoms[2].pseudorapidity(),weight);
+      _h_missET->fill(neumoms[0].Et()/GeV,weight); // change
+
+      //std::cout << "1"<<std::endl;
+      _h_pT_t1->fill(t1.pT()/GeV,weight);
+      _h_pT_t2->fill(t2.pT()/GeV,weight);
+      _h_pT_W->fill(W3.pT()/GeV,weight);
+      _h_pT_tt->fill((t1+t2).pT()/GeV,weight);
+      _h_pT_ttW->fill((t1+t2+W3).pT()/GeV,weight);
+      _h_mass_tt->fill((t1+t2).mass()/GeV,weight);
+      _h_mass_ttW->fill((t1+t2+W3).mass()/GeV,weight);
+      _h_rap_t1->fill(t1.y(),weight);
+      _h_rap_t2->fill(t2.y(),weight);
+      _h_rap_W->fill(W3.y(),weight);
+      _h_rap_tt->fill((t1+t2).y(),weight);
+      _h_rap_ttW->fill((t1+t2+W3).y(),weight);
+      
+      //     std::cout <<"2"<<std::endl;
+      _h_pT_b1->fill(bjets[0].momentum().pT()/GeV,weight);
+      //      if (bjets.size()>0){
+      _h_pT_b2->fill(bjets[1].momentum().pT()/GeV,weight);
+      //    }
+      //    std::cout << "2.5"<<std::endl;
+      //if (ljets.size()>0){
+      //_h_pT_lj->fill(ljets[0].momentum().pT()/GeV,weight);
+      // }
+      //      std::cout <<"2.75"<<std::endl;
+      //      if (bjets.size()>0){
+      // _h_eta_b1->fill(bjets[0].momentum().pseudorapidity(),weight); 
+      //      }
+      //     if (bjets.size()>1){
+      // _h_eta_b2->fill(bjets[1].momentum().pseudorapidity(),weight);  
+      //     }
+      //      std::cout << "3"<<std::endl;
+      //if (ljets.size()>0){
+      //	_h_eta_lj->fill(ljets[0].momentum().pT()/GeV,weight);
+      //}  
+      //     std::cout << "Filled!"<<std::endl;
+      //   }
     }
-
-
+    
     void finalize() {
+      // std::cout << "Finalising"<<std::endl;
       normalize(_h_njets); 
       normalize(_h_mass_ll_ss); 
       normalize(_h_mass_ll_os); 
@@ -149,6 +261,7 @@ namespace Rivet {
       normalize(_h_pT_l1); 
       normalize(_h_pT_l2); 
       normalize(_h_pT_l3); 
+      // std::cout << "first set"<<std::endl;
       normalize(_h_eta_l1); 
       normalize(_h_eta_l2); 
       normalize(_h_eta_l3); 
@@ -160,22 +273,27 @@ namespace Rivet {
       normalize(_h_pT_ttW); 
       normalize(_h_mass_tt); 
       normalize(_h_mass_ttW); 
-      normalize(_h_rap_t1); 
+      normalize(_h_rap_t1);
+      //std::cout << "second set"<<std::endl;
       normalize(_h_rap_t2); 
       normalize(_h_rap_W); 
       normalize(_h_rap_tt); 
       normalize(_h_rap_ttW); 
       normalize(_h_pT_b1); 
       normalize(_h_pT_b2); 
-      normalize(_h_pT_lj); 
-      normalize(_h_eta_b1); 
-      normalize(_h_eta_b2); 
-      normalize(_h_eta_lj); 
+      // normalize(_h_pT_lj); 
+      // std::cout << "b eta"<<std::endl;
+      // normalize(_h_eta_b1); 
+      //std::cout << "b1 eta"<<std::endl;
+      //normalize(_h_eta_b2); 
+      //std::cout << "b2 eta"<<std::endl;
+      // normalize(_h_eta_lj); 
+      // std::cout << "Done"<<std::endl;
     }
   };
 
-
-
+  
+  
   // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(MC_TTW);
 
