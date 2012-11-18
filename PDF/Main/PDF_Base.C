aec459cb07fca2a076135b5fe9235d604756fd41
Stefan Hoeche <shoeche@slac.stanford.edu>
2012-11-18 00:24:18 +0000

    make pdf complain only once per bad point

--- PDF/Main/PDF_Base.C
+++ PDF/Main/PDF_Base.C
@@ -65,27 +65,39 @@ void PDF_Base::Calculate(double x,double Q2)
 {
   Q2*=m_fac_scale_factor;
   if(Q2<m_q2min) {
+    static double lasterr(-1.0);
+    if (Q2!=lasterr)
     msg_Error()<<METHOD<<"(): Q-range violation Q = "<<sqrt(Q2)
 	       <<" < "<<sqrt(m_q2min)<<". Set Q -> "
 	       <<sqrt(m_q2min)<<"."<<std::endl;
+    lasterr=Q2;
     Q2=1.000001*m_q2min;
   }
   if(Q2>m_q2max) {
+    static double lasterr(-1.0);
+    if (Q2!=lasterr)
     msg_Error()<<METHOD<<"(): Q-range violation Q = "<<sqrt(Q2)
 	       <<" > "<<sqrt(m_q2max)<<". Set Q -> "
 	       <<sqrt(m_q2max)<<"."<<std::endl;
+    lasterr=Q2;
     Q2=0.999999*m_q2max;
   }
   if(x<m_xmin*m_rescale) {
+    static double lasterr(-1.0);
+    if (x!=lasterr)
     msg_Error()<<METHOD<<"(): x = "<<x<<" ("<<m_rescale
 	       <<") < "<<m_xmin<<". Set x -> "
 	       <<m_xmin<<"."<<std::endl;
+    lasterr=x;
     x=1.000001*m_xmin*m_rescale;
   }
   if(x>m_xmax*m_rescale) {
+    static double lasterr(-1.0);
+    if (x!=lasterr)
     msg_Error()<<METHOD<<"(): x = "<<x<<" ("<<m_rescale
 	       <<") > "<<m_xmax<<". Set x -> "
 	       <<m_xmax<<"."<<std::endl;
+    lasterr=x;
     x=0.999999*m_xmax*m_rescale;
   }
   return CalculateSpec(x,Q2);
