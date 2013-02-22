13f16cac26e41aa9ce43b84fcb4596459e7df0bd
Stefan Hoeche <shoeche@slac.stanford.edu>
2013-02-22 20:42:14 +0000

    revert css kin scheme to zero for lo backward compatibility

--- CSSHOWER++/Showers/Shower.C
+++ CSSHOWER++/Showers/Shower.C
@@ -23,7 +23,7 @@ Shower::Shower(PDF::ISR_Handler * isr,const int qed,const double & ktfac,
   m_fs_as_fac = ToType<double>(rpa->gen.Variable("CSS_FS_AS_FAC"));
   m_is_as_fac = ToType<double>(rpa->gen.Variable("CSS_IS_AS_FAC"));
   m_use_bbw   = dataread->GetValue<int>("CSS_USE_BBW",1);
-  m_kscheme   = dataread->GetValue<int>("CSS_KIN_SCHEME",1);
+  m_kscheme   = dataread->GetValue<int>("CSS_KIN_SCHEME",0);
   m_noem      = dataread->GetValue<int>("CSS_NOEM",0);
   std::vector<std::vector<std::string> > helpsvv;
   dataread->MatrixFromFile(helpsvv,"CSS_ENHANCE");
