d3d50fa995baf50a24c2e38cb7d2341a1584105c
Stefan Hoeche <shoeche@slac.stanford.edu>
2013-04-14 18:26:21 +0000

    pretty up MCFM gg->yy interface

--- AddOns/MCFM/MCFM_gg_yy.C
+++ AddOns/MCFM/MCFM_gg_yy.C
@@ -5,27 +5,22 @@ namespace MCFM {
 
   class MCFM_gg_yy: public PHASIC::Tree_ME2_Base {
   private:
-    int     m_pID;
     double  m_norm;
     double *p_p, *p_msqv;
     size_t  m_oqcd, m_oew;
     double  m_asfac, m_afac;
     double CouplingFactor(const int oqcd,const int oew) const;
-    double CallMCFM(const int & i,const int & j);
   public:
-    MCFM_gg_yy(const int & pID,const bool & swapped, 
-		const PHASIC::Process_Info& pi,
-		const ATOOLS::Flavour_Vector& flavs);
+    MCFM_gg_yy(const PHASIC::Process_Info &pi,
+	       const ATOOLS::Flavour_Vector &flavs);
     ~MCFM_gg_yy();
-    double Calc(const ATOOLS::Vec4D_Vector& momenta);
-    void SetCouplings(const MODEL::Coupling_Map& cpls);
+    double Calc(const ATOOLS::Vec4D_Vector &momenta);
+    void SetCouplings(const MODEL::Coupling_Map &cpls);
   };
 
 }// end of namespace MCFM
 
-extern "C" { 
-  void qqb_gamgam_(double *p,double *msqv); 
-}
+extern "C" { void qqb_gamgam_(double *p,double *msqv); }
 
 #include "MODEL/Main/Model_Base.H"
 #include "ATOOLS/Org/Run_Parameter.H"
@@ -37,11 +32,9 @@ using namespace MODEL;
 using namespace PHASIC;
 using namespace ATOOLS;
 
-MCFM_gg_yy::MCFM_gg_yy(const int & pID,const bool & swapped,
-		       const PHASIC::Process_Info& pi,
+MCFM_gg_yy::MCFM_gg_yy(const PHASIC::Process_Info& pi,
 		       const Flavour_Vector& flavs):
-  Tree_ME2_Base(pi,flavs), m_pID(pID),
-  m_norm(2.0*sqr(2.0*8.0))
+  Tree_ME2_Base(pi,flavs), m_norm(2.0*sqr(2.0*8.0))
 {
   rpa->gen.AddCitation
     (1,"The NLO matrix elements have been taken from MCFM \\cite{}.");
@@ -70,12 +63,6 @@ double MCFM_gg_yy::CouplingFactor(const int oqcd,const int oew) const
   return fac;
 }
 
-double MCFM_gg_yy::CallMCFM(const int & i,const int & j)
-{
-  qqb_gamgam_(p_p,p_msqv);
-  return p_msqv[mr(i,j)];
-}
-
 double MCFM_gg_yy::Calc(const Vec4D_Vector &p)
 {
   for (int n(0);n<2;++n) GetMom(p_p,n,-p[n]);
@@ -87,11 +74,8 @@ double MCFM_gg_yy::Calc(const Vec4D_Vector &p)
   qcdcouple_.gsq=4.*M_PI*qcdcouple_.as;
   qcdcouple_.ason2pi=qcdcouple_.as/(2.*M_PI);
   qcdcouple_.ason4pi=qcdcouple_.as/(4.*M_PI);
-  double res(CallMCFM(0,0)*m_norm);
-  epinv_.epinv=1.0;
-  double res1(CallMCFM(0,0)*m_norm);
-  epinv2_.epinv2=1.0;
-  double res2(CallMCFM(0,0)*m_norm);
+  qqb_gamgam_(p_p,p_msqv);
+  double res(p_msqv[mr(0,0)]*m_norm);
   return res*CouplingFactor(2,2);
 }
 
@@ -102,29 +86,20 @@ Tree_ME2_Base *ATOOLS::Getter
 <Tree_ME2_Base,Process_Info,MCFM_gg_yy>::
 operator()(const Process_Info &pi) const
 {
-  if (pi.m_loopgenerator!="MCFM")                       return NULL;
-  if (pi.m_fi.m_nloewtype!=nlo_type::lo)                return NULL;
+  if (pi.m_loopgenerator!="MCFM") return NULL;
+  if (pi.m_fi.m_nloewtype!=nlo_type::lo) return NULL;
   if (pi.m_fi.m_nloqcdtype==nlo_type::lo ||
       pi.m_fi.m_nloqcdtype==nlo_type::born) {
     Flavour_Vector fl(pi.ExtractFlavours());
-    if (!fl[0].IsGluon() || !fl[1].IsGluon())           return NULL;
-    if (fl.size()!=4)                                   return NULL;
-    int pID(0);
-    bool swapped(false);
+    if (fl.size()!=4) return NULL;
     if (pi.m_fi.m_ps.size()==2) {
-      ATOOLS::Flavour fl1(pi.m_fi.m_ps[0].m_fl[0]);
-      ATOOLS::Flavour fl2(pi.m_fi.m_ps[1].m_fl[0]);
-      if (fl1==Flavour(kf_photon) && fl2==Flavour(kf_photon)) {
-	pID = 285;
-	if (nproc_.nproc>=0) {
-	  if (nproc_.nproc!=pID)
-	    THROW(not_implemented,
-		  "Only one process class allowed when using MCFM");
-	}
-	nproc_.nproc=pID;
+      if (fl[0].IsGluon() && fl[1].IsGluon() &&
+	  fl[2].IsPhoton() && fl[3].IsPhoton()) {
+	if (nproc_.nproc>=0 && nproc_.nproc!=285)
+	  THROW(not_implemented,"Only one process allowed in MCFM");
+	nproc_.nproc=285;
 	chooser_();
-	msg_Info()<<"Initialise MCFM with nproc = "<<nproc_.nproc<<"\n";
-	return new MCFM_gg_yy(pID,swapped,pi,fl);
+	return new MCFM_gg_yy(pi,fl);
       }
     }
   }
