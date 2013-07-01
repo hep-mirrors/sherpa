c509ef3f09335bffdd5d7a48dc07e83165e3945d
Stefan Hoeche <shoeche@slac.stanford.edu>
2013-07-01 08:13:28 +0000

    set q2 in partonic decays

--- SHERPA/Single_Events/Decay_Handler_Base.C
+++ SHERPA/Single_Events/Decay_Handler_Base.C
@@ -431,11 +431,13 @@ Cluster_Amplitude* Decay_Handler_Base::ClusterConfiguration(Blob *const bl)
   double mu2=p_ampl->Leg(0)->Mom().Abs2();
   p_ampl->SetMuF2(mu2);
   p_ampl->SetKT2(mu2);
+  p_ampl->SetQ2(mu2);
   msg_Debugging()<<*p_ampl<<"\n";
   while (p_ampl->Prev()) {
     p_ampl=p_ampl->Prev();
     p_ampl->SetMuF2(mu2);
     p_ampl->SetKT2(mu2);
+    p_ampl->SetQ2(mu2);
   }
   msg_Debugging()<<"}\n";
   return p_ampl;
