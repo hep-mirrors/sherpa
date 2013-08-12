bc09a22ba57912d6f0b49bf753d2ba7a8966f459
Marek Schoenherr <marek.schoenherr@cern.ch>
2013-08-12 19:36:12 +0000

    use enums where there are enums

--- SHRiMPS/Main/Hadron_Init.C
+++ SHRiMPS/Main/Hadron_Init.C
@@ -7,8 +7,8 @@ using namespace ATOOLS;
 using namespace std;
 
 void Hadron_Init::Init() {
-  if(s_kftable.find(990)==s_kftable.end()) // if not initialized in amisic
-    s_kftable[990]=new Particle_Info(990,0.0,0.0,0,0,0,1,0,"pomeron","pomeron");
-  if(s_kftable.find(110)==s_kftable.end()) // if not initialized in amisic
-    s_kftable[110]=new Particle_Info(110,0.0,0.0,0,0,0,1,0,"reggeon","reggeon");
+  if(s_kftable.find(kf_pomeron)==s_kftable.end()) // if not initialized in amisic
+    s_kftable[kf_pomeron]=new Particle_Info(kf_pomeron,0.0,0.0,0,0,0,1,0,"pomeron","pomeron");
+  if(s_kftable.find(kf_reggeon)==s_kftable.end()) // if not initialized in amisic
+    s_kftable[kf_reggeon]=new Particle_Info(kf_reggeon,0.0,0.0,0,0,0,1,0,"reggeon","reggeon");
 }
