33ca41a509751bed2df514c2d289872fa7c22034
Stefan Hoeche <shoeche@slac.stanford.edu>
2013-09-23 04:43:09 +0000

    amplitude changes from yl

--- AddOns/Higgs/qqgg_tree.C
+++ AddOns/Higgs/qqgg_tree.C
@@ -1,21 +1,18 @@
-Complex qqgamgam_tree_mpmp(int i1, int i2, int i3, int i4) { return spa(i1,i2)*spa(i1,i3)*spa(i1,i3)/spa(i1,i4)/spa(i1,i2)/spa(i2,i4); }
-Complex qqgamgam_tree_pmpm(int i1, int i2, int i3, int i4) { return -spb(i1,i2)*spb(i1,i3)*spb(i1,i3)/spb(i1,i4)/spb(i1,i2)/spb(i2,i4); }
-Complex qqgamgam_tree_pmmp(int i1, int i2, int i3, int i4) { return -spa(i1,i2)*spa(i2,i3)*spa(i2,i3)/spa(i1,i4)/spa(i1,i2)/spa(i2,i4); }
-Complex qqgamgam_tree_mppm(int i1, int i2, int i3, int i4) { return spb(i1,i2)*spb(i2,i3)*spb(i2,i3)/spb(i1,i4)/spb(i1,i2)/spb(i2,i4); }
+Complex qqgamgam_tree_mpmp(int i1, int i2, int i3, int i4) { return pow(spa(i1,i3),2)/spa(i1,i4)/spa(i2,i4); }
+Complex qqgamgam_tree_pmpm(int i1, int i2, int i3, int i4) { return pow(spb(i1,i3),2)/spb(i1,i4)/spb(i2,i4); }
 
-// h2 == -h1 otherwise the amplitude vanishes
 Complex qqgamgam_tree_gen(int i1, int h1, int i2, int i3, int h3, int i4, int h4) {
-  if ( h3 == h4 ) { return 0.; }
-  if ( h3 == 1) {
-    if ( h1 == 1 ) { return qqgamgam_tree_pmpm(i1,i2,i3,i4); }
-    else { return qqgamgam_tree_mppm(i1,i2,i3,i4); }
-  }
-  else { // h3 == -1
-    if ( h1 == 1 ){ return qqgamgam_tree_pmmp(i1,i2,i3,i4); }
-    else { return qqgamgam_tree_mpmp(i1,i2,i3,i4); }
-  }
-  return 0.;
+    if ( h3==h4 ) return 0;
+    else if ( h1==h3 ) {
+        if ( h1==1 ) return qqgamgam_tree_pmpm(i1,i2,i3,i4);
+        else return qqgamgam_tree_mpmp(i1,i2,i3,i4);
+    }
+    else { // h1==h4
+        if ( h1==1 ) return qqgamgam_tree_pmpm(i1,i2,i4,i3);
+        else return qqgamgam_tree_mpmp(i1,i2,i4,i3);
+    }
 }
+
 
 // q(1) qbar(2) -> gam(3) gam(4)
 Complex qqbgamgam_tree(int h1, int h3, int h4) { return qqgamgam_tree_gen(1,h1,2,3,h3,4,h4); }
