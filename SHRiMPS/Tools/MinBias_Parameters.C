2d478c6c7fa5184168d7bd77a6d87173514bde8f
Frank Krauss <frank.krauss@durham.ac.uk>
2013-09-24 08:56:39 +0000

    new default parameters

--- SHRiMPS/Tools/MinBias_Parameters.C
+++ SHRiMPS/Tools/MinBias_Parameters.C
@@ -38,24 +38,23 @@ void MinBias_Parameters::Init(ATOOLS::Data_Reader * dr) {
   // ladder generation
   m_params["NLaddersFix"] = dr->GetValue<int>("N_Ladders_Fix",-1);
   m_params["KTMin_Mode"]  = dr->GetValue<int>("KTMin_Mode",0);
-  m_params["Q02"]         = dr->GetValue<double>("Q_0^2",0.148);
-  //m_params["Q_as2"]       = dr->GetValue<double>("Q_as^2",m_params["Q02"]);
-  m_params["Q_as2"]       = dr->GetValue<double>("Q_as^2",0.557);
-  m_params["Q12"]         = dr->GetValue<double>("Q_1^2",0.);
-  m_params["QN2"]         = dr->GetValue<double>("Q_N^2",0.029);
-  m_params["SingletWt"]   = dr->GetValue<double>("Chi_S",1.);
-  m_params["Ddiff2"]      = dr->GetValue<double>("D_diff^2",0.);
-  m_params["kdiff"]       = dr->GetValue<double>("K_diff",0.);
+  m_params["Q02"]         = dr->GetValue<double>("Q_0^2",1.0);
+  m_params["Q_as2"]       = dr->GetValue<double>("Q_as^2",1.0);
+  m_params["Q12"]         = dr->GetValue<double>("Q_1^2",0.0);
+  m_params["QN2"]         = dr->GetValue<double>("Q_N^2",0.0);
+  m_params["SingletWt"]   = dr->GetValue<double>("Chi_S",1.0);
+  m_params["Ddiff2"]      = dr->GetValue<double>("D_diff^2",0.0);
+  m_params["kdiff"]       = dr->GetValue<double>("K_diff",0.0);
   // showering off soft stuff
   m_params["shower_mode"] = dr->GetValue<int>("Shower_Mode",3);
-  m_params["min_kt2"]     = dr->GetValue<double>("Shower_Min_KT2",16.);
-  m_params["kt2_factor"]  = dr->GetValue<double>("KT2_Factor",4.);
-  m_params["diff_factor"] = dr->GetValue<double>("Diff_Factor",1.);
+  m_params["min_kt2"]     = dr->GetValue<double>("Shower_Min_KT2",1.0);
+  m_params["kt2_factor"]  = dr->GetValue<double>("KT2_Factor",1.0);
+  m_params["diff_factor"] = dr->GetValue<double>("Diff_Factor",1.0);
   // rescatterings
-  m_params["RescProb"]    = dr->GetValue<double>("RescProb",1.71);
-  m_params["RescProb1"]   = dr->GetValue<double>("RescProb1",0.489);
-  m_params["QRC2"]        = dr->GetValue<double>("Q_RC^2",1.01);
-  m_params["ReconnProb"]  = dr->GetValue<double>("ReconnProb",-10.2);
+  m_params["RescProb"]    = dr->GetValue<double>("RescProb",1.0);
+  m_params["RescProb1"]   = dr->GetValue<double>("RescProb1",0.0);
+  m_params["QRC2"]        = dr->GetValue<double>("Q_RC^2",1.0);
+  m_params["ReconnProb"]  = dr->GetValue<double>("ReconnProb",-10.);
   m_params["Misha"]       = dr->GetValue<int>("Misha",1);
 
   std::string ffform = 
