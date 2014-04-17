8129c9ce7889754f4c0aa3ae7b7bca18d5c3e697
Stefan Hoeche <shoeche@slac.stanford.edu>
2014-04-17 23:33:03 +0000

    svn merge -c23823 trunk/SHERPA

--- AMEGIC++/Main/Process_Group.C
+++ AMEGIC++/Main/Process_Group.C
@@ -85,6 +85,23 @@ bool AMEGIC::Process_Group::Initialize(PHASIC::Process_Base *const proc)
     if (!p_pinfo) p_pinfo=Translate(m_pinfo);
     p_testmoms = new Vec4D[m_nin+m_nout];
     Phase_Space_Handler::TestPoint(p_testmoms,&Info(),Generator());
+    Vec4D sum;
+    Poincare lab(Vec4D(sqrt(10.0),0.0,0.0,1.0));
+    msg_Debugging()<<"After boost:\n";
+    for (size_t i(0);i<m_nin+m_nout;++i) {
+      lab.Boost(p_testmoms[i]);
+      sum+=i<m_nin?-p_testmoms[i]:p_testmoms[i];
+      msg_Debugging()<<"  p["<<i<<"] = "<<p_testmoms[i]<<"\n";
+    }
+    msg_Debugging()<<"} -> sum = "<<sum<<"\n";
+    Poincare rot(Vec4D::ZVEC,Vec4D(sqrt(14.0),1.0,2.0,3.0));
+    msg_Debugging()<<"After rotation:\n";
+    for (size_t i(0);i<m_nin+m_nout;++i) {
+      rot.Rotate(p_testmoms[i]);
+      sum+=i<m_nin?-p_testmoms[i]:p_testmoms[i];
+      msg_Debugging()<<"  p["<<i<<"] = "<<p_testmoms[i]<<"\n";
+    }
+    msg_Debugging()<<"} -> sum = "<<sum<<"\n";
   }
   AMEGIC::Process_Base* apb=proc->Get<AMEGIC::Process_Base>();
   apb->SetPrintGraphs(m_pinfo.m_gpath);
