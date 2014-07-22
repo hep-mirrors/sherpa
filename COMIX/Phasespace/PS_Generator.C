5903a570a3a8312de294d77a8fee4195fce386df
Stefan Hoeche <shoeche@slac.stanford.edu>
2014-07-22 06:08:58 +0000

    bugfix in comix ps

--- COMIX/Phasespace/PS_Generator.C
+++ COMIX/Phasespace/PS_Generator.C
@@ -255,11 +255,12 @@ bool PS_Generator::Construct(Amplitude *const ampl)
 	      for (CB_MMap::const_iterator bit(m_cmap.lower_bound(jb));
 		   bit!=m_cmap.upper_bound(jb);++bit) {
 		Vertex_Key vkey(ait->second,bit->second,NULL,cit->second,NULL);
-		int type(DecayType(curs[n][j],ja,jb));
+		int type(DecayType(cit->second,ait->second,bit->second));
 		bool vf(false);
-		for (size_t k(0);k<rin.size();++k) 
+		for (size_t k(0);k<rin.size();++k)
 		  if (vkey.p_a==rin[k]->JA() && vkey.p_b==rin[k]->JB() &&
-		      (type&((PS_Vertex*)rin[k])->Type())) {
+		      (type==((PS_Vertex*)rin[k])->Type() ||
+		       (type&((PS_Vertex*)rin[k])->Type()))) {
 		    vf=true;
 		    break;
 		  }
