414cc8b46bed04a91e748d71fe35cdf3066470d5
Jennifer Thompson <thompson@thphys.uni-heidelberg.de>
2015-01-24 09:05:28 +0000

    updates to SingleTop_Core_Scale.C

--- PHASIC++/Scales/SingleTop_Core_Scale.C
+++ PHASIC++/Scales/SingleTop_Core_Scale.C
@@ -29,13 +29,13 @@ PDF::CParam SingleTop_Core_Scale::Calculate(Cluster_Amplitude *const ampl)
   double that(2.0*ampl->Leg(0)->Mom()*ampl->Leg(2)->Mom());
   double uhat(2.0*ampl->Leg(0)->Mom()*ampl->Leg(3)->Mom());
   if (ampl->Legs().size()>4) {
+    //    msg_Out()<<"Not enough clustering, so we are left with n legs= "<<ampl->Legs().size()<<std::endl;
     return PDF::CParam(shat,shat,0.0,shat,-1);
   }
   double muf2(-1.),mur2(-1.),muq2(-1.);
   Flavour f[4]={ampl->Leg(0)->Flav(),ampl->Leg(1)->Flav(),
 		ampl->Leg(2)->Flav(),ampl->Leg(3)->Flav()};
-
-  // tW production: muF = mT(top)
+   // tW production: muF = mT(top)
   if ((f[2].Kfcode()==24 && f[3].Kfcode()==6) ||
       (f[2].Kfcode()==6 && f[3].Kfcode()==24)) {
     muf2 = mur2 = muq2 = sqr(Flavour(kf_t).Mass()) +
@@ -58,15 +58,18 @@ PDF::CParam SingleTop_Core_Scale::Calculate(Cluster_Amplitude *const ampl)
        ampl->Leg(2)->Mom().PPerp2():ampl->Leg(3)->Mom().PPerp2());
   }
   // t-channel top: muF = \hat{t} 
-  else if ((f[2].Kfcode()!=5 && f[2].IsQuark() && f[3].Kfcode()==6) ||
-	   (f[2].Kfcode()==6 && f[3].Kfcode()!=5 && f[3].IsQuark())) {
+  else if ((f[2].IsQuark() && f[2].QuarkFamily()!=3 && f[3].Kfcode()==6) ||
+	   (f[2].Kfcode()==6 && f[3].IsQuark() && f[3].QuarkFamily()!=3)) {
     muf2 = mur2 = muq2 = dabs(that);
   }
   // top in initial state
   else if ((f[0].Kfcode()==6 && f[1].Strong() && f[1].Kfcode()!=6) ||
 	   f[0].Strong() && f[0].Kfcode()!=6 && f[1].Kfcode()==6) {
     unsigned short int nottop=1;
-    if (f[1].Kfcode()==6) nottop=0;
+    if (f[1].Kfcode()==6){ 
+      if (f[0].Kfcode()==6) std::cout<<"tt initial state\n";
+      nottop=0;
+    }
     muf2 = muq2 = mur2 = (f[nottop].Kfcode()==5?shat:dabs(that));
   }
   // W in initial state
@@ -76,12 +79,14 @@ PDF::CParam SingleTop_Core_Scale::Calculate(Cluster_Amplitude *const ampl)
   }
   // pure QCD process
   else if (f[0].Strong() && f[0].QuarkFamily()!=3 &&
-	   f[1].Strong() && f[1].QuarkFamily()!=3) {
-    muf2 = mur2 = muq2 = dabs(that*uhat)/shat;    
+	   f[1].Strong() && f[1].QuarkFamily()!=3 && 
+	   f[2].Strong() && f[3].Strong()) {
+    muf2 = mur2 = muq2 = -1.0/(1.0/shat+1.0/that+1.0/uhat);    
   }
   else if ((f[0].Strong() && f[1].Strong()) && 
-          (f[0].Kfcode()==5 || f[1].Kfcode()==5))
-    muf2 = mur2 = muq2 = dabs(that*uhat)/shat;    
+          (f[0].Kfcode()==5 || f[1].Kfcode()==5)
+	   && f[2].Strong() && f[3].Strong())
+    muf2 = mur2 = muq2 = -1.0/(1.0/shat+1.0/that+1.0/uhat);    
   if (muf2<0.) {
     msg_Out()<<METHOD<<": found something unexpected: "
 	     <<f[0]<<" "<<f[1]<<" --> "<<f[2]<<" "<<f[3]<<",\n"
