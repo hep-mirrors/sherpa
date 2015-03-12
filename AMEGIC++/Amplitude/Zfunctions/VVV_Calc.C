11055a878dd1e713864a3d0582758eb921f48243
Steffen Schumann <steffen.schumann@phys.uni-goettingen.de>
2015-03-12 21:30:50 +0000

    remove output for AMEGIC_CUT_MASSIVE_VECTOR_PROPAGATORS set to zero

--- AMEGIC++/Amplitude/Zfunctions/VVV_Calc.C
+++ AMEGIC++/Amplitude/Zfunctions/VVV_Calc.C
@@ -40,8 +40,7 @@ Kabbala VVV_Calc::Do()
   if (!IsZero(X(0,0)*M(0)) || 
       !IsZero(X(1,1)*M(1)) || 
       !IsZero(X(2,2)*M(2))) {
-    std::cerr<<"Error in VVV_Calc::Do(): not cutted massive vertex!"<<std::endl;
-
+    
     return factor*( M(0)*M(1)*V(1,2)*X(0,0)*X(1,1)*X(2,0)-M(1)*X(0,1)*X(1,1)*X(2,0)
                     -M(0)*X(0,0)*X(1,2)*X(2,0)+M(0)*X(0,0)*X(1,0)*X(2,1)
                     -M(0)*M(1)*V(0,2)*X(0,0)*X(1,1)*X(2,1)+M(1)*X(0,2)*X(1,1)*X(2,1)
