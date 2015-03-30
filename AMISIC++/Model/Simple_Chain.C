9ec97eaebdb77ccee6f5b57d8e8aba56306022b8
Stefan Hoeche <shoeche@slac.stanford.edu>
2015-03-30 05:28:47 +0000

    bugfix in mpi orders

--- AMISIC++/Model/Simple_Chain.C
+++ AMISIC++/Model/Simple_Chain.C
@@ -241,7 +241,7 @@ void Simple_Chain::InitializeProcessList(const Flavour& in1,
   pi.m_fi.m_ps.push_back(PHASIC::Subprocess_Info(out1,"",""));
   pi.m_fi.m_ps.push_back(PHASIC::Subprocess_Info(out2,"",""));
   pi.m_maxcpl[1]=pi.m_mincpl[1]=0;
-  pi.m_maxcpl[0]=pi.m_mincpl[0]=4;
+  pi.m_maxcpl[0]=pi.m_mincpl[0]=2;
   pi.m_scale=p_read->GetValue<std::string>("MPI_SCALE","MPI");
   pi.m_kfactor=p_read->GetValue<std::string>("MPI_KFACTOR","NO");
   pi.m_coupling="Alpha_QCD 1";
