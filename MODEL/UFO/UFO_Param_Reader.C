eedc56782c62abf26595ef20e2e03ca771b43edc
Silvan Kuttimalai <silvan@slac.stanford.edu>
2015-04-09 07:33:38 +0000

    Restrict UFO reader to (ufo){...}(ufo) section if run card is used as model input

--- MODEL/UFO/UFO_Param_Reader.C
+++ MODEL/UFO/UFO_Param_Reader.C
@@ -1,8 +1,6 @@
 #include "MODEL/UFO/UFO_Param_Reader.H"
 #include "ATOOLS/Org/Data_Reader.H"
 #include "ATOOLS/Org/Exception.H"
-#include "ATOOLS/Org/Read_Write_Base.H"
-#include "ATOOLS/Org/MyStrStream.H"
 #include "ATOOLS/Org/Run_Parameter.H"
 
 #include <assert.h>
@@ -15,16 +13,19 @@ using namespace UFO;
 using std::string;
 using std::vector;
 using std::stringstream;
+using ATOOLS::ToType;
+using ATOOLS::Data_Reader;
+using ATOOLS::rpa;
 
 UFO_Param_Reader::UFO_Param_Reader(const string& filepath)
 {
   // if filepath not explicitly given, use run card
-  bool given(filepath!="");
-  string file_path = given ? filepath :ATOOLS::rpa->gen.Variable("RUN_DATA_FILE");
+  m_use_runcard = (filepath=="");
+  string file_path = m_use_runcard ? rpa->gen.Variable("RUN_DATA_FILE") : filepath;
   string filename(""), path("");
   // split filepath into path and name
   size_t pos(file_path.find_last_of("/"));
-  if (pos!=std::string::npos){
+  if (pos!=string::npos){
     path=file_path.substr(0,pos+1);
     filename=file_path.substr(pos+1);
   }
@@ -32,81 +33,75 @@ UFO_Param_Reader::UFO_Param_Reader(const string& filepath)
     path=string("./");
     filename=file_path;
   }
-  if (filename.find("|")!=std::string::npos) 
+  if (filename.find("|")!=string::npos) 
       filename=filename.substr(0,filename.find("|"));
-  p_dataread = new ATOOLS::Data_Reader(" ",";","#","=");
-  p_dataread->AddWordSeparator("\t");
-  p_dataread->SetInputPath(path);
-  p_dataread->SetInputFile(filename);
-  p_dataread->SetIgnoreCase(true);
-  p_dataread->SetAddCommandLine(false);
+  if(m_use_runcard) filename+="|(ufo){|}(ufo)";
+  Data_Reader dataread(" ",";","#","=");
+  dataread.AddWordSeparator("\t");
+  dataread.SetInputPath(path);
+  dataread.SetInputFile(filename);
+  dataread.SetIgnoreCase(true);
+  dataread.SetAddCommandLine(false);
+  dataread.MatrixFromFile(m_lines);
 }
-
-UFO_Param_Reader::~UFO_Param_Reader()
-{
-  delete p_dataread;
-}
-
 
 template<class Read_Type> Read_Type 
-UFO_Param_Reader::GetEntry(const string& block, const unsigned int& n, const unsigned int& m)
+UFO_Param_Reader::GetEntry(const string& block,
+			   const unsigned int& n,
+			   const unsigned int& m)
 {
-  p_dataread->SetFileBegin(string("block ").append(block));
-  p_dataread->RereadInFile();
-  vector< vector<string> > vals;
-  p_dataread->MatrixFromFile(vals);
-  for(vector< vector<string> >::const_iterator it = vals.begin(); it != vals.end(); ++it)
-    {
-      for(vector<string>::const_iterator jt = it->begin(); jt != it->end(); ++jt)
-	if (p_dataread->Find(*jt, "block") != string::npos)
-	  NotFound(block, n, m);
-      if (it->size() < 3)
-	continue;
-      if (ATOOLS::ToType<int>((*it)[0]) == n && ATOOLS::ToType<int>((*it)[1]) == m)
-	return ATOOLS::ToType<Read_Type>((*it)[2]);
-    }
-  NotFound(block, n, m);
+  vector< vector<string> >::const_iterator line = FindBlock(block);
+  for(++line; line!=m_lines.end(); ++line){
+    if (line->empty()) continue;
+    if (IgnoreCaseCompare((*line)[0],"block")) NotFound(block,n);
+    if (line->size() < 3) continue;
+    if (ToType<int>((*line)[0])==n && ToType<int>((*line)[1])==m)
+      return ToType<Read_Type>((*line)[2]);
+  }
+  NotFound(block, n);
 }
 
 template<class Read_Type> Read_Type 
 UFO_Param_Reader::GetEntry(const string& block, const unsigned int& n)
 {
-  string lc_block(block);
-  for (size_t i=0;i<lc_block.length();++i) lc_block[i]=tolower(lc_block[i]);
-  // width specifications don't follow UFO conventions for some reason
-  if (lc_block == "decay") 
-    return GetWidth<Read_Type>(n);
-  p_dataread->SetFileBegin(string("block ").append(block));
-  p_dataread->RereadInFile();
-  vector< vector<string> > vals;
-  p_dataread->MatrixFromFile(vals);
-  for(vector< vector<string> >::const_iterator it = vals.begin(); it != vals.end(); ++it)
-    {
-      for(vector<string>::const_iterator jt = it->begin(); jt != it->end(); ++jt)
-	if (p_dataread->Find(*jt, "block") != string::npos)
-	    NotFound(block, n);
-      if (it->size() < 2)
-	continue;
-      if (ATOOLS::ToType<int>((*it)[0]) == n)
-	return ATOOLS::ToType<Read_Type>((*it)[1]);
-    }
+  // widths in UFO param cards are handled differently for some reason
+  if (IgnoreCaseCompare(block, "decay")) return GetWidth<Read_Type>(n);
+  vector< vector<string> >::const_iterator line = FindBlock(block);
+  for(++line; line!=m_lines.end(); ++line){
+    if (line->empty()) continue;
+    if (IgnoreCaseCompare((*line)[0],"block")) NotFound(block,n);
+    if (line->size() < 2) continue;
+    if (ToType<int>((*line)[0]) == n) return ToType<Read_Type>((*line)[1]);
+  }
   NotFound(block, n);
 }
 
 template<class Read_Type> Read_Type
 UFO_Param_Reader::GetWidth(const unsigned int& n)
 {
-  p_dataread->ClearFileBegin();
-  p_dataread->RereadInFile();
-  vector< vector<string> > vals;
-  p_dataread->MatrixFromFile(vals);
-  for(vector< vector<string> >::const_iterator it = vals.begin(); it != vals.end(); ++it)
-    {
-      assert(it->size());
-      if (p_dataread->Find((*it)[0], "decay") != string::npos && it->size() > 2 && ATOOLS::ToType<int>((*it)[1]) == n )
-	  return ATOOLS::ToType<Read_Type>((*it)[2]);
-    }
+  for(vector< vector<string> >::const_iterator line = m_lines.begin(); line != m_lines.end(); ++line){
+    if (line->size() < 3) continue;
+    if (IgnoreCaseCompare((*line)[0],"decay") && ToType<int>((*line)[1]) == n )
+      return ToType<Read_Type>((*line)[2]);
+  }
   NotFound(string("decay"),n);
+}
+
+vector< vector<string> >::const_iterator UFO_Param_Reader::FindBlock(const string& block){
+  vector< vector<string> >::const_iterator ret=m_lines.begin();
+  for(; ret!=m_lines.end(); ++ret){
+    if(ret->size()<2) continue;
+    if(IgnoreCaseCompare((*ret)[1],block)) return ret;
+  }
+  THROW(fatal_error, "Block "+block+" not found")
+}
+
+bool UFO_Param_Reader::IgnoreCaseCompare(const std::string& a, const std::string& b){
+  if (a.size() != b.size())return false;
+  for (string::const_iterator ia = a.begin(), ib = b.begin(); ia!=a.end(); ++ia, ++ib)
+    if (tolower(*ia) != tolower(*ib)) 
+      return false;
+  return true;
 }
 
 void UFO_Param_Reader::NotFound(const string &block, const unsigned int& n, const unsigned int& m)
@@ -122,7 +117,6 @@ void UFO_Param_Reader::NotFound(const string &block, const unsigned int& n)
   message << ("Entry [") << n << "] " << "in block " << block << " not found.";
   THROW(fatal_error, message.str().c_str() );
 }
-
 
 namespace UFO
 {
