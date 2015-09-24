0b53aa002c42a4da9d85604ee3e304bb51c60425
Marek Schoenherr <marek.schoenherr@cern.ch>
2015-09-24 19:09:21 +0000

    BUGFIX -- fix H_TY2

--- PHASIC++/Scales/Tag_Setter.C
+++ PHASIC++/Scales/Tag_Setter.C
@@ -86,7 +86,7 @@ namespace PHASIC {
       double hty(0.0);
       for (size_t i(p_setter->NIn());i<p.size();++i) 
 	hty+=p[i].PPerp()*exp(htyfac*pow(abs(p[i].Y()-yboost),htyexp));
-      Term *res(Term::New(hty));
+      Term *res(Term::New(sqr(hty)));
       p_interpreter->AddTerm(res);
       return res;
     }
