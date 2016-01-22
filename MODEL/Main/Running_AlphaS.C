a7da22fcf5287dd7d5e0080e0e21a7242baa0eb5
Marek Schoenherr <marek.schoenherr@cern.ch>
2016-01-22 22:32:47 +0000

    svn merge -c 27410 trunk/SHERPA check (x,Q2) whether bad before asking LHAPDF, check q2 before calcing alphaS

--- MODEL/Main/Running_AlphaS.C
+++ MODEL/Main/Running_AlphaS.C
@@ -514,6 +514,11 @@ double One_Running_AlphaS::operator[](double q2)
 
 double One_Running_AlphaS::operator()(double q2)
 {
+  if (IsBad(q2)) {
+    msg_Error()<<METHOD<<"(): Encountered bad q2="<<q2<<"), "
+                       <<"returning zero."<<std::endl;
+    return 0.;
+  }
   if (m_pdf) return p_pdf->AlphaSPDF(q2);
   double as(0.);
   if (q2<0.) q2=-q2;
