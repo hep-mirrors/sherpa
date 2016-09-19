fdc0e0426c2651480738cfe5879f2804864a826a
Stefan Hoeche <shoeche@slac.stanford.edu>
2016-09-19 06:29:57 +0000

    svn merge -r28980:28982 ^/branches/dire (changed if 123 kinematics to explicit decay, added jacobian)

--- DIRE/Shower/Lorentz_IF.C
+++ DIRE/Shower/Lorentz_IF.C
@@ -89,10 +89,15 @@ Lorentz_IF_123::Lorentz_IF_123(const Kernel_Key &k):
 double Lorentz_IF_123::Jacobian(const Splitting &s) const
 {
   double za(s.m_x), xa(s.m_z2);
-  double u(s.m_s*za/s.m_Q2), x(xa+s.m_s/s.m_Q2*za/xa);
-  double ct(1.0-2.0/(1.0+xa*xa/za/(s.m_s/s.m_Q2-u*(x/za-1.0))));
-  double J((1.0-ct*ct)/xa+(x-za)/(2.0*xa*xa)*sqr(1.0+ct)*((x-u)/xa-2.0));
-  return J/2.0*Lorentz_IF::Jacobian(s);
+  double Q2(s.m_mij2+s.m_mk2-(s.p_c->Mom()+s.p_s->Mom()).Abs2());
+  double saijk(Q2*xa/za+s.m_t/xa), sjk(saijk-Q2+s.m_s);
+  double ej((sjk+s.m_mj2-s.m_mk2)/(2.0*sjk));
+  double bai(sqrt(1.0+4.0*s.m_s*sjk/sqr(saijk)));
+  double bj(sqrt(1.0-4.0*s.m_mj2*sjk/sqr(s.m_mj2+sjk)));
+  double J(Q2*s.m_t/ej+za*sjk/sqr(bai)*(1.0+2.0*s.m_s/saijk)
+	   *(1.0-s.m_t/xa/saijk/ej)*(Q2*xa/za-s.m_t/xa));
+  J/=bai*bj*xa*za*sqr(saijk);
+  return J*Lorentz_IF::Jacobian(s);
 }
 
 int Lorentz_IF_123::Construct(Splitting &s,const int mode) const
@@ -110,19 +115,25 @@ int Lorentz_IF_123::Construct(Splitting &s,const int mode) const
   Kin_Args ff(s.m_s*za/Q2,xa+s.m_s*za/Q2+s.m_t*za/(Q2*xa),s.m_phi,s.m_kin);
   if (ff.m_z>1.0) return -1.0;
   ff.m_mk2=Q2*(xa/za-1.0)+s.m_s+s.m_t/xa;
+  if (ff.m_mk2<sqr(sqrt(s.m_mj2)+sqrt(s.m_mk2))) return -1.0;
   if (ConstructIFDipole
       (s.m_mi2,s.m_ml2,s.m_mij2,s.m_mk2,b?p_ms->Mass2(b->Flav()):0.0,
        -s.p_c->Mom(),s.p_s->Mom(),b?-b->Mom():Vec4D(),ff)<0)
     return -1;
-  double y2(2.0*(ff.m_pk*(ff.m_pi-ff.m_pj))/(ff.m_mk2-s.m_mk2-s.m_mj2));
-  Kin_Args ff2(ff.m_mk2?1.0/(1.0+y2):0.0,1.0-2.0/
-	       (1.0+xa*xa/za/(s.m_t/Q2-ff.m_y*(ff.m_z/za-1.0))),s.m_phi2);
-  if (ConstructFFDipole
-      (s.m_mk2,s.m_mj2,ff.m_mk2,-s.m_s,
-       ff.m_pk,ff.m_pi-ff.m_pj,ff2)<0) return -1;
-  ff.m_pk=ff2.m_pi;
+  Poincare cms(ff.m_pk);
+  Vec4D pai(ff.m_pi-ff.m_pj);
+  cms.Boost(pai);
+  Poincare zrot(pai,Vec4D::ZVEC);
+  double E((ff.m_mk2+s.m_mj2-s.m_mk2)/sqrt(4.0*ff.m_mk2));
+  double ct(1.0-sqrt(ff.m_mk2)/E*s.m_t*za/(s.m_t*za+Q2*xa*xa));
+  ct/=sqrt(1.0+s.m_s/sqr(pai[0]))*sqrt(1.0-s.m_mj2/(E*E));
+  double p(sqrt(E*E-s.m_mj2)), st(sqrt(1.0-ct*ct));
+  Vec4D pj(E,p*st*cos(s.m_phi2),p*st*sin(s.m_phi2),p*ct);
+  zrot.RotateBack(pj);
+  cms.BoostBack(pj);
+  ff.m_pk-=pj;
   ff.m_pi=-ff.m_pi;
-  return Update(s,ff,mode,ff2.m_pj);
+  return Update(s,ff,mode,pj);
 }
 
 bool Lorentz_IF_123::Cluster(Splitting &s,const int mode) const
