3d653d2d7fa2fe182235c4a38eb2ab0a13df5fae
Frank Krauss <frank.krauss@durham.ac.uk>
2017-04-18 22:04:48 +0000

    added missing file - sorry.

--- AHADIC++/Main/SVN_Info.C
+++ AHADIC++/Main/SVN_Info.C
@@ -1,3 +1,3 @@
 #include "ATOOLS/Org/SVN_Info.H"
 static ATOOLS::SVN_Info initializer
-("AHADIC++/Main","branches/rel-2-2-3","29868M","7da67ec9e02ee740235bd445b857762c");
+("AHADIC++/Main","branches/rel-2-2-3","29880M","7da67ec9e02ee740235bd445b857762c");
--- AHADIC++/Tools/SVN_Info.C
+++ AHADIC++/Tools/SVN_Info.C
@@ -1,3 +1,3 @@
 #include "ATOOLS/Org/SVN_Info.H"
 static ATOOLS::SVN_Info initializer
-("AHADIC++/Tools","branches/rel-2-2-3","29868M","f8962fdb2ea16de8f5668b2bd93b9dae");
+("AHADIC++/Tools","branches/rel-2-2-3","29880M","f8962fdb2ea16de8f5668b2bd93b9dae");
