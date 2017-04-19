4c6dd30ee8e8e18d3cf9688a0cac62614a4c242e
Frank Krauss <frank.krauss@durham.ac.uk>
2017-04-19 09:17:56 +0000

    make it run

--- AHADIC++/Formation/SVN_Info.C
+++ AHADIC++/Formation/SVN_Info.C
@@ -1,3 +1,3 @@
 #include "ATOOLS/Org/SVN_Info.H"
 static ATOOLS::SVN_Info initializer
-("AHADIC++/Formation","branches/rel-2-2-3","29880M","4fa5c381cceb619231ab3c564b67201d");
+("AHADIC++/Formation","branches/rel-2-2-4","29887M","4fa5c381cceb619231ab3c564b67201d");
