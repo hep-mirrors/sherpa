a5950a4d4ec406da95d7fc95dc52f6dc5c3ee4f1
Enrico Bothmann <enrico.bothmann@ed.ac.uk>
2018-02-05 12:50:44 +0000

    Support more DL coefficients
    The prominent exception are the non-diagonal terms for external neutral gauge
    bosons.

--- PHASIC++/EWSudakov/Sudakov.C
+++ PHASIC++/EWSudakov/Sudakov.C
@@ -21,6 +21,8 @@ using namespace ATOOLS;
 // Denner:2000jv
 constexpr auto sinw2 = 0.22356;
 constexpr auto cosw2 = 1 - sinw2;
+constexpr auto sinw = 0.472821;
+constexpr auto cosw = 0.881158;
 
 Sudakov::Sudakov(Process_Base& proc):
   m_proc{ proc },
@@ -29,18 +31,22 @@ Sudakov::Sudakov(Process_Base& proc):
 { }
 
 /// effective electroweak Casimir operator, cf. eq. (B.10)
-double Sudakov::Cew(const Flavour& flav, int pol)
+double Sudakov::DiagonalCew(const Flavour& flav, int pol)
 {
   // pol is either chirality or polarisation:
   // 0: + (right-handed or transverse polarisation)
   // 1: - (left-handed or transverse polarisation)
   // 2: 0 (longitudinal polarisation)
-  if (flav.IsLepton()) {
-    if (pol == 0)
+  constexpr auto CewLefthandedLepton = (1 + 2*cosw2) / (4*sinw2*cosw2);
+  if (flav.IsLepton()) {  // cf. eq. (B.16)
+    if (pol == 0) {
+      if (flav.IsUptype())
+        THROW(fatal_error, "Right-handed neutrino are not supported");
       return 1/cosw2;
-    else
-      return (1 + 2*cosw2) / (4*sinw2*cosw2);
-  } else if (flav.IsQuark()) {
+    } else {
+      return CewLefthandedLepton;
+    }
+  } else if (flav.IsQuark()) {  // cf. eq. (B.16)
     if (pol == 1) {
       return (sinw2 + 27*cosw2) / (36*sinw2*cosw2);
     } else {
@@ -49,13 +55,33 @@ double Sudakov::Cew(const Flavour& flav, int pol)
       else
         return 1 / (9*cosw2);
     }
+  } else if (flav.IsScalar()) {  // cf. eq. (B.18) and (B.16)
+    return CewLefthandedLepton;
+  } else if (flav.Kfcode() == kf_Wplus) {
+    // NOTE: for longitudinal bosons, use the Goldstone equivalence theorem
+    if (pol == 2)
+      return CewLefthandedLepton;
+    else
+      return 2/sinw2;
   } else {
     THROW(not_implemented, "Missing implementation");
   }
 }
 
-ATOOLS::Cluster_Amplitude* Sudakov::CreateAmplitude(Process_Base& proc)
+double Sudakov::NondiagonalCew(kf_code from, kf_code to)
+{
+  if ((from != kf_Z && from != kf_photon) || (to != kf_Z && to != kf_photon))
+    THROW(fatal_error, "Only neutral gauge bosons are supported");
+  if (from != to)
+    return -2.0 * cosw/sinw;
+  if (from == kf_photon)
+    return 2.0;
+  if (from == kf_Z)
+    return 2.0 * cosw2/sinw2;
+  THROW(fatal_error, "Logic error");
+}
 
+ATOOLS::Cluster_Amplitude* Sudakov::CreateAmplitude(Process_Base& proc)
 {
   ATOOLS::Cluster_Amplitude* ampl{ Cluster_Amplitude::New() };
   ampl->SetNIn(proc.NIn());
@@ -95,9 +121,19 @@ double Sudakov::CalculateDoubleLogarithmicCoefficient() const
     for (size_t j{ 0 }; j < spincombination.size(); ++j) {
       const Flavour flav{ p_ampl->Leg(j)->Flav() };
       if (flav.IsBoson() && flav.Charge() == 0) {
-        THROW(not_implemented, "Missing implementation");
+        // mixing between neutral gauge bosons: non-diagonal terms appear
+        const auto from = flav.Kfcode();
+        coeffs[i] -= NondiagonalCew(from, from) / 2.0;
+        const auto to = (from == kf_photon) ? kf_Z : kf_photon;
+        const auto prefactor = -NondiagonalCew(from, to) / 2.0;
+        const auto amplratio = 0.0;
+        // TODO: obtain SU(2)-rotated ampl (i.e. `from` replaced with `to`)
+        // const auto amplratio = SU(2)-rotated ampl / old ampl
+        coeffs[i] += prefactor * amplratio;
+        THROW(not_implemented, "Missing non-diagonal terms");
       } else {
-        coeffs[i] -= Cew(p_ampl->Leg(j)->Flav(), spincombination[j]) / 2.0;
+        // only diagonal terms appear
+        coeffs[i] -= DiagonalCew(flav, spincombination[j]) / 2.0;
       }
     }
   }
