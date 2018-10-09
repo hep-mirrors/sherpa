192a0b23b2d49033f0aefeef115fb9df4f05f5f8
Marek Schoenherr <marek.schoenherr@physik.uzh.ch>
2018-10-09 15:29:40 +0200

    fix to fix
    (cherry picked from commit 9a784df6bf6a87c475b0fa1c5c603f33d87a18c1)

--- AddOns/OpenLoops/OpenLoops_Interface.C
+++ AddOns/OpenLoops/OpenLoops_Interface.C
@@ -180,7 +180,7 @@ namespace OpenLoops {
     // set negative of requested associated amps such that they are only
     // initialised, but not computed by default
 #ifdef USING__OPENLOOPS__ASSOCIATED
-    SetParameter("add_associated_ew",-ConvertAssociatedContributions(fs.m_asscontribs));
+    if (s_ass_ew==0) SetParameter("add_associated_ew",-ConvertAssociatedContributions(fs.m_asscontribs));
 #else
     if (ConvertAssociatedContributions(fs.m_asscontribs))
       THROW(fatal_error,"Separate evaluation of associated EW contribution not "
@@ -188,7 +188,7 @@ namespace OpenLoops {
 #endif
     int procid(ol_register_process(procname.c_str(), amptype));
 #ifdef USING__OPENLOOPS__ASSOCIATED
-    SetParameter("add_associated_ew",s_ass_ew);
+    if (s_ass_ew==0) SetParameter("add_associated_ew",0);
 #endif
 
     return procid;
