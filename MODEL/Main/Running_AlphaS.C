ae446ca60e097dbf30811ed4319f2da69173110c
Enrico Bothmann <enrico.bothmann@ed.ac.uk>
2019-01-09 12:49:46 +0000

    Revert "implemented fix to catch nan in alphaS"
    This reverts commit 05749814851ce5fff676fb5b2cc2b015bf91ee66.

--- MODEL/Main/Running_AlphaS.C
+++ MODEL/Main/Running_AlphaS.C
@@ -230,9 +230,8 @@ double One_Running_AlphaS::AlphaSLam(const double Q2,const int nr)
   double *  b      = p_thresh[nr].b;
   double & lambda2 = p_thresh[nr].lambda2;
   double L         = log(Q2/lambda2);
-  if (!(L>0.)) return 1.;
-
   double pref      = 1./(beta0*L);
+
   double a         = pref;
   if (m_order==0) return M_PI*a;
 
