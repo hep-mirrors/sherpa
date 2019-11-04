hep-mirrors/sherpa

commit 221cc09fa41165ef453f5c67b322964e10f876f2

Exit when the IS ordering does not comply
... with our internal ordering, because this might cause issues further
down the line, especially with assumptions made in scale setters or with
the beam settings when we use different beams 1 and 2.

SHERPA/PerturbativePhysics/Matrix_Element_Handler.C
@@ -327,8 +327,9 @@ bool Matrix_Element_Handler::GenerateOneTrialEvent()
 std::vector<Process_Base*> Matrix_Element_Handler::InitializeProcess(
     Process_Info pi, NLOTypeStringProcessMap_Map*& pmap)
 {
-  std::set<Process_Info> trials;
+  CheckInitialStateOrdering(pi);
   std::vector<Process_Base*> procs;
+  std::set<Process_Info> initialized_pi_set;
   std::vector<Flavour_Vector> fls(pi.ExtractMPL());
   std::vector<int> fid(fls.size(),0);
   Flavour_Vector fl(fls.size());
@@ -337,12 +338,11 @@ std::vector<Process_Base*> Matrix_Element_Handler::InitializeProcess(
     if(fid[hc]==fls[hc].size()){fid[hc--]=0;++fid[hc];continue;}
     fl[hc]=fls[hc][fid[hc]];if(hc<fid.size()-1){++hc;continue;}
     Flavour_Vector cfl(fl);
-    size_t n(0);
-    pi.m_ii.SetExternal(cfl,n);
-    pi.m_fi.SetExternal(cfl,n);
+    pi.m_ii.SetExternal(cfl);
+    pi.m_fi.SetExternal(cfl);
     Process_Base::SortFlavours(pi,1);
-    if (trials.find(pi)==trials.end()) {
-      trials.insert(pi);
+    if (initialized_pi_set.find(pi)==initialized_pi_set.end()) {
+      initialized_pi_set.insert(pi);
       std::vector<Process_Base*> cp=InitializeSingleProcess(pi,pmap);
       procs.insert(procs.end(),cp.begin(),cp.end());
     }
@@ -483,6 +483,25 @@ std::vector<Process_Base*> Matrix_Element_Handler::InitializeSingleProcess
     else THROW(fatal_error,"NLO_Mode "+ToString(m_nlomode)+" unknown.");
   }
   return procs;
+}
+
+void Matrix_Element_Handler::CheckInitialStateOrdering(const Process_Info& pi)
+{
+  auto cpi = pi;
+  Process_Base::SortFlavours(cpi, 1);
+  if (cpi.m_ii == pi.m_ii) {
+  } else {
+    msg_Error() << ATOOLS::om::red << "\n\nERROR:" << ATOOLS::om::reset
+      << " Wrong ordering of initial-state particles detected.\n"
+      << "Please re-order the initial state in your Process definition(s) "
+      << "like this:\n  ";
+    pi.m_ii.PrintFlavours(msg_Error());
+    msg_Error() << " ->  ";
+    cpi.m_ii.PrintFlavours(msg_Error());
+    msg_Error() << "\nYou may need to adjust your other beam-specific "
+      << "parameters accordingly.";
+    exit(-1);
+  }
 }
 
 int Matrix_Element_Handler::InitializeProcesses(

PHASIC++/Process/Subprocess_Info.C
@@ -383,3 +383,11 @@ void Subprocess_Info::Print(std::ostream &ostr,const size_t &ni) const
   ostr<<"\n";
 }
 
+void Subprocess_Info::PrintFlavours(std::ostream &ostr) const
+{
+  if (m_ps.size()>0) {
+    for (size_t i(0);i<m_ps.size();++i) m_ps[i].PrintFlavours(ostr);
+  } else {
+    ostr<<m_fl.Kfcode()<<" "<<m_mpl;
+  }
+}

PHASIC++/Process/Subprocess_Info.H
@@ -65,6 +65,7 @@ namespace PHASIC {
     void BuildDecayInfos(size_t nin=2);
 
     void Print(std::ostream &ostr,const size_t &ni=0) const;
+    void PrintFlavours(std::ostream &ostr) const;
 
     void SetNMax(const Subprocess_Info &ref);
     void GetNMax(const Subprocess_Info &ref);

SHERPA/PerturbativePhysics/Matrix_Element_Handler.H
@@ -134,6 +134,7 @@ namespace SHERPA {
         PHASIC::Process_Info, PHASIC::NLOTypeStringProcessMap_Map*&);
     std::vector<PHASIC::Process_Base*> InitializeSingleProcess
     (const PHASIC::Process_Info &pi,PHASIC::NLOTypeStringProcessMap_Map *&pmap);
+    void CheckInitialStateOrdering(const PHASIC::Process_Info&);
 
     void BuildProcesses();
     void ReadFinalStateMultiIndependentProcessSettings(

commit baed273eb7d452928524c411c57b7544fb1b2c1d

Fix process set-up issue introduced in 221cc09fa

SHERPA/PerturbativePhysics/Matrix_Element_Handler.C
@@ -338,8 +338,9 @@ std::vector<Process_Base*> Matrix_Element_Handler::InitializeProcess(
     if(fid[hc]==fls[hc].size()){fid[hc--]=0;++fid[hc];continue;}
     fl[hc]=fls[hc][fid[hc]];if(hc<fid.size()-1){++hc;continue;}
     Flavour_Vector cfl(fl);
-    pi.m_ii.SetExternal(cfl);
-    pi.m_fi.SetExternal(cfl);
+    size_t n{0};
+    pi.m_ii.SetExternal(cfl, n);
+    pi.m_fi.SetExternal(cfl, n);
     Process_Base::SortFlavours(pi,1);
     if (initialized_pi_set.find(pi)==initialized_pi_set.end()) {
       initialized_pi_set.insert(pi);