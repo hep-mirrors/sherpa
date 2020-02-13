hep-mirrors/sherpa

commit 927df06cbfa9aa789fa2b051ae9df676f703140e

Update Rivet_Interface in case Rivet is compiled with HepMC3
(cherry picked from commit e741e9f5738561ea743f159a07bcb382bb43c414)

AddOns/Rivet/Rivet_Interface.C
@@ -12,6 +12,17 @@
 #include "ATOOLS/Org/My_MPI.H"
 #include "ATOOLS/Org/Scoped_Settings.H"
 #include "SHERPA/Single_Events/Event_Handler.H"
+#ifdef USING__RIVET3
+#include "Rivet/Config/RivetConfig.hh"
+#include "Rivet/AnalysisHandler.hh"
+#include "Rivet/Tools/Logging.hh"
+#ifdef RIVET_ENABLE_HEPMC_3
+#include "SHERPA/Tools/HepMC3_Interface.H"
+#include "HepMC3/GenEvent.h"
+#include "HepMC3/GenCrossSection.h"
+#define SHERPA__HepMC_Interface SHERPA::HepMC3_Interface
+#define HEPMC_HAS_CROSS_SECTION
+#else
 #include "SHERPA/Tools/HepMC2_Interface.H"
 #include "ATOOLS/Phys/KF_Table.H"
 
@@ -24,10 +35,8 @@
 #ifdef USING__HEPMC2__DEFS
 #include "HepMC/HepMCDefs.h"
 #endif
-
-#ifdef USING__RIVET3
-#include "Rivet/AnalysisHandler.hh"
-#include "Rivet/Tools/Logging.hh"
+#define SHERPA__HepMC_Interface SHERPA::HepMC2_Interface
+#endif
 
 
 namespace SHERPARIVET {
@@ -47,7 +56,7 @@ namespace SHERPARIVET {
     size_t m_hepmcoutputprecision, m_xsoutputprecision;
 
     Rivet_Map         m_rivet;
-    SHERPA::HepMC2_Interface       m_hepmc2;
+    SHERPA__HepMC_Interface       m_hepmc2;
     std::vector<ATOOLS::btp::code> m_ignoreblobs;
     std::map<std::string,size_t>   m_weightidxmap;
 
@@ -303,8 +312,13 @@ bool Rivet_Interface::Run(ATOOLS::Blob_List *const bl)
   std::vector<HepMC::GenEvent*> subevents(m_hepmc2.GenSubEventList());
 #ifdef HEPMC_HAS_CROSS_SECTION
   // leave this, although will be overwritten later
+#ifndef  RIVET_ENABLE_HEPMC_3
   HepMC::GenCrossSection xs;
   xs.set_cross_section(p_eventhandler->TotalXS(), p_eventhandler->TotalErr());
+#else
+  std::shared_ptr<HepMC3::GenCrossSection> xs=std::make_shared<HepMC3::GenCrossSection>();
+  xs->set_cross_section(p_eventhandler->TotalXS(), p_eventhandler->TotalErr());
+#endif
   event.set_cross_section(xs);
   for (size_t i(0);i<subevents.size();++i) {
     subevents[i]->set_cross_section(xs);
@@ -490,6 +504,17 @@ PrintInfo(std::ostream &str,const size_t width) const
 
 
 #ifdef USING__RIVET2
+#include "SHERPA/Tools/HepMC2_Interface.H"
+
+#ifdef USING__HEPMC2
+#include "HepMC/GenEvent.h"
+#include "HepMC/GenCrossSection.h"
+#include "HepMC/WeightContainer.h"
+#endif
+
+#ifdef USING__HEPMC2__DEFS
+#include "HepMC/HepMCDefs.h"
+#endif
 #include "Rivet/AnalysisHandler.hh"
 #include "Rivet/Tools/Logging.hh"
 

SHERPA/Initialization/Initialization_Handler.C
@@ -1010,8 +1010,8 @@ bool Initialization_Handler::InitializeTheAnalyses()
       if (!s_loader->LoadLibrary("SherpaAnalysis")) 
         THROW(missing_module,"Cannot load Analysis library (--enable-analysis).");
     if (analyses[i]=="Rivet" || analyses[i]=="RivetME" || analyses[i]=="RivetShower") {
-      if (!s_loader->LoadLibrary("SherpaHepMCOutput")) 
-        THROW(missing_module,"Cannot load HepMC library (--enable-hepmc2).");
+      if (!s_loader->LoadLibrary("SherpaHepMCOutput")&& !s_loader->LoadLibrary("SherpaHepMC3Output")) 
+        THROW(missing_module,"Cannot load HepMC library (--enable-hepmc2 or --enable-hepmc3).");
       if (!s_loader->LoadLibrary("SherpaRivetAnalysis")) 
         THROW(missing_module,"Cannot load RivetAnalysis library (--enable-rivet).");
     }

commit 726e203419eb88d2521ca17a876774940bf041a9

Fix header inclusion in Rivet_Interface
This has been broken in 927df06cb.

AddOns/Rivet/Rivet_Interface.C
@@ -11,7 +11,9 @@
 #include "ATOOLS/Org/MyStrStream.H"
 #include "ATOOLS/Org/My_MPI.H"
 #include "ATOOLS/Org/Scoped_Settings.H"
+#include "ATOOLS/Phys/KF_Table.H"
 #include "SHERPA/Single_Events/Event_Handler.H"
+
 #ifdef USING__RIVET3
 #include "Rivet/Config/RivetConfig.hh"
 #include "Rivet/AnalysisHandler.hh"
@@ -24,7 +26,6 @@
 #define HEPMC_HAS_CROSS_SECTION
 #else
 #include "SHERPA/Tools/HepMC2_Interface.H"
-#include "ATOOLS/Phys/KF_Table.H"
 
 #ifdef USING__HEPMC2
 #include "HepMC/GenEvent.h"