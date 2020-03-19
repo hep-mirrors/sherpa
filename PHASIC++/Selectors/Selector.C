hep-mirrors/sherpa

commit 2e61d46c19e9e35545a2760641150a949ccfe861

Reorganise selector parsing to fix a crash when default differ
The crash appeared when two selectors set defaults for the same
subsetting, e.g. like this:
```
SELECTORS:
- Type: a
  Some_Param: x
- Type: b
  Some_Param: y
```
... and we have different defaults for Some_Param for a vs. b. The
exception is thrown because a previously set default would otherwise be
modified (since they were not scoped properly). Now selector settings
are supposed to be given in the following way:
```
SELECTORS:
- a:
   Some_Param: x
- b:
   Some_Param: y
```
This scopes the different Some_Param settings (and their defaults) in
the right way (i.e. under a and b), and follows the DRY principle,
because the Type boilerplate is now removed.

All examples are adapted.

PHASIC++/Selectors/Selector.C
@@ -38,7 +38,7 @@ void Selector_Key::AddSelectorYAML(const std::string& yaml)
 {
   if (m_yaml.empty())
     m_yaml = "SELECTORS:";
-  m_yaml += "\n  - " + yaml;
+  m_yaml += "\n- " + yaml;
 }
 
 Selector_Base::Selector_Base(const std::string &name,Process_Base *const proc):

commit b1de22864c11867fb2f3575ca60fe56da5bc3812

Fix reading in subselectors
This was likely broken since 2e61d46c.

PHASIC++/Selectors/Selector.C
@@ -102,13 +102,20 @@ void Selector_Base::Output() {
 
 void Selector_Base::ReadInSubSelectors(const Selector_Key &key)
 {
-  for (auto s : key.m_settings["Subselectors"].GetItems()) {
+  for (auto s : key.m_settings[m_name]["Subselectors"].GetItems()) {
     Selector_Key subkey;
     subkey.m_settings = s;
     subkey.p_proc = key.p_proc;
-    auto type = s["Type"].SetDefault("").Get<std::string>();
-    if (type == "") {
+    std::string type;
+    if (s.IsList()) {
       type = s.SetDefault<std::string>({}).GetVector<std::string>()[0];
+    } else {
+      if (s.GetKeys().size() != 1)
+        THROW(fatal_error,
+              "Mapping-like selector settings must consist of exactly one"
+              " key-value pair, where the key is the name of the selector,"
+              " and the value is another mapping for the selector settings.");
+      type = s.GetKeys().front();
     }
     auto* sel = Selector_Getter::GetObject(type, subkey);
     if (sel!=NULL) m_sels.push_back(sel);