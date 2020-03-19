c99f68623f02a7c2c6cfe9e9786ad0549b190eca
Enrico Bothmann <enrico.bothmann@uni-goettingen.de>
2020-03-19 21:04:48 +0100

    Move EWSudakov_Log_Type definition since it is used in ATOOLS
    ... since we added the hack to extract single contributions after the
    run, i.e. one result per EWSud log. However, including headers from
    other modules from within ATOOLS can lead to linker issues. Hence, this
    infrastructure is now moved to ATOOLS.
    
    Should fix pipeline #127731920.

---
+++ ATOOLS/Phys/EWSudakov_Info.C
@@ -0,0 +1,42 @@
+#include "ATOOLS/Phys/EWSudakov_Info.H"
+
+#include "ATOOLS/Org/Exception.H"
+
+using namespace ATOOLS;
+
+EWSudakov_Log_Type EWSudakovLogTypeFromString(const std::string& logt)
+{
+  if (logt == "Ls")
+    return EWSudakov_Log_Type::Ls;
+  else if (logt == "lZ")
+    return EWSudakov_Log_Type::lZ;
+  else if (logt == "lSSC")
+    return EWSudakov_Log_Type::lSSC;
+  else if (logt == "lC")
+    return EWSudakov_Log_Type::lC;
+  else if (logt == "lYuk")
+    return EWSudakov_Log_Type::lYuk;
+  else if (logt == "lPR")
+    return EWSudakov_Log_Type::lPR;
+  else
+    THROW(fatal_error,
+          "Can not convert " + logt + " to EW Sudakov log type.");
+}
+
+std::ostream& operator<<(std::ostream& os, const EWSudakov_Log_Type& t)
+{
+  switch (t) {
+    case EWSudakov_Log_Type::Ls:
+      return os << "L(s)";
+    case EWSudakov_Log_Type::lZ:
+      return os << "l_Z";
+    case EWSudakov_Log_Type::lSSC:
+      return os << "l_s";
+    case EWSudakov_Log_Type::lC:
+      return os << "l_C";
+    case EWSudakov_Log_Type::lYuk:
+      return os << "l_Yuk";
+    case EWSudakov_Log_Type::lPR:
+      return os << "l_PR";
+  }
+}
--- PHASIC++/EWSudakov/EWSudakov.C
+++ PHASIC++/EWSudakov/EWSudakov.C
@@ -8,43 +8,6 @@ using namespace ATOOLS;
 
 namespace PHASIC {
 
-  EWSudakov_Log_Type EWSudakovLogTypeFromString(const std::string& logt)
-  {
-    if (logt == "Ls")
-      return EWSudakov_Log_Type::Ls;
-    else if (logt == "lZ")
-      return EWSudakov_Log_Type::lZ;
-    else if (logt == "lSSC")
-      return EWSudakov_Log_Type::lSSC;
-    else if (logt == "lC")
-      return EWSudakov_Log_Type::lC;
-    else if (logt == "lYuk")
-      return EWSudakov_Log_Type::lYuk;
-    else if (logt == "lPR")
-      return EWSudakov_Log_Type::lPR;
-    else
-      THROW(fatal_error,
-            "Can not convert " + logt + " to EW Sudakov log type.");
-  }
-
-  std::ostream& operator<<(std::ostream& os, const EWSudakov_Log_Type& t)
-  {
-    switch (t) {
-      case EWSudakov_Log_Type::Ls:
-        return os << "L(s)";
-      case EWSudakov_Log_Type::lZ:
-        return os << "l_Z";
-      case EWSudakov_Log_Type::lSSC:
-        return os << "l_s";
-      case EWSudakov_Log_Type::lC:
-        return os << "l_C";
-      case EWSudakov_Log_Type::lYuk:
-        return os << "l_Yuk";
-      case EWSudakov_Log_Type::lPR:
-        return os << "l_PR";
-    }
-  }
-
   std::ostream& operator<<(std::ostream& os, const Leg_Kfcode_Map& legmap)
   {
     os << "leg:kf_code list: { ";
