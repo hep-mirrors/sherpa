cd2543d8610bb4177442106b3e5e7c807bdbdfe5
Stefan Hoeche <shoeche@gmail.com>
2020-04-13 01:22:10 -0500

    added zj virtual (no axial contributions)

--- EXTRA_XS/NLO/DY_Jet_QCD_Virtual.C
+++ EXTRA_XS/NLO/DY_Jet_QCD_Virtual.C
@@ -100,12 +100,12 @@ namespace EXTRAXS {
       Fax.Finite()=-zb(j5,j3)*zb(j3,j1)*za(j2,j4)
 	*(L1(-s(j1,j2),-s(j4,j5))/sqr(s(j4,j5))
 	  -1.0/(12.0*s(j4,j5)*sqr(mt)));
+      Fax*=Complex(0.,-1.);
       return Fax;
     }
 
     void A5NLO(const int j1,const int j2,const int j3,
-	       const int j4, const int j5,
-	       const double &musq,const double &mt,
+	       const int j4, const int j5,const double &musq,
 	       Complex &A5LOm,METOOLS::DivArrC &A5NLOm)
     {
       A5LOm=-sqr(za(j1,j4))/(za(j2,j5)*za(j5,j1)*za(j4,j3));
@@ -113,33 +113,22 @@ namespace EXTRAXS {
       A5NLOm=A51(j2,j5,j1,j4,j3,musq)+A52(j2,j1,j5,j4,j3,musq)/9.0;
     }
 
-    DivArrD Virt5(const double &musq,const double &as,const double &nf,
-		  const double &gw,const double &mw,const double &ww,
-		  const double &mt)
+    DivArrD Virt5(const int j1,const int j2,const int j3,
+		  const int j4, const int j5,
+		  const double &musq,double &born)
     {
       Complex A5LOm, A5LOp;
       METOOLS::DivArrC A5NLOm, A5NLOp;
       m_swap=1;
-      A5NLO(1,2,3,4,5,musq,mt,A5LOm,A5NLOm);
+      A5NLO(j1,j2,j3,j4,j5,musq,A5LOm,A5NLOm);
       m_swap=0;
-      A5NLO(2,1,4,3,5,musq,mt,A5LOp,A5NLOp);
-      METOOLS::DivArrD res, subuv;
+      A5NLO(j2,j1,j4,j3,j5,musq,A5LOp,A5NLOp);
+      METOOLS::DivArrD res;
       res.IR2()=(std::conj(A5LOp)*A5NLOp.IR2()+std::conj(A5LOm)*A5NLOm.IR2()).real();
       res.IR()=(std::conj(A5LOp)*A5NLOp.IR()+std::conj(A5LOm)*A5NLOm.IR()).real();
       res.Finite()=(std::conj(A5LOp)*A5NLOp.Finite()+std::conj(A5LOm)*A5NLOm.Finite()).real();
-      double born((std::conj(A5LOp)*A5LOp+std::conj(A5LOm)*A5LOm).real());
-      double prop=sqr(s(3,4))/(sqr(s(3,4)-sqr(mw))+sqr(mw*ww));
-      double fac=2.0*4.0/3.0*3.0*sqr(gw*gw)*4.0*M_PI*as*prop;
-      res*=3.0*fac;
-      born*=fac;
-      subuv.IR()=3.0*(11.0-2.0/3.0*nf)/6.0*born;
-      subuv.Finite()=-3.0/6.0*born;
-      res=res-subuv;
-      msg_Debugging()<<"B     = "<<born<<"\n";
-      msg_Debugging()<<"V_fin = "<<res.Finite()/born<<"\n";
-      msg_Debugging()<<"V_e1  = "<<res.IR()/born<<"\n";
-      msg_Debugging()<<"V_e2  = "<<res.IR2()/born<<"\n";
-      return res/born;
+      born=(std::conj(A5LOp)*A5LOp+std::conj(A5LOm)*A5LOm).real();
+      return 3.0*res;
     }
 
     void PreCompute(const Vec4D_Vector &p)
@@ -163,21 +152,16 @@ namespace EXTRAXS {
     
   };// end of class VJ_Amplitude
   
-  const double NC(3.0), CA(3.0), CF(0.5*(NC*NC-1.0)/NC), TR(0.5);
-
-  class QQZG_QCD_Virtual: public Virtual_ME2_Base,
+  class QQGW_QCD_Virtual: public Virtual_ME2_Base,
 			  public VJ_Amplitude {
   private:
-    bool m_flipq, m_flipl;
     double m_nf, m_gw, m_mw, m_ww;
   public:
-    QQZG_QCD_Virtual(const Process_Info &pi,const Flavour_Vector &flavs):
+    QQGW_QCD_Virtual(const Process_Info &pi,const Flavour_Vector &flavs):
       Virtual_ME2_Base(pi, flavs), m_nf(Flavour(kf_jet).Size()/2.0)
     {
       m_drmode=1;
-      m_flipq=flavs[0].IsAnti();
-      m_flipl=flavs[2].IsAnti();
-      msg_Info()<<"QQZG"<<flavs<<"\n";
+      msg_Info()<<"QQWG"<<flavs<<"\n";
       double gf(1.0/sqrt(2.0)/std::abs(sqr(s_model->ComplexConstant("cvev"))));
       m_mw=Flavour(kf_Wplus).Mass();
       m_ww=Flavour(kf_Wplus).Width();
@@ -187,6 +171,37 @@ namespace EXTRAXS {
     double Eps_Scheme_Factor(const Vec4D_Vector& mom)
     {
       return 4.*M_PI;
+    }
+    void Compute(const Vec4D_Vector &p)
+    {
+      PreCompute(p);
+      m_res=Virt5(1,2,3,4,5,m_mur2,m_born);
+      double prop=sqr(s(3,4))/(sqr(s(3,4)-sqr(m_mw))+sqr(m_mw*m_ww));
+      double fac=2.0*4.0/3.0*3.0*sqr(m_gw*m_gw)*4.0*M_PI*AlphaQCD()*prop;
+      m_res*=fac;
+      m_born*=fac;
+      DivArrD subuv;
+      subuv.IR()=3.0*(11.0-2.0/3.0*m_nf)/6.0*m_born;
+      subuv.Finite()=-3.0/6.0*m_born;
+      m_res=(m_res-subuv)/m_born;
+      msg_Debugging()<<"B     = "<<m_born<<"\n";
+      msg_Debugging()<<"V_fin = "<<m_res.Finite()<<"\n";
+      msg_Debugging()<<"V_e1  = "<<m_res.IR()<<"\n";
+      msg_Debugging()<<"V_e2  = "<<m_res.IR2()<<"\n";
+    }
+  };// end of class QQGW_QCD_Virtual
+
+  class QQWG_QCD_Virtual: public QQGW_QCD_Virtual {
+  private:
+    bool m_flipq, m_flipl;
+  public:
+    QQWG_QCD_Virtual(const Process_Info &pi,
+		     const Flavour_Vector &flavs):
+      QQGW_QCD_Virtual(pi,flavs)
+    {
+      msg_Info()<<"QQWG"<<flavs<<"\n";
+      m_flipq=flavs[0].IsAnti();
+      m_flipl=flavs[2].IsAnti();
     }
     void Calc(const Vec4D_Vector &momenta)
     {
@@ -196,33 +211,21 @@ namespace EXTRAXS {
       p[2]=momenta[2+m_flipl];
       p[3]=momenta[3-m_flipl];
       p[4]=momenta[4];
-      PreCompute(p);
-      m_res=Virt5(m_mur2,AlphaQCD(),m_nf,m_gw,m_mw,m_ww,0.0);
+      Compute(p);
     }
-  };// end of class QQZG_QCD_Virtual
+  };// end of class QQWG_QCD_Virtual
 
-  class GQZQ_QCD_Virtual: public Virtual_ME2_Base,
-			  public VJ_Amplitude {
+  class GQWQ_QCD_Virtual: public QQGW_QCD_Virtual {
   private:
     bool m_flipq, m_flipl;
-    double m_nf, m_gw, m_mw, m_ww;
   public:
-    GQZQ_QCD_Virtual(const Process_Info &pi,const Flavour_Vector &flavs):
-      Virtual_ME2_Base(pi, flavs)
+    GQWQ_QCD_Virtual(const Process_Info &pi,
+		     const Flavour_Vector &flavs):
+      QQGW_QCD_Virtual(pi,flavs)
     {
-      m_drmode=1;
+      msg_Info()<<"GQWQ"<<flavs<<"\n";
       m_flipq=flavs[1].IsAnti();
       m_flipl=flavs[2].IsAnti();
-      msg_Info()<<"GQZQ"<<flavs<<"\n";
-      double gf(1.0/sqrt(2.0)/std::abs(sqr(s_model->ComplexConstant("cvev"))));
-      m_mw=Flavour(kf_Wplus).Mass();
-      m_ww=Flavour(kf_Wplus).Width();
-      m_gw=sqrt(8.0*sqr(m_mw)*gf/sqrt(2.0));
-      m_nf=(Flavour(kf_jet).Size()-1)/2;
-    }
-    double Eps_Scheme_Factor(const Vec4D_Vector& mom)
-    {
-      return 4.*M_PI;
     }
     void Calc(const Vec4D_Vector& momenta)
     {
@@ -232,8 +235,117 @@ namespace EXTRAXS {
       p[2]=momenta[2+m_flipl];
       p[3]=momenta[3-m_flipl];
       p[4]=-momenta[0];
+      Compute(p);
+    }
+  };// end of class GQWQ_QCD_Virtual
+
+  class QQGZ_QCD_Virtual: public Virtual_ME2_Base,
+			  public VJ_Amplitude {
+  private:
+    double m_nf, m_g1, m_mz, m_wz, m_ql, m_qq;
+    Complex m_ll, m_rl, m_lq, m_rq;
+  public:
+    QQGZ_QCD_Virtual(const Process_Info &pi,const Flavour_Vector &flavs):
+      Virtual_ME2_Base(pi, flavs), m_nf(Flavour(kf_jet).Size()/2.0) {}
+    void Init(const Flavour &qfl,const Flavour &lfl)
+    {
+      m_drmode=1;
+      m_mz=Flavour(kf_Z).Mass();
+      m_wz=Flavour(kf_Z).Width();
+      m_g1=sqrt(4.0*M_PI*s_model->ScalarConstant("alpha_QED"));
+      Complex xw=s_model->ComplexConstant("csin2_thetaW");
+      Complex sin2w=2.0*sqrt(xw*(1.0-xw));
+      m_ql=lfl.Charge();
+      m_qq=qfl.Charge();
+      m_rq=-2.0*qfl.Charge()*xw/sin2w;
+      m_lq=m_rq+2.0*qfl.IsoWeak()/sin2w;
+      m_rl=-2.0*lfl.Charge()*xw/sin2w;
+      m_ll=m_rl+2.0*lfl.IsoWeak()/sin2w;
+      m_nf=(Flavour(kf_jet).Size()-1)/2;
+    }
+    double Eps_Scheme_Factor(const Vec4D_Vector& mom)
+    {
+      return 4.*M_PI;
+    }
+    void Compute(const Vec4D_Vector &p)
+    {
       PreCompute(p);
-      m_res=Virt5(m_mur2,AlphaQCD(),m_nf,m_gw,m_mw,m_ww,0.0);
+      Complex prop=s(3,4)/Complex(s(3,4)-sqr(m_mz),m_mz*m_wz);
+      double fac=8.0*4.0/3.0*3.0*sqr(m_g1*m_g1)*4.0*M_PI*AlphaQCD();
+      double BLL, BLR, BRL, BRR;
+      DivArrD LL=fac*Virt5(1,2,3,4,5,m_mur2,BLL);
+      DivArrD LR=fac*Virt5(1,2,4,3,5,m_mur2,BLR);
+      DivArrD RL=fac*Virt5(2,1,3,4,5,m_mur2,BRL);
+      DivArrD RR=fac*Virt5(2,1,4,3,5,m_mur2,BRR);
+      m_res=sqr(std::abs(m_qq*m_ql+m_lq*m_ll*prop))*LL
+	+sqr(std::abs(m_qq*m_ql+m_lq*m_rl*prop))*LR
+	+sqr(std::abs(m_qq*m_ql+m_rq*m_ll*prop))*RL
+	+sqr(std::abs(m_qq*m_ql+m_rq*m_rl*prop))*RR;
+      m_born=sqr(std::abs(m_qq*m_ql+m_lq*m_ll*prop))*BLL
+	+sqr(std::abs(m_qq*m_ql+m_lq*m_rl*prop))*BLR
+	+sqr(std::abs(m_qq*m_ql+m_rq*m_ll*prop))*BRL
+	+sqr(std::abs(m_qq*m_ql+m_rq*m_rl*prop))*BRR;
+      m_born*=fac;
+      DivArrD subuv;
+      subuv.IR()=3.0*(11.0-2.0/3.0*m_nf)/6.0*m_born;
+      subuv.Finite()=-3.0/6.0*m_born;
+      m_res=(m_res-subuv)/m_born;
+      msg_Debugging()<<"B     = "<<m_born<<"\n";
+      msg_Debugging()<<"V_fin = "<<m_res.Finite()<<"\n";
+      msg_Debugging()<<"V_e1  = "<<m_res.IR()<<"\n";
+      msg_Debugging()<<"V_e2  = "<<m_res.IR2()<<"\n";
+    }
+  };// end of class QQGZ_QCD_Virtual
+
+  class QQZG_QCD_Virtual: public QQGZ_QCD_Virtual {
+  private:
+    bool m_flipq, m_flipl;
+  public:
+    QQZG_QCD_Virtual(const Process_Info &pi,
+		     const Flavour_Vector &flavs):
+      QQGZ_QCD_Virtual(pi,flavs)
+    {
+      msg_Info()<<"QQZG"<<flavs<<"\n";
+      m_flipq=flavs[0].IsAnti();
+      m_flipl=flavs[2].IsAnti();
+      Init(flavs[m_flipq?1:0],flavs[m_flipl?3:2]);
+    }
+    void Calc(const Vec4D_Vector &momenta)
+    {
+      Vec4D_Vector p(5);
+      p[0]=-momenta[0+m_flipq];
+      p[1]=-momenta[1-m_flipq];
+      p[2]=momenta[2+m_flipl];
+      p[3]=momenta[3-m_flipl];
+      p[4]=momenta[4];
+      Compute(p);
+    }
+  };// end of class QQZG_QCD_Virtual
+
+  class GQZQ_QCD_Virtual: public QQGZ_QCD_Virtual {
+  private:
+    bool m_flipq, m_flipl;
+    double m_nf, m_g1, m_mz, m_wz, m_ql, m_qq;
+    Complex m_ll, m_rl, m_lq, m_rq;
+  public:
+    GQZQ_QCD_Virtual(const Process_Info &pi,
+		     const Flavour_Vector &flavs):
+      QQGZ_QCD_Virtual(pi,flavs)
+    {
+      msg_Info()<<"GQZQ"<<flavs<<"\n";
+      m_flipq=flavs[1].IsAnti();
+      m_flipl=flavs[2].IsAnti();
+      Init(m_flipq?flavs[1].Bar():m_flavs[1],flavs[m_flipl?3:2]);
+    }
+    void Calc(const Vec4D_Vector& momenta)
+    {
+      Vec4D_Vector p(5);
+      p[0]=m_flipq?momenta[4]:-momenta[1];
+      p[1]=m_flipq?-momenta[1]:momenta[4];
+      p[2]=momenta[2+m_flipl];
+      p[3]=momenta[3-m_flipl];
+      p[4]=-momenta[0];
+      Compute(p);
     }
   };// end of class GQZQ_QCD_Virtual
 
@@ -241,9 +353,9 @@ namespace EXTRAXS {
 
 using namespace EXTRAXS;
 
-DECLARE_VIRTUALME2_GETTER(QQZG_QCD_Virtual,"DY_Jet_QCD_Virtual")
+DECLARE_VIRTUALME2_GETTER(QQWG_QCD_Virtual,"W_Jet_QCD_Virtual")
 Virtual_ME2_Base *ATOOLS::Getter
-<Virtual_ME2_Base,Process_Info,QQZG_QCD_Virtual>::
+<Virtual_ME2_Base,Process_Info,QQWG_QCD_Virtual>::
 operator()(const Process_Info &pi) const
 {
   DEBUG_FUNC(pi);
@@ -260,9 +372,34 @@ operator()(const Process_Info &pi) const
 	  fl[3].LeptonFamily()==fl[2].LeptonFamily())) return NULL;
     if (fl[4].IsGluon() && fl[0].IsQuark() &&
 	fl[1].QuarkFamily()==fl[0].QuarkFamily())
-      return new QQZG_QCD_Virtual(pi,fl);
+      return new QQWG_QCD_Virtual(pi,fl);
     if (fl[0].IsGluon() && fl[1].IsQuark() &&
 	fl[4].QuarkFamily()==fl[1].QuarkFamily())
+      return new GQWQ_QCD_Virtual(pi,fl);
+  }
+  return NULL;
+}
+
+DECLARE_VIRTUALME2_GETTER(QQZG_QCD_Virtual,"Z_Jet_QCD_Virtual")
+Virtual_ME2_Base *ATOOLS::Getter
+<Virtual_ME2_Base,Process_Info,QQZG_QCD_Virtual>::
+operator()(const Process_Info &pi) const
+{
+  DEBUG_FUNC(pi);
+  if (pi.m_loopgenerator!="Internal") return NULL;
+  if (pi.m_fi.m_nlotype&nlo_type::loop) {
+    if (pi.m_mincpl[0]!=2. || pi.m_maxcpl[0]!=2.) return NULL;
+    if (pi.m_mincpl[1]!=2. || pi.m_maxcpl[1]!=2.) return NULL;
+    if (pi.m_fi.m_nlocpl[0]!=1. || pi.m_fi.m_nlocpl[1]!=0.) return NULL;
+    if (pi.m_fi.m_ps.size()!=3) return NULL;
+    Flavour_Vector fl=pi.ExtractFlavours();
+    for (size_t i(0);i<fl.size();++i)
+      if (fl[i].Mass()) return NULL;
+    if (!(fl[2].IsLepton() && fl[3]==fl[2].Bar()) &&
+	!(fl[2].IsNeutrino() && fl[3]==fl[2].Bar())) return NULL;
+    if (fl[4].IsGluon() && fl[0].IsQuark() && fl[1]==fl[0].Bar())
+      return new QQZG_QCD_Virtual(pi,fl);
+    if (fl[0].IsGluon() && fl[1].IsQuark() && fl[4]==fl[1])
       return new GQZQ_QCD_Virtual(pi,fl);
   }
   return NULL;
