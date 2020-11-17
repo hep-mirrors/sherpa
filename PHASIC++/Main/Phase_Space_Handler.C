hep-mirrors/sherpa

commit a2bbe82e6a3e5a27101c2de6ba0556acdbee9d77

Enhance_Observable fix for MC@NLO setups.
(cherry picked from commit cf5031411ec7c412418c6be0d44f7c0cab99918d)

PHASIC++/Main/Phase_Space_Handler.C
@@ -278,6 +278,11 @@ void Phase_Space_Handler::CalculatePS()
   m_psweight*=p_fsrchannels->Weight();
 }
 
+void Phase_Space_Handler::CalculateEnhance()
+{
+  m_enhance=EnhanceFactor(p_active->Process());
+}
+
 Weights_Map Phase_Space_Handler::Differential(
     Process_Integrator* const process, Variations_Mode varmode, const psm::code mode)
 {
@@ -348,14 +353,16 @@ Weights_Map Phase_Space_Handler::Differential(
     Check4Momentum(p_lab);
     CalculatePS();
     CalculateME(varmode);
+    CalculateEnhance();
     p_lab=process->Momenta();
     if (m_printpspoint || msg_LevelIsDebugging()) {
       size_t precision(msg->Out().precision());
       msg->SetPrecision(15);
       msg_Out()<<p_active->Process()->Name();
       msg_Out()<<"  ME = "<<m_result
-               <<" ,  PS = "<<m_psweight<<"  ->  "
-               <<m_result*m_psweight<<std::endl;
+               <<" ,  PS = "<<m_psweight
+               <<" ,  enh = "<<m_enhance<<"  ->  "
+               <<m_result*m_psweight*m_enhance<<std::endl;
       if (p_active->Process()->GetSubevtList()) {
         NLO_subevtlist * subs(p_active->Process()->GetSubevtList());
         for (size_t i(0);i<subs->size();++i) msg_Out()<<(*(*subs)[i])<<"\n";
@@ -365,7 +372,7 @@ Weights_Map Phase_Space_Handler::Differential(
       msg_Out()<<"==========================================================\n";
       msg->SetPrecision(precision);
     }
-    double wgtfac(m_psweight*iscount);
+    double wgtfac(m_psweight*m_enhance*iscount);
     m_wgtmap*=wgtfac;
     NLO_subevtlist* nlos=p_active->Process()->GetSubevtList();
     if (nlos) {
@@ -421,8 +428,6 @@ Weights_Map Phase_Space_Handler::Differential(
     }
     m_wgtmap*=0.0;
   }
-  m_enhance=EnhanceFactor(p_process->Process());
-  m_wgtmap*=m_enhance;
   return m_wgtmap;
 }
 
@@ -507,7 +512,7 @@ Weight_Info *Phase_Space_Handler::OneEvent(Process_Base *const proc,int mode)
   mu12=p_isrhandler->MuF2(0);
   mu22=p_isrhandler->MuF2(1);
   auto res =
-      new Weight_Info(wgtmap, dxs, 1.0, fl1, fl2, x1, x2, xf1, xf2, mu12, mu22);
+      new Weight_Info(wgtmap, dxs, m_enhance, fl1, fl2, x1, x2, xf1, xf2, mu12, mu22);
   return res;
 }
 
@@ -644,20 +649,23 @@ void Phase_Space_Handler::AddPoint(const double _value)
     p_fsrchannels->AddPoint(value*enhance);
     if (p_enhancehisto) {
       if (!p_process->Process()->Info().Has(nlo_type::rsub)) {
-	double val((*p_enhanceobs)(&p_lab.front(),
-				    &p_flavours.front(),m_nin+m_nout));
-	p_enhancehisto_current->Insert(val,value/m_enhance);
+	double obs((*p_enhanceobs)(&p_lab.front(),
+                                   &p_flavours.front(),m_nin+m_nout));
+	p_enhancehisto_current->Insert(obs,value/m_enhance);
       }
       else {
-	for (size_t i(0);i<p_process->Process()->Size();++i) {
-	  NLO_subevtlist* nlos=(*p_process->Process())[i]->GetSubevtList();
-	  for (size_t j(0);j<nlos->size();++j) {
-	    if ((*nlos)[j]->m_result==0.0) continue;
-	    double val((*p_enhanceobs)((*nlos)[j]->p_mom,
-					(*nlos)[j]->p_fl,(*nlos)[j]->m_n));
-	    p_enhancehisto_current->Insert(val,(*nlos)[j]->m_result/m_enhance);
-	  }
+        // fixed-order RS, fill with RS weight and R kinematics
+        if (p_process->Process()->Info().m_nlomode==1) {
+          double obs((*p_enhanceobs)(&p_lab.front(),
+                                     &p_flavours.front(),m_nin+m_nout));
+          p_enhancehisto_current->Insert(obs,value/m_enhance);
 	}
+        // MC@NLO H/RS, fill with H weight and kinematics
+        else {
+          double obs((*p_enhanceobs)(&p_lab.front(),
+                                     &p_flavours.front(),m_nin+m_nout));
+          p_enhancehisto_current->Insert(obs,value/m_enhance);
+        }
       }
     }
   }
@@ -720,16 +728,14 @@ double Phase_Space_Handler::EnhanceFactor(Process_Base *const proc)
     obs=(*p_enhanceobs)(&p_lab.front(),&p_flavours.front(),m_nin+m_nout);
   }
   else {
-    double nobs(0.0);
-    for (size_t i(0);i<proc->Size();++i) {
-      NLO_subevtlist* nlos=(*proc)[i]->GetSubevtList();
-      if (nlos->back()->m_result==0.0) continue;
-      obs+=log((*p_enhanceobs)(nlos->back()->p_mom,
-				nlos->back()->p_fl,nlos->back()->m_n));
-      nobs+=1.0;
+    // fixed-order RS, read out with R kinematics
+    if (proc->Info().m_nlomode==1) {
+      obs=(*p_enhanceobs)(&p_lab.front(),&p_flavours.front(),m_nin+m_nout);
     }
-    if (nobs) obs=exp(obs/nobs);
-    else obs=1.0;
+    // MC@NLO H, read out with H kinematics
+    else {
+      obs=(*p_enhanceobs)(&p_lab.front(),&p_flavours.front(),m_nin+m_nout);
+    }
   }
   if (p_enhancehisto==NULL) return obs;
   if (obs>=p_enhancehisto->Xmax()) obs=p_enhancehisto->Xmax()-1e-12;

ATOOLS/Math/Histogram.C
@@ -924,11 +924,11 @@ void Histogram::InsertRange(double start, double end, double value) {
 	m_mvalues[0][i] += value;
       } 
       else if ((low<start)&&(up<=end)) {
-	fac = (start-low)/m_binsize;
+	fac = (up-start)/m_binsize;
 	m_mvalues[0][i] += value *fac;
       }
       else if ((start<=low)&&(end < up)) {
-	fac = (up-end)/m_binsize;
+	fac = (end-low)/m_binsize;
 	m_mvalues[0][i] += value *fac;
       }
       else if ((low<start)&&(end <up)) {
@@ -973,11 +973,11 @@ void Histogram::InsertRange(double start, double end, double value) {
 	m_yvalues[i] += value;
       } 
       else if ((low<start)&&(up<=end)) {
-	fac = (start-low)/m_binsize;
+	fac = (up-start)/m_binsize;
 	m_yvalues[i] += value *fac;
       }
       else if ((start<=low)&&(end < up)) {
-	fac = (up-end)/m_binsize;
+	fac = (end-low)/m_binsize;
 	m_yvalues[i] += value *fac;
       }
       else if ((low<start)&&(end <up)) {
@@ -1011,7 +1011,7 @@ double Histogram::Bin(double coordinate) const
 
     if (coordinate<m_lower) return m_yvalues[0];
     if (coordinate>m_upper) return m_yvalues[m_nbin-1];
-    for (int i=1;i<m_nbin+1;i++) {
+    for (int i=1;i<m_nbin;i++) {
       if ( (coordinate >= m_lower + (i-1)*m_binsize) &&
 	   (coordinate <  m_lower + i*m_binsize) ) 
 	return m_yvalues[i];

ATOOLS/Phys/Weight_Info.H
@@ -30,17 +30,17 @@ namespace ATOOLS {
   }
 
   struct Weight_Info {
-    double m_dxs, m_ntrial;
+    double m_dxs, m_enh, m_ntrial;
     Weights_Map m_weightsmap;
     PDF_Info m_pdf;
     Weight_Info(const Weights_Map &weightsmap={0.0},
-                const double &dxs=0.0,const double &ntrial=0.0,
+                const double &dxs=0.0, const double& enh=1.0, const double &ntrial=0.0,
                 const int &fl1=0, const int &fl2=0,
                 const double &x1=0.,const double &x2=0.,
                 const double &xf1=0.0,const double &xf2=0.0,
                 const double &muf12=0.0,const double &muf22=0.0):
       m_weightsmap(weightsmap),
-      m_dxs(dxs), m_ntrial(ntrial),
+        m_dxs(dxs), m_enh(enh), m_ntrial(ntrial),
       m_pdf(PDF_Info(fl1,fl2,x1,x2,muf12,muf22,xf1,xf2)) {}
   }; // end of struct Weight_Info
 
@@ -48,7 +48,7 @@ namespace ATOOLS {
                                    const ATOOLS::Weight_Info & wi)
   {
     return s << " w = " << wi.m_weightsmap.Nominal() << ", dxs = " << wi.m_dxs
-             << "\n";
+             <<", enh = "<<wi.m_enh<<std::endl;
   }
 
   struct mewgttype {

PHASIC++/Main/Phase_Space_Handler.H
@@ -140,6 +140,7 @@ namespace PHASIC {
 
     void CalculateME(ATOOLS::Variations_Mode);
     void CalculatePS();
+    void CalculateEnhance();
 
     double Weight(ATOOLS::Vec4D_Vector &plab);
 
@@ -194,6 +195,8 @@ namespace PHASIC {
     { m_maxtrials=maxtrials; }
     void SetEnhanceObservable(const std::string &enhanceobs);
     void SetEnhanceFunction(const std::string &enhancefunc);
+    inline void SetEnhance(const double& enhance)
+    { m_enhance=enhance; }
 
     inline Phase_Space_Integrator* Integrator() const 
     { return p_integrator; }

PHASIC++/Process/MCatNLO_Process.C
@@ -499,6 +499,7 @@ Weights_Map MCatNLO_Process::OneSEvent(const int wmode)
     if (rproc==NULL) THROW(fatal_error,"Invalid splitting");
     p_selected=p_rproc;
     p_rproc->SetSelected(rproc);
+    rproc->Integrator()->PSHandler()->SetEnhance(bproc->Integrator()->PSHandler()->Enhance());
     rproc->Integrator()->SetMax(bproc->Integrator()->Max());
     rproc->Integrator()->SetMomenta(*ampl);
     rproc->GetMEwgtinfo()->m_mur2=bproc->GetMEwgtinfo()->m_mur2;

commit e4c69be1a54bb7eb0da15e410d0cbb93e0091b1e

Fix PDFInfo filling bug introduced in a2bbe82e6
An argument in the Weight_Info constructor for the event was missing,
such that all PDFInfo values where shifted by one position, resulting in
a completely wrongly filled PDFInfo object.

The argument has been accidentally deleted in a2bbe82e6, which has been
cherry picked from cf5031411. Not that only a2bbe82e6 introduces this
bug, while the original cf5031411 is fine.

PHASIC++/Main/Phase_Space_Handler.C
@@ -512,7 +512,7 @@ Weight_Info *Phase_Space_Handler::OneEvent(Process_Base *const proc,int mode)
   mu12=p_isrhandler->MuF2(0);
   mu22=p_isrhandler->MuF2(1);
   auto res =
-      new Weight_Info(wgtmap, dxs, fl1, fl2, x1, x2, xf1, xf2, mu12, mu22);
+      new Weight_Info(wgtmap, dxs, 1.0, fl1, fl2, x1, x2, xf1, xf2, mu12, mu22);
   return res;
 }
 