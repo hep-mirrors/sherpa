81464415d7b152198bf2c8c5c81cae4ee1021fc5
Stefan Hoeche <shoeche@fnal.gov>
2021-05-11 14:36:24 -0500

    added new cutoff parameter to mcatnlo

--- MCATNLO/Showers/Sudakov.C
+++ MCATNLO/Showers/Sudakov.C
@@ -123,10 +123,12 @@ void Sudakov::InitSplittingFunctions(MODEL::Model_Base *md,const int kfmode)
 
 void Sudakov::SetCoupling(MODEL::Model_Base *md,
 			  const double &k0sqi,const double &k0sqf,
-			  const double &isfac,const double &fsfac)
+			  const double &isfac,const double &fsfac,
+			  const double &k0sq_gsplit_fac)
 {
   m_k0sqi=k0sqi;
   m_k0sqf=k0sqf;
+  m_k0sq_gsplit_fac=k0sq_gsplit_fac;
   for (std::vector<Splitting_Function_Base*>::iterator
 	 sit(m_splittings.begin());sit!=m_splittings.end();)
     if (!(*sit)->Coupling()->SetCoupling(md,m_k0sqi,m_k0sqf,isfac,fsfac)) {
@@ -305,7 +307,9 @@ bool Sudakov::Generate(Parton * split)
     p_selected->ColorPoint(split);
     m_z = Z();
     double k0sq(p_split->GetType()==pst::IS?m_k0sqi:m_k0sqf);
-    if (m_kperp2<k0sq)  return false;
+    if (p_selected->GetFlavourA().IsGluon() &&
+	p_selected->GetFlavourB().IsQuark() &&
+	m_kperp2<k0sq*m_k0sq_gsplit_fac) return false;
     double Q2 = 0.;
     m_type=split->GetType()==pst::FS?
       (split->GetSpect()->GetType()==pst::FS?cstp::FF:cstp::FI):
