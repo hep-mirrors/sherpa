cd517846422fd14c7399ea634edc7e393c89787a
Stefan Hoeche <shoeche@fnal.gov>
2021-07-15 11:42:20 -0500

    updated lund tune parameters

--- SHERPA/LundTools/Lund_Interface.C
+++ SHERPA/LundTools/Lund_Interface.C
@@ -90,9 +90,10 @@ Lund_Interface::Lund_Interface(std::string):
   pysubs.msel=0;
 
   // default hadronisation parameters
-  pydat1.parj[21-1]=0.36;
+  pydat1.parj[21-1]=0.33;
   pydat1.parj[41-1]=0.3;
   pydat1.parj[42-1]=0.6;
+  pydat1.parj[46-1]=0.875;
 
   Settings& s = Settings::GetMainSettings();
   ReadSettings(s, "MSUB", pysubs.msub);
