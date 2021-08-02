d191dca0f623b5c4008fa6d3f35821468c1628ad
Peter Meinzinger <peter.meinzinger@durham.ac.uk>
2021-08-02 16:58:48 +0100

    Extended the GRS photon PDF to include the NLO parametrizations

--- PDF/GRS/GRSph_Fortran_Interface.C
+++ PDF/GRS/GRSph_Fortran_Interface.C
@@ -25,7 +25,8 @@ inline void MakeFortranString(char *output, std::string input,
     output[j] = (char)input[j];
 }
 
-GRSph_Fortran_Interface::GRSph_Fortran_Interface(const ATOOLS::Flavour _bunch) {
+GRSph_Fortran_Interface::GRSph_Fortran_Interface(const ATOOLS::Flavour _bunch,
+                                                 const std::string _set) {
   /*
    * ISET = 1  LEADING ORDER SET (DATA FILE 'grsg99lo.grid')        *
    *           ISET = 2  NEXT-TO-LEADING ORDER MSbar SET            *
@@ -33,7 +34,6 @@ GRSph_Fortran_Interface::GRSph_Fortran_Interface(const ATOOLS::Flavour _bunch) {
    *           ISET = 3  NEXT-TO-LEADING ORDER DISgamma SET         *
    *                    (DATA FILE 'grsg99d.grid')
    */
-  iset = 1;
   m_path = rpa->gen.Variable("SHERPA_SHARE_PATH") + "/GRSGrid";
 
   m_xmin = 1.e-5;
@@ -42,11 +42,24 @@ GRSph_Fortran_Interface::GRSph_Fortran_Interface(const ATOOLS::Flavour _bunch) {
   m_q2max = 1.e6;
   m_nf = 3;
 
-  m_set = "GRS";
+  m_set = _set;
+  if (m_set == std::string("GRSLO"))
+    m_iset = 1;
+  else if (m_set == std::string("GRSMSbar"))
+    m_iset = 2;
+  else if (m_set == std::string("GRSMSbar"))
+    m_iset = 3;
+  else {
+    msg_Out() << METHOD
+              << ": Cannot recognize the chosen PDF parametrization. Will "
+                 "use the Leading Order parametrization. \n";
+    m_iset = 1;
+  }
+
   m_bunch = _bunch;
   m_d = m_u = m_s = m_g = 0.;
 
-  for (int i = 1; i < 6; i++) {
+  for (int i = 1; i < m_nf + 1; i++) {
     m_partons.insert(Flavour((kf_code)(i)));
     m_partons.insert(Flavour((kf_code)(i)).Bar());
   }
@@ -58,14 +71,14 @@ GRSph_Fortran_Interface::GRSph_Fortran_Interface(const ATOOLS::Flavour _bunch) {
 }
 
 PDF_Base *GRSph_Fortran_Interface::GetCopy() {
-  return new GRSph_Fortran_Interface(m_bunch);
+  return new GRSph_Fortran_Interface(m_bunch, m_set);
 }
 
 void GRSph_Fortran_Interface::CalculateSpec(const double &_x,
                                             const double &_Q2) {
   double x = _x / m_rescale, Q2 = _Q2;
 
-  grsg99_(iset, x, Q2, m_u, m_d, m_s, m_g);
+  grsg99_(m_iset, x, Q2, m_u, m_d, m_s, m_g);
 }
 
 double GRSph_Fortran_Interface::GetXPDF(const ATOOLS::Flavour &infl) {
@@ -109,15 +122,26 @@ DECLARE_PDF_GETTER(GRSph_Getter);
 PDF_Base *GRSph_Getter::operator()(const Parameter_Type &args) const {
   if (!args.m_bunch.IsPhoton())
     return NULL;
-  return new GRSph_Fortran_Interface(args.m_bunch);
+  return new GRSph_Fortran_Interface(args.m_bunch, args.m_set);
 }
 
 void GRSph_Getter::PrintInfo(std::ostream &str, const size_t width) const {
-  str << "GRS photon PDF, see Phys. Rev. D60(1999)054019";
+  str << "GRS photon PDF, see Phys. Rev. D60(1999)054019"
+         "The following sets can be chosen"
+         " - GRSLO for leading order"
+         " - GRSMSbar for next-to-leading order in MSbar\n"
+         " - GRSDISg for next-to-leading order in DISgamma\n";
 }
 
-GRSph_Getter *p_get_GRS;
+GRSph_Getter *p_get_GRS[3];
 
-extern "C" void InitPDFLib() { p_get_GRS = new GRSph_Getter("GRS"); }
+extern "C" void InitPDFLib() {
+  p_get_GRS[0] = new GRSph_Getter("GRSLO");
+  p_get_GRS[1] = new GRSph_Getter("GRSMSbar");
+  p_get_GRS[2] = new GRSph_Getter("GRSDISg");
+}
 
-extern "C" void ExitPDFLib() { delete p_get_GRS; }
+extern "C" void ExitPDFLib() {
+  for (int i = 0; i < 3; i++)
+    delete p_get_GRS[i];
+}
