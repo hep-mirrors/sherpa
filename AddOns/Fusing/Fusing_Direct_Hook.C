d3a7483a84071d408db979b0fb8b8eb129db6066
Frank Siegert <frank.siegert@cern.ch>
2021-12-20 09:18:32 +0100

    Fix memory leak and clean up in fusing hooks

--- AddOns/Fusing/Fusing_Direct_Hook.C
+++ AddOns/Fusing/Fusing_Direct_Hook.C
@@ -24,7 +24,6 @@ class Fusing_Direct_Hook : public Userhook_Base {
 
 private:
   MODEL::Running_AlphaS *p_as;
-  Blob_List * p_bl;
   Sherpa* p_sherpa;
   double m_factor;
 public:
@@ -40,88 +39,71 @@ public:
   ~Fusing_Direct_Hook() {}
 
   ATOOLS::Return_Value::code Run(ATOOLS::Blob_List* blobs, double &weight) {
-
-    p_bl = blobs;
-    String_BlobDataBase_Map  bdmap = p_bl->FindFirst(btp::Signal_Process)->GetData();
-
-    String_BlobDataBase_Map::iterator search_mewinfo = bdmap.find("MEWeightInfo");
-    if (search_mewinfo==bdmap.end()) {
-      THROW(fatal_error,"No MEWeightinfo found in signal blob!");
-    }
-    String_BlobDataBase_Map::iterator search_weight = bdmap.find("Weight");
-    if (search_weight==bdmap.end()) {
-      THROW(fatal_error,"No Weight found in signal blob!");
+    String_BlobDataBase_Map  bdmap = blobs->FindFirst(btp::Signal_Process)->GetData();
+    String_BlobDataBase_Map::iterator search_mewinfo(bdmap.find("MEWeightInfo")), search_weight(bdmap.find("Weight"));
+    if (search_mewinfo==bdmap.end() || search_weight==bdmap.end()) {
+      THROW(fatal_error,"Internal error: No weight info found in signal blob!");
     }
 
     ME_Weight_Info  * me_w_info = search_mewinfo->second->Get<ME_Weight_Info *>();
-    double weight_bl = search_weight->second->Get<double>();
     double mur2 = me_w_info->m_mur2;
     ATOOLS::Flavour bquark = ATOOLS::Flavour(5);
     double mb2 = bquark.Mass()*bquark.Mass();
     double alphas((*p_as)(mur2));
     double TR = 0.5;
-
-
     double born_weight = me_w_info->m_B;
-
-
-    double new_weight(weight_bl);
     double correction(0);
     double sum_meweight = me_w_info->m_B + me_w_info->m_K + me_w_info->m_KP + me_w_info->m_VI;
 
-
     //apply only to S-Events
     if ( me_w_info->m_type ==(ATOOLS::mewgttype::METS |ATOOLS::mewgttype::H)  ){
-            msg_Debugging() << "H-Event, skip alpha_s correction." << std::endl;
-            return Return_Value::Nothing;
-
-        }
+      msg_Debugging() << "H-Event, skip alpha_s correction." << std::endl;
+      return Return_Value::Nothing;
+    }
 
     PHASIC::MCatNLO_Process * mcproc = dynamic_cast<PHASIC::MCatNLO_Process* >
       (p_sherpa->GetInitHandler()->GetMatrixElementHandler()->Process()->Parent());
     if (mcproc==NULL){
       THROW(fatal_error,"no MC@NLO process found! For use with separate LO-Process, use K-Factor!");
     }
+    // TODO: make more efficient, without whole amplitude copying!
     Cluster_Amplitude * ampl = mcproc->GetAmplitude();
     double muf2 = ampl->KT2();
+    ampl->Delete();
 
 
     //  **********    gg initial state    ****************
     if ((me_w_info->m_fl1 ==21) && (me_w_info->m_fl2 ==21)){
-            double l = log(mur2/muf2);
-            correction = alphas * 2.*TR/(3.*M_PI) * l ;
-            msg_Debugging() << "gg initial state. correction(" << correction <<")." << std::endl;
-        }
+      double l = log(mur2/muf2);
+      correction = alphas * 2.*TR/(3.*M_PI) * l ;
+      msg_Debugging() << "gg initial state. correction(" << correction <<")." << std::endl;
+    }
 
     //  **********    qq initial state    ****************
     if ((me_w_info->m_fl1 !=21) && (me_w_info->m_fl2 !=21)){
-            double l = log(mur2/mb2);
-            correction = alphas * 2.*TR/(3.*M_PI) * l ;
-            msg_Debugging() << "qq initial state. correction(" << correction <<")." << std::endl;
-        }
+      double l = log(mur2/mb2);
+      correction = alphas * 2.*TR/(3.*M_PI) * l ;
+      msg_Debugging() << "qq initial state. correction(" << correction <<")." << std::endl;
+    }
 
 
     correction *= m_factor;
-    new_weight = weight_bl* (1. - correction* born_weight/sum_meweight);
 
-    weight = new_weight;
+    double new_weight = search_weight->second->Get<double>() * (1. - correction* born_weight/sum_meweight);
+    (*blobs->FindFirst(btp::Signal_Process))["Weight"]->Set(new_weight);
+    weight = new_weight; // obsolete?
 
-    (*p_bl->FindFirst(btp::Signal_Process))["Weight"]->Set(new_weight);
-
-
-	// TODO: calculate counter-terms based on the muR variations. not done yet, since numerical impact is small.
+    // TODO: calculate counter-terms based on the muR variations. not done yet, since numerical impact is small.
     String_BlobDataBase_Map::iterator search_varweights = bdmap.find("Variation_Weights");
     if (search_varweights==bdmap.end()) {
       THROW(fatal_error,"No VarWeight found in signal blob!");
     }
     Variation_Weights  var_weights= search_varweights->second->Get<Variation_Weights >();
     var_weights*=(1. - correction* born_weight/sum_meweight);
-    (*p_bl->FindFirst(btp::Signal_Process))["Variation_Weights"]->Set(var_weights);
+    (*blobs->FindFirst(btp::Signal_Process))["Variation_Weights"]->Set(var_weights);
 
     return Return_Value::Nothing;
-    }
-
-
+  }
 
   void Finish() {
   }
