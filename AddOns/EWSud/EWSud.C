a77aeda77b785dd1da7efecd953d3a566283c11b
Enrico Bothmann <enrico.bothmann@uni-goettingen.de>
2022-01-04 15:46:59 +0100

    Move contents of EWSudakov_Info into EWSud
    This should finalise the decoupling of the Sherpa framework and the
    EWSud add-on, i.e. all EWSud-related code is now under AddOns/EWSud and
    under namespace EWSud.

--- AddOns/EWSud/EWSud.C
+++ AddOns/EWSud/EWSud.C
@@ -8,6 +8,71 @@ using namespace ATOOLS;
 
 namespace EWSud {
 
+  EWSudakov_Log_Type EWSudakovLogTypeFromString(const std::string& logt)
+  {
+    if (logt == "LSC")
+      return EWSudakov_Log_Type::Ls;
+    else if (logt == "Z")
+      return EWSudakov_Log_Type::lZ;
+    else if (logt == "SSC")
+      return EWSudakov_Log_Type::lSSC;
+    else if (logt == "C")
+      return EWSudakov_Log_Type::lC;
+    else if (logt == "Yuk")
+      return EWSudakov_Log_Type::lYuk;
+    else if (logt == "PR")
+      return EWSudakov_Log_Type::lPR;
+    else if (logt == "I")
+      return EWSudakov_Log_Type::lI;
+    else
+      THROW(fatal_error,
+            "Can not convert " + logt + " to EW Sudakov log type.");
+  }
+
+  std::ostream& operator<<(std::ostream& os, const EWSudakov_Log_Type& t)
+  {
+    switch (t) {
+      case EWSudakov_Log_Type::Ls:
+        return os << "LSC";
+      case EWSudakov_Log_Type::lZ:
+        return os << "Z";
+      case EWSudakov_Log_Type::lSSC:
+        return os << "SSC";
+      case EWSudakov_Log_Type::lC:
+        return os << "C";
+      case EWSudakov_Log_Type::lYuk:
+        return os << "Yuk";
+      case EWSudakov_Log_Type::lPR:
+        return os << "PR";
+      case EWSudakov_Log_Type::lI:
+        return os << "I";
+      default:
+        return os;
+    }
+  }
+
+  double EWSudakov_Log_Corrections_Map::KFactor() const
+  {
+    double kfac = 1.0;
+    for (const auto &kv : *this) {
+      kfac += kv.second;
+    }
+    return kfac;
+  }
+
+
+  std::ostream& operator<<(std::ostream& o,
+                           const EWSudakov_Log_Corrections_Map& m)
+  {
+    o << "1 - K_EWSud = " << m.KFactor() << " (";
+    bool is_first {true};
+    for (const auto &kv : m) {
+      o << kv.first << ": " << (is_first ? "" : ", ") << kv.second;
+      is_first = false;
+    }
+    return o << ')';
+  }
+
   std::ostream& operator<<(std::ostream& os, const Leg_Kfcode_Map& legmap)
   {
     os << "leg:kf_code list: { ";
