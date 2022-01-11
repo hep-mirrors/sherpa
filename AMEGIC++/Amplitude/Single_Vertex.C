c691ee2f0abf0071ab094adce3181dca8e03b44c
Enrico Bothmann <enrico.bothmann@uni-goettingen.de>
2022-01-11 18:26:56 +0100

    Revert whitespace changes from this MR
    This makes the comparison to master for the MR review more
    straightforward.

--- AMEGIC++/Amplitude/Single_Vertex.C
+++ AMEGIC++/Amplitude/Single_Vertex.C
@@ -21,8 +21,8 @@ Single_Vertex::Single_Vertex(const Single_Vertex& v):
 
 Single_Vertex::~Single_Vertex()
 {
-  for (size_t i(0);i<Lorentz.size();++i){
-    if(Lorentz[i])
+  for (size_t i(0);i<Lorentz.size();++i) {
+    if (Lorentz[i])
       Lorentz[i]->Delete();
   }
 }
