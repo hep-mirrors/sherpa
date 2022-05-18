31ff5c2637193e0566300c36f35ef138c8782c14
Enrico Bothmann <enrico.bothmann@uni-goettingen.de>
2022-05-18 12:19:12 +0200

    Construct VAR and STRICT_METS setters in Pilot Scale Setter
    The class is still not functional, the dispatches to the two sub scale
    setters still need to be implemented.
    
    (cherry picked from commit f319674e83104bd95c5cda101b107567bf1a6419)

--- PHASIC++/Scales/Pilot_Scale_Setter.C
+++ PHASIC++/Scales/Pilot_Scale_Setter.C
@@ -1,4 +1,5 @@
 #include "PHASIC++/Scales/Scale_Setter_Base.H"
+#include "ATOOLS/Org/MyStrStream.H"
 
 namespace PHASIC {
 
@@ -10,6 +11,11 @@ namespace PHASIC {
 
     double Calculate(const std::vector<ATOOLS::Vec4D> &p,
 		     const size_t &mode);
+
+    private:
+
+    Scale_Setter_Base* p_var_scale_setter;
+    Scale_Setter_Base* p_mets_scale_setter;
 
   };
 
@@ -39,6 +45,24 @@ Pilot_Scale_Setter::Pilot_Scale_Setter
 (const Scale_Setter_Arguments &args):
   Scale_Setter_Base(args)
 {
+  // Construct VAR scale setter from string argument passed to the PILOT scale
+  // setter.
+  Scale_Setter_Arguments varargs = args;
+  varargs.m_scale = StringReplace(args.m_scale, "PILOT", "VAR");
+  p_var_scale_setter = Scale_Setter_Base::Scale_Getter_Function::GetObject(
+      varargs.m_scale, varargs);
+  if (p_var_scale_setter == NULL)
+    THROW(fatal_error,
+          "Could not construct VAR scale setter within PILOT one.");
+
+  // Construct STRICT_METS scale setter.
+  Scale_Setter_Arguments metsargs = args;
+  metsargs.m_scale = "STRICT_METS";
+  p_mets_scale_setter = Scale_Setter_Base::Scale_Getter_Function::GetObject(
+      metsargs.m_scale, metsargs);
+  if (p_mets_scale_setter == NULL)
+    THROW(fatal_error,
+          "Could not construct STRICT_METS scale setter within PILOT one.");
 }
 
 double Pilot_Scale_Setter::Calculate
