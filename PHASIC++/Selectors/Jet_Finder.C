8e0e37cb7b719cbf314df742bdb30c83a811ada9
Peter Meinzinger <peter.meinzinger@durham.ac.uk>
2023-03-02 17:40:04 -0600

    Streamline the RSTrigger

--- PHASIC++/Selectors/Jet_Finder.C
+++ PHASIC++/Selectors/Jet_Finder.C
@@ -57,8 +57,12 @@ bool Jet_Finder::Trigger(Selector_List &sl)
   m_pass=true;
   m_results = {Weights_Map{}};
   p_ampl->SetProc(p_proc);
-  for (size_t i(0);i<sl.size();++i)
-    p_ampl->Leg(i)->SetMom((int)i<m_nin?-sl[i].Momentum():sl[i].Momentum());
+  for (int i(0); i<p_ampl->Legs().size();++i)
+    p_ampl->Leg(i)->Delete();
+  p_ampl->Legs().clear();
+  for (int i(0);i<sl.size();++i)
+    p_ampl->CreateLeg((int)i<m_nin?-sl[i].Momentum():sl[i].Momentum(),
+                      i<m_nin?sl[i].Flavour().Bar():sl[i].Flavour(),ColorID());
   m_qcut=p_yccalc->Calculate()->Get<double>();
   if (!m_on) return true;
   msg_Debugging()<<METHOD<<"("<<this<<"): '"<<p_proc->Name()
@@ -82,71 +86,7 @@ bool Jet_Finder::Trigger(Selector_List &sl)
   return 1-m_sel_log->Hit(!triggered);
 }
 
-bool Jet_Finder::RSTrigger(NLO_subevtlist *const subs)
-{
-  for (size_t i(0);i<m_nin+m_nout;++i)
-    p_ampl->Leg(i)->SetMom(i<m_nin && subs->back()->p_mom[i][0]>0.0?
-			   -subs->back()->p_mom[i]:subs->back()->p_mom[i]);
-  m_qcut=p_yccalc->Calculate()->Get<double>();
-  if (!m_on) return true;
-  m_pass=0;
-  std::vector<int> any_variation_passes(subs->size(), 0);
-  m_results = std::vector<Weights_Map>(subs->size(), Weights_Map {1.0});
-  for (size_t n(0);n<subs->size();++n) {
-    int nominal_passes {0};
-    msg_Debugging()<<METHOD<<"("<<n<<"): '"<<p_proc->Name()
-		   <<"' Q_cut = "<<m_qcut<<(m_on?" {":", off")<<"\n";
-    {
-      msg_Indent();
-      p_ampl->SetProc(p_proc);
-      if (p_ampl->Legs().size()<(*subs)[n]->m_n)
-	p_ampl->CreateLeg(Vec4D(),Flavour(kf_jet),ColorID());
-      else if (p_ampl->Legs().size()>(*subs)[n]->m_n) {
-	p_ampl->Legs().back()->Delete();
-	p_ampl->Legs().pop_back();
-      }
-      size_t idij((1<<(*subs)[n]->m_i)|(1<<(*subs)[n]->m_j));
-      if ((*subs)[n]->m_i==(*subs)[n]->m_j) idij=0;
-      for (size_t i(0);i<(*subs)[n]->m_n;++i) {
-	p_ampl->Leg(i)->SetFlav
-	  ((int)i<m_nin?(*subs)[n]->p_fl[i].Bar():(*subs)[n]->p_fl[i]);
-	p_ampl->Leg(i)->SetMom(i<m_nin && subs->back()->p_mom[i][0]>0.0?
-			       -(*subs)[n]->p_mom[i]:(*subs)[n]->p_mom[i]);
-	p_ampl->Leg(i)->SetId((*subs)[n]->p_id[i]);
-	p_ampl->Leg(i)->SetK((*subs)[n]->p_id[i]==idij?
-			     (1<<(*subs)[n]->m_k):0);
-      }
-      p_ampl->Decays()=p_proc->Info().m_fi.GetDecayInfos();
-      const double jcv = p_jc->Value(p_ampl, idij ? 0 : 1);
-      m_results[n]["QCUT"] = Weights {Variations_Type::qcut};
-      for (size_t i {0}; i < s_variations->Size(Variations_Type::qcut) + 1;
-           ++i) {
-        const double fac {
-            i != 0 ? s_variations->Qcut_Parameters(i - 1).m_scale_factor : 1.0};
-        const auto pass = jcv > sqr(m_qcut * fac);
-        if (i == 0) {
-          (*subs)[n]->m_trig = pass;
-          if (pass)
-            nominal_passes = m_pass = pass;
-        }
-        if (pass) {
-          any_variation_passes[n] = 1;
-        }
-        m_results[n]["QCUT"][i] = pass;
-      }
-    }
-    msg_Debugging()<<"} -> "<<nominal_passes<<"\n";
-  }
-
-  int result = m_pass;
-  for (size_t n(0); n < subs->size(); ++n) {
-    result |= (*subs)[n]->m_trig |= (any_variation_passes[n] ? 2 : 0);
-  }
-
-  return 1-m_sel_log->Hit(!result);
-}
-
-void Jet_Finder::BuildCuts(Cut_Data *cuts) 
+void Jet_Finder::BuildCuts(Cut_Data *cuts)
 {
 }
 
