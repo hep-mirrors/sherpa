96adc1611fc236065d935e2fe0ea26a670403b1c
Peter Meinzinger <peter.meinzinger@durham.ac.uk>
2023-04-07 15:05:01 -0400

    Fix undefined behaviour of returning address to local variable in Monochromatic.C

--- BEAM/Spectra/Monochromatic.C
+++ BEAM/Spectra/Monochromatic.C
@@ -6,9 +6,9 @@ using namespace BEAM;
 using namespace std;
 
 Monochromatic::Monochromatic(const Flavour _beam,const double _energy,
-			     const double _polarisation,const int _dir) :
-  Beam_Base(beamspectrum::monochromatic,_beam,_energy,_polarisation,_dir)
-{ }
+			     const double _polarisation,const int _dir)
+    : Beam_Base(beamspectrum::monochromatic,_beam,_energy,_polarisation,_dir),
+      m_none(kf_none) {}
 
 
 Beam_Base * Monochromatic::Copy()
@@ -16,7 +16,7 @@ Beam_Base * Monochromatic::Copy()
   return new Monochromatic(m_beam,m_energy,m_polarisation,m_dir);
 }
 
-ATOOLS::Flavour Monochromatic::Remnant()                { return kf_none; }
+ATOOLS::Flavour Monochromatic::Remnant()                { return m_none; }
 
 
 
