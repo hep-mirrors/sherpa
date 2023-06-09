f3be34cc701eaa5dd2fb61d818b00635d1467c5e
Alan Price <Alan.Price@uni-siegen.de>
2023-06-09 15:35:09 +0200

    Revert reals to Tree_Getter

--- YFS/NLO/Real.C
+++ YFS/NLO/Real.C
@@ -23,7 +23,7 @@ Real::Real(const PHASIC::Process_Info& pi)  {
    real_pi.m_maxcpl[0] = pi.m_maxcpl[0];
    real_pi.m_mincpl[1] = pi.m_mincpl[1];
    real_pi.m_maxcpl[1] = pi.m_maxcpl[1];
-   p_real_me =  PHASIC::Virtual_ME2_Base::GetME2(real_pi);
+   p_real_me =  PHASIC::Tree_ME2_Base::GetME2(real_pi);
    if (!p_real_me)  THROW(not_implemented, "Couldn't find real ME for this process.");
    MODEL::s_model->GetCouplings(m_cpls);
    /* Load color-correlated ME. TODO: orders */
@@ -37,7 +37,7 @@ Real::Real(const PHASIC::Process_Info& pi)  {
    m_sym *= ATOOLS::Flavour::ISSymmetryFactor(args.m_inflavs);
    double bornsym = ATOOLS::Flavour::FSSymmetryFactor(args.m_outflavs);
    bornsym*= ATOOLS::Flavour::FSSymmetryFactor(born_flavs);
-   m_sym/=bornsym;
+   // m_sym/=bornsym;
    ATOOLS::Settings& s = ATOOLS::Settings::GetMainSettings();
    // m_factor = m_sym*p_real_me->AlphaQED()/2/M_PI;
   m_factor = p_real_me->AlphaQED();///m_sym;
@@ -75,8 +75,8 @@ double Real::Calc_R(const ATOOLS::Vec4D_Vector& p)
       }
       out_ps<<"    ]"<<std::endl;
   }
-    p_real_me->Calc(p);
-    double R = p_real_me->ME_Finite();
+    
+    double R = p_real_me->Calc(p);
     if(m_check_real) real_out<<std::setprecision(15)<<R/2/M_PI<<std::endl;
-    return R*m_rescale_alpha*m_sym/2/M_PI;//*m_factor/2/M_PI;
+    return R*m_rescale_alpha/m_sym;//*m_factor/2/M_PI;
   }
