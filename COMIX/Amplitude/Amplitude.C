b13bd0fe31b461fa722c02786fd6ceabad10f9a0
Mareen Hoppe <mareen.hoppe@mailbox.tu-dresden.de>
2023-11-01 16:36:25 +0100

    Add spin 3/2 particles in further of parts of the code

--- COMIX/Amplitude/Amplitude.C
+++ COMIX/Amplitude/Amplitude.C
@@ -535,6 +535,7 @@ void Amplitude::AddCurrent(const Int_Vector &ids,const size_t &n,
     case 0: pols[i]=1; break;
     case 1: pols[i]=2; break;
     case 2: pols[i]=m_fl[ids[i]].IsMassive()?3:2; break;
+    case 3: pols[i]=m_fl[ids[i]].IsMassive()?5:2; break;
     default:
       THROW(not_implemented,"Cannot handle spin "+
 	    ToString(m_fl[i].Spin())+" particles");
