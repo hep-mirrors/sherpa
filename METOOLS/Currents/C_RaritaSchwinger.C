9c48838bad0cad662464563b6e5d2b1ba4d3f3aa
Mareen Hoppe <mareen.hoppe@mailbox.tu-dresden.de>
2023-11-15 09:47:09 +0100

    Add first version of massive RS particles

--- METOOLS/Currents/C_RaritaSchwinger.C
+++ METOOLS/Currents/C_RaritaSchwinger.C
@@ -22,7 +22,8 @@ METOOLS::operator<<(std::ostream &s,const CRaritaSchwinger<Scalar> &rs)
   else if (rs.H()==1) helicity = "+";
   else if (rs.H()==-1) helicity = "-";
   else if (rs.H()==-3) helicity = "--";
-  else THROW(fatal_error, "The value of the helicity of the Rarita-Schwinger particle is not permitted.")
+  // TODO: Wieder reinnehmen, wenn geklärt ist, wie H() nun aussehen soll!!!
+  //else THROW(fatal_error, "The value of the helicity of the Rarita-Schwinger particle is not permitted.")
 
   return s<<'{'<<(rs.B()<0?(rs.R()>0?"Ubar"+helicity:"Vbar"+helicity):
   (rs.R()>0?"U"+helicity:"V"+helicity)) <<","<<rs.S()<<";"<<rs(0)<<","<<rs(1)<<'|'
@@ -45,21 +46,50 @@ void CRaritaSchwinger<Scalar>::ResetAccu()
   s_accu=Accu(); 
 }
 
+//TODO: Bessere Lösung als THROW? Was genau bedeutet das auskommentierte? -> Abgeschrieben von Spinor
+// TODO: FUnktioniert das auch für m_on =3? Sind dann beide ifs true?
 template<class Scalar>
 void CRaritaSchwinger<Scalar>::Add(const CObject *c) {
-
+  const CRaritaSchwinger *rs(static_cast<const CRaritaSchwinger*>(c));
+  if (rs->m_b!=m_b || rs->m_r!=m_r) THROW(fatal_error, "Summing particle and anti-particle wave functions / bar "
+                                                       "and non-bar wave functions is not allowed!")
+  m_on|=rs->m_on;
+  if (m_on&1) {
+    m_x[0]+=rs->m_x[0]; m_x[1]+=rs->m_x[1]; m_x[4]+=rs->m_x[4]; m_x[5]+=rs->m_x[5]; m_x[8]+=rs->m_x[8];
+    m_x[9]+=rs->m_x[9]; m_x[12]+=rs->m_x[12]; m_x[13]+=rs->m_x[13];
+  }
+  if (m_on&2) {
+    m_x[2]+=rs->m_x[2]; m_x[3]+=rs->m_x[3]; m_x[6]+=rs->m_x[6]; m_x[7]+=rs->m_x[7]; m_x[10]+=rs->m_x[10];
+    m_x[11]+=rs->m_x[11]; m_x[14]+=rs->m_x[14]; m_x[15]+=rs->m_x[15];
+  }
 }
 template<class Scalar>
 void CRaritaSchwinger<Scalar>::Multiply(const Complex &c) {
-
+  if (m_on&1) {
+    m_x[0]*=SComplex(c) ; m_x[1]*=SComplex(c); m_x[4]*=SComplex(c); m_x[5]*=SComplex(c); m_x[8]*=SComplex(c);
+    m_x[9]*=SComplex(c); m_x[12]*=SComplex(c); m_x[13]*=SComplex(c);
+  }
+  if (m_on&2) {
+    m_x[2]*=SComplex(c); m_x[3]*=SComplex(c); m_x[6]*=SComplex(c); m_x[7]*=SComplex(c); m_x[10]*=SComplex(c);
+    m_x[11]*=SComplex(c); m_x[14]*=SComplex(c); m_x[15]*=SComplex(c);
+  }
 }
+
 template<class Scalar>
 void CRaritaSchwinger<Scalar>::Divide(const double &d) {
-
+  if (d==0.0) THROW(fatal_error, "Zero Division");
+  if (m_on&1) {
+    m_x[0]/=SComplex(d) ; m_x[1]/=SComplex(d); m_x[4]/=SComplex(d); m_x[5]/=SComplex(d); m_x[8]/=SComplex(d);
+    m_x[9]/=SComplex(d); m_x[12]/=SComplex(d); m_x[13]/=SComplex(d);
+  }
+  if (m_on&2) {
+    m_x[2]/=SComplex(d); m_x[3]/=SComplex(d); m_x[6]/=SComplex(d); m_x[7]/=SComplex(d); m_x[10]/=SComplex(d);
+    m_x[11]/=SComplex(d); m_x[14]/=SComplex(d); m_x[15]/=SComplex(d);
+  }
 }
 template<class Scalar>
 void CRaritaSchwinger<Scalar>::Invert() {
-
+  for (size_t i(0); i<16; ++i) m_x[i] = -m_x[i];
 }
 
 // TODO: WANN LÖSCHE ICH DAS ZEUG HIER WIEDER?
@@ -75,10 +105,13 @@ ATOOLS::TCMatrix<Scalar> CRaritaSchwinger<Scalar>::Contract4Index(const CRaritaS
   return ATOOLS::TCMatrix<Scalar>(intermediate, 4);
 }
 
+  //TODO: Bessere Lösung als THROW? Was genau bedeutet das auskommentierte? -> Abgeschrieben von Spinor
 template <class Scalar> std::complex<Scalar>
 CRaritaSchwinger<Scalar>::operator*(const CRaritaSchwinger<Scalar> &rs) const
 {
-  if (rs.m_b==m_b) return (*this)*rs.CConj();
+  //if (rs.m_b==m_b) return (*this)*rs.CConj();
+  if (rs.m_b!=m_b || rs.m_r!=m_r) THROW(fatal_error, "Multiplying particle and anti-particle wave functions / bar "
+                                                       "and non-bar wave functions is not allowed!")
   std::complex<Scalar> result(0.0, 0.0);
   std::complex<Scalar> sign(1);
   for (size_t i(0); i<16; ++i){
@@ -229,6 +262,59 @@ template <class Scalar> bool CRaritaSchwinger<Scalar>::SetOn()
 // TODO: Springt er hier wirklich zum richtigen Operator? UNBEDINGT BEI ALLEN OPERATOREN TESTEN IM DEBUG-MODE
 template<class Scalar>
 bool CRaritaSchwinger<Scalar>::Test_Properties(const ATOOLS::Vec4D &p, int r, int b) {
+  std::vector<SComplex> test_spinor(4, SComplex());
+  test_spinor[0] = sqrt(p[0]-p.P())*(1/(sqrt(2*p.P()*(p.P()+p[3]))))*(p.P()+p[3]);
+  test_spinor[1] = SComplex(sqrt(p[0]-p.P())*(1/(sqrt(2*p.P()*(p.P()+p[3])))))*(SComplex(p[1])+SComplex(0,p[2]));
+  test_spinor[2] = sqrt(p[0]+p.P())*(1/(sqrt(2*p.P()*(p.P()+p[3]))))*(p.P()+p[3]);
+  test_spinor[3] = SComplex(sqrt(p[0]+p.P())*(1/(sqrt(2*p.P()*(p.P()+p[3])))))*(SComplex(p[1])+SComplex(0,p[2]));
+  std::vector<SComplex> test_polvec(4, SComplex());
+  test_polvec[1] = SComplex(1/sqrt(2))*(SComplex(-(1/(p.P()*sqrt(p[1]*p[1]+p[2]*p[2])))*(p[1]*p[3]))+SComplex(0,1)*SComplex((1/(sqrt(p[1]*p[1]+p[2]*p[2])))*p[2]));
+  test_polvec[2] = SComplex(1/sqrt(2))*(SComplex(-(1/(p.P()*sqrt(p[1]*p[1]+p[2]*p[2])))*(p[2]*p[3]))-SComplex(0,1)*SComplex((1/(sqrt(p[1]*p[1]+p[2]*p[2])))*p[1]));
+  test_polvec[3] = SComplex(1/sqrt(2))*(SComplex((1/(p.P()*sqrt(p[1]*p[1]+p[2]*p[2])))*(sqrt(p[1]*p[1]+p[2]*p[2])*sqrt(p[1]*p[1]+p[2]*p[2]))));
+
+  CRaritaSchwinger<Scalar> test_u(CRaritaSchwinger(1, -1, test_spinor[0]*test_polvec[0], test_spinor[1]*test_polvec[0],
+                                                   test_spinor[2]*test_polvec[0], test_spinor[3]*test_polvec[0],
+                                                   test_spinor[0]*test_polvec[1], test_spinor[1]*test_polvec[1],
+                                                   test_spinor[2]*test_polvec[1], test_spinor[3]*test_polvec[1],
+                                                   test_spinor[0]*test_polvec[2], test_spinor[1]*test_polvec[2],
+                                                   test_spinor[2]*test_polvec[2], test_spinor[2]*test_polvec[3],
+                                                   test_spinor[3]*test_polvec[0], test_spinor[3]*test_polvec[1],
+                                                   test_spinor[3]*test_polvec[2], test_spinor[3]*test_polvec[3]));
+  std::cout << "Test-u" << std::endl;
+  // gamma_mu^A_B times RS^B,mu = 0
+  std::cout<<METHOD<<": Testing gamma_mu Psi^mu = 0..."<<std::endl;
+  METOOLS::Gamma gammavec1 = Gamma<Scalar>();
+  std::vector<SComplex> result_test(4);
+  for (size_t i(0); i<4; ++i){
+    for (size_t j(0); j<4; ++j){
+      if (b>0) result_test[i] += gammavec1[0][i][j] * test_u[j] - gammavec1[1][i][j] * test_u[j+4] -
+                             gammavec1[2][i][j] * test_u[j+8] - gammavec1[3][i][j] * test_u[j+12];
+      else{
+        for (size_t k(0); k<4; ++k){
+          result_test[k] += test_u[i] * gammavec1[0][i][j] * gammavec1[0][j][k] -
+                        test_u[i+4] * gammavec1[1][i][j] * gammavec1[0][j][k] -
+                        test_u[i+8] * gammavec1[2][i][j] * gammavec1[0][j][k] -
+                        test_u[i+12] * gammavec1[3][i][j] * gammavec1[0][j][k];
+        }
+      }
+    }
+  }
+  /*for (int i(0); i<4; ++i){
+    result2[0] += gammavec[i][0][0] * (*this)[2*i] + gammavec[i][0][1] * (*this)[1+2*i] + gammavec[i][0][2] * (*this)[8+2*i] + gammavec[i][0][3] * (*this)[9+2*i];
+    result2[1] += gammavec[i][1][0] * (*this)[2*i] + gammavec[i][1][1] * (*this)[1+2*i] + gammavec[i][1][2] * (*this)[8+2*i] + gammavec[i][1][3] * (*this)[9+2*i];
+    result2[2] += gammavec[i][2][0] * (*this)[2*i] + gammavec[i][2][1] * (*this)[1+2*i] + gammavec[i][2][2] * (*this)[8+2*i] + gammavec[i][2][3] * (*this)[9+2*i];
+    result2[3] += gammavec[i][3][0] * (*this)[2*i] + gammavec[i][3][1] * (*this)[1+2*i] + gammavec[i][3][2] * (*this)[8+2*i] + gammavec[i][3][3] * (*this)[9+2*i];
+    // result2 += gammavec[i] * METOOLS::CVec4<Scalar>((*this)[i+i*4], (*this)[i+i*4+1], (*this)[i+i*4+2], (*this)[i+i*4+3]);
+  }*/
+  if (!(std::abs(result_test[0].real())<s_accu && std::abs(result_test[1].real())<s_accu && std::abs(result_test[2].real())<s_accu && std::abs(result_test[3].real())<s_accu)){
+    msg_Out() << "gamma_mu Psi^mu is " << result_test[0] << result_test[1] << result_test[2] << result_test[3] << " not zero!" << std::endl;
+  }
+  if (!(std::abs(result_test[0].imag())<s_accu && std::abs(result_test[1].imag())<s_accu && std::abs(result_test[2].imag())<s_accu && std::abs(result_test[3].imag())<s_accu)){
+    msg_Out() << "gamma_mu Psi^mu is " << result_test[0] << result_test[1] << result_test[2] << result_test[3] << " not zero!" << std::endl;
+  }
+
+
+
   // Dirac equation (gamma_mu * p_mu -m)^A_B RS^B, nu -> auch +m? für V statt U?
   std::cout<<METHOD<<": Testing Dirac equation..."<<std::endl;
   METOOLS::Gamma gammavec = Gamma<Scalar>();
@@ -286,8 +372,6 @@ bool CRaritaSchwinger<Scalar>::Test_Properties(const ATOOLS::Vec4D &p, int r, in
   if (testresult) msg_Out()<< "passed" << std::endl;
 
   // gamma_mu^A_B times RS^B,mu = 0
-  // ÜBERPRÜFEN!!!
-  //METOOLS::CVec4<Scalar> result2 = CVec4<Scalar>();
   std::cout<<METHOD<<": Testing gamma_mu Psi^mu = 0..."<<std::endl;
   std::vector<SComplex> result2(4);
   for (size_t i(0); i<4; ++i){
@@ -313,14 +397,10 @@ bool CRaritaSchwinger<Scalar>::Test_Properties(const ATOOLS::Vec4D &p, int r, in
   }*/
   if (!(std::abs(result2[0].real())<s_accu && std::abs(result2[1].real())<s_accu && std::abs(result2[2].real())<s_accu && std::abs(result2[3].real())<s_accu)){
     msg_Out() << "gamma_mu Psi^mu is " << result2[0] << result2[1] << result2[2] << result2[3] << " not zero!" << std::endl;
-    // explizit berechnet für ubar++ und Gamma-Matrizen laut Weyl-Basis Deutsches Dirac-Matrizen Wiki
-    msg_Out() << "TEST: gamma_mu Psi^mu is " << SComplex(0,-1)*(*this)[9]-(*this)[12] << SComplex(0,1)*(*this)[8]+(*this)[13] << SComplex(0,1)*(*this)[11]+(*this)[14] << SComplex(0,-1)*(*this)[10]-(*this)[15] << " not zero!" << std::endl;
-    //return false;
     testresult = false;
   }
   if (!(std::abs(result2[0].imag())<s_accu && std::abs(result2[1].imag())<s_accu && std::abs(result2[2].imag())<s_accu && std::abs(result2[3].imag())<s_accu)){
     msg_Out() << "gamma_mu Psi^mu is " << result2[0] << result2[1] << result2[2] << result2[3] << " not zero!" << std::endl;
-    //return false;
     testresult = false;
   }
   if (testresult) msg_Out()<< "passed" << std::endl;
@@ -337,19 +417,14 @@ bool CRaritaSchwinger<Scalar>::Test_Properties(const ATOOLS::Vec4D &p, int r, in
   }*/
   if (!(std::abs(result3[0].real())<s_accu && std::abs(result3[1].real())<s_accu && std::abs(result3[2].real())<s_accu && std::abs(result3[3].real())<s_accu)){
     msg_Out() << "p_mu Psi^mu is " << result3[0] << result3[1] << result3[2] << result3[3] << " not zero!" << std::endl;
-    return false;
+    testresult = false;
   } ;
   if (!(std::abs(result3[0].imag())<s_accu && std::abs(result3[1].imag())<s_accu && std::abs(result3[2].imag())<s_accu && std::abs(result3[3].imag())<s_accu)) {
     msg_Out() << "p_mu Psi^mu is " << result3[0] << result3[1] << result3[2] << result3[3] << " not zero!" << std::endl;
-    return false;
+    testresult = false;
   }
   if (testresult) msg_Out()<< "passed" << std::endl;
-
-  // gauge independence
-
-  // completeness -> in Stromklasse testen!!!
   return testresult;
-  //return true;
 }
 
 namespace METOOLS {
