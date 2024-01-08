6b26c3871d6b67109aeca5b17ee07b30dc0a2deb
Peter Meinzinger <peter.meinzinger@durham.ac.uk>
2024-01-08 13:53:06 +0100

    Fix the Archlinux CI
    The new docker image uses Catch2 v3 now, so the unit test code needed to be adapted

--- Tests/Unit/test_CObject.C
+++ Tests/Unit/test_CObject.C
@@ -1,5 +1,16 @@
-// for Catch2 v3 change <catch2/catch.hpp> to <catch2/catch_test_macros.hpp>
-// and remove test_main.C
+#include <catch2/catch_test_macros.hpp>
+#include "METOOLS/Currents/C_Spinor.H"
+
+TEST_CASE("Spinor construction", "[spinor]") {
+  ATOOLS::Vec4<double> v0(0, 0, 0, 0);
+  METOOLS::CSpinor<double> s0(1, 1, 1, v0);
+  REQUIRE(s0.IsZero());
+}
+
+// ########################################
+// for Catch2 v2 use the below code instead
+
+/*
 #include <catch2/catch.hpp>
 #include "METOOLS/Currents/C_Spinor.H"
 
@@ -8,3 +19,10 @@ TEST_CASE("Spinor construction", "[spinor]") {
   METOOLS::CSpinor<double> s0(1, 1, 1, v0);
   REQUIRE(s0.IsZero());
 }
+*/
+
+// and run the following bash line here:
+/*
+echo "#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>" > test_main.C
+*/
