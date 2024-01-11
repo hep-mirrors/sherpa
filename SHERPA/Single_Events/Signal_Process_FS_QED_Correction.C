7546edc12ec9e17616c62df44ae712d84d6aa94d
krauss <frank.krauss@durham.ac.uk>
2024-01-11 10:33:13 +0000

    fixed a problem with soft qed when no ME is available - needs further work to have functional default

--- SHERPA/Single_Events/Signal_Process_FS_QED_Correction.C
+++ SHERPA/Single_Events/Signal_Process_FS_QED_Correction.C
@@ -103,8 +103,9 @@ Return_Value::code Signal_Process_FS_QED_Correction::Treat(Blob_List* bloblist)
   }
   if (!sigblob) return Return_Value::Nothing;
   // if already treated -> nothing to do
-  if (sigblob->TypeSpec()=="YFS-type_QED_Corrections_to_ME")
+  if (sigblob->TypeSpec()=="YFS-type_QED_Corrections_to_ME") {
     return Return_Value::Nothing;
+  }
   if (sigblob->TypeSpec()=="setting_leptons_on-shell")
     return Return_Value::Nothing;
   // extract FS leptons
@@ -189,27 +190,53 @@ Return_Value::code Signal_Process_FS_QED_Correction::Treat(Blob_List* bloblist)
   // build new QED radiation blob
   Blob * QEDblob = bloblist->AddBlob(btp::QED_Radiation);
   QEDblob->SetTypeSpec("YFS-type_QED_Corrections_to_ME");
-  for (Particle_Vector::iterator it=fslep.begin();it!=fslep.end();++it) {
+  if (blobs.size()==0 && !m_onme) {
+    //msg_Out()<<METHOD
+    //	     <<"("<<m_onme<<", "<<blobs.size()<<" blobs, "<<mfslep.size()<<" particles "
+    //	     <<"from blob = "<<sigblob->Id()<<") :\n"
+    //	     <<(*QEDblob)<<"\n";
+    while (!mfslep.empty()) {
+      //msg_Out()<<"add & copy "<<mfslep.back()->Number()<<" (original = "
+      //       <<mfslep.back()->OriginalPart()->Number()<<")\n";
+      Particle * inpart  = (*mfslep.begin())->OriginalPart();
+      inpart->SetStatus(part_status::decayed);
+      QEDblob->AddToInParticles(inpart);
+      Particle * outpart = new Particle(*inpart);
+      outpart->SetNumber();
+      outpart->SetStatus(part_status::active);
+      outpart->SetOriginalPart(inpart);
+      QEDblob->AddToOutParticles(outpart);
+      delete (*mfslep.begin());
+      mfslep.erase(mfslep.begin());
+    }
+  }
+  else {
+    // all as usual, only IS particles, as FS will be filled by QED radiation
     // set info back to hard process, otherwise
     // check for momentum conservation does not work
-    (*it)->SetInfo('H');
-    (*it)->SetStatus(part_status::decayed);
-    QEDblob->AddToInParticles(*it);
+    for (Particle_Vector::iterator it=fslep.begin();it!=fslep.end();++it) {
+      (*it)->SetInfo('H');
+      (*it)->SetStatus(part_status::decayed);
+      QEDblob->AddToInParticles(*it);
+    }
   }
   // if fixed-order NLO RS and on, copy QED radiation also to subevtlist
   if (m_onme) ModifySubEvtList(sigblob,fslep,blobs);
-  // first fill in all LO particles
-  for (Blob_Vector::iterator it=blobs.begin();it!=blobs.end();++it) {
-    while ((*it)->NOutP() && (*it)->OutParticle(0)->Info()!='S') {
-      Particle * part((*it)->RemoveOutParticle(0,true));
-      QEDblob->AddToOutParticles(part);
+  // blobs found - all proceeds as usual
+  if (blobs.size()>0) {
+    // first fill in all LO particles
+    for (Blob_Vector::iterator it=blobs.begin();it!=blobs.end();++it) {
+      while ((*it)->NOutP() && (*it)->OutParticle(0)->Info()!='S') {
+	Particle * part((*it)->RemoveOutParticle(0,true));
+	QEDblob->AddToOutParticles(part);
+      }
     }
-  }
-  // then append all photons
-  for (Blob_Vector::iterator it=blobs.begin();it!=blobs.end();++it) {
-    while ((*it)->NOutP()) {
-      Particle * part((*it)->RemoveOutParticle(0,true));
-      QEDblob->AddToOutParticles(part);
+    // then append all photons
+    for (Blob_Vector::iterator it=blobs.begin();it!=blobs.end();++it) {
+      while ((*it)->NOutP()) {
+	Particle * part((*it)->RemoveOutParticle(0,true));
+	QEDblob->AddToOutParticles(part);
+      }
     }
   }
   QEDblob->SetStatus(blob_status::needs_reconnections | blob_status::needs_hadronization);
