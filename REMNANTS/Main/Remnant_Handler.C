1dda5d99a7a3e0920fdb235cb151c40671bcacc5
Peter Meinzinger <peter.meinzinger@durham.ac.uk>
2024-01-19 16:48:43 +0000

    Enable the rescattering for HERA setups

--- REMNANTS/Main/Remnant_Handler.C
+++ REMNANTS/Main/Remnant_Handler.C
@@ -16,7 +16,7 @@ using namespace ATOOLS;
 using namespace std;
 
 Remnant_Handler::
-Remnant_Handler(PDF::ISR_Handler *isr,BEAM::Beam_Spectra_Handler *beam,const vector<size_t> & tags) :
+Remnant_Handler(PDF::ISR_Handler *isr,BEAM::Beam_Spectra_Handler *beam,const array<size_t, 2> & tags) :
   p_softblob(nullptr), m_check(true), m_output(false), m_fails(0) {
   InitializeRemnants(isr, beam,tags);
   DefineRemnantStrategy();
@@ -33,7 +33,7 @@ Remnant_Handler::~Remnant_Handler() {
 
 void Remnant_Handler::
 InitializeRemnants(PDF::ISR_Handler *isr,BEAM::Beam_Spectra_Handler *beam,
-		   const vector<size_t> & tags) {
+		   const array<size_t, 2> & tags) {
   for (size_t i = 0; i < 2; ++i) {
     m_tags[i]     = tags[i];
     p_remnants[i] = nullptr;
