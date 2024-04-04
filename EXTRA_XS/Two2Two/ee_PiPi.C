0683c35885349e56a901985d5c9ac95eff4706d2
Alan Price <alan.price@uni-siegen.de>
2024-04-04 10:12:28 +0200

    Fix issue where doxygen was complaining about  no uniquely matching class member found for

--- EXTRA_XS/Two2Two/ee_PiPi.C
+++ EXTRA_XS/Two2Two/ee_PiPi.C
@@ -4,6 +4,7 @@
 #include "MODEL/Main/Running_AlphaQED.H"
 #include "PHASIC++/Process/External_ME_Args.H"
 #include "EXTRA_XS/Main/ME2_Base.H"
+#include "MODEL/UFO/UFO_Model.H"
 #include "ATOOLS/Phys/Pion_FormFactor.H"
 
 
@@ -45,21 +46,23 @@ namespace EXTRAXS {
   double ee_PiPi::operator()(const ATOOLS::Vec4D_Vector& momenta)
   {
     // Eq 90 in 0912.0749
-    double s((momenta[2]+momenta[3]).Abs2());
-    double t((momenta[0]-momenta[2]).Abs2());
+    double s((momenta[0]+momenta[1]).Abs2());
+    double t((momenta[0]-momenta[3]).Abs2());
     double masspi = m_flv.Mass();
     double beta = sqrt(1.-4*sqr(masspi)/s);
     double sinth2 = 1.-sqr(1.+2.*t/s);
     // double sinth2 = sqr(sin(theta));
     if(sinth2 < 0) msg_Error()<<METHOD<<" Sin^2 less than zero: "<<sinth2<<std::endl;
-    return sqr(4*M_PI*(*aqed)(s)) / 4. / s * sinth2 * pow(beta,3) * p_formfactor->Eval(s);
+    return sqr(4*M_PI*(*aqed)(s)) / 2. * sinth2 * pow(beta,3) * p_formfactor->Eval(s);
   }
 }
 
-DECLARE_TREEME2_GETTER(ee_PiPi,"ee_PiPi")
-Tree_ME2_Base *ATOOLS::Getter<Tree_ME2_Base,External_ME_Args,ee_PiPi>::
+DECLARE_TREEME2_GETTER(EXTRAXS::ee_PiPi,"ee_PiPi")
+Tree_ME2_Base *ATOOLS::Getter<PHASIC::Tree_ME2_Base,PHASIC::External_ME_Args,EXTRAXS::ee_PiPi>::
 operator()(const External_ME_Args &args) const
 {
+  if (dynamic_cast<UFO::UFO_Model*>(MODEL::s_model)) return NULL;
+  
   const Flavour_Vector fl = args.Flavours();
   if (fl.size()!=4) return NULL;
   if (fl[0]==Flavour(kf_e) && fl[1]==fl[0].Bar() &&
