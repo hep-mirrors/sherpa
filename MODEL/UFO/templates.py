10299328dae84b7225602a7503811c5e2cd046b3
Mareen Hoppe <mareen.hoppe@mailbox.tu-dresden.de>
2024-05-07 12:19:13 +0200

    Avoid pkg_resources in templates.py

--- MODEL/UFO/templates.py
+++ MODEL/UFO/templates.py
@@ -1,14 +1,12 @@
-from __future__ import division 
-import pkg_resources
+import pkgutil
 from string import Template
 
+model_template = Template(pkgutil.get_data(__name__, "model_template.C").decode('utf-8'))
 
-model_template = Template(pkg_resources.resource_string(__name__, "model_template.C").decode("utf-8"))
+lorentz_calc_template = Template(pkgutil.get_data(__name__, "lorentz_calc_template.C").decode('utf-8'))
 
-lorentz_calc_template = Template(pkg_resources.resource_string(__name__, "lorentz_calc_template.C").decode("utf-8"))
+color_calc_template = Template(pkgutil.get_data(__name__, "color_calc_template.C").decode('utf-8'))
 
-color_calc_template = Template(pkg_resources.resource_string(__name__, "color_calc_template.C").decode("utf-8"))
+sconstruct_template = Template(pkgutil.get_data(__name__, "sconstruct_template").decode('utf-8'))
 
-sconstruct_template = Template(pkg_resources.resource_string(__name__, "sconstruct_template").decode("utf-8"))
-
-run_card_template = Template(pkg_resources.resource_string(__name__, "run_card_template").decode("utf-8"))
+run_card_template = Template(pkgutil.get_data(__name__, "run_card_template").decode('utf-8'))
