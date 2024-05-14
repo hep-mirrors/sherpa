7958b667eabee2e3dd9ebb9e0372e6b93d9b0f42
Alan Price <alan.price@uni-siegen.de>
2024-05-14 14:31:20 +0100

    add warning if user tries to use pdfs and yfs

--- SHERPA/Initialization/Initialization_Handler.C
+++ SHERPA/Initialization/Initialization_Handler.C
@@ -710,6 +710,9 @@ bool Initialization_Handler::InitializeTheYFS(){
   p_yfshandler = new YFS::YFS_Handler();
   if(p_yfshandler->GetMode()!=0) {
     msg_Info()<<"Initialized YFS for Soft Photon Resummation"<<std::endl;
+    for (const auto &pdf: m_pdflibs) {
+      if(pdf!="None") THROW(fatal_error,"Cannot use PDFs with initial state YFS. Disable the PDF (PDF_LIBRARY: None) or YFS (YFS: MODE: OFF)");
+    }
     for (size_t beam=0;beam<2;++beam) {
       p_yfshandler->SetInFlav(m_bunch_particles[beam]);
       p_yfshandler->SetBeam(p_beamspectra);
