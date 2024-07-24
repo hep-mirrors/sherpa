c084ebb39b74cedf6f498e846343d3c0ba447f10
Reichelt <danielreichelt@me.com>
2024-07-24 20:04:16 +0100

    correct momenta in ps tests

--- AMEGIC++/DipoleSubtraction/FF_DipoleSplitting.C
+++ AMEGIC++/DipoleSubtraction/FF_DipoleSplitting.C
@@ -35,7 +35,8 @@ void FF_DipoleSplitting::SetMomenta(const Vec4D* mom)
   m_pj = mom[m_j];
   m_pk = mom[m_k];
 
-  if (m_subtype==subscheme::Alaric)// && m_ftype==spt::soft)
+
+  if (m_subtype==subscheme::Alaric) // && m_ftype==spt::soft)
     return SetMomentaAlaric(mom);
 
   m_yijk = m_pi*m_pj/(m_pi*m_pj+m_pj*m_pk+m_pk*m_pi);
