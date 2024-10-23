49cf1357b43732b9a1d8f7c538acf88edb6d8be9
Alan Price <alan.price@uni-siegen.de>
2024-10-23 18:51:06 +0200

    Further bug fixes. Hmumu looking good

--- YFS/Main/Define_Dipoles.C
+++ YFS/Main/Define_Dipoles.C
@@ -613,7 +613,7 @@ double Define_Dipoles::CalculateFlux(const Vec4D &k){
   if(fluxtype==dipoletype::final){
     for (auto &D : m_dipolesFF) {
       Q  = D.GetBornMomenta(0)+D.GetBornMomenta(1);
-      QX = D.GetNewMomenta(0)+D.GetNewMomenta(1);
+      QX = D.GetMomenta(0)+D.GetMomenta(1);
       sq = (Q).Abs2();
       sx = (Q+k).Abs2();
       flux = (sq/sx);
