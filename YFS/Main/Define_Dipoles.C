4386f56dba833dc8d447b4fcd891f5c11c019009
Alan Price <alan.price@uni-siegen.de>
2024-10-25 14:44:54 +0200

    Add method to identify which final state dipole should undergo mapping when more than one is present

--- YFS/Main/Define_Dipoles.C
+++ YFS/Main/Define_Dipoles.C
@@ -226,8 +226,6 @@ void Define_Dipoles::MakeDipoles(ATOOLS::Flavour_Vector const &fl, ATOOLS::Vec4D
         if (k == 2) {
           Flavour f1 = fl[d1];
           Flavour f2 = fl[d2];
-          PRINT_VAR(f1);
-          PRINT_VAR(f2);
           if(f1.Charge()!=0 && f2.Charge()!=0){
             ff.push_back(f1);
             ff.push_back(f2);
@@ -352,10 +350,12 @@ double Define_Dipoles::CalculateRealSub(const Vec4D &k) {
     }
   }
   for (auto &D : m_dipolesFF) {
-    for(size_t i = 0; i < D.GetMomenta().size(); ++i)
-    {
-      Vec4D p = D.GetMomenta(i);
-      eik += -D.m_Q[i]*p/(p*k);
+    if(D.IsDecayAllowed()){
+      for(size_t i = 0; i < D.GetMomenta().size(); ++i)
+      {
+        Vec4D p = D.GetMomenta(i);
+        eik += -D.m_Q[i]*p/(p*k);
+      }
     }
   }
   sub = -m_alpha / (4 * M_PI * M_PI)*eik*eik;
@@ -611,6 +611,7 @@ double Define_Dipoles::CalculateFlux(const Vec4D &k){
 
   }
   if(fluxtype==dipoletype::final){
+    // YFS::Dipole D = WhichDipole(k);
     for (auto &D : m_dipolesFF) {
       Q  = D.GetBornMomenta(0)+D.GetBornMomenta(1);
       QX = D.GetMomenta(0)+D.GetMomenta(1);
@@ -739,8 +740,8 @@ double Define_Dipoles::ResonantDist(YFS::Dipole &D, const Vec4D &k){
   double mcheck(100000000);
   for (auto it = m_proc_restab_map.begin(); it != m_proc_restab_map.end(); ++it) {
     for (auto *v : it->second) {
-      // mcheck = abs(mass_d - v->in[0].Mass()) / v->in[0].Width();
-      mcheck = (mass_i-mass_d);
+      mcheck = abs(mass_d - v->in[0].Mass()) / v->in[0].Width();
+      // mcheck = (mass_i-mass_d);
       if(mcheck < mdist) mdist = mcheck;
     }
   }
@@ -760,6 +761,33 @@ dipoletype::code Define_Dipoles::WhichResonant(const Vec4D &k){
   }
   if(mdistfsr < mdistisr) return dipoletype::final;
   else return dipoletype::initial;
+}
+
+YFS::Dipole Define_Dipoles::WhichDipole(const Vec4D &k, YFS::Dipole_Vector *Dlo){
+  if(m_dipolesFF.size()==1) return m_dipolesFF[0];
+  YFS::Dipole winner;
+  double mdistfsr;
+  double dist = 100000000;
+  for(auto &D: m_dipolesFF){
+    // for(auto &_D: Dlo){
+     for (Dipole_Vector::iterator _D = Dlo->begin(); _D != Dlo->end(); ++_D){
+      if(!IsEqualFlav(D,(*_D))) continue;
+      // if(!_D->IsDecayAllowed()) continue;
+      for(auto &_k: _D->m_mephotons){
+        if(IsEqual(_k,k)){
+          winner=D;
+        }
+      }
+    }
+  }
+  return winner;
+}
+
+bool Define_Dipoles::IsEqualFlav(YFS::Dipole d1, YFS::Dipole d2){
+  bool match=true;
+  if(d1.GetFlav(0)!=d2.GetFlav(1) && d1.GetFlav(0)!=d2.GetFlav(0)) match = false;
+  if(d1.GetFlav(1)!=d2.GetFlav(1) && d1.GetFlav(1)!=d2.GetFlav(0)) match = false;
+  return match;
 }
 
 void Define_Dipoles::generate_pairings(std::vector<std::vector<int>>& pairings, std::vector<int>& curr_pairing, std::vector<int>& available_nums) {
