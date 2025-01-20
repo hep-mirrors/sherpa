622bbe1807b12d7edcc1e0f62f264adc052210b5
Alan Price <alan.price@uni-siegen.de>
2025-01-20 15:32:30 +0100

    Udates for bhabha

--- AddOns/Griffin/Griffin_Interface.C
+++ AddOns/Griffin/Griffin_Interface.C
@@ -10,6 +10,7 @@ Griffin::Griffin_Interface::~Griffin_Interface() {}
 // define statics
 int Griffin::Griffin_Interface::m_inital=11;
 int Griffin::Griffin_Interface::m_final=13;
+double Griffin::Griffin_Interface::m_norm=1;
 Griffin::griffinorder::code Griffin::Griffin_Interface::m_order=griffinorder::nnlo;
 griffin::inval Griffin::Griffin_Interface::m_griffin;
 
@@ -38,7 +39,6 @@ bool Griffin::Griffin_Interface::Initialize(MODEL::Model_Base *const model,
   if(IsEqual(delap,-1)){
     delap = 1-(*aqed)(0)/(*aqed)(mz2);
   }
-  PRINT_VAR(s_model->ScalarConstant("alpha_QED"));
   m_griffin.set(MZ, Flavour(kf_Z).Mass());
   m_griffin.set(MW, Flavour(kf_Wplus).Mass());
   m_griffin.set(al, s_model->ScalarConstant("alpha_QED"));
@@ -64,40 +64,34 @@ void Griffin::Griffin_Interface::RegisterProcess(const PHASIC::Process_Info &pi)
   // Since griffin is only 2->2 m_initial[0]=bar(m_initial[1])
   m_inital = pi.m_ii.GetExternal()[0];
   m_final = pi.m_fi.GetExternal()[0];
+  if( m_inital == m_final ) m_norm = 2;  // For BhaBha
 }
 
 void Griffin::Griffin_Interface::RegisterProcess(const External_ME_Args& args){
   // Since griffin is only 2->2 m_initial[0]=bar(m_initial[1])
   m_inital = args.m_inflavs[0];
   m_final =  args.m_outflavs[0];
+  if( m_inital == m_final ) m_norm = 2;  // For BhaBha
 }
 
 void Griffin::Griffin_Interface::EvaluateLoop(const Vec4D_Vector& momenta, METOOLS::DivArrD& virt){
   if(momenta.size()!=4){
     THROW(fatal_error, "Griffin library is for 2->2 scattering only");
   }
-  // EvaluateLO(momenta, virt);
-  // PRINT_VAR(virt.Finite());
-  EvaluateNNLO(momenta, virt);
-  // PRINT_VAR(virt.Finite());
-  // if(m_order==griffinorder::lo) EvaluateLO(momenta, virt);
-  // else if(m_order==griffinorder::nlo) EvaluateNLO(momenta, virt);
-  // else EvaluateNNLO(momenta,virt);
-  // PRINT_VAR(virt.Finite());
+  if(m_order==griffinorder::lo) EvaluateLO(momenta, virt);
+  else if(m_order==griffinorder::nlo) EvaluateNLO(momenta, virt);
+  else EvaluateNNLO(momenta,virt);
+  virt *= m_norm;
 }
 
 void Griffin::Griffin_Interface::EvaluateLO(const Vec4D_Vector& momenta, DivArrD &res){
   double s = (momenta[0]+momenta[1]).Abs2();
   double t = (momenta[0]-momenta[2]).Abs2();
   double cost = 1.+2.*t/s;
+  // cost = cos((momenta[2]).Theta());
   if(cost > 1. || cost < -1){
     msg_Error()<<"CosTheta out of range in "<<METHOD<<endl;
   }
-  // double cost = (momenta[2]).CosTheta(momenta[0]);
-  // PRINT_VAR(costt/cost);
-  // if(sqrt(s)<40) {
-  //   res.Finite() = 0;
-  // }
   FA_SMLO FAi(m_inital, m_griffin), FAf(m_final, m_griffin);
   // SW_SMLO SWi(m_inital, m_griffin), SWf(m_final, m_griffin);
   // cout << "sineff^i (LO+) = " << SWi.result() << endl;
@@ -130,12 +124,13 @@ void Griffin::Griffin_Interface::EvaluateNLO(const Vec4D_Vector& momenta, DivArr
   //   res.Finite() = 0;
   // }
   FA_SMNLO FAi(m_inital, m_griffin), FAf(m_final, m_griffin);
-  // SW_SMNLO SWi(m_inital, m_griffin), SWf(m_final, m_griffin);
+  SW_SMNLO SWi(m_inital, m_griffin), SWf(m_final, m_griffin);
   // cout << "FAi = " << FAi.result() << endl;
   // cout << "FAi = " << FAf.result() << endl;
+  // cout << "SWi = " << SWi.result() << endl;
   double sw=s_model->ComplexConstant("csin2_thetaW").real();
 
-  mat_SMNLO M(m_inital, m_final, VEC, VEC, FAi.result().real(), FAf.result().real(), sw, sw*1.001, s, cost, m_griffin);
+  mat_SMNLO M(m_inital, m_final, VEC, VEC, FAi.result().real(), FAf.result().real(), sw, sw, s, cost, m_griffin);
 
   M.setkinvar(s, cost);
   Cplx resvv, resva, resav, resaa;
@@ -155,6 +150,7 @@ void Griffin::Griffin_Interface::EvaluateNLO(const Vec4D_Vector& momenta, DivArr
     // -4*cost*(resvv*conj(resva) + resav*conj(resaa)));
   // return res*3*s*s*m_rescale_alpha/32/M_PI;
   // res.Finite() *= 3*s/32/M_PI;
+  // res.Finite() *= 2;
 }
 
 void Griffin::Griffin_Interface::EvaluateNNLO(const Vec4D_Vector& momenta, DivArrD &res){
@@ -232,10 +228,10 @@ std::istream &Griffin::operator>>(std::istream &str, griffinorder::code &mode)
   std::string tag;
   str>>tag;
   mode=griffinorder::nnlo;
-  if      (tag.find("LO")!=std::string::npos)    mode=griffinorder::lo;
+  if      (tag.find("NNLO")!=std::string::npos)    mode=griffinorder::nnlo;
   else if (tag.find("NLO")!=std::string::npos)   mode=griffinorder::nlo;
-  else if (tag.find("NNLO")!=std::string::npos)    mode=griffinorder::nnlo;
-  else THROW(fatal_error, "Unknown GRIFFIN: Order");
+  else if (tag.find("LO")!=std::string::npos)    mode=griffinorder::lo;
+  else THROW(fatal_error, "Unknown GRIFFIN: Order = " + tag);
   return str;
 }
 
