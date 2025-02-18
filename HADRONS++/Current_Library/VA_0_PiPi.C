4b811cbef9ff2f385e30988f33961e6e924ee1a2
krauss <frank.krauss@durham.ac.uk>
2025-02-18 09:46:48 +0000

    step 1 of restructuring: current for 2 pseudoscalars

---
+++ HADRONS++/Current_Library/VA_0_PiPi.C
@@ -0,0 +1,531 @@
+#include "HADRONS++/Current_Library/VA_0_PiPi.H"
+#include "ATOOLS/Phys/Flavour.H"
+#include "ATOOLS/Org/Run_Parameter.H"
+#include "ATOOLS/Org/Exception.H"
+
+using namespace HADRONS;
+using namespace ATOOLS;
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////
+//
+// Form factors for charged pi pi, K K, K pi final-state currents from:
+// - RChT, 2: Resonance Chiral Perturbation Theory
+//   * pi pi/KK fit: Eur.Phys.J.C 79 (2019) 5, 436
+//     (https://doi.org/10.1140/epjc/s10052-019-6943-9)
+//   * K pi: Phys.Lett.B 640 (2006) 176
+//     (https://doi.org/10.1016/j.physletb.2006.06.058)
+// - KS, 1 (Kuehn-Santamaria model):
+//   * pi pi (original version): Z.Phys.C 48 (1990) 445-452
+//     (https://doi.org/10.1007/BF01572024)
+//   * pi pi (KS with Gounaris-Sakurai form factors):
+//     from Belle measurement & analysis:
+//     (https://arxiv.org/pdf/0805.3773)
+//   * pi eta (KS inspired): Phys.Rev.D 82 (2010) 057301
+//     (https://doi.org/10.1103/PhysRevD.82.057301)
+//   * pi eta' (KS inspired): Phys.Rev.D 84 (2011) 017302
+//     (https://doi.org/10.1103/PhysRevD.84.017302)
+//   * K pi: Z.Phys.C 69 (1996) 243 (vector form factor)
+//     (https://doi.org/10.1007/s002880050024)
+//   * K pi: Z.Phys.C 72 (1996) 619 (scalar form factor)
+//     (https://doi.org/10.1007/s002880050284)
+// - none, 0: no form factor
+//
+///////////////////////////////////////////////////////////////////////////
+
+VA_0_PiPi::VA_0_PiPi(const ATOOLS::Flavour_Vector& flavs,
+		     const std::vector<int>& indices,
+		     const std::string& name) :
+  Current_Base(flavs, indices, name),
+  m_restype(resonance_type::running),
+  m_PSmode(PSmode::unknown),
+  m_ffmodel(ffmodel::KS),
+  m_global(1.), m_deltaM2(0.), m_fpi(0.13041), m_GV(0.0619), 
+  m_m2_pi(sqr(Flavour(kf_pi_plus).Mass(true))),
+  m_m2_K(sqr(Flavour(kf_K_plus).Mass(true))),
+  m_m2_eta(sqr(Flavour(kf_eta).Mass(true))),
+  m_mu2(sqr(Flavour(kf_rho_770_plus).Mass(true)))
+{
+  if (m_flavs[p_i[0]].Kfcode()==kf_pi &&
+      m_flavs[p_i[1]].Kfcode()==kf_pi_plus)             m_PSmode = PSmode::pipi;
+  else if ( (m_flavs[p_i[0]].Kfcode()==kf_K_L ||
+	     m_flavs[p_i[0]].Kfcode()==kf_K_S ||
+	     m_flavs[p_i[0]].Kfcode()==kf_K) &&
+	    m_flavs[p_i[1]].Kfcode()==kf_K_plus)        m_PSmode = PSmode::KK;
+  else if ( (m_flavs[p_i[0]].Kfcode()==kf_pi_plus && 
+	     (m_flavs[p_i[1]].Kfcode()==kf_K_S ||
+	      m_flavs[p_i[1]].Kfcode()==kf_K_L ||
+	      m_flavs[p_i[1]].Kfcode()==kf_K) ) ||
+	    (m_flavs[p_i[0]].Kfcode()==kf_pi && 
+	     (m_flavs[p_i[1]].Kfcode()==kf_K_plus) ) )  m_PSmode = PSmode::Kpi;
+  else if ( m_flavs[p_i[0]].Kfcode()==kf_pi_plus &&
+	    m_flavs[p_i[1]].Kfcode()==kf_eta )          m_PSmode = PSmode::etapi;
+  else if ( m_flavs[p_i[0]].Kfcode()==kf_pi_plus &&
+	    m_flavs[p_i[1]].Kfcode()==kf_eta_prime_958) m_PSmode = PSmode::etaprimepi;
+  msg_Out()<<METHOD<<"("<<m_flavs[p_i[0]]<<" + "<<m_flavs[p_i[1]]<<") "
+	   <<"--> "<<int(m_PSmode)<<"\n";
+  if (m_PSmode==PSmode::unknown)
+    THROW(fatal_error,"Current called for illegal flavour combination.");
+}
+
+VA_0_PiPi::~VA_0_PiPi() {
+  while (!m_vectors.empty()) {
+    delete m_vectors.front().second;
+    m_vectors.pop_front();
+  }
+  while (!m_scalars.empty()) {
+    delete m_scalars.front().second;
+    m_scalars.pop_front();
+  }
+}
+
+void VA_0_PiPi::Calc(const ATOOLS::Vec4D_Vector& moms, bool m_anti)
+{
+  Vec4D  q  = moms[p_i[1]]+moms[p_i[0]];
+  double Q2 = q.Abs2();
+  Insert(m_global *
+	 ( VectorFF(Q2) * ((moms[p_i[1]]-moms[p_i[0]]) - m_deltaM2/Q2 * q ) +
+	   ScalarFF(Q2) * q
+	   ),   0);
+}
+
+void VA_0_PiPi::SetModelParameters(struct GeneralModel model)
+{
+  m_fpi     = model("fpi", 0.13041 );
+  m_GV      = model("GV",  0.0619 );
+  m_ffmodel = ffmodel(model("FORM_FACTOR",1));
+  if (m_ffmodel==ffmodel::RChT) {
+    m_fpi   = 0.092316;
+    if (m_PSmode==PSmode::pipi || m_PSmode==PSmode::KK) {
+      m_RChTnorm = -1./(96.*sqr(M_PI*m_fpi));
+    }
+    else if (m_PSmode==PSmode::Kpi || m_PSmode==PSmode::Keta) {
+      m_RChTnorm = (sqr(m_GV)*Flavour(kf_K_star_892_plus).Mass(true)/
+		    (64.*M_PI*pow(m_fpi,4)));
+    }
+  }
+  // global pre-factor: 1/sqrt(2) for pi_0 wave-function, V_ud for the
+  // quark-level coupling producing a rho (or rho-resonance), 1/sqrt(2)
+  // for the overall normalisation.
+  double iso = ( (m_flavs[p_i[0]].Kfcode()==kf_pi_plus ||
+		  m_flavs[p_i[1]].Kfcode()==kf_pi_plus) ? sqrt(0.5) : 1.);
+  double CKM = ( (m_PSmode==PSmode::pipi || m_PSmode==PSmode::KK) ?
+		 model("Vud", Tools::Vud) : model("Vus", Tools::Vus) );
+  m_global = iso * CKM / sqrt(0.5);
+  switch (int(model("RUNNING_WIDTH",1))) {
+  case 10: m_restype = resonance_type::bespoke; break; 
+  case 2:  m_restype = resonance_type::GS;      break; 
+  case 1:  m_restype = resonance_type::running; break;
+  case 0:  m_restype = resonance_type::fixed;   break;
+  default: m_restype = resonance_type::running; break;
+  }
+  if (m_ffmodel!=ffmodel::none) { 
+    list<kf_code> tags;
+    if (SelectResonances(tags,false)) {
+      for (list<kf_code>::iterator kfit=tags.begin();kfit!=tags.end();kfit++)
+	InitResonance(model, *kfit, false);
+    }
+    tags.clear();
+    if ( (m_PSmode==PSmode::Kpi ||
+	  m_PSmode==PSmode::etapi || m_PSmode==PSmode::etaprimepi) &&
+	  SelectResonances(tags,true)) {
+      m_deltaM2 = m_flavs[p_i[1]].Mass(true)-m_flavs[p_i[0]].Mass(true);
+      for (list<kf_code>::iterator kfit=tags.begin();kfit!=tags.end();kfit++)
+	InitResonance(model, *kfit, true);
+    }
+    if (m_vectors.empty() && m_scalars.empty()) {
+      THROW(fatal_error,"Could not find suitable vector and scalar resonances.");
+    }
+  }
+}
+
+bool VA_0_PiPi::SelectResonances(list<kf_code> & tags,const bool & isScalar) {
+  if (isScalar) {
+    if (m_PSmode==PSmode::etapi   ||
+	m_PSmode==PSmode::etaprimepi) {
+      tags.push_back(kf_a_0_980_plus);
+      tags.push_back(kf_a_0_1450_plus);
+      return true;
+    }
+    else if (m_PSmode==PSmode::Kpi) {
+      tags.push_back(kf_K_0_star_1430_plus);
+      return true;
+    }
+  }
+  else {
+    if (m_PSmode==PSmode::pipi    ||
+	m_PSmode==PSmode::KK      ||
+	m_PSmode==PSmode::etapi   ||
+	m_PSmode==PSmode::etaprimepi) {
+      tags.push_back(kf_rho_770_plus);
+      tags.push_back(kf_rho_1450_plus);
+      tags.push_back(kf_rho_1700_plus);
+      return true;
+    }
+    else if (m_PSmode==PSmode::Kpi) {
+      tags.push_back(kf_K_star_892_plus);
+      tags.push_back(kf_K_star_1410_plus);
+      return true;
+    }
+  }
+  return false;
+}
+
+bool VA_0_PiPi::FillDefaults(const kf_code & tag,vector<double> & defs) {
+  // Default values as vector of 4 doubles:
+  // weight, mass, width, phase
+  switch (m_ffmodel) {
+  case ffmodel::KS:
+    return KSDefaults(tag,defs);
+  case ffmodel::RChT:
+    return RChTDefaults(tag,defs);
+  }
+  THROW(fatal_error,"unknown form factor model.");
+}
+
+bool VA_0_PiPi::KSDefaults(const kf_code & tag,vector<double> & defs) {
+  if (m_PSmode==PSmode::pipi) {
+    switch (tag) {
+    case kf_rho_1700_plus:
+      if (m_restype==resonance_type::running)
+	defs = { -0.0370, 1.7000, 0.2350, 0.0000 };
+      else if (m_restype==resonance_type::GS)
+	defs = {  0.0280, 1.6940, 0.1350, -3./180.*M_PI };
+      break;
+    case kf_rho_1450_plus: 
+      if (m_restype==resonance_type::running)
+	defs = { -0.1030, 1.3200, 0.3900, 0.0000 };
+      else if (m_restype==resonance_type::GS)
+	defs = {  0.1300, 1.4280, 0.4130, 197./180.*M_PI };
+      break;
+    case kf_rho_770_plus:  
+      if (m_restype==resonance_type::running)
+	defs = {  1.0000, 0.7749, 0.1480, 0.0000 };
+      else if (m_restype==resonance_type::GS)
+	defs = {  1.0000, 0.7740, 0.1440, 0.0000 };
+      break;
+    }
+  }
+  if (m_PSmode==PSmode::etapi || m_PSmode==PSmode::etaprimepi) {
+    switch (tag) {
+    case kf_rho_1700_plus:
+      defs = { -0.0370, 1.7000, 0.2350, 0.0000 }; break;
+    case kf_rho_1450_plus: 
+      defs = { -0.1030, 1.3200, 0.3900, 0.0000 }; break;
+    case kf_rho_770_plus:  
+      defs = {  1.0000, 0.7749, 0.1480, 0.0000 }; break;
+    case kf_a_0_1450_plus:  
+      defs = {  0.0000, 0.1450, 0.2650, 0.0000 }; break;
+    case kf_a_0_980_plus:  
+      defs = {  1.0000, 0.9800, 0.1000, 0.0000 }; break;
+    }
+  }
+  else if (m_PSmode==PSmode::KK && m_restype==resonance_type::running) {
+    switch (tag) {
+    case kf_rho_1700_plus:
+      defs = {  0.0500, 1.7000, 0.2350, 0.0000 }; break;
+    case kf_rho_1450_plus:
+      defs = { -0.1670, 1.3630, 0.3100, 0.0000 }; break;
+    case kf_rho_770_plus:  
+      defs = {  1.0000, 0.7749, 0.1480, 0.0000 }; break;
+    }
+  }
+  else if (m_PSmode==PSmode::Kpi && m_restype==resonance_type::running) {
+    switch (tag) {
+    case kf_K_star_1410_plus:
+      defs = { -0.1350, 1.4120, 0.2270, 0.0000 }; break;
+    case kf_K_star_892_plus:      
+      defs = {  1.0000, 0.8920, 0.0500, 0.0000 }; break;
+    case kf_K_0_star_1430_plus:      
+      defs = {  1.7000, 1.4300, 0.2870, 0.0000 }; break;
+    }
+  }
+  if (!defs.empty()) return true;
+  msg_Error()<<METHOD<<" will throw error:\n"
+	     <<"Combination of decay mode = "<<int(m_PSmode)<<", "
+	     <<"resonancve type = "<<int(m_restype)<<" and "
+	     <<"flavour = "<<tag<<" not yet implemented.\n";
+  THROW(fatal_error,"unknown combination in KS defaults");
+  return false;
+}
+
+bool VA_0_PiPi::RChTDefaults(const kf_code & tag,vector<double> & defs) {
+  if (m_PSmode==PSmode::pipi || m_PSmode==PSmode::KK) {
+    switch (tag) {
+    case kf_rho_1700_plus:
+      defs ={ -0.1200, 1.7540, 0.4120, -0.0200 }; break;
+    case kf_rho_1450_plus: 
+      defs = { 0.1500, 1.4380, 0.5350, -0.3600 }; break;
+    case kf_rho_770_plus:  
+      defs = { 1.0000, 0.7752, 0.0000,  0.0000 }; break;
+    }
+  }
+  else if (m_PSmode==PSmode::Kpi) {
+    switch (tag) {
+    case kf_K_star_1410_plus:
+      defs = {  0.0130, 1.4140, 0.2320, 0.0000 }; break;
+    case kf_K_star_892_plus:      
+      defs = {  1.0000, 0.8917, 0.0500, 0.0000 }; break;
+    case kf_K_0_star_1430_plus:      
+      defs = {  1.7000, 1.4300, 0.2870, 0.0000 }; break;
+    }
+  }
+  if (!defs.empty()) return true;
+  msg_Error()<<METHOD<<" will throw error:\n"
+	     <<"Combination of decay mode = "<<int(m_PSmode)<<", "
+	     <<"resonancve type = "<<int(m_restype)<<" and "
+	     <<"flavour = "<<tag<<" not yet implemented.\n";
+  THROW(fatal_error,"unknown combination in KS defaults");
+  return false;
+}
+    
+void VA_0_PiPi::InitResonance(struct GeneralModel model,const kf_code & tag,
+			      const bool & isScalar)
+{
+  vector<double> defaults;
+  if (!FillDefaults(tag,defaults) || dabs(defaults[0])<1.e-8) return;
+  string id = string("");
+  switch (tag) {
+  case kf_rho_1700_plus:      id = string("rho(1700)+");  break;
+  case kf_rho_1450_plus:      id = string("rho(1450)+");  break;
+  case kf_rho_770_plus:       id = string("rho(770)+");   break;
+  case kf_K_star_1410_plus:   id = string("K*(1410)+");   break;
+  case kf_K_star_892_plus:    id = string("K*(892)+");    break;
+  case kf_K_0_star_1430_plus: id = string("K*_0(1430)+"); break;
+  case kf_a_0_980_plus:       id = string("a_0(980)+");   break;
+  case kf_a_0_1450_plus:      id = string("a_0(1450)+");  break;
+  }
+  vector<Flavour> outflavs;
+  if (m_PSmode==PSmode::pipi || m_PSmode==PSmode::KK) {
+    outflavs.push_back(Flavour(kf_pi_plus));
+    outflavs.push_back(Flavour(kf_pi));
+  }
+  else if (m_PSmode==PSmode::etapi || m_PSmode==PSmode::etaprimepi) {
+    outflavs.push_back(m_flavs[p_i[0]]);
+    outflavs.push_back(m_flavs[p_i[1]]);
+  }
+  else if (m_PSmode==PSmode::Kpi) {
+    outflavs.push_back(m_flavs[p_i[0]]);
+    outflavs.push_back(m_flavs[p_i[1]]);
+  }
+  Flavour flav = Flavour(tag);
+  double weight = model(string("Weight")+id, defaults[0]);
+  double mass   = model(string("Mass")  +id, defaults[1]);
+  double width  = model(string("Width") +id, defaults[2]);
+  double phase  = model(string("Phase") +id, defaults[3]);
+
+  Resonance_Parameters params(flav,outflavs,m_restype,mass,width,phase);
+  Resonance_Base * res = NULL;
+  switch (m_restype) {
+  case resonance_type::fixed:
+    res = new FixedWidth_Resonance(params); break;
+  case resonance_type::GS:
+    res = new GS_Resonance(params); break;
+  case resonance_type::running:
+  default:
+    res = new RunningWidth_Resonance(params); break;
+  }
+  if (!isScalar) m_vectors.push_back(make_pair(weight,res));
+  else           m_scalars.push_back(make_pair(weight,res));
+}
+
+Complex VA_0_PiPi::VectorFF(const double & s) {
+  if (m_vectors.empty() && m_ffmodel!=ffmodel::none) return Complex(0.,0.); 
+  if (m_PSmode==PSmode::pipi && s<4.*m_m2_pi) return Complex(0.,0.);
+  if (m_PSmode==PSmode::KK   && s<4.*m_m2_K)  return Complex(0.,0.);
+  switch (int(m_ffmodel)) {
+  case int(ffmodel::RChT): return VectorRChT(s);
+  case int(ffmodel::KS):   return VectorKS(s);
+  case int(ffmodel::none): 
+  default: break;
+  }
+  return Complex(1.,0.);
+}
+
+Complex VA_0_PiPi::ScalarFF(const double & s) {
+  if (m_scalars.empty() && m_ffmodel!=ffmodel::none)  return Complex(0.,0.); 
+  if (m_PSmode==PSmode::pipi || m_PSmode==PSmode::KK) return Complex(0.,0.);
+  switch (int(m_ffmodel)) {
+  case int(ffmodel::RChT): return ScalarRChT(s);
+  case int(ffmodel::KS):   return ScalarKS(s);
+  case int(ffmodel::none): 
+  default: break;
+  }
+  return Complex(1.,0.);
+}
+
+Complex VA_0_PiPi::VectorKS(const double & s)
+{
+  Complex ff = Complex(0.,0.), norm = Complex(0.,0.), pref;
+  for (list<pair<double, Resonance_Base * > >::iterator rtit=m_vectors.begin();
+       rtit!=m_vectors.end();rtit++) {
+    pref  = rtit->first * Complex(cos(rtit->second->Phase()),
+				  sin(rtit->second->Phase()));
+    ff   += pref * rtit->second->BreitWigner(s);
+    norm += pref;
+  }
+  return ff/norm;
+}
+
+Complex VA_0_PiPi::ScalarKS(const double & s)
+{
+  Complex ff = Complex(0.,0.), pref;
+  for (list<pair<double, Resonance_Base * > >::iterator rtit=m_vectors.begin();
+       rtit!=m_vectors.end();rtit++) {
+    pref  = ( rtit->first * m_deltaM2/rtit->second->Mass2() *
+	      Complex(cos(rtit->second->Phase()),sin(rtit->second->Phase())) );
+    ff   += pref * rtit->second->BreitWigner(s);
+  }
+  return ff;
+}
+  
+Complex VA_0_PiPi::VectorRChT(const double & s) {
+  switch (m_PSmode) {
+  case PSmode::pipi:
+  case PSmode::KK:   return VectorRChT_pipi(s);
+  case PSmode::Kpi:  return VectorRChT_Kpi(s);
+  default: break;
+  }
+  return Complex(0.,0.);
+}
+
+Complex VA_0_PiPi::VectorRChT_pipi(const double & s)
+{
+  Complex ff      = Complex(0.,0.);
+  Complex ExtraBW = Complex(0.,0.);
+  double  RhoExpo = 0., pref = 0., Gamma = 0.;
+  for (list<pair<double, Resonance_Base * > >::iterator
+	 rtit=m_vectors.begin();rtit!=m_vectors.end();rtit++) {
+    if (rtit->second->Flav()==Flavour(kf_rho_770_plus)) {
+      Complex Ampl = ( Loop(m_m2_pi, s, m_mu2) +
+		       Loop(m_m2_K,  s, m_mu2)/2. );
+      pref    = s*m_RChTnorm;
+      RhoExpo = exp(pref*Ampl.real());
+      Gamma   = ( rtit->second->Mass() * s/(96.*M_PI*sqr(m_fpi)) *
+		  (s>=4.*m_m2_pi ? pow(1.-4.*m_m2_pi/s, 1.5) : 0.) +
+		  (s>=4.*m_m2_K  ? pow(1.-4.*m_m2_K/s,  1.5) : 0.)/2.
+		  );
+      ExtraBW = rtit->second->AltBreitWigner(s,rtit->second->Mass2(),
+					     -rtit->second->Mass()*Gamma);
+      ff  += (rtit->first *
+	      rtit->second->BreitWigner(s,rtit->second->Mass2(),
+					-rtit->second->Mass()*Gamma) *
+	      RhoExpo );
+    }
+    else {
+      Gamma = (s>4.*m_m2_pi ? rtit->second->Width(s) : 0.);
+      pref = - ( s*rtit->second->Width()/M_PI/
+		 pow(rtit->second->Mass()*
+		     rtit->second->Lambda(rtit->second->Mass2(),m_m2_pi,m_m2_pi), 3));
+      ff += - (rtit->first * exp(Complex(0.,rtit->second->Phase())) *
+	       ( rtit->second->AltBreitWigner(s,rtit->second->Mass2(),
+					      -rtit->second->Mass()*Gamma) *
+		 exp(pref * Loop(m_m2_pi, s, m_mu2).real()) -
+		 ExtraBW * RhoExpo) );
+    }
+  }
+  return ff;
+}
+
+Complex VA_0_PiPi::VectorRChT_Kpi(const double & s)
+{
+  Complex ff      = Complex(0.,0.);
+  Complex ExtraBW = Complex(0.,0.);
+  double  Expo    = exp(3/2.*(Htilde(s,m_m2_K,m_m2_pi)+
+			      Htilde(s,m_m2_K,m_m2_eta)).real());
+  double  Gamma;
+  for (list<pair<double, Resonance_Base * > >::iterator
+	 rtit=m_vectors.begin();rtit!=m_vectors.end();rtit++) {
+    if (rtit->second->Flav()==Flavour(kf_K_star_892_plus)) {
+      Gamma   = (m_RChTnorm * 8./sqrt(s) *
+		 (pow(rtit->second->Lambda(s,m_m2_K,m_m2_pi),3)+
+		  pow(rtit->second->Lambda(s,m_m2_K,m_m2_eta),3)));
+      ExtraBW = rtit->second->AltBreitWigner(s,rtit->second->Mass2(),
+					     -rtit->second->Mass()*Gamma);
+      ff     += (rtit->first *
+		 rtit->second->BreitWigner(s,rtit->second->Mass2(),
+					   -rtit->second->Mass()*Gamma) );
+    }
+    else {
+      Gamma = (s>sqr(sqrt(m_m2_pi)+sqrt(m_m2_K)) ? rtit->second->Width(s) : 0.);
+      ff     += (rtit->first *
+		 rtit->second->AltBreitWigner(s,rtit->second->Mass2(),
+					      -rtit->second->Mass()*Gamma) );
+      ff     -= rtit->first * ExtraBW;
+    }
+	
+  }
+  return ff * Expo;
+}
+
+
+Complex VA_0_PiPi::ScalarRChT(const double & s) {
+  switch (m_PSmode) {
+  case PSmode::pipi:
+  case PSmode::KK:   return Complex(0.,0.);
+  case PSmode::Kpi:  // return ScalarRChT_Kpi(s);
+  default: break;
+  }
+  return Complex(0.,0.);
+}
+
+
+
+Complex VA_0_PiPi::Loop(const double & m2, const double & s,const double & mu2) {
+  Complex sigma = csqrt(1.-4.*m2/s);
+  return ( log(m2/mu2) + 8.*m2/s - 5./3. +
+	   pow(sigma,3.)*log((sigma+1.)/(sigma-1.)) );
+}
+ 
+Complex VA_0_PiPi::Htilde(const double & s, const double & MP2,const double & MQ2) {
+  return (s*Mr(s,MP2,MQ2)-L(s,MP2,MQ2))/sqr(m_fpi);
+}
+
+Complex VA_0_PiPi::L(const double & s, const double & MP2,const double & MQ2) {
+  return sqr(MP2-MQ2)/(4.*s) * JBar(s,MP2,MQ2);
+}
+
+Complex VA_0_PiPi::Mr(const double & s, const double & MP2,const double & MQ2) {
+  double sigma = MP2+MQ2, delta = MP2-MQ2, delta2 = sqr(delta);
+  return ( (s-2.*sigma)/(12.*s) * JBar(s,MP2,MQ2) +
+	   delta2/(3.*sqr(s)) * JBarBar(s,MP2,MQ2) -
+	   1./6. * k(MP2,MQ2) + 1./(288.*sqr(M_PI)) );
+}
+
+Complex VA_0_PiPi::k(const double & MP2,const double & MQ2) {
+  return (MP2*log(MP2/m_mu2)-MQ2*log(MQ2/m_mu2))/(32.*sqr(M_PI)*(MP2-MQ2));
+}
+    
+Complex VA_0_PiPi::JBar(const double & s, const double & MP2,const double & MQ2) {
+  double disc  = sqr(s-MP2-MQ2)-4.*MP2*MQ2;
+  if (disc<0.) return 0.;
+  double nu    = sqrt(disc), nuPs = sqr(nu+s), nuMs = sqr(nu-s);
+  double sigma = MP2+MQ2, delta = MP2-MQ2, delta2 = sqr(delta);
+  return ( (2. + (delta/s-sigma/delta)*log(MQ2/MP2) -
+	    nu/s*log((nuPs-delta2)/(nuMs-delta2)))/
+	   (32.*sqr(M_PI)) );
+}
+
+Complex VA_0_PiPi::JBarBar(const double & s, const double & MP2,const double & MQ2) {
+  double sigma    = MP2+MQ2, delta = MP2-MQ2, delta2 = sqr(delta);
+  double Jprime_0 = (sigma + 2.*MP2*MQ2/delta*log(MQ2/MP2))/(32.*sqr(M_PI * delta));
+  return JBar(s,MP2,MQ2) - Jprime_0;
+}
+
+
+// need to update the references once we have the tau's under control
+DEFINE_CURRENT_GETTER(HADRONS::VA_0_PiPi,"VA_0_PiPi")
+
+void ATOOLS::Getter<HADRONS::Current_Base,
+		    HADRONS::ME_Parameters,HADRONS::VA_0_PiPi>::
+PrintInfo(std::ostream &st,const size_t width) const {
+  st<<"Example: $ 0 \\rightarrow \\pi \\pi $ \n\n"
+    <<"Order: 0 = $\\pi^0$, 1 = $\\pi^\\pm$ \n\n"
+    <<"Available form factors: \n "
+    <<"  \\begin{itemize} \n"
+    <<"    \\item {\\tt FORM\\_FACTOR = 1 :} Kuehn-Santamaria \n"
+    <<"    \\item {\\tt FORM\\_FACTOR = 2 :} Resonance Chiral Theory \n"
+    <<"  \\end{itemize} \n"
+    <<"Reference: https://sherpa.hepforge.org/olddokuwiki/data/media/publications/theses/diplom\\__laubrich.pdf \n"
+    <<std::endl;
+}
---
+++ HADRONS++/PS_Library/Resonance.C
@@ -0,0 +1,105 @@
+#include "HADRONS++/PS_Library/Resonance.H"
+#include "ATOOLS/Org/Message.H"
+#include "ATOOLS/Org/Run_Parameter.H"
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+
+using namespace HADRONS;
+using namespace ATOOLS;
+using namespace std;
+
+
+Resonance_Base::Resonance_Base(const Resonance_Parameters & params) :
+  m_inflav(params.m_inflav), m_type(params.m_type),
+  m_OSmass(params.m_OSmass<0.   ? m_inflav.Mass(true) : params.m_OSmass), m_OSmass2(m_OSmass*m_OSmass),
+  m_OSwidth(params.m_OSwidth<0. ? m_inflav.Width() : params.m_OSwidth),   m_OSwidth2(m_OSwidth*m_OSwidth),
+  m_phase(params.m_phase), m_threshold(0.), m_threshold2(0.), m_lambda(0.),
+  m_exponent(m_inflav.IsVector() ? 3 : (m_inflav.IsScalar() ? 1 : 0)) 
+{
+  m_name = std::string("R_")+m_inflav.IDName();
+  if (m_type==resonance_type::bespoke)      m_name += std::string("bespoke");
+  else if (m_type==resonance_type::running) m_name += std::string("running");
+  else                                      m_name += std::string("fixed");
+  m_decmasses2.resize(params.m_outflavs.size(),0.);
+  for (size_t i=0;i<params.m_outflavs.size();i++) {
+    double mass     = params.m_outflavs[i].Mass(true);
+    m_threshold    += mass;
+    m_decmasses2[i] = ATOOLS::sqr(mass);
+  }
+  m_threshold2  = ATOOLS::sqr(m_threshold);
+  m_lambda      = Lambda(m_OSmass2,m_decmasses2[0],m_decmasses2[1]);
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+double RunningWidth_Resonance::CalculateWidth(const double & s) {
+  if (s>m_threshold2) {
+    if (dabs(1.-s/m_OSmass2)<1.e-12) return m_OSwidth;
+    if (m_decmasses2.size()==2) {
+      return ( m_OSwidth *
+	       pow(Lambda(s,m_decmasses2[0],m_decmasses2[1])/(2.*sqrt(s)) /
+		   (m_lambda / (2.*m_OSmass)), m_exponent)
+	       );
+    }
+  }
+  return 0.;
+}
+
+Complex RunningWidth_Resonance::BreitWigner(const double & s) {
+  double MW = sqrt(s)*CalculateWidth(s);
+  return m_OSmass2/Complex( m_OSmass2-s, -MW );
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+
+GS_Resonance::GS_Resonance(const Resonance_Parameters & params) : 
+  Resonance_Base(params),
+  m_m_pi(Flavour(kf_pi_plus).Mass(true))
+{
+  double mpi2 = sqr(m_m_pi);
+  m_d   = ( 3/M_PI * mpi2/sqr(m_lambda) * log((m_OSmass+2.*m_lambda)/(2.*m_m_pi)) +
+	    m_OSmass/(2.*M_PI*m_lambda) * (1. - 2.*mpi2/sqr(m_lambda)) );
+  m_h0  = h(m_OSmass2);
+  m_dh0 = m_h0 * (1./(8.*sqr(m_lambda))-1./(2.*m_OSmass2));
+}
+
+double GS_Resonance::CalculateWidth(const double & s) {
+  if (s>m_threshold2) {
+    if (dabs(1.-s/m_OSmass2)<1.e-12) return m_OSwidth;
+    if (m_decmasses2.size()==2) {
+      return ( m_OSwidth *
+	       pow(Lambda(s,m_decmasses2[0],m_decmasses2[1])/sqrt(s) /
+		   (m_lambda / m_OSmass), m_exponent)
+	       );
+    }
+  }
+  return m_OSwidth;
+}
+
+Complex GS_Resonance::BreitWigner(const double & s) {
+  double Gamma = CalculateWidth(s);
+  return ( (m_OSmass2+m_d*m_OSmass*m_OSwidth)/
+	   Complex(m_OSmass2-s+f(s), -sqrt(s)*Gamma) );
+}
+
+double GS_Resonance::h(const double & s) {
+  double ks = Lambda(s,m_decmasses2[0],m_decmasses2[1]), E = sqrt(s);
+  return ( 2.*ks/(M_PI*E)*log((E+2.*ks)/(2.*m_m_pi)) );
+}
+
+double GS_Resonance::f(const double & s) { 
+  return ( m_OSwidth*m_OSmass2/pow(m_lambda,3) *
+	   ( sqr(Lambda(s,m_decmasses2[0],m_decmasses2[1])) * (h(s)-m_h0) +
+	     (m_OSmass2-s) * sqr(m_lambda) * m_dh0 )  );
+}
+
+
+ 
