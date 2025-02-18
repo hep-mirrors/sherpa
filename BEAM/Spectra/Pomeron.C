f68c37db94ad243846701d70e895f105ac5d76e6
Peter Meinzinger <peter.meinzinger@uzh.ch>
2025-02-18 12:03:29 +0100

    Remove unnecessary "virtual" keywords in Beam_Base.H

--- BEAM/Spectra/Pomeron.C
+++ BEAM/Spectra/Pomeron.C
@@ -65,11 +65,9 @@ bool Pomeron::CalculateWeight(double x, double q2)
   return true;
 }
 
-void Pomeron::SetOutMomentum(const ATOOLS::Vec4D &out, const size_t & i) {
-  if (i==0) {
-    m_vecouts[0] = out;
-    m_vecouts[1] = m_lab-out;
-  }
+void Pomeron::SetOutMomentum(const ATOOLS::Vec4D &out) {
+  m_vecouts[0] = out;
+  m_vecouts[1] = m_lab-out;
 }
 
 void Pomeron::FixPosition() {
