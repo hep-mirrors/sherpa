57ebbff5bea33ce1d9abc50b2014b8e45fe0c76b
Andrii Verbytskyi <2178089-andriish@users.noreply.gitlab.com>
2025-03-10 16:59:24 +0000

    Edit Output_HepMC3.C

--- AddOns/HepMC/Output_HepMC3.C
+++ AddOns/HepMC/Output_HepMC3.C
@@ -26,7 +26,7 @@ Output_HepMC3::Output_HepMC3(const Output_Arguments &args) :
   m_basename=args.m_outpath+"/"+args.m_outfile;
   m_compression = Settings::GetMainSettings()["HEPMC3_COMPRESSION"].SetDefault("").Get<std::string>();
   m_iotype = Settings::GetMainSettings()["HEPMC3_IO_TYPE"].SetDefault(0).Get<int>();
-  int precision       = Settings::GetMainSettings()["HEPMC3_OUTPUT_PRECISION"].SetDefault(12).Get<int>();
+  int precision = Settings::GetMainSettings()["HEPMC3_OUTPUT_PRECISION"].SetDefault(12).Get<int>();
   m_short = Settings::GetMainSettings()["HEPMC3_SHORT"].SetDefault(false).Get<bool>();
 #ifdef USING__GZIP
   m_compression = "GZ"; 
