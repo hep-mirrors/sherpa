9fe07e23a982854a709c60ce62c6876326a16caf
Alan Price <alan.price@uni-siegen.de>
2025-03-11 12:49:49 +0100

    Disable cuts for virt processes

--- YFS/NLO/RealVirtual.C
+++ YFS/NLO/RealVirtual.C
@@ -59,10 +59,10 @@ double RealVirtual::Calc_V(const ATOOLS::Vec4D_Vector& p,
   {
     double V(0.0), run_corr(0.0), scale(0.0);
     m_failcut = false;
-    if(!p_rvproc->Trigger(p)) {
-      m_failcut = true;
-      return 0;
-    }
+    // if(!p_rvproc->Trigger(p)) {
+    //   m_failcut = true;
+    //   return 0;
+    // }
     // p_loop_me->SetRenScale(mur);
     if(aqed->m_mode!=vpmode::off) {
      if(m_tchannel) scale = -(p[0]-p[2]).Abs2();  
