752b19ebc8bd398437982b733399550486bb1124
krauss <frank.krauss@durham.ac.uk>
2025-03-11 09:05:17 +0000

    particle tags retagged

--- ALPACA/Main/Alpaca.C
+++ ALPACA/Main/Alpaca.C
@@ -23,12 +23,15 @@ bool Alpaca::Harvest(ATOOLS::Blob_List * blobs) {
     if ((*bit)->Has(blob_status::needs_rescattering)) {
       msg_Out()<<(**bit)<<"\n";
       vector<Particle *> outparts((*bit)->GetOutParticles());
-      for (vector<Particle *>::iterator piter=outparts.begin(); piter != outparts.end(); piter++) {
-        if((*piter)->Info() == 'F' ) m_partons.push_back(m_translator(*piter, (*bit)->Position()));
+      for (vector<Particle *>::iterator piter=outparts.begin();
+	   piter != outparts.end(); piter++) {
+        if((*piter)->Info() == 'F' )
+	  m_partons.push_back(m_translator(*piter, (*bit)->Position()));
       }
     }
   }
-  for (vector<Parton *>::iterator piter=m_partons.begin(); piter != m_partons.end(); piter++) {
+  for (vector<Parton *>::iterator piter=m_partons.begin();
+       piter != m_partons.end(); piter++) {
       msg_Out()<<**piter<<endl;
   }
   return true;
@@ -37,18 +40,23 @@ bool Alpaca::Harvest(ATOOLS::Blob_List * blobs) {
 bool Alpaca::AddBlob(ATOOLS::Blob_List * blobs) {
   Blob * blob = blobs->AddBlob(btp::Hard_Collision);
   blob->SetId();
-  blob->SetTypeSpec("Rescattering");
+  blob->SetTypeSpec("ALPACA");
   blob->SetStatus(blob_status::needs_reconnections);
   for (Blob_List::iterator bit=blobs->begin();bit!=blobs->end();bit++) {
     if ((*bit)->Has(blob_status::needs_rescattering)) {
       (*bit)->UnsetStatus(blob_status::needs_rescattering);
       vector<Particle *> outparts((*bit)->GetOutParticles());
-      for (vector<Particle *>::iterator piter=outparts.begin(); piter != outparts.end(); piter++) {
-        if((*piter)->Info() == 'F' ) blob->AddToInParticles(*piter);
+      for (vector<Particle *>::iterator
+	     piter=outparts.begin(); piter != outparts.end(); piter++) {
+        if((*piter)->Info() == 'F' ) {
+	  blob->AddToInParticles(*piter);
+	  (*piter)->SetStatus(part_status::decayed);
+	}
       }
     }
   }
-  for (vector<Parton *>::iterator piter = m_partons.begin(); piter != m_partons.end(); piter++) {
+  for (vector<Parton *>::iterator piter = m_partons.begin();
+       piter != m_partons.end(); piter++) {
       blob->AddToOutParticles(m_translator((*piter)));
   }
   PRINT_VAR(*blobs);
