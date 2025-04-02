3fc21bf95547e3972c3cdcc520cd57da0d60b90a
Peter Meinzinger <peter.meinzinger@uzh.ch>
2025-04-02 16:22:10 +0200

    Absorb Fixed_Target into Beam_Base.C

--- BEAM/Main/Beam_Base.C
+++ BEAM/Main/Beam_Base.C
@@ -12,16 +12,20 @@ Beam_Base::Beam_Base(beamspectrum _type, const ATOOLS::Flavour& _beam,
   m_position(ATOOLS::Vec4D(0.,0.,0.,0.)),
   m_dir(_dir), m_energy(_energy), m_polarisation(_polarisation),
   m_x(1.), m_Q2(0.), m_weight(1.), m_on(false) {
-  double disc = mode ? 1.0 : 1.0 - ATOOLS::sqr(m_beam.Mass() / m_energy);
-  if (disc < 0.) {
-    msg_Error() << "Error in Beam_Base :" << m_type << std::endl
-                << "   Mismatch of energy and mass of beam particle : "
-                << m_beam << " / " << m_energy << std::endl
-                << "   Will lead to termination of program." << std::endl;
-    ATOOLS::Abort();
+  if (_energy == 0.) { // Fixed Target mode
+    m_lab = ATOOLS::Vec4D(m_beam.Mass(true), 0., 0., 0.);
+  } else {
+    double disc = mode ? 1.0 : 1.0 - ATOOLS::sqr(m_beam.Mass() / m_energy);
+    if (disc < 0.) {
+      msg_Error() << "Error in Beam_Base :" << m_type << std::endl
+                  << "   Mismatch of energy and mass of beam particle : "
+                  << m_beam << " / " << m_energy << std::endl
+                  << "   Will lead to termination of program." << std::endl;
+      ATOOLS::Abort();
+    }
+    double pz = m_dir * m_energy * sqrt(disc);
+    m_lab     = ATOOLS::Vec4D(m_energy, 0., 0., pz);
   }
-  double pz = m_dir * m_energy * sqrt(disc);
-  m_lab     = ATOOLS::Vec4D(m_energy, 0., 0., pz);
   m_bunches.resize(m_Nbunches, m_beam);
   m_vecouts.resize(m_Nbunches, m_lab);
 }
