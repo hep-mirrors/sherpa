ce46b17c2d6c1544af5fbd6f0ae1ca7b57d6758b
Peter Meinzinger <peter.meinzinger@uzh.ch>
2025-04-09 10:20:58 +0100

    Implement first version of impact parameter rejection

--- BEAM/Main/Collider_Weight.C
+++ BEAM/Main/Collider_Weight.C
@@ -1,10 +1,12 @@
 #include "BEAM/Main/Collider_Weight.H"
 
-using namespace BEAM;
-using namespace ATOOLS;
+#include "ATOOLS/Math/Random.H"
 
-Collider_Weight::Collider_Weight(Kinematics_Base *kinematics)
-    : Weight_Base(kinematics), m_mode(collidermode::unknown) {
+using namespace BEAM;
+
+Collider_Weight::Collider_Weight(Kinematics_Base* kinematics)
+    : Weight_Base(kinematics), m_mode(collidermode::unknown)
+{
   if (p_beams[0]->Type() == beamspectrum::monochromatic &&
       p_beams[1]->Type() == beamspectrum::monochromatic)
     m_mode = collidermode::monochromatic;
@@ -19,11 +21,37 @@ Collider_Weight::Collider_Weight(Kinematics_Base *kinematics)
     m_mode = collidermode::both_spectral;
   if (m_mode == collidermode::unknown)
     THROW(fatal_error, "Bad settings for collider mode.");
+
+  m_rejection = ATOOLS::Settings::GetMainSettings()["BEAM_OVERLAP_REJECTION"]
+                        .SetDefault(false)
+                        .Get<int>();
+  if (m_rejection == 1)
+    p_rejector = new Radius_Rejection(p_beams[0]->Beam(), p_beams[1]->Beam());
+  else if (m_rejection > 1 && p_beams[0]->Beam().Kfcode() == kf_p_plus &&
+           p_beams[1]->Beam().Kfcode() == kf_p_plus)
+    p_rejector =
+            new Proton_Proton_Rejection(p_beams[0]->Beam(), p_beams[1]->Beam());
+  else if (m_rejection > 1 && (p_beams[0]->Beam().Kfcode() == kf_p_plus &&
+                               p_beams[1]->Beam().IsIon()) ||
+           (p_beams[0]->Beam().IsIon() &&
+            p_beams[1]->Beam().Kfcode() == kf_p_plus))
+    p_rejector = new Proton_Nucleon_Rejection(p_beams[0]->Beam(),
+                                              p_beams[1]->Beam());
+  else if (m_rejection > 1 && p_beams[0]->Beam().IsIon() &&
+           p_beams[1]->Beam().IsIon())
+    p_rejector = new Nucleon_Nucleon_Rejection(p_beams[0]->Beam(),
+                                               p_beams[1]->Beam());
+  else
+    msg_Error() << METHOD << ATOOLS::om::red
+                << ": Could not find appropriate beam radius rejection model, "
+                   "will not do any rejection.\n"
+                << ATOOLS::om::reset;
 }
 
 Collider_Weight::~Collider_Weight() = default;
 
-void Collider_Weight::AssignKeys(Integration_Info *const info) {
+void Collider_Weight::AssignKeys(ATOOLS::Integration_Info* const info)
+{
   m_sprimekey.Assign(m_keyid + std::string("s'"), 5, 0, info);
   m_ykey.Assign(m_keyid + std::string("y"), 3, 0, info);
   // Convention for m_xkey:
@@ -33,13 +61,22 @@ void Collider_Weight::AssignKeys(Integration_Info *const info) {
   m_xkey.Assign(m_keyid + std::string("x"), 6, 0, info);
 }
 
-bool Collider_Weight::Calculate(const double &scale) {
+bool Collider_Weight::Calculate(const double& scale)
+{
   m_weight = 0.;
   return (p_beams[0]->CalculateWeight(m_xkey[4], scale) &&
           p_beams[1]->CalculateWeight(m_xkey[5], scale));
 }
 
-double Collider_Weight::operator()() {
+double Collider_Weight::operator()()
+{
+  if (m_rejection > 0 && Reject()) return 0.;
   m_weight = p_beams[0]->Weight() * p_beams[1]->Weight();
   return m_weight;
+}
+
+bool Collider_Weight::Reject() {
+  double b1(p_beams[0]->ImpactParameter()), b2(p_beams[1]->ImpactParameter());
+  double b = std::sqrt(b1 * b1 + b2 * b2 - 2 * b1 * b2 * std::cos(2 * M_PI * ATOOLS::ran->Get()));
+  return (*p_rejector)(b);
 }
