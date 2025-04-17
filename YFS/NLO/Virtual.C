21837f19c279ba24aed719bbd8eb9c52b23e544a
Alan Price <alan.price@uni-siegen.de>
2025-04-17 13:58:33 +0200

    Take virtual proc to be same as the born

--- YFS/NLO/Virtual.C
+++ YFS/NLO/Virtual.C
@@ -63,10 +63,10 @@ double Virtual::Calc_V(const ATOOLS::Vec4D_Vector& p,
     p_loop_me->SetRenScale(100);
     // if(!p_rvproc->Trigger(p)) return 0;
     // m_failcut = false;
-    // if(!p_vproc->Trigger(p)) {
-    //   m_failcut = true;
-    //   return 0;
-    // }
+    if(!p_vproc->Trigger(p)) {
+      m_failcut = true;
+      return 0;
+    }
     double V(0.0), run_corr(0.0), scale(0.0);
     if(aqed->m_mode!=vpmode::off) {
      if(m_tchannel==2) scale = -(p[0]-p[2]).Abs2();  
