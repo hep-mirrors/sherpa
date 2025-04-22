bee64de8c51904793bc24405166d4d680b96e115
Alan Price <alan.price@uni-siegen.de>
2025-04-22 19:44:29 +0200

    No longer subtract born for griffin in YFS module. Now we calculate it with Griffin and remove it there.

--- YFS/NLO/Virtual.C
+++ YFS/NLO/Virtual.C
@@ -83,9 +83,7 @@ double Virtual::Calc_V(const ATOOLS::Vec4D_Vector& p,
         V =  m_factor *  p_loop_me->ME_Finite() * B ; break;
       case 1:
         // For Griffin
-        // PRINT_VAR(p_loop_me->ME_Finite()*m_rescale_alpha);
-        V =  p_loop_me->ME_Finite()-B;//*((*aqed)(0)/s_model->ScalarConstant("alpha_QED"));
-        V *= (*aqed)(0)/s_model->ScalarConstant("alpha_QED");
+        V =  p_loop_me->ME_Finite();
         break;
       default:
         THROW(not_implemented, "Loop ME mode not implemented: "+ATOOLS::ToString(p_loop_me->Mode()));
