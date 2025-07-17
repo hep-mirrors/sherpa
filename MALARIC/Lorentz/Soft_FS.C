d97ec90396b38e1133d705285d4164d19cf3c467
Stefan Hoeche <shoeche@fnal.gov>
2025-07-17 16:07:14 -0700

    removed partitioning in malaric weight calculation for g->gg

--- MALARIC/Lorentz/Soft_FS.C
+++ MALARIC/Lorentz/Soft_FS.C
@@ -46,7 +46,7 @@ namespace MALARIC {
 	       -pk.Abs2()/sqr(skj));
       A*=sij*skj*(pi*n)/D;
       double sf(1.0);
-      if (m_id) sf=p_sk->Mode()?1.0-s.m_z:s.m_z;
+      if (m_id && s.m_clu!=2) sf=p_sk->Mode()?1.0-s.m_z:s.m_z;
 #ifdef DEBUG__Kinematics
       if (s.m_p.size()) {
 	Vec4D K(s.m_p[0]+s.m_p[1]), pij(s.p_c->Mom());
