a37bee2bc91a89e19f34b457fa7189d713b28d58
Alan Price <alan.price@uj.edu.pl>
2025-11-12 20:09:17 +0100

    Added option to skip negative weights in YFS. Dangerous to use, in principle YFS is positive definite but some negative weights appear

--- YFS/Main/YFS_Base.C
+++ YFS/Main/YFS_Base.C
@@ -83,6 +83,7 @@ void YFS_Base::RegisterDefaults(){
   s["IR_SCALE"].SetDefault(100);
   s["NLO_CUTS"].SetDefault(false);
   s["Fixed_Order"].SetDefault(fixed_order::full);
+  s["SKIP_NEG_WEIGHTS"].SetDefault(false);
 }
 
 void YFS_Base::RegisterSettings(){
@@ -139,6 +140,7 @@ void YFS_Base::RegisterSettings(){
   m_irscale = s["IR_SCALE"].Get<double>();
   m_nlocuts = s["NLO_CUTS"].Get<bool>();
   m_fixedOrder = s["Fixed_Order"].Get<fixed_order::code>();
+  m_skipNegWeights = s["SKIP_NEG_WEIGHTS"].Get<bool>();
   m_CalForm = false;
   m_realtool = false;
   //update when beamstrahlung is added
