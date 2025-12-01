babbbab75a7ad80c4b63672467d3b1e2a3e6ad15
Peter Meinzinger <peter.meinzinger@uzh.ch>
2025-12-01 13:41:30 +0100

    Reinstate small xmin limit for ions

--- BEAM/Spectra/EPA.C
+++ BEAM/Spectra/EPA.C
@@ -105,7 +105,7 @@ void EPA::RegisterDefaults() const
   s["Q2Max"].SetDefault(1.);
   s["Q2Min"].SetDefault(-1.);
   s["xMax"].SetDefault(1.);
-  s["xMin"].SetDefault(1.e-6);
+  s["xMin"].SetDefault(m_beam.IsIon() ? 1.e-8 : 1.e-6);
   s["xBins"].SetDefault(50);
   s["bMin"].SetDefault(0.1);
   s["bThreshold"].SetDefault(4.);
