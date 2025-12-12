98d4fc6055d1852eaa159ad521f728a4aa8a7651
Peter Meinzinger <peter.meinzinger@uzh.ch>
2025-12-12 20:18:11 +0100

    Set reasonable xMin

--- BEAM/Spectra/EPA.C
+++ BEAM/Spectra/EPA.C
@@ -114,7 +114,7 @@ void EPA::RegisterDefaults() const
   s["Q2Max"].SetDefault(1.);
   s["Q2Min"].SetDefault(-1.);
   s["xMax"].SetDefault(1.);
-  s["xMin"].SetDefault(m_beam.IsIon() ? 1.e-8 : 1.e-6);
+  s["xMin"].SetDefault(1.e-5);
   s["xBins"].SetDefault(100);
   s["bMin"].SetDefault(0.1);
   s["bThreshold"].SetDefault(10.);
