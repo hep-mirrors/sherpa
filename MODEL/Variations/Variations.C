106847b918bad8ccc59cc89a0d21752a178c8705
florens.foerster@web.de <florens.foerster@web.de>
2026-02-04 16:44:21 +0100

    added correlated parameters, changed some variable names, changed structure of variations class

--- MODEL/Variations/Variations.C
+++ MODEL/Variations/Variations.C
@@ -13,17 +13,18 @@ namespace MODEL {
             if (!vertices_pointer && !constants_poiner) THROW(fatal_error, "no vertices or constants given by the model")
             p_vertices = vertices_pointer;
             p_constants = constants_poiner;       
-            m_variations = std::map<std::string, std::vector<double_t>>();
-            variables = std::vector<std::string>();
+            variations_map = std::map<std::string, std::vector<double_t>>();
+            variable_names = std::vector<std::string>();
             // read settings
             msg_Out() << std::endl << "\x1b[34mReading the model parameter variations...\x1b[0m" << std::endl;
             ReadVariations();
             // Register the dependent Model vertices, check for unknown/unused variations
             FindDependentVertices();
-            CheckForUnusedVertices();
+            CheckParameters();
+            // find wanted correlations, using MODEL_VARIATIONS_CORRELATE
+            ReadCorrelations();
             // add variation key elements from combinations
-            if (CheckForCombinations()) AddAllCombinations();
-            else AddWithoutCombining();  
+            CombineParameters();
             // Store the nominal parameter values 
             StoreNominal();
             // check if there are too many variations
@@ -39,24 +40,51 @@ namespace MODEL {
                 delete set.second;
             }
         }
-
+        
         /*
-        Check if combination of parameters is wanted or not, using the flag:   MODEL_VARIATIONS_COMBINE: <0/1>
+        Read Correlations from MODEL_VARIATIONS_CORRELATE, as list of lists of parameter names
         */
-        bool Variations::CheckForCombinations() {
-            if (!s["MODEL_VARIATIONS_COMBINE"].SetDefault(1.).IsScalar()) return true;
-            if (s["MODEL_VARIATIONS_COMBINE"].SetDefault(1.).Get<double>() == 1.) return true;
-            return false;
-        }
-
-        /*
-        add the variations from the map without combining different parameters together
-        */
-        void Variations::AddWithoutCombining() {
-            for (std::pair<std::string, std::vector<double_t>> item : m_variations) {
-                for (double value : item.second){
-                    v_variations.push_back(VariationKey(item.first, value));
+        void Variations::ReadCorrelations(){
+            if (!s["MODEL_VARIATIONS_CORRELATE"].SetDefault(1.).IsList()) {
+                msg_Out() << "\x1b[31m\tParameter correlations of not formatted properly. Ignoring them...\x1b[0m" << std::endl;
+                return;
+            }
+            for (ATOOLS::Scoped_Settings& ss : s["MODEL_VARIATIONS_CORRELATE"].GetItems()){
+                if (!ss.IsList()) {
+                    msg_Out() << "\x1b[31m\tParameter correlations of not formatted properly. Ignoring them...\x1b[0m" << std::endl;
+                    return;
                 }
+                std::vector<std::string> variables = ss.GetVector<std::string>();
+                int length = -1;
+                for (const std::string& var : variables){
+                    // check if the variable names exist in variations
+                    bool exists = false;
+                    for (const std::string& var2 : variable_names){
+                        if (var == var2) {exists = true; break;};
+                    }
+                    if (!exists) {
+                        msg_Out() << "\x1b[31m\tParameter " << var << " specified for correlations does not exist. Ignoring correlations...\x1b[0m" << std::endl;
+                        return;
+                    }
+                    // check if correlation is specfied twice (illegal)
+                    for (const std::vector<std::string>& list : correlated_variables){
+                        for (const std::string& var2 : list){
+                            if (var == var2) {exists = false; break;};
+                        }
+                    }
+                    if (!exists) {
+                        msg_Out() << "\x1b[31m\tParameter " << var << " was specified twice for correlations, which does not make sense. Ignoring correlations...\x1b[0m" << std::endl;
+                        return;
+                    }
+                    // check for same length > 0
+                    if (length == -1) length = variations_map[var].size();
+                    if (length != variations_map[var].size() || length == 0) {
+                        msg_Out() << "\x1b[31m\tParameter " << var << " has a different number of variations than the others (or is zero) for correlations, which does not make sense. Ignoring correlations...\x1b[0m" << std::endl;
+                        return;
+                    }
+                }   
+                correlated_variables.push_back(variables);
+                msg_Out() << std::endl << "\x1b[34m\tFound correlation: "<< variables <<"\x1b[0m" << std::endl;
             }
         }
 
@@ -68,17 +96,17 @@ namespace MODEL {
             std::vector<double_t> values = std::vector<double_t>();
             if (ss.IsMap()){
                 DEBUG_INFO("was map");
-                if (!(ss["From"].SetDefault(-1.).IsScalar() && ss["To"].SetDefault(-1.).IsScalar() && ss["Step"].SetDefault(-1.).IsScalar())) {
+                if (!(ss["Min"].SetDefault(-1.).IsScalar() && ss["Max"].SetDefault(-1.).IsScalar() && ss["Step"].SetDefault(-1.).IsScalar())) {
                     msg_Out() << "\x1b[31m\tVariations of " << parameter << ": range not formatted properly. Ignoring it...\x1b[0m" << std::endl;
-                    variables.pop_back();
+                    variable_names.pop_back();
                     return;
                 }
-                double from = ss["From"].GetScalar<double_t>();
-                double to = ss["To"].GetScalar<double_t>();
+                double from = ss["Min"].GetScalar<double_t>();
+                double to = ss["Max"].GetScalar<double_t>();
                 double step = ss["Step"].GetScalar<double_t>();
                 if (!(from > 0 && to > 0 && step > 0 && from < to)) {
                     msg_Out() << "\x1b[31m\tRange for " << parameter << " is not specified correctly. Ignoring it...\x1b[0m" << std::endl;
-                    variables.pop_back();
+                    variable_names.pop_back();
                     return;
                 }
                 double val = from;
@@ -98,10 +126,10 @@ namespace MODEL {
             }
             if (values.empty() || values.back() < 0) {
                 msg_Out() << "\x1b[31m\tVariations of " << parameter << " not formatted properly. Ignoring it...\x1b[0m" << std::endl;
-                variables.pop_back();
+                variable_names.pop_back();
                 return;
             }
-            m_variations.insert(std::make_pair(parameter, values));
+            variations_map.insert(std::make_pair(parameter, values));
         }
 
         /*
@@ -110,7 +138,7 @@ namespace MODEL {
         void Variations::FindDependentVertices() {
             msg_Debugging() << "Finding Depending Coulings..." << std::endl;
             // for all the varied parameters
-            for (std::string var_name : variables){
+            for (std::string var_name : variable_names){
                 // empty set init
                 dependent_vertices.insert(std::make_pair(var_name, new std::set<MODEL::Single_Vertex*>()));
                 // go through vertices;
@@ -124,11 +152,16 @@ namespace MODEL {
         /*
         go over the parameters and remove those who dont have dependents
         */
-        void Variations::CheckForUnusedVertices() {
-            for (auto var_it=variables.begin(); var_it != variables.end(); var_it++) {
+        void Variations::CheckParameters() {
+            for (auto var_it=variable_names.begin(); var_it != variable_names.end(); var_it++) {
                 if (dependent_vertices[*var_it]->size() == 0){
                     msg_Out() << "\t\x1b[31m" << *var_it << " does not seem to have anything depending on it. Ignoring it...\x1b[0m" << std::endl;
-                    variables.erase(var_it);
+                    variable_names.erase(var_it);
+                    var_it--;
+                }
+                if (p_constants->find(*var_it) == p_constants->end()){
+                    msg_Out() << "\t\x1b[31m" << *var_it << " is not found in the real model constants. Ignoring it...\x1b[0m" << std::endl;
+                    variable_names.erase(var_it);
                     var_it--;
                 }
             }
@@ -140,11 +173,11 @@ namespace MODEL {
         void Variations::StoreNominal() {
             msg_Debugging() << "Store Nominal Paramter values... " << std::endl;
             std::vector<double_t> values = {};
-            for (std::string name : variables){
+            for (std::string name : variable_names){
                 if (p_constants->count(name) != 1) return;
                 values.push_back(p_constants->at(name));
             }
-            nominal = VariationKey(variables, values);
+            nominal = VariationKey(variable_names, values);
         }
 
         /*
@@ -153,87 +186,8 @@ namespace MODEL {
         void Variations::ReadVariations() {
             for (const std::string& parameter : s["MODEL_VARIATIONS"].GetKeys()) {
                 DEBUG_INFO("Reading variations of " + parameter);
-                variables.push_back(parameter);
+                variable_names.push_back(parameter);
                 ReadSingleParamVariation(parameter);
-            }
-        }
-
-        /*
-        Adds all possible combinations of parameters and values from the map to the VariationKey vector found at the given pointer.
-        */
-        void Variations::AddAllCombinations(){
-            msg_Debugging() << "Calculate and add all combinations..." << std::endl;
-            // save all possible combinations of names to a vector
-            std::vector<std::vector<std::string>> name_combinations = {};
-            CombineNames(&name_combinations);
-            // for each combination of variable names find all the value combinations
-            for (std::vector<std::string> combination : name_combinations) {
-                // combine them to a list of all possibilities
-                std::vector<std::vector<double_t>> combined_val_lists = {};
-                CombineValues(combination, &combined_val_lists);
-                // now save a key to the passed variations list for each value combination
-                for (std::vector<double_t> values : combined_val_lists){
-                    v_variations.push_back(VariationKey(combination, values));
-                }
-            }
-            msg_Debugging() << "...done" << std::endl;
-        }
-
-        /*
-        construct all the variable names combinations from a sorted list of names
-        */
-        void Variations::CombineNames(std::vector<std::vector<std::string>> *name_combinations){
-            // get sorted list of all variable names from the map
-            std::vector<std::string> sorted_names (variables);
-            std::sort(sorted_names.begin(), sorted_names.end());
-            *name_combinations = {};
-            for (std::string name : sorted_names)
-                name_combinations->push_back({name});
-            int i = 0;
-            while (i < sorted_names.size()) {
-                std::vector<std::vector<std::string>> next_result = std::vector<std::vector<std::string>>(*name_combinations);
-                for (std::vector<std::string> list : *name_combinations) {
-                    if (list.size() == i) {
-                        for (std::string name : sorted_names){
-                            if (list.back() < name) {
-                                std::vector<std::string> new_list = std::vector<std::string>(list);
-                                new_list.push_back(name);
-                                next_result.push_back(new_list);
-                            }
-                        }
-                    }
-                }
-                *name_combinations = next_result;
-                i++;
-            }
-        }
-
-        /*
-        construct all the value combinations from specific lists
-        */
-        void Variations::CombineValues(std::vector<std::string> combination, std::vector<std::vector<double_t>> *result_lists){
-            // find the possible values for each variable name from the map
-            std::vector<std::vector<double_t>> lists = {};
-            for (std::string var_name : combination) {
-                for (auto item : m_variations){
-                    if (item.first == var_name) lists.push_back(item.second);
-                }
-            }
-            *result_lists = {};
-            result_lists->push_back({});
-            int i = 0;
-            while (i < lists.size()){
-                std::vector<std::vector<double_t>> next_result = {};
-                std::vector<double_t> list = lists.at(i);
-                for (std::vector<double_t> r_list : *result_lists) {
-                    for (double_t val : list) {
-                        std::vector<double_t> new_list = std::vector<double_t>(r_list);
-                        new_list.push_back(val);
-                        next_result.push_back(new_list);
-                    }
-                }
-                *result_lists = next_result;
-                i++;
             }
         }
 
@@ -253,13 +207,96 @@ namespace MODEL {
                 // Output
                 if (msg_LevelIsDebugging()) {
                     msg_Out() << "All read Variations are: " << std::endl;
-                    for (VariationKey key : v_variations){
+                    for (VariationKey key : variations_list){
                         msg_Out() << "\t" << key << std::endl;
                     }
                 }
                 else {
                     msg_Out() << "\x1b[34m\t--> Found " << Size() << " variations.\x1b[0m" << std::endl;
                 }
+            }
+        }
+
+        /*
+        Combine Parameters depending on the settings and correlations
+        TODO this function is too long
+        */
+        void Variations::CombineParameters(){
+            std::vector<std::vector<VariationKey>> individual_variations_list;
+            std::vector<VariationKey> individual_variations;
+            PRINT_VAR(1);
+            // deal with the correlations first, reading is dealt with earlier
+            for (const std::vector<std::string>& list : correlated_variables){
+                individual_variations.clear();
+                int length = variations_map[list.front()].size();
+                for (int i = 0; i < length; i++){
+                    std::vector<double_t> values;
+                    for (const std::string& variable : list){
+                        values.push_back(variations_map[variable][i]);
+                    }
+                    individual_variations.push_back(VariationKey(list, values));
+                }
+                // add nominal values as a key
+                VariationKey key = VariationKey();
+                for (const std::string& variable : list) key.Add(variable, p_constants->at(variable));
+                individual_variations.push_back(key);
+                individual_variations_list.push_back(individual_variations);
+            }
+            PRINT_VAR(2);
+            // now push all other uncorrelated parameters to the individuals list
+            for (const std::string& variable : variable_names){
+                individual_variations.clear();
+                // check if it is correlated, if so skip it
+                bool skip = false;
+                for (const std::vector<std::string>& list : correlated_variables) {
+                    for (const std::string& var2 : list) {
+                        if (variable == var2) {skip = true; break;}
+                    }
+                    if (skip) break;
+                }
+                if (skip) continue;
+                // now push to the list
+                PRINT_VAR(8);
+                for (double_t value : variations_map[variable]) individual_variations.push_back(VariationKey(variable, value));
+                // add nominal
+                PRINT_VAR(9);
+                individual_variations.push_back(VariationKey(variable, p_constants->at(variable)));
+                PRINT_VAR(10);
+                individual_variations_list.push_back(individual_variations);
+            }
+            PRINT_VAR(3);
+            // now we are ready to combine the things on the indivduals list
+            // first get mode, 0 for no combining, 1 for combining all (default)
+            int mode = 1;
+            if (s["MODEL_VARIATIONS_COMBINE"].SetDefault(1.).IsScalar()) mode = s["MODEL_VARIATIONS_COMBINE"].SetDefault(1.).Get<int>();
+            // maybe more modes in the future???
+            switch (mode) {
+                case 0: 
+                            PRINT_VAR("4a");
+                    // no combination, just put all keys into the variations list
+                    // dont add the nominals though, which are the last key
+                    for (const std::vector<VariationKey>& keys : individual_variations_list)
+                        for (std::vector<VariationKey>::const_iterator it = keys.begin(); it != keys.end() - 1; it++)
+                            variations_list.push_back(*(it));
+                    break;
+                default:
+                            PRINT_VAR("4b");
+                    // combine all the elements from the individual lists
+                    // to do this nominal value was added to each list
+                    std::vector<VariationKey> new_list;
+                    variations_list.push_back(VariationKey());
+                    for (int i = 0; i < individual_variations_list.size(); i++){
+                        for (const VariationKey& key : individual_variations_list[i]) {
+                            for (const VariationKey& old_key : variations_list) {
+                                new_list.push_back(key + old_key);
+                            }
+                        }
+                        variations_list = new_list;
+                        new_list.clear();
+                    }
+                    // remove the nominal
+                    variations_list.pop_back();
+                    break;
             }
         }
     }
