1cd214d13fe357798ab995b85e9cdf777c7df753
florens.foerster@web.de <florens.foerster@web.de>
2026-02-04 16:44:21 +0100

    added correlated parameters, changed some variable names, changed structure of variations class

--- MODEL/Variations/Variation_Key.C
+++ MODEL/Variations/Variation_Key.C
@@ -3,20 +3,34 @@
 namespace MODEL {
     namespace VARIATIONS {
         /*
-        Constructor from 1 parameter with value
+        Constructor from multiple parameters with one value each
         */
-        VariationKey::VariationKey(std::string var_name, double_t var_value) {
-            names = {var_name}; values = {var_value};
+        VariationKey::VariationKey(std::vector<std::string> var_names, std::vector<double_t> var_values) {
+            // check inputs
+            if (var_names.size() != var_values.size()) THROW(invalid_input, "input lists are incompatible");
+            names = var_names; values = var_values;
             UpdateIdentifier();
         }
 
         /*
-        Constructor from multiple parameters with one value each
+        Add things
         */
-        VariationKey::VariationKey(std::vector<std::string> var_names, std::vector<double_t> var_values){
+        void VariationKey::Add(std::vector<std::string> var_names, std::vector<double_t> var_values){
             if (var_names.size() != var_values.size()) THROW(invalid_input, "input lists are incompatible");
-            names = var_names; values = var_values;
+            names.insert(names.end(), var_names.begin(), var_names.end());
+            values.insert(values.end(), var_values.begin(), var_values.end());
             UpdateIdentifier();
+            return;
+        }
+        
+        /*
+        Add one thing
+        */
+        void VariationKey::Add(std::string var_name, double_t var_value){
+            names.push_back(var_name);
+            values.push_back(var_value);
+            UpdateIdentifier();
+            return;
         }
 
         /*
@@ -32,6 +46,7 @@ namespace MODEL {
         Comparison for usage as map key
         */
         bool const VariationKey::operator< (const VariationKey& other) const {
+            if (this->Size() == 0) return other.Size() > 0;
             for (int i = 0; i < this->Size(); i++) {
                 if (i >= other.Size()) return true;
                 if (this->Name(i) == other.Name(i)) {
@@ -41,6 +56,15 @@ namespace MODEL {
                 else return this->Name(i) < other.Name(i);
             }
             return true;
+        }
+
+        /*
+        Add variations to each other
+        */
+        VariationKey operator+(const VariationKey& k1, const VariationKey& k2){
+            VariationKey result (k1);
+            result.Add(k2.Names(), k2.Values());
+            return result;
         }
     }
 }
