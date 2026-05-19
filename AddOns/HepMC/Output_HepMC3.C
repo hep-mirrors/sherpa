2311a6b81161c052dbee715292fb343b284b5bf6
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2026-05-19 17:16:35 +0200

    Sync with master

--- AddOns/HepMC/Output_HepMC3.C
+++ AddOns/HepMC/Output_HepMC3.C
@@ -66,20 +66,20 @@ std::shared_ptr<HepMC3::Writer> get_output_file(std::ofstream& m_outstream, cons
 }
 
 Output_HepMC3::Output_HepMC3(const Output_Arguments &args) :
-    Output_Base("HepMC3")
+  Output_Base("HepMC3")
 {
-    m_basename = args.m_outpath + "/" + args.m_outfile;
-    m_compression = Settings::GetMainSettings()["HEPMC3_COMPRESSION"].SetDefault("GZ").Get<std::string>();
-    m_iotype = Settings::GetMainSettings()["HEPMC3_IO_TYPE"].SetDefault(0).Get<int>();
-    int precision = Settings::GetMainSettings()["HEPMC3_OUTPUT_PRECISION"].SetDefault(12).Get<int>();
-    m_short = Settings::GetMainSettings()["HEPMC3_SHORT"].SetDefault(false).Get<bool>();
-
+  m_basename = args.m_outpath + "/" + args.m_outfile;
+  m_compression = Settings::GetMainSettings()["HEPMC3_COMPRESSION"].SetDefault("GZ").Get<std::string>();
+  m_iotype = Settings::GetMainSettings()["HEPMC3_IO_TYPE"].SetDefault(0).Get<int>();
+  int precision = Settings::GetMainSettings()["HEPMC3_OUTPUT_PRECISION"].SetDefault(12).Get<int>();
+  m_short = Settings::GetMainSettings()["HEPMC3_SHORT"].SetDefault(false).Get<bool>();
 #ifdef USING__MPI
-    if (mpi->Size() > 1) {
-        m_basename += "_" + rpa->gen.Variable("RNG_SEED");
-    }
+  if (mpi->Size()>1) {
+    m_basename += "_"+rpa->gen.Variable("RNG_SEED");
+  }
 #endif
-    switch (m_iotype)
+
+switch (m_iotype)
     {
     case 0:
         p_writer = get_output_file<HepMC3::WriterAscii>(m_outstream, m_compression, precision, m_basename, m_ext);
@@ -108,56 +108,56 @@ Output_HepMC3::Output_HepMC3(const Output_Arguments &args) :
         THROW(fatal_error, "Output format HEPMC3_IO_TYPE is undefined.");
         break;
     }
-    m_run_info = std::make_shared<HepMC::GenRunInfo>();
-    HepMC::GenRunInfo::ToolInfo tool;
-    tool.name = std::string("SHERPA-MC");
-    tool.version = std::string(SHERPA_VERSION)+"."+std::string(SHERPA_SUBVERSION);
-    tool.description = std::string(SHERPA_NAME);
-    m_run_info->tools().push_back(tool);
+  m_run_info= std::make_shared<HepMC::GenRunInfo>();
+  HepMC::GenRunInfo::ToolInfo tool;
+  tool.name = std::string("SHERPA-MC");
+  tool.version = std::string(SHERPA_VERSION)+"."+std::string(SHERPA_SUBVERSION);
+  tool.description = std::string(SHERPA_NAME);
+  m_run_info->tools().push_back(tool);
 }
 
 Output_HepMC3::~Output_HepMC3()
 {
-    p_writer->close();
-    m_outstream.close();
+  p_writer->close();
+  m_outstream.close();
 }
 
 void Output_HepMC3::SetXS(const ATOOLS::Weights_Map& xs,
-                          const ATOOLS::Weights_Map& xserr)
+			        const ATOOLS::Weights_Map& xserr)
 {
-    // Only copy for now, we have to wait until the event weights have been
-    // added (when Output()), otherwise HepMC3::GenCrossSection will not be
-    // initialised correctly.
-    m_xs = xs;
-    m_err = xserr;
+  // Only copy for now, we have to wait until the event weights have been
+  // added (when Output()), otherwise HepMC3::GenCrossSection will not be
+  // initialised correctly.
+  m_xs = xs;
+  m_err = xserr;
 }
 
 void Output_HepMC3::Output(Blob_List* blobs)
 {
-    if (m_short) {
-        m_hepmc3.Sherpa2ShortHepMC(blobs, m_run_info);
-    } else {
-        m_hepmc3.Sherpa2HepMC(blobs, m_run_info);
-    }
-    HepMC::GenEvent* q = m_hepmc3.GenEvent();
-    if (q)  m_hepmc3.AddCrossSection(*q, m_xs, m_err);
-    std::vector<HepMC::GenEvent*> subevents(m_hepmc3.GenSubEventList());
+  if (m_short) {
+    m_hepmc3.Sherpa2ShortHepMC(blobs, m_run_info);
+  } else {
+    m_hepmc3.Sherpa2HepMC(blobs, m_run_info);
+  }
+  HepMC::GenEvent* q=m_hepmc3.GenEvent();
+  if (q)  m_hepmc3.AddCrossSection(*q, m_xs, m_err);
+  std::vector<HepMC::GenEvent*> subevents(m_hepmc3.GenSubEventList());
+  for (size_t i = 0; i<subevents.size(); ++i) {
+    m_hepmc3.AddCrossSection(*subevents[i], m_xs, m_err);
+  }
+  if (subevents.size()) {
     for (size_t i = 0; i<subevents.size(); ++i) {
-        m_hepmc3.AddCrossSection(*subevents[i], m_xs, m_err);
+      if (p_writer)    p_writer->write_event(*subevents[i]);
     }
-    if (subevents.size()) {
-        for (size_t i = 0; i<subevents.size(); ++i) {
-            if (p_writer)    p_writer->write_event(*subevents[i]);
-        }
-    }
-    else if (q) {
-        if (p_writer)  p_writer->write_event(*(q));
-    }
+  }
+  else if (q) {
+    if (p_writer)  p_writer->write_event(*(q));
+  }
 }
 
 void Output_HepMC3::ChangeFile()
 {
-    /*This should be implemented in HepMC3 library.*/
+  /*This should be implemented in HepMC3 library.*/
 }
 
 DECLARE_GETTER(Output_HepMC3,"HepMC3", Output_Base,Output_Arguments);
@@ -165,13 +165,13 @@ DECLARE_GETTER(Output_HepMC3,"HepMC3", Output_Base,Output_Arguments);
 Output_Base *ATOOLS::Getter<Output_Base,Output_Arguments,Output_HepMC3>::
 operator()(const Output_Arguments &args) const
 {
-    return new Output_HepMC3(args);
+  return new Output_HepMC3(args);
 }
 
 void ATOOLS::Getter<Output_Base,Output_Arguments,Output_HepMC3>::
 PrintInfo(std::ostream &str,const size_t width) const
 {
-    str<<"HepMC3 output";
+  str<<"HepMC3 output";
 }
 
 DECLARE_GETTER(Output_HepMC3_GenEvent,"HepMC3_GenEvent", Output_Base,Output_Arguments);
@@ -179,13 +179,13 @@ DECLARE_GETTER(Output_HepMC3_GenEvent,"HepMC3_GenEvent", Output_Base,Output_Argu
 Output_Base *ATOOLS::Getter<Output_Base,Output_Arguments,Output_HepMC3_GenEvent>::
 operator()(const Output_Arguments &args) const
 {
-    return new Output_HepMC3_GenEvent(args);
+  return new Output_HepMC3_GenEvent(args);
 }
 
 void ATOOLS::Getter<Output_Base,Output_Arguments,Output_HepMC3_GenEvent>::
 PrintInfo(std::ostream &str,const size_t width) const
 {
-    str<<"HepMC3 GenEvent output";
+  str<<"HepMC3 GenEvent output";
 }
 
 DECLARE_GETTER(Output_HepMC3_Short,"HepMC3_Short", Output_Base,Output_Arguments);
@@ -193,12 +193,12 @@ DECLARE_GETTER(Output_HepMC3_Short,"HepMC3_Short", Output_Base,Output_Arguments)
 Output_Base *ATOOLS::Getter<Output_Base,Output_Arguments,Output_HepMC3_Short>::
 operator()(const Output_Arguments &args) const
 {
-    return new Output_HepMC3_Short(args);
+  return new Output_HepMC3_Short(args);
 }
 
 void ATOOLS::Getter<Output_Base,Output_Arguments,Output_HepMC3_Short>::
 PrintInfo(std::ostream &str,const size_t width) const
 {
-    str<<"HepMC3 Short output";
+  str<<"HepMC3 Short output";
 }
 
