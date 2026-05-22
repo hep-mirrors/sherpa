af14e74b966e3cb10d3571e05154a6f8da3a7061
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2026-05-22 11:12:15 +0200

    Fix compilation

--- AddOns/HepMC/Output_HepMC3.C
+++ AddOns/HepMC/Output_HepMC3.C
@@ -15,61 +15,74 @@
 #include "HepMC3/WriterRootTree.h"
 #include "HepMC3/WriterRoot.h"
 #endif
-#if defined(USING__GZIP) && HEPMC3_USE_COMPRESSION
-#include "HepMC3/ReaderGZ.h"
-#include "HepMC3/WriterGZ.h"
+/** If Sherpa globaly uses GZIP, enable HepMC3 compression with ZLIB support */
+#if defined(USING__GZIP)
+#define HEPMC3_USE_COMPRESSION 1
+#define HEPMC3_Z_SUPPORT 1
 #endif
+#include "HepMC3/WriterGZ.h"
 
 using namespace SHERPA;
 using namespace ATOOLS;
 using namespace std;
 
+template <class T> void set_writer_precision(T& writer, const int precision) { writer->set_precision(precision);}
+template <> void set_writer_precision(std::shared_ptr<HepMC3::WriterHEPEVT>& writer, const int precision) { }
 template <class T>
-std::shared_ptr<HepMC3::Writer> get_output_file(std::ofstream& m_outstream, const std::string &use_compression, const int precision,  std::string m_basename,  std::string & m_ext) {
-#if defined(USING__GZIP) && HEPMC3_USE_COMPRESSION
+std::shared_ptr<HepMC3::Writer> create_writer(std::ofstream& outstream, const std::string &use_compression, const int precision,  const std::string& basename,  std::string & ext) {
+#if HEPMC3_USE_COMPRESSION
 #if HEPMC3_Z_SUPPORT
     if (use_compression == "GZ" )   {
-        m_ext += ".gz";
-        m_outstream.open((m_basename + m_ext).c_str());
-        if (!m_outstream.good()) THROW(fatal_error, "Could not open event file " + m_basename + m_ext+".");
-        auto X = std::make_shared< HepMC3::WriterGZ<T,HepMC3::Compression::z> >(m_outstream);
-        X->writer()->set_precision(precision);
+        ext += ".gz";
+        outstream.open((basename + ext).c_str());
+        if (!outstream.good()) THROW(fatal_error, "Could not open event file " + basename + ext + ".");
+        auto X = std::make_shared< HepMC3::WriterGZ<T,HepMC3::Compression::z> >(outstream);
+        set_writer_precision(X->writer(), precision);
         return X;
     }
 #endif
 #if HEPMC3_LZMA_SUPPORT
     if (use_compression == "LZMA" ) {
-        m_ext += ".lz";
-        m_outstream.open((m_basename + m_ext).c_str());
-        if (!m_outstream.good()) THROW(fatal_error, "Could not open event file " + m_basename + m_ext+".");
-        auto X = std::make_shared< HepMC3::WriterGZ<T,HepMC3::Compression::lzma> >(m_outstream);
-        X->writer()->set_precision(precision);
+        ext += ".lz";
+        outstream.open((basename + ext).c_str());
+        if (!outstream.good()) THROW(fatal_error, "Could not open event file " + basename + ext + ".");
+        auto X = std::make_shared< HepMC3::WriterGZ<T,HepMC3::Compression::lzma> >(outstream);
+        set_writer_precision(X->writer(), precision);
         return X;
     }
 #endif
 #if HEPMC3_BZ2_SUPPORT
     if (use_compression == "BZ2" )  {
-        m_ext += ".bz2";
-        m_outstream.open((m_basename + m_ext).c_str());
-        if (!m_outstream.good()) THROW(fatal_error, "Could not open event file " + m_basename + m_ext+".");
-        auto X = std::make_shared< HepMC3::WriterGZ<T,HepMC3::Compression::bz2> >(m_outstream);
-        X->writer()->set_precision(precision);
+        ext += ".bz2";
+        outstream.open((basename + ext).c_str());
+        if (!outstream.good()) THROW(fatal_error, "Could not open event file " + basename + ext + ".");
+        auto X = std::make_shared< HepMC3::WriterGZ<T,HepMC3::Compression::bz2> >(outstream);
+        set_writer_precision(X->writer(), precision);
         return X;
     }
 #endif
 #endif
-    m_outstream.open((m_basename + m_ext).c_str());
-    if (!m_outstream.good()) THROW(fatal_error, "Could not open event file " + m_basename + m_ext+".");
-    auto X = std::make_shared<T>(m_outstream);
-    X->set_precision(precision);
-    return X;
+    if (use_compression == "" )  {
+       outstream.open((basename + ext).c_str());
+       if (!outstream.good()) THROW(fatal_error, "Could not open event file " + basename + ext + ".");
+       auto X = std::make_shared<T>(outstream);
+       set_writer_precision(X, precision);
+       return X;
+    }
+    THROW(fatal_error, "Usupported HEPMC3_COMPRESSION type " + use_compression + ".");
+    return nullptr;
 }
 
 Output_HepMC3::Output_HepMC3(const Output_Arguments &args) :
   Output_Base("HepMC3")
 {
   m_basename = args.m_outpath + "/" + args.m_outfile;
+/** If compression is enabled and ZLIB type is enabled, that should be the default compression */
+#if HEPMC3_USE_COMPRESSION && HEPMC3_Z_SUPPORT
   m_compression = Settings::GetMainSettings()["HEPMC3_COMPRESSION"].SetDefault("GZ").Get<std::string>();
+#else
+  m_compression = Settings::GetMainSettings()["HEPMC3_COMPRESSION"].SetDefault("").Get<std::string>();
+#endif
   m_iotype = Settings::GetMainSettings()["HEPMC3_IO_TYPE"].SetDefault(0).Get<int>();
   int precision = Settings::GetMainSettings()["HEPMC3_OUTPUT_PRECISION"].SetDefault(12).Get<int>();
   m_short = Settings::GetMainSettings()["HEPMC3_SHORT"].SetDefault(false).Get<bool>();
@@ -82,13 +95,13 @@ Output_HepMC3::Output_HepMC3(const Output_Arguments &args) :
 switch (m_iotype)
     {
     case 0:
-        p_writer = get_output_file<HepMC3::WriterAscii>(m_outstream, m_compression, precision, m_basename, m_ext);
+        p_writer = create_writer<HepMC3::WriterAscii>(m_outstream, m_compression, precision, m_basename, m_ext);
         break;
     case 1:
-        p_writer = get_output_file<HepMC3::WriterHEPEVT>(m_outstream, m_compression, precision, m_basename, m_ext);
+        p_writer = create_writer<HepMC3::WriterHEPEVT>(m_outstream, m_compression, precision, m_basename, m_ext);
         break;
     case 2:
-        p_writer = get_output_file<HepMC3::WriterAsciiHepMC2>(m_outstream, m_compression, precision, m_basename, m_ext);
+        p_writer = create_writer<HepMC3::WriterAsciiHepMC2>(m_outstream, m_compression, precision, m_basename, m_ext);
         break;
     case 3:
 #ifdef USING__HEPMC3__ROOT
