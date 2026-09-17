d41c1960388d16e24515968348604dc3571c67df
Stefan Hoeche <shoeche@fnal.gov>
2026-09-17 07:58:31 -0700

    changed maxent weight notation, unified weight storage

--- AddOns/MaxEnt/DY_Weight_Hook.C
+++ AddOns/MaxEnt/DY_Weight_Hook.C
@@ -220,7 +220,7 @@ public:
 	  GetMatrixElementHandler()->Process()->Parent();
 	size_t nout=proc->NOut();
 	if (proc->Get<MCatNLO_Process>()!=nullptr) --nout;
-	if (nout>3) w=svweight;
+	if (nout>3) w=m_kfs[i]*svweight;
 	msg_Debugging()<<m_names[i]<<": w = "<<w<<" (n_{jet} = "<<nout-2
 		       <<") <-> "<<svweight<<" ("<<svname<<")\n";
 	if (dabs(w)<m_wmax) wmap["MaxEnt_QCD"][m_names[i]]=w;
@@ -249,7 +249,7 @@ public:
 	  GetMatrixElementHandler()->Process()->Parent();
 	size_t nout=proc->NOut();
 	if (proc->Get<MCatNLO_Process>()!=nullptr) --nout;
-	if (nout>3) w=m_kfs[i]*svweight;
+	if (nout>3) w=svweight;
 	msg_Debugging()<<m_names[i]<<": w = "<<w<<" (n_{jet} = "<<nout-2
 		       <<") <-> "<<svweight<<" ("<<svname<<")\n";
 	if (dabs(w)<m_wmax) wmap["MaxEnt3_QCD"][m_names[i]]=w;
