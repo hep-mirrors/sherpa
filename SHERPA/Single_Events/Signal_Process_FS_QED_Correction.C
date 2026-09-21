bc800ee3214f4dfad33b226cf2631f1df1a8213a
Alan Price <alan.price@uj.edu.pl>
2026-09-21 17:13:35 +0200

    SHERPA: report the actual YFS mode in the run summary, not None

--- SHERPA/Single_Events/Signal_Process_FS_QED_Correction.C
+++ SHERPA/Single_Events/Signal_Process_FS_QED_Correction.C
@@ -71,9 +71,19 @@ Signal_Process_FS_QED_Correction::Signal_Process_FS_QED_Correction(
   // Force Photons++ to be off if YFS ISR is switched on.
   // In principle they can be combined but that will
   // require some tweaks - A.Price
-  if(p_mehandler->GetYFS()->Mode()!=YFS::yfsmode::off) m_on=false;
-  if (m_on && m_qed) m_name += p_sphotons->SoftQEDGenerator();
-  else               m_name += "None";
+  const YFS::yfsmode::code yfsmode(p_mehandler->GetYFS()->Mode());
+  if (yfsmode != YFS::yfsmode::off) m_on = false;
+  // Report who is actually doing the photon radiation. Switching Photons++ off
+  // above sets m_on = false, so without this the run summary says "None"
+  // precisely when YFS IS radiating - which reads as no QED at all.
+  switch (yfsmode) {
+  case YFS::yfsmode::isr:    m_name += " YFS(ISR)";     break;
+  case YFS::yfsmode::fsr:    m_name += " YFS(FSR)";     break;
+  case YFS::yfsmode::isrfsr: m_name += " YFS(ISR+FSR)"; break;
+  default:
+    if (m_on && m_qed) m_name += p_sphotons->SoftQEDGenerator();
+    else               m_name += "None";
+  }
 }
 
 Signal_Process_FS_QED_Correction::~Signal_Process_FS_QED_Correction()
